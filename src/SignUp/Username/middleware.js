a68424ac6d082f76f00e399747ae1427aab494e9
bojack <g@g.gg>
2016-12-03 13:59:19 +0800

    missing translations

--- src/SignUp/Username/middleware.js
+++ src/SignUp/Username/middleware.js
@@ -5,7 +5,7 @@ import abcContext from '../../abc/abcContext'
 import { openErrorModal } from '../../ErrorModal/action'
 import { openLoading, closeLoading } from '../../Loader/action'
 import { navigatorPush } from '../../Navigator/action'
-
+import t from "../../lib/LocaleStrings"
 export const checkUsername = username => {
 
 	return dispatch => {
@@ -16,13 +16,13 @@ export const checkUsername = username => {
 				if(username.length >= 3){
 					callback(null, null);
 				}else{
-					callback( 'Username must be at least 3 characters', null);
+					callback( t('activity_signup_insufficient_username_message'), null);
 				}
 
 			},
 			openLoading: function(callback) {
 
-				dispatch(openLoading('Checking username availability'))
+				dispatch(openLoading(t('activity_signup_checking_username')));
 				callback(null, null);
 
 			},
@@ -31,7 +31,7 @@ export const checkUsername = username => {
 				setTimeout( () => {
 					abcContext.usernameAvailable(username,function (error, available) {
 						if(error) {
-							callback('Username is not available', null);
+							callback(t('activity_signup_username_unavailable'), null);
 						}
 						if (!error) {
 							callback(null, null);
