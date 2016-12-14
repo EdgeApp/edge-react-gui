251212e69ac81e7a15da1b10f4f973d2e0673b03
bojack <g@g.gg>
2016-12-14 11:25:21 +0800

    remove the timeout from sign up

--- src/modules/Password/Password.middleware.js
+++ src/modules/Password/Password.middleware.js
@@ -7,7 +7,6 @@ import abcctx from '../../lib/abcContext'
 import { openLoading, closeLoading } from '../Loader/Loader.action'
 
 import t from '../../lib/LocaleStrings'
-const timeoutTimer = setTimeout(() => {}, 0)
 
 export const checkPassword = (password, passwordRepeat, validation, username, pinNumber) => {
   return dispatch => {
@@ -24,7 +23,6 @@ export const checkPassword = (password, passwordRepeat, validation, username, pi
 
       abcctx(ctx => {
         ctx.createAccount(username, password, pinNumber, (err, result) => {
-          clearTimeout(timeoutTimer)
           dispatch(closeLoading())
 
           if (err) {
@@ -43,10 +41,7 @@ export const checkPassword = (password, passwordRepeat, validation, username, pi
             Actions.review()
           }
         })
-        timeoutTimer = setTimeout(() => {
-          dispatch(closeLoading())
-          dispatch(openErrorModal(t('string_no_connection_response')))
-        }, 10000)
+
       })
     } else {
       return dispatch(openErrorModal(t('activity_signup_insufficient_password')))
@@ -60,7 +55,6 @@ export const skipPassword = (username, pinNumber) => {
     dispatch(passwordNotificationHide())
     abcctx((ctx) => {
       ctx.createAccount(username, null, pinNumber, (err, result) => {
-        clearTimeout(timeoutTimer)
         dispatch(closeLoading())
         if (err) {
           console.log('account creation error', err)
@@ -74,10 +68,7 @@ export const skipPassword = (username, pinNumber) => {
         }
         Actions.review()
       })
-      timeoutTimer = setTimeout(() => {
-        dispatch(closeLoading())
-        dispatch(openErrorModal(t('string_no_connection_response')))
-      }, 10000)
+
     })
   }
 }
