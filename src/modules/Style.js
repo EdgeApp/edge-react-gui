b906c96064563785ba1a55aeea31265b2dbc3489
bojack <g@g.gg>
2016-12-16 16:13:55 +0800

    just tweaking the loading bar if I can

--- src/modules/Style.js
+++ src/modules/Style.js
@@ -88,11 +88,7 @@ const style = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0,0,0,0)',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    position: 'absolute'
+    flex: 1
   },
 
   loadingMessage: {
