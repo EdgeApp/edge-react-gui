5b2da15f7149fad15f71c7ec5e08853b8dbdda77
bojack <g@g.gg>
2016-12-19 14:35:49 +0800

    linking the react-native randombytes and actually adding those font files I mentioned in last commit

--- android/app/src/main/java/com/airbitz_ui/MainApplication.java
+++ android/app/src/main/java/com/airbitz_ui/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.rnfs.RNFSPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RandomBytesPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RNFSPackage(),
