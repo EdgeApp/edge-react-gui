f5fa7a00fa5070f1286adbef1768930ea863b673
bojack <g@g.gg>
2016-12-19 14:34:45 +0800

    linked older modules. also looks like some fonts got added when i relinked android ionicons

--- android/app/src/main/java/com/airbitz_ui/MainApplication.java
+++ android/app/src/main/java/com/airbitz_ui/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.rnfs.RNFSPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new LinearGradientPackage(),
             new RNFSPackage(),
             new RNDeviceInfo()
       );
