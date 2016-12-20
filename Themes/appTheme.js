4072e7200d562f319396b5a28523735122c18a6d
bojack <g@g.gg>
2016-12-20 13:08:56 +0800

    ios assign fonts

--- Themes/appTheme.js
+++ Themes/appTheme.js
@@ -9,7 +9,7 @@ export default {
   badgeColor: '#fff',
 
     // Button
-  btnFontFamily: (Platform.OS === 'ios') ? 'HelveticaNeue' : 'Lato',
+  btnFontFamily: (Platform.OS === 'ios') ? 'Lato-Regular' : 'Lato',
   btnDisabledBg: '#b5b5b5',
   btnDisabledClr: '#f1f1f1',
 
@@ -83,7 +83,7 @@ export default {
   brandSidebar: '#252932',
 
     // Font
-  fontFamily: (Platform.OS === 'ios') ? 'HelveticaNeue' : 'Lato',
+  fontFamily: (Platform.OS === 'ios') ? 'Lato-Regular' : 'Lato',
   fontSizeBase: 15,
 
   get fontSizeH1 () {
