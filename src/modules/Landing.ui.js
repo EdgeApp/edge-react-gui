71350fac9a6937979072073dc06fd7df9c216651
Art Manalili <artmanalili@gmail.com>
2017-03-23 10:46:53 +0800

    Process of updating all dependency, will remove old code

--- src/modules/Landing.ui.js
+++ src/modules/Landing.ui.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { View, Text }  from 'react-native'
 import { connect } from 'react-redux'
 
+import TabBar from './TabBar/TabBar.ui'
+
 class Landing extends Component {
 
   render () {
