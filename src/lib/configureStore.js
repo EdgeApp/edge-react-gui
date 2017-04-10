ee0360589e598ddad9e41d1688262b16f165dccc
Kylan Hurt <kylan@datagoniaweb.com>
2017-04-10 15:35:07 -0700

    Enable Redux-logger

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -3,15 +3,17 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+
 
 let middleware = [thunk]
 
 // if (__DEV__) {
-//   const createLogger = require('redux-logger')
-//   const logger = createLogger({ collapsed: true })
-//   middleware = [...middleware, logger]
+   //const createLogger = require('redux-logger')
+   const logger = createLogger({ collapsed: true })
+   middleware = [...middleware, logger]
 // } else {
-  middleware = [...middleware]
+  //middleware = [...middleware]
 // }
 
 export default function configureStore (initialState) {
