b9a14cafa5fd74fbcb8526a3bcabc3311dfc9f1b
Art Manalili <artmanalili@gmail.com>
2017-04-27 20:11:50 +0800

    Added left back button icon, added route stack in states, removing icon button when there is not stack.

--- src/lib/routesReducer.js
+++ src/lib/routesReducer.js
@@ -1,16 +1,19 @@
 import { ActionConst } from 'react-native-router-flux'
 
 const initialState = {
-  scene: {}
-}
+    scene: {},
+    stackDepth: 0
+};
 
 export default function reducer (state = initialState, action = {}) {
   switch (action.type) {
 
     case ActionConst.FOCUS:
+      let stackDepth = parseInt(action.scene.key.replace(action.scene.sceneKey,"").replace("_",""));
       return {
         ...state,
-        scene: action.scene
+        scene: action.scene,
+        stackDepth: stackDepth
       }
 
     default:
--- src/modules/Header/Header.ui.js
+++ src/modules/Header/Header.ui.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Text, Header, Left, Title, Right, Body, Button, Icon } from 'native-base';
 import LinearGradient from 'react-native-linear-gradient'
+import { Actions } from 'react-native-router-flux'
 
 class HeaderUI extends Component {
 
@@ -9,12 +10,20 @@ class HeaderUI extends Component {
     return this.props.routes.scene.title || 'Header'
   }
 
+  _renderLeftButton = () => {
+    if(this.props.routes.stackDepth) {
+      return (
+        <Icon name='arrow-back' onPress={ e => Actions.pop() } />
+      )
+    }
+  }
+
   render () {
     return (
       <LinearGradient start={{x:0,y:0}} end={{x:1, y:0}} colors={["#3b7adb","#2b569a"]}>
         <Header>
           <Left>
-            <Icon name='arrow-back' />
+            {this._renderLeftButton()}
           </Left>
           <Body>
             <Title>{this._renderTitle()}</Title>
