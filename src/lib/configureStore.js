d1229e3c1d1a75f96a747a8203cb394523c6a078
Kylan Hurt <kylan@datagoniaweb.com>
2017-05-05 17:01:53 -0700

    Fix redux logging issue

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -19,6 +19,7 @@ export default function configureStore (initialState) {
   return createStore(
     rootReducer,
     initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(...middleware)  
   )
 }
