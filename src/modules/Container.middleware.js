4b8bf4c155fa5e96fb2540d908c3ff2a87834a6b
Kylan Hurt <kylan@datagoniaweb.com>
2017-05-12 09:39:16 -0700

    Move makeEngine and startEngine to later period in start-up

--- src/modules/Container.middleware.js
+++ src/modules/Container.middleware.js
@@ -69,6 +69,7 @@ export class ABCDataStore {
 
 const BTCEngine = {}
 const engineStarted = {}
+const ioObject = {}
 
 const abcTxLibAccess = {
   accountDataStore: new ABCDataStore(),
@@ -78,70 +79,78 @@ const abcTxLibAccess = {
 }
 
 export const initializeAccount = (dispatch) => {
-
-    console.log('walletLocalDataStore is: ', abcTxLibAccess.walletLocalDataStore)
+    console.log('kylan: walletLocalDataStore is: ', abcTxLibAccess.walletLocalDataStore)
     makeReactNativeIo().then( io => {
+      ioObject = io
+        console.log('kylan: first io is: ', io)
         const context = makeContext({
             apiKey: '0b5776a91bf409ac10a3fe5f3944bf50417209a0',
             io
         })
-        console.log('io is: ', io)
-        abcTxLibAccess.io = io
-        var mk = TxLibBTC.createMasterKeys(io, 'shitcoin')
-        console.log('mk: ', mk)
-        let options = {
-            walletType: "shitcoin",
-            masterPrivateKey: mk.masterPrivateKey,
-            masterPublicKey: mk.masterPublicKey
-        }
+
         dispatch(addAirbitzToRedux(context))
         const account = context.loginWithPassword('bob19', 'Funtimes19')
 
-        const callbacks = {
-            addressesChecked ( ...rest) {
-              console.log('addressesChecked', rest)
-              if(rest[0] === 1) {
-                console.log('addresses done checking')
-                dispatch(updatingBalance('DISABLE'))
-              }
-            },
-            transactionsChanged ( ...rest) {
-              // core will cache previous transactions, should have trigger for updating UI and re-do getTransactions(). Start off with a getTransactions()
-            console.log('transactionsChanged', rest)
-            },
-            blockHeightChanged ( ...rest) {
-            console.log('blockHeightChanged', rest) // will also trigger transactionsChanged if that is the case.
-            }
-        }
-
-        console.log('about to makeEngine')
-        BTCEngine = TxLibBTC.makeEngine(abcTxLibAccess, options, callbacks)
-        console.log('after BTCEngine')
         return account
 
     })
-    .then(account => {
-      console.log('inside second then clause, engineStarted is: ', engineStarted)
+    .then((account) => {
+      console.log('kylan: second io is: ', ioObject)
       
       dispatch(addAccountToRedux(account))
 
       return account
     })
     .then((account) => {
+      console.log('kylan: third io is: ', ioObject)
       const walletIds = account.listWalletIds()
       const wallets = walletIds.map(id => {
         wallet = account.getWallet(id)
         wallet.id = id
-        wallet.name = 'myFakeWallet - ' + id.slice(0, 5)
+        wallet.name = 'myFakeWallet - ' + id.slice(0, 5)     
 
         return wallet
       })
 
-      wallets.slice(0,5).forEach(wallet => {
+      wallets.slice(0,1).forEach((wallet) => {
         dispatch(addWallet(wallet, wallets.length))
+
+        abcTxLibAccess.io = ioObject
+        console.log('kylan: TxLibBTC is: ', TxLibBTC, ' , io is : ', ioObject)
+        let mk = TxLibBTC.createMasterKeys(ioObject, 'shitcoin')
+        console.log('kylan: mk: ', mk)
+        let options = {
+            walletType: "shitcoin",
+            masterPrivateKey: mk.masterPrivateKey,
+            masterPublicKey: mk.masterPublicKey
+        }
+        const callbacks = {
+            addressesChecked ( ...rest) {
+              console.log('kylan: addressesChecked', rest)
+              if(rest[0] === 1) {
+                console.log('kylan: addresses done checking')
+                dispatch(updatingBalance('DISABLE'))
+                console.log('kylan: address done checking and BTCEngine is: ', BTCEngine)
+                let currentBalance = BTCEngine.getBalance()
+                console.log('kylan: shitcoin balance is: ', currentBalance )
+              }
+            },
+            transactionsChanged ( ...rest) {
+              // core will cache previous transactions, should have trigger for updating UI and re-do getTransactions(). Start off with a getTransactions()
+            console.log('kylan: transactionsChanged', rest)
+            },
+            blockHeightChanged ( ...rest) {
+            console.log('kylan: blockHeightChanged', rest) // will also trigger transactionsChanged if that is the case.
+            }
+        }
+        console.log('kylan: TxLibBTC is: ', TxLibBTC, ' , io is : ', ioObject)
+        console.log('kylan: about to makeEngine')
+        let BTCEngine = TxLibBTC.makeEngine(abcTxLibAccess, options, callbacks)
+        console.log('kylan: after BTCEngine')   
+        BTCEngine.startEngine()
       })
 
-      BTCEngine.startEngine()
+
 
     })
     return dispatch(disableLoadingScreenVisibility())
