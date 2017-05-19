65f2683b74ffe36f26f94f2552309114e3fd31b8
Kevin Sullivan <kevin@airbitz.co>
2017-05-19 09:50:44 -0700

    Fix typo

--- src/modules/UI/scenes/Directory/Directory.ui.js
+++ src/modules/UI/scenes/Directory/Directory.ui.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Text, TouchableOpacity }  from 'react-native'
-import { Content }  from 'native-base'
+import { Text, TouchableOpacity } from 'react-native'
+import { Content } from 'native-base'
 import { connect } from 'react-redux'
 import { openTransactionAlert } from '../../components/TransactionAlert/action.js'
 
@@ -9,15 +9,15 @@ class Directory extends Component {
   render () {
     return (
       <Content>
-        <TouchableOpacity onPress={ e => this.props.dispatch(openTransactionAlert('Fucking message', 1234)) }>
-        <Text style={{ color: 'black', fontSize: 50 }}>
-          Directory Page
-        </Text>
+        <TouchableOpacity onPress={e =>
+          this.props.dispatch(openTransactionAlert('message', 1234))}>
+          <Text style={{ color: 'black', fontSize: 50 }}>
+            Directory Page
+          </Text>
         </TouchableOpacity>
       </Content>
     )
   }
-
 }
 
 export default connect()(Directory)
