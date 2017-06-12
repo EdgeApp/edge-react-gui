6a3592aea5787554b208e355259819bcd6ba119c
Kevin Sullivan <kevin@airbitz.co>
2017-06-12 12:30:18 -0700

    Fix react native debugger

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -7,19 +7,16 @@ import createLogger from 'redux-logger'
 
 let middleware = [thunk]
 
-// if (__DEV__) {
-   // const createLogger = require('redux-logger')
-const logger = createLogger({ collapsed: true, level: 'info' })
-middleware = [...middleware, logger]
-// } else {
-  // middleware = [...middleware]
-// }
+if (__DEV__) {
+  middleware = [...middleware]
+} else {
+  middleware = [...middleware]
+}
 
 export default function configureStore (initialState) {
   return createStore(
     rootReducer,
-    initialState,  
-    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),     
-    applyMiddleware(...middleware),    
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(applyMiddleware(...middleware)),
   )
 }
