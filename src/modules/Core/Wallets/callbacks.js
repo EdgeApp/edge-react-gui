e56b6d07221327e22542d35ed1bb87b390d1fbe9
Kevin Sullivan <wolverine.ks@gmail.com>
2017-07-02 08:12:22 -0700

    Enable new/changed transactions callbacks

--- src/modules/Core/Wallets/callbacks.js
+++ src/modules/Core/Wallets/callbacks.js
@@ -1,4 +1,10 @@
-import { renameWalletSuccess } from '../../UI/Wallets/action.js'
+// import { renameWalletSuccess } from '../../UI/Wallets/action.js'
+import {
+  // balanceChanged,
+  newTransactionsRequest,
+  changedTransactionsRequest
+  // walletNameChanged
+} from '../../UI/scenes/TransactionList/action.js'
 
 export const makeWalletCallbacks = (dispatch, walletId) => {
   const callbacks = {
@@ -10,22 +16,26 @@ export const makeWalletCallbacks = (dispatch, walletId) => {
 
     onBalanceChanged (balance) {
       console.log('onBalanceChanged', balance)
+      // dispatch(balanceChanged(balance, walletId))
     },
 
     onTransactionsChanged (transactions) {
       console.log('onTransactionsChanged', transactions)
+      dispatch(changedTransactionsRequest(transactions, walletId))
     },
 
     onNewTransactions (transactions) {
       console.log('onNewTransaction', transactions)
+      dispatch(newTransactionsRequest(transactions, walletId))
     },
 
     onBlockHeightChanged (blockHeight) {
       console.log('onBlockHeightChanged', blockHeight)
     },
 
-    onWalletNameChanged (newWalletName) {
-      console.log('onWalletNameChanged', newWalletName)
+    onWalletNameChanged (walletName) {
+      console.log('onWalletNameChanged', walletName)
+      // dispatch(walletNameChanged(walletName, walletId))
     }
   }
 
