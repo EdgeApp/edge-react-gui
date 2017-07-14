7113128d3b6768b2f55ee6c48fe3e974ffb53541
Kevin Sullivan <wolverine.ks@gmail.com>
2017-07-14 11:52:54 -0700

    Fix create wallet

--- src/modules/UI/scenes/CreateWallet/action.js
+++ src/modules/UI/scenes/CreateWallet/action.js
@@ -1,10 +1,16 @@
 export const UPDATE_WALLET_NAME = 'UPDATE_WALLET_NAME'
 export const SELECT_BLOCKCHAIN = 'SELECT_BLOCKCHAIN'
 export const SELECT_FIAT = 'SELECT_FIAT'
+
+import * as ACCOUNT_API from '../../../Core/Account/api.js'
+import * as CORE_SELECTORS from '../../../Core/selectors.js'
+
+// import * as WALLET_ACTIONS from '../../Wallets/action.js'
+import * as LOGIN_ACTIONS from '../../../Login/action.js'
+
 import { Actions } from 'react-native-router-flux'
 
 import { makeShitcoinPlugin } from 'airbitz-currency-shitcoin'
-import { createWalletRequest } from '../../../Core/Account/api.js'
 
 export const updateWalletName = walletName => {
   return {
@@ -30,11 +36,14 @@ export const selectFiat = fiat => {
 export const createWallet = (walletName, walletType) => {
   return (dispatch, getState) => {
     const state = getState()
-    const { account, context } = state
-    const { io } = context
+    const account = CORE_SELECTORS.getAccount(state)
+    const io = CORE_SELECTORS.getIO(state)
     const keys = makeShitcoinPlugin({ io }).createMasterKeys('shitcoin')
 
-    createWalletRequest(account, keys, walletType)
-    .then(Actions.walletList)
+    ACCOUNT_API.createWalletRequest(account, keys, walletType)
+    .then((walletId) => {
+      Actions.walletList()
+      dispatch(LOGIN_ACTIONS.updateWallets())
+    })
   }
 }
