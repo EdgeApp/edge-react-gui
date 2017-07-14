7177d7e4f00b75a2e0d28fbd051bcc131490f001
Paul Puey <paul@airbitz.co>
2017-07-14 14:10:02 -0700

    Add more exchange rates to prevent crashes

--- src/modules/UI/components/ExchangeRate/action.js
+++ src/modules/UI/components/ExchangeRate/action.js
@@ -1,18 +1,35 @@
 export const UPDATE_EXCHANGE_RATES = 'UPDATE_EXCHANGE_RATES'
 
-let randomSeed = Math.random() //produces random number between 0 and 1
+let randomSeed = Math.random() / 10 //produces random number between 0 and 1
 
 const fakeExchangeRate = {
-    'TRD': {
-        source: 'TRD', 
-        dest: 'USD', 
-        value: 1 + randomSeed
-    },
-    'USD': {
-        source: 'USD', 
-        dest: 'TRD', 
-        value: 1 / (1 + randomSeed)
-    }
+  'BTC': {
+    value: (1/214800000000) * randomSeed
+  },
+  'ETH': {
+    value: (1/231000000000000000000) * randomSeed
+  },
+  'TRD': {
+    value: 2 * randomSeed
+  },
+  'DOGESHIT': {
+    value: .3 * randomSeed
+  },
+  'HOLYSHIT': {
+    value: .2 * randomSeed
+  },
+  'ANA': {
+    value: .1 * randomSeed
+  },
+  'REP': {
+    value: (1/18000000000000000000) * randomSeed
+  },
+  'WINGS': {
+    value: (1/5000000000000000000) * randomSeed
+  },
+  'LUN': {
+    value: (1/2000000000000000000) * randomSeed
+  }
 }
 
 export const updateExchangeRates = () => {
