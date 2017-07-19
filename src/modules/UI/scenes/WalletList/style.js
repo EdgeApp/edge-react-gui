d604b8895a50ee6318fbe755ab19eb79e9a255ad
Kylan Hurt <kylan@datagoniaweb.com>
2017-07-19 15:23:59 -0700

    Adjust walletListRow denomination placement to 'right'

--- src/modules/UI/scenes/WalletList/style.js
+++ src/modules/UI/scenes/WalletList/style.js
@@ -137,7 +137,8 @@ module.exports = StyleSheet.create({
     textAlign: 'right'
   },
   rowBalanceDenominationText: {
-    color: '#58595C'
+    color: '#58595C',
+    textAlign: 'right'
   },  
   rowDotsWrap: {
     flexDirection: 'row'
