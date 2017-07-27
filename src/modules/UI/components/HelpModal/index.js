0fb3e85f1ccded8d074f00650cf213fc62ab1740
Kevin Sullivan <wolverine.ks@gmail.com>
2017-07-27 16:55:42 -0700

    Lint HelpModal

--- src/modules/UI/components/HelpModal/index.js
+++ src/modules/UI/components/HelpModal/index.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, Image, TouchableWithoutFeedback, WebView } from 'react-native'
+import { View, TouchableOpacity, Image, TouchableWithoutFeedback, WebView } from 'react-native'
 import { Icon } from 'native-base'
-import { Actions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
 import Modal from 'react-native-modal'
-
 import { closeHelpModal } from './actions.js'
-
-const logo = require('../../../../img/logo2x.png')
+import logo from '../../../../img/logo2x.png'
 
 class HelpModal extends Component {
-
   _renderWebView = () => {
     switch (this.props.routes.scene.sceneKey) {
       case 'scan':
@@ -34,17 +30,17 @@ class HelpModal extends Component {
       <TouchableWithoutFeedback onPress={e => this.props.dispatch(closeHelpModal())}>
         <Modal isVisible={this.props.modal} animationIn='bounceIn' animationOut='bounceOut' style={{margin: 0, paddingVertical: 60, paddingHorizontal: 20}}>
           <TouchableWithoutFeedback>
-            <View style={{ flex: 1, backgroundColor: '#FFF' }}>
-              <View style={{ flexDirection: 'row'}}>
-                <View style={{ width: 40 }} />
-                <Image source={logo} style={{ flex: 1, height: 48, margin: 15 }} resizeMode='contain' />
-                <View style={{ width: 40, alignItems: 'center' }}>
+            <View style={{flex: 1, backgroundColor: '#FFF'}}>
+              <View style={{flexDirection: 'row'}}>
+                <View style={{width: 40}} />
+                <Image source={logo} style={{flex: 1, height: 48, margin: 15}} resizeMode='contain' />
+                <View style={{width: 40, alignItems: 'center'}}>
                   <TouchableOpacity onPress={e => this.props.dispatch(closeHelpModal())}>
-                    <Icon name='close' style={{ marginTop: 5, fontSize: 36 }} />
+                    <Icon name='close' style={{marginTop: 5, fontSize: 36}} />
                   </TouchableOpacity>
                 </View>
               </View>
-              <View style={{ flex: 1, paddingHorizontal: 20}}>
+              <View style={{flex: 1, paddingHorizontal: 20}}>
                 <WebView source={this._renderWebView()} />
               </View>
             </View>
@@ -59,5 +55,4 @@ const mapStateToProps = state => ({
   modal: state.ui.scenes.helpModal,
   routes: state.routes
 })
-
 export default connect(mapStateToProps)(HelpModal)
