4b8255875da3996b0b452942f1b337c3efe6c308
Kevin Sullivan <wolverine.ks@gmail.com>
2017-07-26 23:47:32 -0700

    Lint Request

--- src/modules/UI/scenes/Request/Request.ui.js
+++ src/modules/UI/scenes/Request/Request.ui.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {
   Clipboard,
   View,
   ToastAndroid,
   AlertIOS,
   Platform,
-  Dimensions,
-  Share,
-  Text,
-  TouchableHighlight
+  Share
 } from 'react-native'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import styles from './styles.js'
 import ExchangeRate from '../../components/ExchangeRate/index.js'
 import FlipInput from '../../components/FlipInput/index.js'
 import ABQRCode from '../../components/QRCode/index.js'
 import RequestStatus from '../../components/RequestStatus/index.js'
 import ShareButtons from '../../components/ShareButtons/index.js'
-import { getCryptoFromFiat, getFiatFromCrypto, sanitizeInput, dev } from '../../../utils.js'
+import {getCryptoFromFiat, getFiatFromCrypto, sanitizeInput} from '../../../utils.js'
 import ContactsWrapper from 'react-native-contacts-wrapper'
 import LinearGradient from 'react-native-linear-gradient'
 
@@ -26,13 +23,10 @@ import * as UI_SELECTORS from '../../selectors.js'
 import * as SETTINGS_SELECTORS from '../../Settings/selectors.js'
 
 import {
-  addReceiveAddress,
   updateReceiveAddress,
   updateAmountRequestedInCrypto,
   updateAmountRequestedInFiat,
-  updateAmountReceivedInCrypto,
-  saveReceiveAddress,
-  updateInputCurrencySelected
+  saveReceiveAddress
 } from './action.js'
 
 class Request extends Component {
@@ -55,82 +49,44 @@ class Request extends Component {
     this.setState({keyboardVisible: false})
   }
 
-  /* onInputCurrencyToggle = () => {
-    console.log('SendConfirmation->onInputCurrencyToggle called')
-    const { inputCurrencySelected } = this.props
-    const nextInputCurrencySelected =
-      inputCurrencySelected === 'crypto'
-        ? 'fiat'
-        : 'crypto'
-
-      this.props.dispatch(updateInputCurrencySelected(nextInputCurrencySelected))
-  } */
-
   render () {
     console.log('rendering Request.ui, this.state is: ', this.state, ' this.props is: ', this.props)
-    const { request = {} } = this.props
-    const { receiveAddress = {} } = request
-    const { publicAddress = '', amountSatoshi = null, metadata = {} } = receiveAddress
-    const { amountFiat = null } = metadata
+    const {request = {}} = this.props
+    const {receiveAddress = {}} = request
+    const {
+      publicAddress = '',
+      amountSatoshi = null,
+      metadata = {}
+    } = receiveAddress
+    const {amountFiat = null} = metadata
 
     return (
-      <LinearGradient
-        style={styles.view}
-        start={{x: 0, y: 0}} end={{x: 1, y: 0}}
-        colors={['#3b7adb', '#2b569a']}
-      >
+      <LinearGradient style={styles.view} start={{x: 0, y: 0}} end={{x: 1, y: 0}}
+        colors={['#3b7adb', '#2b569a']}>
 
         <View style={styles.exchangeRateContainer}>
-          <ExchangeRate
-            fiatPerCrypto={this.props.fiatPerCrypto}
-            fiatCurrencyCode={this.props.fiatCurrencyCode}
-            cryptoDenom={this.props.inputCurrencyDenom}
-          />
+          <ExchangeRate fiatPerCrypto={this.props.fiatPerCrypto} fiatCurrencyCode={this.props.fiatCurrencyCode}
+            cryptoDenom={this.props.inputCurrencyDenom} />
         </View>
 
         <View style={styles.main}>
-          {this.props.inputCurrencySelected === 'crypto' ? (
-            <FlipInput
-              onCryptoInputChange={this.onCryptoInputChange}
-              onFiatInputChange={this.onFiatInputChange}
-              amountSatoshi={amountSatoshi || 0}
-              amountFiat={amountFiat}
-              inputCurrencySelected={this.props.inputCurrencySelected} // crypto
-              cryptoDenom={this.props.inputCurrencyDenom}
-              fiatCurrencyCode={this.props.fiatCurrencyCode}
-              inputOnFocus={this._onFocus}
-              inputOnBlur={this._onBlur}
-            />
-          ) : (
-            <FlipInput
-              onCryptoInputChange={this.onCryptoInputChange}
-              onFiatInputChange={this.onFiatInputChange}
-              amountSatoshi={amountSatoshi || 0}
-              amountFiat={amountFiat}
-              inputCurrencySelected={this.props.inputCurrencySelected} // fiat
-              cryptoDenom={this.props.inputCurrencyDenom}
-              fiatCurrencyCode={this.props.fiatCurrencyCode}
-              inputOnFocus={this._onFocus}
-              inputOnBlur={this._onBlur}
-            />
-          ) }
+          {this.props.inputCurrencySelected === 'crypto'
+            ? <FlipInput onCryptoInputChange={this.onCryptoInputChange}
+              onFiatInputChange={this.onFiatInputChange} amountSatoshi={amountSatoshi || 0}
+              amountFiat={amountFiat} inputCurrencySelected={this.props.inputCurrencySelected} // crypto
+              cryptoDenom={this.props.inputCurrencyDenom} fiatCurrencyCode={this.props.fiatCurrencyCode}
+              inputOnFocus={this._onFocus} inputOnBlur={this._onBlur} />
+            : <FlipInput onCryptoInputChange={this.onCryptoInputChange} onFiatInputChange={this.onFiatInputChange}
+              amountSatoshi={amountSatoshi || 0} amountFiat={amountFiat} inputCurrencySelected={this.props.inputCurrencySelected} // fiat
+              cryptoDenom={this.props.inputCurrencyDenom} fiatCurrencyCode={this.props.fiatCurrencyCode}
+              inputOnFocus={this._onFocus} inputOnBlur={this._onBlur} />}
 
           <ABQRCode qrCodeText={this.getQrCodeText(publicAddress, amountSatoshi)} />
-          <RequestStatus
-            requestAddress={publicAddress}
-            amountRequestedInCrypto={amountSatoshi}
-            amountReceivedInCrypto={amountFiat}
-          />
+          <RequestStatus requestAddress={publicAddress} amountRequestedInCrypto={amountSatoshi} amountReceivedInCrypto={amountFiat} />
         </View>
 
         <View style={styles.shareButtonsContainer}>
-          <ShareButtons
-            styles={styles.shareButtons}
-            shareViaEmail={this.shareViaEmail}
-            shareViaSMS={this.shareViaSMS}
-            shareViaShare={this.shareViaShare}
-            copyToClipboard={() => this.copyToClipboard(publicAddress, amountSatoshi)}
-          />
+          <ShareButtons styles={styles.shareButtons} shareViaEmail={this.shareViaEmail} shareViaSMS={this.shareViaSMS} shareViaShare={this.shareViaShare} copyToClipboard={() => this.copyToClipboard(publicAddress, amountSatoshi)} />
         </View>
 
       </LinearGradient>
@@ -141,7 +97,9 @@ class Request extends Component {
   onCryptoInputChange = (amountRequestedInCrypto) => {
     console.log('inside Request.ui->onCryptoInputChange, amountRequestedInCrypto is: ', amountRequestedInCrypto)
     amountRequestedInCrypto = sanitizeInput(amountRequestedInCrypto)
-    if (this.invalidInput(amountRequestedInCrypto)) { return }
+    if (this.invalidInput(amountRequestedInCrypto)) {
+      return
+    }
     const amountRequestedInFiat = getFiatFromCrypto(amountRequestedInCrypto, this.props.fiatPerCrypto)
 
     this.props.dispatch(updateAmountRequestedInCrypto(amountRequestedInCrypto))
@@ -151,17 +109,17 @@ class Request extends Component {
   onFiatInputChange = (amountRequestedInFiat) => {
     console.log('inside Request.ui->onCryptoInputChange, amountRequestedInCrypto is: ', amountRequestedInCrypto)
     amountRequestedInFiat = sanitizeInput(amountRequestedInFiat)
-    if (this.invalidInput(amountRequestedInFiat)) { return }
+    if (this.invalidInput(amountRequestedInFiat)) {
+      return
+    }
 
     const amountRequestedInCrypto = getCryptoFromFiat(amountRequestedInFiat, this.props.fiatPerCrypto)
     this.props.dispatch(updateAmountRequestedInCrypto(amountRequestedInCrypto))
     this.props.dispatch(updateAmountRequestedInFiat(amountRequestedInFiat))
   }
-// /////////////// End Critical Input and Conversion Area //////////////////////
+  // /////////////// End Critical Input and Conversion Area //////////////////////
   copyToClipboard = (publicAddress, amountSatoshi) => {
-    Clipboard.setString(
-      this.getRequestInfoForClipboard(publicAddress, amountSatoshi)
-    )
+    Clipboard.setString(this.getRequestInfoForClipboard(publicAddress, amountSatoshi))
 
     if (Platform.OS === 'android') { // needs internationalization and string replacement still
       ToastAndroid.show('Request copied to clipboard', ToastAndroid.SHORT)
@@ -197,7 +155,9 @@ class Request extends Component {
       this.props.dispatch(saveReceiveAddress(this.props.request.receiveAddress))
 
       if (result.activityType) {
-        this.setState({result: 'shared with an activityType: ' + result.activityType})
+        this.setState({
+          result: 'shared with an activityType: ' + result.activityType
+        })
       } else {
         this.setState({result: 'shared'})
       }
@@ -211,32 +171,26 @@ class Request extends Component {
       message: this.getRequestInfoForShare(),
       url: 'https://airbitz.co', // will need to refactor for white labeling
       title: 'Share Airbitz Request'
-    }, {
-      dialogTitle: 'Share Airbitz Request'
-    })
-    .then(this.showResult)
-    .catch((error) => this.setState({result: 'error: ' + error.message}))
+    }, {dialogTitle: 'Share Airbitz Request'}).then(this.showResult).catch((error) => this.setState({
+      result: 'error: ' + error.message
+    }))
   }
 
   shareViaEmail = () => {
-    ContactsWrapper.getContact()
-    .then((contact) => {
+    ContactsWrapper.getContact().then((contact) => {
       this.shareMessage()
       console.log('shareViaEmail')
-    })
-    .catch((error) => {
+    }).catch((error) => {
       console.log('ERROR CODE: ', error.code)
       console.log('ERROR MESSAGE: ', error.message)
     })
   }
 
   shareViaSMS = () => {
-    ContactsWrapper.getContact()
-    .then((contact) => {
+    ContactsWrapper.getContact().then((contact) => {
       this.shareMessage()
       console.log('shareViaSMS')
-    })
-    .catch((error) => {
+    }).catch((error) => {
       console.log('ERROR CODE: ', error.code)
       console.log('ERROR MESSAGE: ', error.message)
     })
@@ -271,10 +225,12 @@ const mapStateToProps = (state) => {
     inputCurrencySelected: state.ui.scenes.request.inputCurrencySelected,
     inputCurrencyDenom,
     fiatCurrencyCode: state.ui.wallets.byId[state.ui.wallets.selectedWalletId].fiatCurrencyCode
-    // fiatPerCrypto:  state.ui.scenes.exchangeRate.exchangeRates[state.ui.wallets.byId[state.ui.wallets.selectedWalletId].currencyCode].value,
+    // fiatPerCrypto:  state.ui.scenes.exchangeRate.exchangeRates[state.ui.wallets.byId[state.ui.wallets.selectedWalletId].currencyCode].value,,,
   }
 }
 const mapDispatchToProps = (dispatch) => ({
-  updateReceiveAddress: (walletId, currencyCode) => { dispatch(updateReceiveAddress(walletId, currencyCode)) }
+  updateReceiveAddress: (walletId, currencyCode) => {
+    dispatch(updateReceiveAddress(walletId, currencyCode))
+  }
 })
 export default connect(mapStateToProps, mapDispatchToProps)(Request)
