9fb941fcfe68de87c683bfb25043c60130dec5bb
Kevin Sullivan <wolverine.ks@gmail.com>
2017-08-14 13:28:43 -0700

    Add precommit / prepush hooks, renamed tests

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,7 +1,11 @@
 /* globals test expect */
 
 // This is an example test file
-import { sum } from './sum.js'
+
+// import { sum } from '../sum.js'
+export const sum = (a, b) => {
+  return a + b
+}
 
 test('adds 1 + 2 to equal 3', function () {
   expect(sum(1, 2)).toBe(3)
--- tests/sum.js
+++
@@ -1,4 +0,0 @@
-// This is an example file used only in the example test
-export const sum = (a, b) => {
-  return a + b
-}
