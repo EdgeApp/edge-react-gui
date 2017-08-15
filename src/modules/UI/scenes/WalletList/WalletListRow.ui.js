8a18828f6ddc00240f1bcce5b69b2a37300ae682
Paul Puey <paul@airbitz.co>
2017-08-14 23:56:38 -0700

    Add another route transition stack reset.

--- src/modules/UI/scenes/WalletList/WalletListRow.ui.js
+++ src/modules/UI/scenes/WalletList/WalletListRow.ui.js
@@ -30,7 +30,7 @@ class WalletListRow extends Component {
   _onPressSelectWallet = (walletId, currencyCode) => {
     // console.log('selecting wallet with walletId: ' , walletId, ' and currencyCode: ', currencyCode)
     this.props.dispatch(selectWallet(walletId, currencyCode))
-    Actions.transactionList({params: 'walletList'})
+    Actions.transactionList({type: 'reset', params: 'walletList'})
   }
 
   renderTokenRow = (metaTokenBalances) => {
