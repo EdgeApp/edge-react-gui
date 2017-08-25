d197a48a622c8a93b2697019e027148db3166d8c
Paul Puey <paul@airbitz.co>
2017-08-25 01:17:16 -0700

    Enable bitcoin

--- src/modules/Container.ui.js
+++ src/modules/Container.ui.js
@@ -42,10 +42,11 @@ import { addCurrencyPlugin } from './UI/Settings/action.js'
 import { makeReactNativeIo } from 'airbitz-core-react-native'
 import { makeContext } from 'airbitz-core-js'
 import * as EXCHANGE_PLUGINS from 'airbitz-exchange-plugins'
-// import { BitcoinPlugin } from 'airbitz-currency-bitcoin'
+import { BitcoinPlugin } from 'airbitz-currency-bitcoin'
 import { EthereumPlugin } from 'airbitz-currency-ethereum'
 const currencyPlugins = [
-  EthereumPlugin
+  EthereumPlugin,
+  BitcoinPlugin
 ]
 global.madeCurrencyPlugins = []
 
