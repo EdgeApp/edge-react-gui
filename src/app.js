4b9c3bb2dabca9ae2716e90738e5f98599b2c1e4
Andrii Koretskyi <koretskiyav@gmail.com>
2017-09-19 22:57:14 +0300

    add logging to file (by RNFolder from disklet) at production

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ import React, {Component} from 'react'
 import {Provider} from 'react-redux'
 import configureStore from './lib/configureStore'
 import Main from './modules/MainConnector'
+import {log} from './util/logger'
 
 const store: {} = configureStore({})
 
 if (!__DEV__) {
   // $FlowFixMe: suppressing this error until we can find a workaround
-  console.log = () => {}
+  console.log = log
 }
 
 export default class App extends Component {
---
+++ src/util/logger.js
@@ -0,0 +1,38 @@
+import {makeReactNativeFolder} from 'disklet'
+
+const LOGS_FOLDER = 'logs'
+
+const root = makeReactNativeFolder()
+const logsFolder = root.folder(LOGS_FOLDER)
+
+const getTime = () => new Date().toISOString()
+
+const getDate = () => getTime().slice(0, 10)
+
+const isObject = (item) => typeof item === 'object' && item !== null
+
+async function read (fileName) {
+  const fileNames = await logsFolder.listFiles()
+  if (fileNames.includes(fileName)) return logsFolder.file(fileName).getText()
+
+  return ''
+}
+
+async function write (logString) {
+  const fileName =`${getDate()}.txt`
+  const logs = await read(fileName)
+  const updatedLogs = logs ? `${logs}\n${logString}` : logString
+
+  logsFolder.file(fileName).setText(updatedLogs)
+}
+
+
+export function log (...info) {
+  write(`${getTime()} | ${info.map((item) => isObject(item) ? JSON.stringify(item) : item).join(' ')}`)
+}
+
+export async function readLogs () {
+  const fileNames = await logsFolder.listFiles()
+  const files = await Promise.all(fileNames.map((fileName) => logsFolder.file(fileName).getText()))
+  return files.join('\n\n')
+}
