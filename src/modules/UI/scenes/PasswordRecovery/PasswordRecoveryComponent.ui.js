7f775e18688ec0b9db05ef8430a929a01286ecfc
ahujsak <allen@missionvi.com>
2017-09-21 16:49:15 -0400

    Update PasswordRecoveryComponent.ui.js
    simplified call to props function

--- src/modules/UI/scenes/PasswordRecovery/PasswordRecoveryComponent.ui.js
+++ src/modules/UI/scenes/PasswordRecovery/PasswordRecoveryComponent.ui.js
@@ -2,17 +2,13 @@ import React, {Component} from 'react'
 import {PasswordRecoveryScreen} from 'airbitz-core-js-ui'
 
 export default class PasswordRecovery extends Component {
-  onComplete = () => {
-    this.props.onComplete()
-  }
-
   render () {
     return (
       <PasswordRecoveryScreen
         account={this.props.account}
         context={this.props.context}
-        onComplete={this.onComplete}
-        onCancel={this.onComplete}
+        onComplete={this.props.onComplete}
+        onCancel={this.props.onComplete}
       />
     )
   }
