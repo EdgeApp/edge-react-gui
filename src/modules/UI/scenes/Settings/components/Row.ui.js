518510f164af7b6d7f98381f7ad46892ff4d54f1
Kylan Hurt <kylan.hurt@gmail.com>
2017-09-26 16:42:38 -0700

    Rename border debugging function for clarity

--- src/modules/UI/scenes/Settings/components/Row.ui.js
+++ src/modules/UI/scenes/Settings/components/Row.ui.js
@@ -7,7 +7,7 @@ import T from '../../../components/FormattedText'
 import IonIcon from 'react-native-vector-icons/Ionicons'
 import s from './style'
 import {colors} from '../../../../../theme/variables/airbitz.js'
-import {border as b} from '../../../../utils'
+import {border as debugBorder} from '../../../../utils'
 
 class Row extends Component {
   render () {
@@ -20,17 +20,17 @@ class Row extends Component {
     // console.log('Row render, option: ', option)
 
     return (
-      <TouchableOpacity style={[s.rowContainer, b()]}
+      <TouchableOpacity style={[s.rowContainer, debugBorder()]}
         onPress={onPress}>
 
-        <View style={[s.rowTextRow, b()]}>
-          <View style={[s.rowLeftContainer, b()]}>
-            <T style={[s.rowLeftText, b()]}>{left}</T>
+        <View style={[s.rowTextRow, debugBorder()]}>
+          <View style={[s.rowLeftContainer, debugBorder()]}>
+            <T style={[s.rowLeftText, debugBorder()]}>{left}</T>
           </View>
           {
             isSelected
-            ? <IonIcon name='ios-radio-button-on' size={24} style={[s.radioButton, b()]} color={colors.secondary} />
-            : <IonIcon name='ios-radio-button-off' size={24} style={[s.radioButton, b()]} color={colors.gray1} />
+            ? <IonIcon name='ios-radio-button-on' size={24} style={[s.radioButton, debugBorder()]} color={colors.secondary} />
+            : <IonIcon name='ios-radio-button-off' size={24} style={[s.radioButton, debugBorder()]} color={colors.gray1} />
           }
         </View>
 
