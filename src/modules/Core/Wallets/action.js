3b4e83c6c96c5f62a442bb02ab994a8b2ec2275a
Paul Puey <paul@airbitz.co>
2017-09-27 16:55:17 -0700

    Enable flow on Wallets/action.js

--- src/modules/Core/Wallets/action.js
+++ src/modules/Core/Wallets/action.js
@@ -1,19 +1,24 @@
+// @flow
 export const PREFIX = 'Core/Wallets/'
 export const UPDATE_WALLETS = PREFIX + 'UPDATE_WALLETS'
 
 import * as CORE_SELECTORS from '../selectors'
 import * as SETTINGS_SELECTORS from '../../UI/Settings/selectors'
+import type {AbcCurrencyWallet} from 'airbitz-core-types'
 
-export const updateWallets = (activeWalletIds, archivedWalletIds, currencyWallets) => ({
-  type: UPDATE_WALLETS,
-  data: {
-    activeWalletIds,
-    archivedWalletIds,
-    currencyWallets
-  }
-})
+export const updateWallets = (
+  activeWalletIds: Array<string>,
+  archivedWalletIds: Array<string>,
+  currencyWallets: Array<AbcCurrencyWallet>) => ({
+    type: UPDATE_WALLETS,
+    data: {
+      activeWalletIds,
+      archivedWalletIds,
+      currencyWallets
+    }
+  })
 
-export const updateWalletsRequest = () => (dispatch, getState) => {
+export const updateWalletsRequest = () => (dispatch: any, getState: any) => {
   const state = getState()
   const loginStatus = SETTINGS_SELECTORS.getLoginStatus(state)
   if (!loginStatus) {
