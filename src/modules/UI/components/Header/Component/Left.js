046f74462d47db72b1f895e6d350c874bc0a0519
thehobbit85 <thehobbit85@gmail.com>
2017-09-28 15:39:38 -0700

    not sure why buy flow wants me to commit this

--- src/modules/UI/components/Header/Component/Left.js
+++ src/modules/UI/components/Header/Component/Left.js
@@ -18,28 +18,28 @@ export default class Left extends Component {
       <BackButton label={labelText} onPress={() => Actions[consts]({type: 'reset'})} />
 
     switch (sceneName) {
-      case Constants.CREATE_WALLET:
-        return makeBackButton(CANCEL_TEXT, Constants.WALLET_LIST)
-      case Constants.TRANSACTION_DETAILS:
-        return makeBackButton(CANCEL_TEXT, Constants.TRANSACTION_LIST)
-      case Constants.SEND_CONFIRMATION:
-        return makeBackButton(BACK_TEXT, Constants.SCAN)
-      case Constants.BTC_SETTINGS:
-        return makeBackButton(BACK_TEXT, Constants.SETTINGS_OVERVIEW)
-      case Constants.ETH_SETTINGS:
-        return makeBackButton(BACK_TEXT, Constants.SETTINGS_OVERVIEW)
-      case Constants.LTC_SETTINGS:
-        return makeBackButton(BACK_TEXT, Constants.SETTINGS_OVERVIEW)
-      case Constants.BCH_SETTINGS:
-        return makeBackButton(BACK_TEXT, Constants.SETTINGS_OVERVIEW)
-      case Constants.CHANGE_PASSWORD:
-        return makeBackButton(CANCEL_TEXT, Constants.SETTINGS_OVERVIEW)
-      case Constants.CHANGE_PIN:
-        return makeBackButton(CANCEL_TEXT, Constants.SETTINGS_OVERVIEW)
-      case Constants.RECOVER_PASSWORD:
-        return makeBackButton(CANCEL_TEXT, Constants.SETTINGS_OVERVIEW)
-      default:
-        return null
+    case Constants.CREATE_WALLET:
+      return makeBackButton(CANCEL_TEXT, Constants.WALLET_LIST)
+    case Constants.TRANSACTION_DETAILS:
+      return makeBackButton(CANCEL_TEXT, Constants.TRANSACTION_LIST)
+    case Constants.SEND_CONFIRMATION:
+      return makeBackButton(BACK_TEXT, Constants.SCAN)
+    case Constants.BTC_SETTINGS:
+      return makeBackButton(BACK_TEXT, Constants.SETTINGS_OVERVIEW)
+    case Constants.ETH_SETTINGS:
+      return makeBackButton(BACK_TEXT, Constants.SETTINGS_OVERVIEW)
+    case Constants.LTC_SETTINGS:
+      return makeBackButton(BACK_TEXT, Constants.SETTINGS_OVERVIEW)
+    case Constants.BCH_SETTINGS:
+      return makeBackButton(BACK_TEXT, Constants.SETTINGS_OVERVIEW)
+    case Constants.CHANGE_PASSWORD:
+      return makeBackButton(CANCEL_TEXT, Constants.SETTINGS_OVERVIEW)
+    case Constants.CHANGE_PIN:
+      return makeBackButton(CANCEL_TEXT, Constants.SETTINGS_OVERVIEW)
+    case Constants.RECOVER_PASSWORD:
+      return makeBackButton(CANCEL_TEXT, Constants.SETTINGS_OVERVIEW)
+    default:
+      return null
     }
   }
 }
