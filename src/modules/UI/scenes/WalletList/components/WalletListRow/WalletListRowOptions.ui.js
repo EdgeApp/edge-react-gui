ce9ba34eac9634dcc6d2f17d1bdb53edc26850e3
“Allen <allen@missionvi.com>
2017-10-12 19:08:28 -0700

    Refactor of WalletListRowOptions
    • Uses new MenuDropDown component

--- src/modules/UI/scenes/WalletList/components/WalletListRow/WalletListRowOptions.ui.js
+++ src/modules/UI/scenes/WalletList/components/WalletListRow/WalletListRowOptions.ui.js
@@ -1,25 +1,26 @@
 import React, {Component} from 'react'
-import {Text, View} from 'react-native'
-import styles from '../../style'
-import Menu, {MenuOptions, MenuOption, MenuTrigger} from 'react-native-menu'
-import T from '../../../../components/FormattedText/FormattedText.ui'
+// import {Text, View} from 'react-native'
+//import styles from '../../style'
+// import Menu, {MenuOptions, MenuOption, MenuTrigger} from 'react-native-menu'
+// import T from '../../../../components/FormattedText/FormattedText.ui'
 import strings from '../../../../../../locales/default'
-
+import MenuDropDown from '../../../../components/MenuDropDown/MenuDropDown.ui'
+import {MenuDropDownStyle} from '../../../../../../styles/indexStyles'
 export const options = [
   {
     value: 'rename',
-    syntax: strings.enUS['string_rename']
+    label: strings.enUS['string_rename']
   },{
     value: 'sort',
-    syntax: strings.enUS['fragment_wallets_sort']
+    label: strings.enUS['fragment_wallets_sort']
   },{
     value: 'addToken',
-    syntax: strings.enUS['fragmet_wallets_addtoken_option']
+    label: strings.enUS['fragmet_wallets_addtoken_option']
   },/*{
     value: 'archive'
   },*/{
     value: 'delete',
-    syntax: strings.enUS['string_delete']
+    label: strings.enUS['string_delete']
   }
 ]
 
@@ -35,7 +36,7 @@ export default class WalletListRowOptions extends Component {
     }
   }
 
-  optionAction (optionKey) {
+  optionAction = (optionKey) => {
     this.props.executeWalletRowOption(this.props.walletKey, optionKey, this.props.wallets, this.props.archives)
     if (optionKey === 'Rename') {
       this.props.updateRenameWalletInput(this.props.wallets[this.props.walletKey].name)
@@ -43,67 +44,10 @@ export default class WalletListRowOptions extends Component {
   }
 
   render () {
-    // possibly refactor MenuOptions into component that gets looped. Properties could be put into array form
     return (
-      <View style={[styles.rowDotsWrap]}>
-        <Menu style={[styles.menuButton]}
-          onSelect={(value) => this.optionAction(value)}>
-
-          <MenuTrigger style={[styles.menuTrigger]}>
-            <Text style={{fontSize: 20}}>
-              &#8942;
-            </Text>
-          </MenuTrigger>
-
-          <MenuOptions>
-
-            <MenuOption style={styles.menuOption}
-              value={options[0].value}>
-              <View style={[styles.menuOptionItem]}>
-                <T style={[styles.optionText]}>
-                  {options[0].syntax}
-                </T>
-              </View>
-            </MenuOption>
-
-            <MenuOption style={styles.menuOption}
-              value={options[1].value}>
-              <View style={[styles.menuOptionItem]}>
-                <T style={[styles.optionText]}>
-                  {options[1].syntax}
-                </T>
-              </View>
-            </MenuOption>
-
-            <MenuOption style={styles.menuOption}
-              value={options[2].value}>
-              <View style={[styles.menuOptionItem]}>
-                <T style={[styles.optionText]}>
-                  {options[2].syntax}
-                </T>
-              </View>
-            </MenuOption>
-
-            {/*<MenuOption style={styles.menuOption}
-              value={options[3].value}>
-              <View style={[styles.menuOptionItem]}>
-                <T style={[styles.optionText]}>
-                  {this.state.archiveSyntax}
-                </T>
-              </View>
-            </MenuOption>*/}
-
-            <MenuOption style={styles.menuOption}
-              value={options[3].value}>
-              <View style={[styles.menuOptionItem]}>
-                <T style={[styles.optionText]}>
-                  {options[3].syntax}
-                </T>
-              </View>
-            </MenuOption>
-          </MenuOptions>
-        </Menu>
-      </View>
+      <MenuDropDown style={MenuDropDownStyle}
+        onSelect={this.optionAction}
+        data={options} />
     )
   }
 }
