d8c0c12e2c2d0b4a8e0fe5efd07205070afcffdb
“Allen <allen@missionvi.com>
2017-10-20 13:15:54 -0700

    API call for lobby

--- src/actions/EdgeLoginActions.js
+++ src/actions/EdgeLoginActions.js
@@ -1,8 +1,15 @@
 // @flow
 // import * as actions from './indexActions'
+import type {AbcLobby} from 'airbitz-core-types'
 
-export const loginWithEdge = (url: string) => (dispatch: any, getState: any) => {
+export const loginWithEdge = (url: string) => async (dispatch: any, getState: any) => {
+  const splitArray = url.split('edge/')
+  console.log(splitArray[1])
   const state = getState()
-  console.log(url)
-  console.log(state)
+  const account = state.core.account
+  const lobby: AbcLobby = await account.fetchLobby(splitArray[1]).catch((e) => {
+    console.log('We have an eror')
+    console.log(e)
+  })
+  console.log(lobby)
 }
