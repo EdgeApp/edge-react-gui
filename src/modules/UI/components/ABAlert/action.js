8e1900013f36e76a5c0517763b994cd7553cfdfc
Paul V Puey <paullinator@users.noreply.github.com>
2017-10-22 18:50:34 -0700

    Add TODO comment

--- src/modules/UI/components/ABAlert/action.js
+++ src/modules/UI/components/ABAlert/action.js
@@ -1,4 +1,5 @@
 // @flow
+// TODO revisit as there is no reason for this to exist OR it should have an action constant coded into it
 export function openABAlert (type: string, data: any) {
   return {
     type,
