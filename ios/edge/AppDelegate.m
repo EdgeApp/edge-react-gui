6a00d221fb5e785cc0ac8305794eb6fa752797c8
Kevin Sullivan <kevin@airbitz.co>
2017-11-03 10:40:28 -0700

    Add splash screen iOS only

--- ios/edge/AppDelegate.m
+++ ios/edge/AppDelegate.m
@@ -11,6 +11,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RCTSplashScreen.h"
 
 @implementation AppDelegate
 
@@ -24,6 +25,8 @@
                                                       moduleName:@"edge"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
+
+  [RCTSplashScreen open:rootView withImageNamed:@"splash"];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
