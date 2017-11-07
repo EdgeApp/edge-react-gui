48e36ca43ac7225c12ba4fab0705d26a8998a767
“Allen <allen@missionvi.com>
2017-11-07 08:11:21 -0800

    Fixed Carat alignment on TextAndIconButtons

--- src/styles/components/ButtonStyles.js
+++ src/styles/components/ButtonStyles.js
@@ -18,40 +18,46 @@ const IconButtonStyle = {
 
 const TextAndIconButtonStyle = {
   container: {
-    width:'100%',
+    width: '100%',
     height: '100%',
-    justifyContent:'space-around',
-    alignItems:'center',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
-  inner:{
-    width:'80%',
-    position:'relative',
-    flexDirection: 'row',
-    justifyContent:'space-around',
-    alignItems:'center'
+  centeredContent: {
+    width: '100%',
+    alignItems: 'center'
+  },
+  inner: {
+    position: 'relative',
+    flexDirection: 'row'
+  },
+  textContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  iconContainer: {
+    height: '100%',
+    flexDirection: 'column',
+    alignItems: 'center'
+
   },
   text: {
-    flexDirection:'row',
     color: THEME.COLORS.WHITE,
-    fontSize: 20,
-    alignItems: 'center'
+    fontSize: 20
   },
   textPressed: {
     color: THEME.COLORS.GRAY_2,
     fontSize: 20
   },
   icon: {
-    position:'absolute',
-    top:20,
-    color: THEME.COLORS.WHITE,
+    color: THEME.COLORS.WHITE
   },
   iconPressed: {
-    position:'absolute',
-    top:20,
     color: THEME.COLORS.GRAY_2
   },
   iconSize: 25,
-  underlayColor: THEME.COLORS.CLEAR
+  underlayColor: THEME.COLORS.TRANSPARENT
 }
 
 export {IconButtonStyle}
