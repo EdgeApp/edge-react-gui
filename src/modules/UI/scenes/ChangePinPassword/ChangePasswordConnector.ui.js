2b35abfa0eb68ff201d360a82d3064e4ce93f69a
Kevin Sullivan <kevin@airbitz.co>
2017-11-14 13:25:43 -0800

    Add type checking to ChangePinConnector, ChangePasswordConnector

--- src/modules/UI/scenes/ChangePinPassword/ChangePasswordConnector.ui.js
+++ src/modules/UI/scenes/ChangePinPassword/ChangePasswordConnector.ui.js
@@ -1,9 +1,13 @@
+// @flow
+
 import {connect} from 'react-redux'
 import * as CORE_SELECTORS from '../../../Core/selectors.js'
 import {Actions} from 'react-native-router-flux'
 import ChangePasswordComponent from './ChangePasswordComponent.ui'
 
-export const mapStateToProps = (state) => ({
+import type {State} from '../../../ReduxTypes'
+
+export const mapStateToProps = (state: State) => ({
   context: CORE_SELECTORS.getContext(state),
   account: CORE_SELECTORS.getAccount(state),
   showHeader: false
