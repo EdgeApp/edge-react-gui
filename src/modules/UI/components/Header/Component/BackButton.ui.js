f1e8b97eb8857a03dc41988db680649b14dc4101
Andrii Koretskyi <koretskiyav@gmail.com>
2017-11-15 13:53:38 +0200

    fix double tapping on back button bug (go back to twice)

--- src/modules/UI/components/Header/Component/BackButton.ui.js
+++ src/modules/UI/components/Header/Component/BackButton.ui.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react'
 import {TouchableOpacity} from 'react-native'
 import T from '../../../components/FormattedText'
 import styles from '../style'
+import { Icon } from 'native-base'
 
 export default class BackButton extends Component {
+  static defaultProps = {
+    withArrow: false,
+  }
+
   render () {
     return (
-      <TouchableOpacity onPress={this.props.onPress}>
+      <TouchableOpacity style={styles.backButton} onPress={this.props.onPress}>
+        {this.props.withArrow && <Icon size={14} name='ios-arrow-back-outline' style={styles.backIconStyle} />}
         <T style={[styles.sideText]}>{this.props.label}</T>
       </TouchableOpacity>
     )
