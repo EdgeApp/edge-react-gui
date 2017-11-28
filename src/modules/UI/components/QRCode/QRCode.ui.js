be78e09a5595ff1db68a60a5b0bfbb04da6847d1
Art Manalili <artmanalili@gmail.com>
2017-11-28 19:58:48 +0800

    Added keyboard listener event so it styles it to be smaller and more push up when keyboard is up

--- src/modules/UI/components/QRCode/QRCode.ui.js
+++ src/modules/UI/components/QRCode/QRCode.ui.js
@@ -1,19 +1,44 @@
 import React, {Component} from 'react'
-import {View} from 'react-native'
+import {Keyboard, View} from 'react-native'
 import QrCode from 'react-native-qrcode'
 import platform from '../../../../theme/variables/platform.js'
 
 import styles from './styles'
 
 export default class QRCode extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      keyboardUp: false
+    }
+  }
+  componentWillMount () {
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardWillShow', this._keyboardDidShow.bind(this))
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardWillHide', this._keyboardDidHide.bind(this))
+  }
+  componentWillUnmount () {
+    this.keyboardDidShowListener.remove()
+    this.keyboardDidHideListener.remove()
+  }
+  _keyboardDidShow () {
+    this.setState({
+      keyboardUp: true
+    })
+  }
+
+  _keyboardDidHide () {
+    this.setState({
+      keyboardUp: false
+    })
+  }
   render () {
     return (
-      <View style={styles.qrCodeBorder}>
+      <View style={[ styles.qrCodeBorder, this.state.keyboardUp ? {marginBottom: 60} : null]}>
         <QrCode
           value={this.props.value}
           bgColor={styles.qrCodeBackground.color}
           fgColor={styles.qrCodeForeground.color}
-          size={platform.deviceHeight / 4} />
+          size={this.state.keyboardUp ? platform.deviceHeight / 4.3 : platform.deviceHeight / 4} />
       </View>
     )
   }
