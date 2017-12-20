2575839727f6063f6ac20007dabb9be75c2777fc
Kevin Sullivan <kevin@airbitz.co>
2017-12-20 13:08:29 -0800

    Upgrade to new translation syntax

--- src/modules/UI/components/Header/Component/HelpButton.ui.js
+++ src/modules/UI/components/Header/Component/HelpButton.ui.js
@@ -2,20 +2,18 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {TouchableOpacity} from 'react-native'
 
-import {sprintf} from 'sprintf-js'
 import s from '../../../../../locales/strings.js'
 
 import {openHelpModal} from '../../HelpModal/actions.js'
 import T from '../../../components/FormattedText'
 import styles from '../style'
 
-const HELP_TEXT = sprintf(s.strings.string_help)
+const HELP_TEXT = s.strings.string_help
 
 class HelpButton extends Component {
   render () {
     return (
-      <TouchableOpacity style={styles.sideTextWrap}
-        onPress={() => this.props.dispatch(openHelpModal())}>
+      <TouchableOpacity style={styles.sideTextWrap} onPress={() => this.props.dispatch(openHelpModal())}>
         <T style={[styles.sideText]}>
           {HELP_TEXT}
         </T>
