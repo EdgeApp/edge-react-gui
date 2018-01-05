8399c940513b8becc3fe22f29ef15120be9df4fd
thehobbit85 <thehobbit85@gmail.com>
2018-01-05 07:27:37 -0800

    clean up the logs

--- src/modules/Core/Account/callbacks.js
+++ src/modules/Core/Account/callbacks.js
@@ -19,28 +19,24 @@ const makeAccountCallbacks = (dispatch: Dispatch): AbcAccountCallbacks => {
     onRemotePasswordChanged: () => console.log('onRemotePasswordChanged'),
 
     onKeyListChanged: () => {
-      console.log('onKeyListChanged')
       // $FlowFixMe
       dispatch(updateWalletsRequest())
     },
 
     onAddressesChecked (walletId: string, progressRatio: number) {
-      if (progressRatio === 1) console.log('onAddressesChecked: ' + walletId + ' ratio:' + progressRatio)
+      if (progressRatio === 1) {
+        console.log(`${walletId} - onAddressesChecked with ratio: ${progressRatio}`)
+      }
     },
 
     onBalanceChanged (walletId: string, currencyCode: string, balance: string) {
-      console.log('onBalanceChanged: ' + walletId + ' ' + currencyCode + ' ' + balance)
+      console.log(`${walletId} - onBalanceChanged for currency${currencyCode}: ${balance}`)
       dispatch(refreshWallet(walletId))
     },
 
     onTransactionsChanged (walletId: string, transactions: Array<AbcTransaction>) {
-      if (transactions && transactions.length) {
-        console.log('onTransactionsChanged length=' + transactions.length.toString())
-        for (const tx of transactions) {
-          console.log('walletId:' + walletId + ' txid:' + tx.txid)
-        }
-      } else {
-        console.log('onTransactionsChanged: No transactions')
+      if (!transactions || !transactions.length) {
+        console.log(`${walletId} - onTransactionsChanged: No transactions`)
       }
       // $FlowFixMe
       dispatch(refreshTransactionsRequest(walletId))
@@ -48,13 +44,8 @@ const makeAccountCallbacks = (dispatch: Dispatch): AbcAccountCallbacks => {
     },
 
     onNewTransactions (walletId: string, transactions: Array<AbcTransaction>) {
-      if (transactions && transactions.length) {
-        console.log('onNewTransactions length=' + transactions.length.toString())
-        for (const tx of transactions) {
-          console.log('walletId:' + walletId + ' txid:' + tx.txid)
-        }
-      } else {
-        console.log('onNewTransactions: No transactions')
+      if (!transactions || !transactions.length) {
+        console.log(`${walletId} - onNewTransactions: No transactions`)
       }
       dispatch(newTransactionsRequest(walletId, transactions))
       dispatch(refreshWallet(walletId))
@@ -63,12 +54,12 @@ const makeAccountCallbacks = (dispatch: Dispatch): AbcAccountCallbacks => {
     },
 
     onBlockHeightChanged (walletId: string, blockHeight: number) {
-      console.log('onBlockHeightChanged: ' + walletId + ' height:' + blockHeight)
+      console.log(`${walletId} - onBlockHeightChanged with height:${blockHeight}`)
       dispatch(refreshWallet(walletId))
     },
 
     onWalletNameChanged (walletId: string, walletName: string | null) {
-      console.log('onWalletNameChanged:' + walletId + ' newname:' + (walletName ? walletName : ''))
+      console.log(`${walletId} - onWalletNameChanged with new name:${walletName ? walletName : ''}`)
       dispatch(refreshWallet(walletId))
     }
   }
--- src/modules/Core/Context/callbacks.js
+++ src/modules/Core/Context/callbacks.js
@@ -12,7 +12,7 @@ export default (dispatch: Dispatch): AbcContextCallbacks => ({
   },
 
   onExchangeUpdate () {
-    console.log('onExchangeUpdate')
+    // console.log('onExchangeUpdate')
     dispatch(updateExchangeRates())
   }
 })
