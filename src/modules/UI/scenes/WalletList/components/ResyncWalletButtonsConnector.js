5e7e7480377547f778642dfbdf1b806cdb398e71
thehobbit85 <thehobbit85@gmail.com>
2018-01-11 21:46:43 -0800

    flow fixes

--- src/modules/UI/scenes/WalletList/components/ResyncWalletButtonsConnector.js
+++ src/modules/UI/scenes/WalletList/components/ResyncWalletButtonsConnector.js
@@ -1,7 +1,18 @@
 // @flow
 import {connect} from 'react-redux'
 import ResyncWalletButtons from './ResyncWalletButtons.ui'
+import type {State, Dispatch} from '../../../../ReduxTypes'
 import {closeResyncWalletModal, resyncWallet} from '../action'
+
+export type StateToProps = {
+  walletId: string
+}
+
+export type DispatchProps = {
+  onPositive: (walletId: string) => any,
+  onNegative: () => any,
+  onDone: () => any
+}
 
 const mapStateToProps = (state: State): StateToProps => ({
   walletId: state.ui.scenes.walletList.walletId
--- src/modules/UI/scenes/WalletList/components/SplitWalletButtonsConnector.js
+++ src/modules/UI/scenes/WalletList/components/SplitWalletButtonsConnector.js
@@ -1,7 +1,18 @@
 // @flow
 import {connect} from 'react-redux'
 import SplitWalletButtons from './SplitWalletButtons.ui'
+import type {State, Dispatch} from '../../../../ReduxTypes'
 import {closeSplitWalletModal, splitWallet} from '../action'
+
+export type StateToProps = {
+  walletId: string
+}
+
+export type DispatchProps = {
+  onPositive: (walletId: string) => any,
+  onNegative: () => any,
+  onDone: () => any
+}
 
 const mapStateToProps = (state: State): StateToProps => ({
   walletId: state.ui.scenes.walletList.walletId
