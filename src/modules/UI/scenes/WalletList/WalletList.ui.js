206560cfa9a046a4e8f19ce6c88fffdda7a8f6c4
vsashyn <sashyn.v@gmail.com>
2018-01-12 14:57:38 +0200

    Add mint color integration. Update react-native-slider to ^0.11.0

--- src/modules/UI/scenes/WalletList/WalletList.ui.js
+++ src/modules/UI/scenes/WalletList/WalletList.ui.js
@@ -6,13 +6,13 @@ import {
   TouchableOpacity,
   ActivityIndicator,
   Animated,
-  FlatList
+  FlatList,
+  Image
 } from 'react-native'
 import Permissions from 'react-native-permissions'
 import Contacts from 'react-native-contacts'
 import T from '../../components/FormattedText'
 import Ionicon from 'react-native-vector-icons/Ionicons'
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
 import Gradient from '../../components/Gradient/Gradient.ui'
 import {Actions} from 'react-native-router-flux'
 import styles from './style'
@@ -30,6 +30,7 @@ import WalletNameInput from './components/WalletNameInputConnector'
 import RenameWalletButtons from './components/RenameWalletButtonsConnector'
 import DeleteIcon from './components/DeleteIcon.ui'
 import RenameIcon from './components/RenameIcon.ui'
+import WalletIcon from '../../../../assets/images/walletlist/my-wallets.png'
 import platform from '../../../../theme/variables/platform.js'
 
 import type {GuiContact} from '../../../../types'
@@ -203,7 +204,7 @@ export default class WalletList extends Component<Props, State> {
 
             <View style={[styles.walletsBoxHeaderTextWrap, UTILS.border()]}>
               <View style={styles.leftArea}>
-                <SimpleLineIcons name='wallet' style={[styles.walletIcon]} color='white' />
+                <Image source={WalletIcon} style={[styles.walletIcon]} />
                 <T style={styles.walletsBoxHeaderText}>
                   {WALLETS_HEADER_TEXT}
                 </T>
