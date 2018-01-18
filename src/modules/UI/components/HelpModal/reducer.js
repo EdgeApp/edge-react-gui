3ba8a597b9ce4ecbe2ec14c9a2b03424d026e379
Kevin Sullivan <kevin@airbitz.co>
2018-01-17 18:45:01 -0800

    Add Help Modal Reducer test

--- src/modules/UI/components/HelpModal/reducer.js
+++ src/modules/UI/components/HelpModal/reducer.js
@@ -1,10 +1,15 @@
+// @flow
+
 import * as ACTION from './actions.js'
 
-export const helpModal = (state = false, action) => {
+import type { Action } from '../../../ReduxTypes'
+
+type HelpModalState = boolean
+export const helpModal = (state: HelpModalState = false, action: Action) => {
   switch (action.type) {
-    case ACTION.OPEN_HELP_MODAL :
+    case ACTION.OPEN_HELP_MODAL:
       return true
-    case ACTION.CLOSE_HELP_MODAL :
+    case ACTION.CLOSE_HELP_MODAL:
       return false
     default:
       return state
