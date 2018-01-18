ca8695b5ef3c00c863032829462a0b428970bafb
thehobbit85 <thehobbit85@gmail.com>
2018-01-17 21:45:38 -0800

    move style into the component

--- src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/style.js
+++ src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/style.js
@@ -1,17 +1,21 @@
 // @flow
 
 import {StyleSheet} from 'react-native'
-import THEME from '../../../../theme/variables/airbitz'
+import THEME from '../../../../../../theme/variables/airbitz'
 
 const styles = {
-
-    customFeeButtonContainer: {
+  customFeeButton: {
+    backgroundColor: THEME.COLORS.SECONDARY,
+    borderRadius: 3
+  },
+  customFeeButtonContainer: {
     flex: 1,
     flexDirection: 'row',
     paddingTop: 25,
     alignSelf: 'center',
     height: 52,
     width: 250
-  },
+  }
+}
 
-  export default StyleSheet.create(styles)
+export default StyleSheet.create(styles)
