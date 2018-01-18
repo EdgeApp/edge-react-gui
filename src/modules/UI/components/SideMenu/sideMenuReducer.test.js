af095dc75d0c421460457cd80f345361dbcfc3da
Kevin Sullivan <kevin@airbitz.co>
2018-01-17 16:24:50 -0800

    Add Side Menu Reducer test

---
+++ src/modules/UI/components/SideMenu/sideMenuReducer.test.js
@@ -0,0 +1,12 @@
+/* globals test expect */
+
+import { sideMenu as sideMenuReducer } from './reducer.js'
+
+test('initialState', () => {
+  const expected = {
+    view: false
+  }
+  const actual = sideMenuReducer(undefined, {})
+
+  expect(actual).toEqual(expected)
+})
