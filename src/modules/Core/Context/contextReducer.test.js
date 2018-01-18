6ac032b904da9daa2e16d242d19145910dca67ac
Kevin Sullivan <kevin@airbitz.co>
2018-01-17 17:56:50 -0800

    Add Core Context Reducer test

---
+++ src/modules/Core/Context/contextReducer.test.js
@@ -0,0 +1,14 @@
+/* globals test expect */
+
+import { context as contextReducer } from './reducer.js'
+
+test('initialState', () => {
+  const expected = {
+    context: {},
+    nextUsername: '',
+    usernames: []
+  }
+  const actual = contextReducer(undefined, {})
+
+  expect(actual).toEqual(expected)
+})
