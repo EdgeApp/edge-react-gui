48a9816463cbc8527cc10db7d206039a6b7c2769
thehobbit85 <thehobbit85@gmail.com>
2018-01-17 21:43:41 -0800

    Filled up the Modal UI and Connector

--- src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/CustomFeesModal.ui.js
+++ src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/CustomFeesModal.ui.js
@@ -1,20 +1,49 @@
 // @flow
 
 import React, {Component} from 'react'
+import StylizedModal from '../../../../components/Modal/Modal.ui'
+import * as Constants from '../../../../../../constants/indexConstants.js'
+import OptionIcon from '../../../../components/OptionIcon/OptionIcon.ui'
+import OptionSubtext from '../../../../components/OptionSubtext/OptionSubtextConnector.js'
+import OptionButtons from '../../../../components/OptionButtons/OptionButtons.ui.js'
+import s from '../../../../../../locales/strings.js'
 
-import {TextInput} from 'react-native'
+type Props = {
+  visibilityBoolean: boolean,
+  onExitButtonFxn: Function,
+  onNegative: Function,
+  onPositive: Function,
+  onDone: Function,
+  walletId: string
+}
 
-import StylizedModal from '../../../components/Modal/Modal.ui'
+type State = any
 
-type Props = {}
-type State = {}
-export class CustomFeesModal extends Component<Props, State> {
-  constructor (props) {
-    super(props)
-    this.state = {}
+export default class CustomFeesModal extends Component<Props, State> {
+  onNegative = () => {
+    this.props.onNegative()
+    this.props.onDone()
+  }
+  onPositive = () => {
+    this.props.onPositive(this.props.walletId)
+    this.props.onDone()
   }
 
   render () {
-    return <StylizedModal />
+    return <StylizedModal
+      featuredIcon={<OptionIcon iconName={Constants.CUSTOM_FEES_ICON}/>}
+      headerText={s.strings.fragment_wallets_set_custom_fees}
+      modalMiddle={<OptionSubtext
+        confirmationText={s.strings.fragment_wallets_set_custom_fees}
+        label={s.strings.fragment_wallets_set_custom_fees}
+      />}
+      modalBottom={<OptionButtons
+        positiveText={s.strings.string_custom_fee}
+        onPositive={this.onPositive}
+        onNegative={this.onNegative}
+      />}
+      visibilityBoolean={this.props.visibilityBoolean}
+      onExitButtonFxn={this.props.onExitButtonFxn}
+    />
   }
 }
--- src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/CustomFeesModalConnector.js
+++ src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/CustomFeesModalConnector.js
@@ -1,10 +1,45 @@
 // @flow
 
-import { connect } from 'react-redux'
-import type { State } from '../../../../../ReduxTypes'
-import CustomFeesModal from './CustomFeesModal.ui'
+import {connect} from 'react-redux'
 
-export const mapStateToProps = (state: State) => ({})
-export const mapDispatchToProps = ({})
+import CustomFeesModal from './CustomFeesModal.ui'
+import * as Constants from '../../../../../../constants/indexConstants.js'
+import type {Dispatch, GetState, State} from '../../../../../ReduxTypes'
+import {
+  VISIBLE_MODAL_NAME,
+  CLOSE_MODAL_VALUE,
+  START_MODAL_VALUE,
+  SUCCESS_MODAL_VALUE,
+  wrap
+} from '../../../WalletList/components/WalletOptions/action'
+
+import * as CORE_SELECTORS from '../../../../../Core/selectors.js'
+import * as ACCOUNT_API from '../../../../../Core/Account/api.js'
+
+const updateFees = (walletId: string) => (dispatch: Dispatch, getState: GetState) => {
+  const state = getState()
+  const account = CORE_SELECTORS.getAccount(state)
+
+  dispatch(wrap(START_MODAL_VALUE(Constants.CUSTOM_FEES), {walletId}))
+  dispatch(wrap(SUCCESS_MODAL_VALUE(Constants.CUSTOM_FEES), {walletId}))
+
+  // ACCOUNT_API.updateFeesRequest(account, walletId)
+  //   .then(() => {
+  //     dispatch(wrap(SUCCESS_MODAL_VALUE(Constants.CUSTOM_FEES), {walletId}))
+  //   })
+  //   .catch((e) => console.log(e))
+}
+
+const mapStateToProps = (state: State) => ({
+  visibilityBoolean: state.ui.scenes.walletList[VISIBLE_MODAL_NAME(Constants.CUSTOM_FEES)],
+  walletId: state.ui.scenes.walletList.walletId
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  onExitButtonFxn: () => dispatch({ type: CLOSE_MODAL_VALUE(Constants.CUSTOM_FEES) }),
+  onNegative: () => {},
+  onPositive: (walletId) => dispatch(updateFees(walletId)),
+  onDone: () => dispatch({type: CLOSE_MODAL_VALUE(Constants.CUSTOM_FEES)})
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(CustomFeesModal)
