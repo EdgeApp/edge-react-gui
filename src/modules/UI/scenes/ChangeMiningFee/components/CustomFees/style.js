9e7684264ee73b34111c89ac234dccf6c2489c43
thehobbit85 <thehobbit85@gmail.com>
2018-01-19 18:36:39 -0800

    added missing styles

--- src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/style.js
+++ src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/style.js
@@ -1,6 +1,6 @@
 // @flow
 
-import {StyleSheet} from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 import THEME from '../../../../../../theme/variables/airbitz'
 
 const styles = {
@@ -15,6 +15,24 @@ const styles = {
     alignSelf: 'center',
     height: 52,
     width: 250
+  },
+  feeInputWrap: {
+    justifyContent: 'flex-end',
+    alignItems: 'stretch',
+    marginTop: 0,
+    marginBottom: 0,
+    borderBottomColor: THEME.COLORS.GRAY_4,
+    borderBottomWidth: (Platform.OS === 'ios')
+      ? 1
+      : 0
+  },
+  feeInput: {
+    height: (Platform.OS === 'ios')
+      ? 26
+      : 46,
+    textAlign: 'center',
+    fontSize: 20,
+    color: THEME.COLORS.GRAY_1
   }
 }
 
