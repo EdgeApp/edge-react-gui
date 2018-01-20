f1fc2374a70289d8d0edd726de4d0d52e8354008
thehobbit85 <thehobbit85@gmail.com>
2018-01-19 18:38:43 -0800

    Render the middle part dynamicly based on the custom fees per currency and adujst the height to match the number of rows

--- src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/CustomFeesModal.ui.js
+++ src/modules/UI/scenes/ChangeMiningFee/components/CustomFees/CustomFeesModal.ui.js
@@ -1,12 +1,16 @@
 // @flow
 
 import React, {Component} from 'react'
+import { View, TextInput } from 'react-native'
 import StylizedModal from '../../../../components/Modal/Modal.ui'
+import { FormField } from '../../../../../../components/FormField.js'
 import * as Constants from '../../../../../../constants/indexConstants.js'
+import styles from './style'
 import OptionIcon from '../../../../components/OptionIcon/OptionIcon.ui'
 import OptionSubtext from '../../../../components/OptionSubtext/OptionSubtextConnector.js'
 import OptionButtons from '../../../../components/OptionButtons/OptionButtons.ui.js'
 import s from '../../../../../../locales/strings.js'
+import { Platform } from 'react-native'
 
 type Props = {
   visibilityBoolean: boolean,
@@ -20,30 +24,77 @@ type Props = {
 type State = any
 
 export default class CustomFeesModal extends Component<Props, State> {
+  constructor (props: Props) {
+    super(props)
+    this.state = {}
+    this.cleanState()
+    for (const feeSetting of this.props.customFeeSettings) {
+      this.state[feeSetting] = ''
+    }
+  }
+
+  cleanState = () => {
+    for (const feeSetting of this.props.customFeeSettings) {
+      this.state[feeSetting] = ''
+    }
+  }
+  
   onNegative = () => {
     this.props.onNegative()
-    this.props.onDone()
-  }
-  onPositive = () => {
-    this.props.onPositive(this.props.walletId)
+    this.cleanState()
     this.props.onDone()
   }
 
+  onPositive = () => {
+    this.props.onPositive(this.state)
+    this.cleanState()
+    this.props.onDone()
+  }
+
+  _onFeeSettingInputChange = (feeSetting) => (input: string) => {
+    this.setState({ [feeSetting]: input })
+  }
+
+  onExitButtonFxn = () => {
+    this.cleanState()
+    this.props.onExitButtonFxn()
+  }
+
+  renderModalMiddle = () => {
+    const rows = []
+    for (const feeSetting of this.props.customFeeSettings) {
+      rows.push(
+        <View style={[styles.feeInputWrap]} key={feeSetting}>
+          <FormField
+            keyboardType='numeric'
+            style={[styles.feeInput]}
+            placeholder={'0'}
+            onChangeText={this._onFeeSettingInputChange(feeSetting)}
+            value={this.state[feeSetting]}
+            label={feeSetting}
+            autoFocus
+          />
+        </View>
+      )
+    }
+    return rows
+  }
+
   render () {
+    const modalMiddle = this.renderModalMiddle()
+    const height = 50 + ( modalMiddle.length - 1 ) * 58
     return <StylizedModal
       featuredIcon={<OptionIcon iconName={Constants.CUSTOM_FEES_ICON}/>}
       headerText={s.strings.fragment_wallets_set_custom_fees}
-      modalMiddle={<OptionSubtext
-        confirmationText={s.strings.fragment_wallets_set_custom_fees}
-        label={s.strings.fragment_wallets_set_custom_fees}
-      />}
+      modalMiddle={modalMiddle}
+      modalMiddleStyle={{ height }}
       modalBottom={<OptionButtons
         positiveText={s.strings.string_custom_fee}
         onPositive={this.onPositive}
         onNegative={this.onNegative}
       />}
       visibilityBoolean={this.props.visibilityBoolean}
-      onExitButtonFxn={this.props.onExitButtonFxn}
+      onExitButtonFxn={this.onExitButtonFxn}
     />
   }
 }
