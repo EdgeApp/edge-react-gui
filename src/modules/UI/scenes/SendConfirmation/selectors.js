218961d1b787a1cde18314c4aa9272334dd77fe9
thehobbit85 <thehobbit85@gmail.com>
2018-01-22 19:44:13 -0800

    Create selectors for the sendConfirmation scene

---
+++ src/modules/UI/scenes/SendConfirmation/selectors.js
@@ -0,0 +1,16 @@
+// @flow
+
+import * as UI_SELECTORS from '../../selectors.js'
+import type { State } from '../ReduxTypes'
+
+export const getScene = (state: State) => UI_SELECTORS.getSceneState(state, 'sendConfirmation')
+
+export const getNetworkFeeOption = (state: State) => getScene(state).networkFeeOption
+export const getCustomNetworkFee = (state: State) => getScene(state).customNetworkFee
+export const getTransaction = (state: State) => getScene(state).transaction
+export const getPending = (state: State) => getScene(state).pending
+export const getParsedUri = (state: State) => getScene(state).parsedUri
+export const getKeyboardIsVisible = (state: State) => getScene(state).keyboardIsVisible
+export const getError = (state: State) => getScene(state).error
+export const getLabel = (state: State) => getScene(state).label
+export const getPublicAddress = (state: State) => getScene(state).publicAddress
