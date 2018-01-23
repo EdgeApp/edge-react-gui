f30c992323e9991e62c468bc2f2d5e91a21c6a1f
Kevin Sullivan <kevin@airbitz.co>
2018-01-22 19:33:08 -0800

    Add types to state.core

--- src/modules/ReduxTypes.js
+++ src/modules/ReduxTypes.js
@@ -1,11 +1,36 @@
 // @flow
 
 import type { Store as ReduxStore, Dispatch as ReduxDispatch } from 'redux'
+import type { AbcContext, AbcCurrencyWallet, AbcAccount, AbcLobby } from 'airbitz-core-types'
 
 export type Action = { type: string, data?: any }
 
+export type CurrencyCode = string
+export type Id = string
+export type Username = string
+
 export type State = {
-  core: any,
+  core: {
+    account: AbcAccount,
+    context: {
+      context: AbcContext,
+      usernames: Array<Username>,
+      nextUsername: Username
+    },
+    wallets: {
+      byId: {
+        [Id]: AbcCurrencyWallet
+      }
+    },
+    edgeLogin: {
+      lobby: AbcLobby | null,
+      error: Error | null,
+      isProcessing: boolean
+    },
+    deepLinking: {
+      passwordRecoveryLink: string | null
+    }
+  },
   ui: any,
   cryptoExchange: any,
   exchangeRates: any
