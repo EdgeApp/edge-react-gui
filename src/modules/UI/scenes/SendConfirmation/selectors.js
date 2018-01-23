d249523decae264e7cddf29c4d8e96c93ec32e36
thehobbit85 <thehobbit85@gmail.com>
2018-01-23 03:42:50 -0800

    flow

--- src/modules/UI/scenes/SendConfirmation/selectors.js
+++ src/modules/UI/scenes/SendConfirmation/selectors.js
@@ -2,15 +2,18 @@
 
 import * as UI_SELECTORS from '../../selectors.js'
 import type { State } from '../../../ReduxTypes'
+import type {AbcTransaction, AbcParsedUri, AbcMetadata} from 'airbitz-core-types'
 
-export const getScene = (state: State) => UI_SELECTORS.getSceneState(state, 'sendConfirmation')
+export const getScene = (state: State): any => UI_SELECTORS.getSceneState(state, 'sendConfirmation')
 
-export const getNetworkFeeOption = (state: State) => getScene(state).networkFeeOption
-export const getCustomNetworkFee = (state: State) => getScene(state).customNetworkFee
-export const getTransaction = (state: State) => getScene(state).transaction
-export const getPending = (state: State) => getScene(state).pending
-export const getParsedUri = (state: State) => getScene(state).parsedUri
-export const getKeyboardIsVisible = (state: State) => getScene(state).keyboardIsVisible
-export const getError = (state: State) => getScene(state).error
-export const getLabel = (state: State) => getScene(state).label
-export const getPublicAddress = (state: State) => getScene(state).publicAddress
+export const getNetworkFeeOption = (state: State): string => getScene(state).networkFeeOption
+export const getCustomNetworkFee = (state: State): any => getScene(state).customNetworkFee
+export const getTransaction = (state: State): AbcTransaction => getScene(state).transaction
+export const getPending = (state: State): boolean => getScene(state).pending
+export const getParsedUri = (state: State): AbcParsedUri => getScene(state).parsedUri
+export const getKeyboardIsVisible = (state: State): boolean => getScene(state).keyboardIsVisible
+export const getError = (state: State): Error => getScene(state).error
+export const getLabel = (state: State): string => getScene(state).label
+export const getPublicAddress = (state: State): string => getParsedUri(state).publicAddress
+export const getNativeAmount = (state: State): string => getParsedUri(state).nativeAmount
+export const getMetadata = (state: State): AbcMetadata => getParsedUri(state).metadata
