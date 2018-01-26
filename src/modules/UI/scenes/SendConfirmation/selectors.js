375cb878c251e3cf795a20b03de7fac749972485
thehobbit85 <thehobbit85@gmail.com>
2018-01-25 22:24:58 -0800

    cleaned types and made sure selectros would return something viable.

--- src/modules/UI/scenes/SendConfirmation/selectors.js
+++ src/modules/UI/scenes/SendConfirmation/selectors.js
@@ -1,10 +1,11 @@
 // @flow
 
-import * as UI_SELECTORS from '../../selectors.js'
+import { getSceneState } from '../../selectors.js'
 import type { State } from '../../../ReduxTypes'
 import type {AbcTransaction, AbcParsedUri, AbcMetadata} from 'airbitz-core-types'
+import { initialState } from './reducer'
 
-export const getScene = (state: State): any => UI_SELECTORS.getSceneState(state, 'sendConfirmation')
+export const getScene = (state: State): any => getSceneState(state, 'sendConfirmation')
 
 export const getNetworkFeeOption = (state: State): string => getScene(state).networkFeeOption
 export const getCustomNetworkFee = (state: State): any => getScene(state).customNetworkFee
@@ -14,7 +15,14 @@ export const getParsedUri = (state: State): AbcParsedUri => getScene(state).pars
 export const getKeyboardIsVisible = (state: State): boolean => getScene(state).keyboardIsVisible
 export const getError = (state: State): Error => getScene(state).error
 export const getLabel = (state: State): string => getScene(state).label
-export const getNetworkFee = (state: State): string => getTransaction(state).networkFee
-export const getNativeAmount = (state: State): ?string => getParsedUri(state).nativeAmount
-export const getMetadata = (state: State): ?AbcMetadata => getParsedUri(state).metadata
-export const getPublicAddress = (state: State): ?string => getParsedUri(state).publicAddress
+export const getNativeAmount = (state: State): string =>
+  getParsedUri(state).nativeAmount || initialState.parsedUri.nativeAmount || ''
+export const getMetadata = (state: State): AbcMetadata =>
+  getParsedUri(state).metadata || initialState.parsedUri.metadata || {}
+export const getPublicAddress = (state: State): string =>
+  getParsedUri(state).publicAddress || initialState.parsedUri.publicAddress || ''
+export const getNetworkFee = (state: State): string => {
+  const transaction = getTransaction(state)
+  if (transaction && transaction.networkFee) return transaction.networkFee
+  return initialState.parsedUri.nativeAmount || ''
+}
