5e77073447902b1986e225d4a2f68423bdccb7ab
Kevin Sullivan <kevin@airbitz.co>
2018-01-29 11:12:40 -0800

    Add EdgeLogin selectors
    * Add type checking to EdgeLoginReducer
    * Remove unused core selector

--- src/reducers/core/EdgeLoginReducer.js
+++ src/reducers/core/EdgeLoginReducer.js
@@ -1,4 +1,7 @@
 // @flow
+
+import type { AbcLobby } from 'edge-login'
+
 import * as Constants from '../../constants/indexConstants'
 
 const initialState = {
@@ -7,16 +10,37 @@ const initialState = {
   isProcessing: false
 }
 
-export default function (state: any = initialState, action: any) {
+type EdgeLoginReducer = {
+  lobby: AbcLobby | null,
+  error: Error | null,
+  isProcessing: boolean
+}
+
+export default function (state: EdgeLoginReducer = initialState, action: any) {
   switch (action.type) {
     case Constants.PROCESS_ABC_LOGIN:
-      return { ...state, isProcessing: true }
+      return {
+        ...state,
+        isProcessing: true
+      }
     case Constants.INVALIDATE_ABC_LOBBY:
-      return { ...state, lobby: null, isProcessing: false }
+      return {
+        ...state,
+        lobby: null,
+        isProcessing: false
+      }
     case Constants.SET_LOBBY_ERROR:
-      return { ...state, lobby: null, error: action.data }
+      return {
+        ...state,
+        lobby: null,
+        error: action.data
+      }
     case Constants.SAVE_ABC_LOBBY:
-      return { ...state, lobby: action.data, error: null }
+      return {
+        ...state,
+        lobby: action.data,
+        error: null
+      }
     default:
       return state
   }
