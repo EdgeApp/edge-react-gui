31d3abaa4e0af7ae6e073dc5f553c3a30f7fd539
Kevin Sullivan <kevin@airbitz.co>
2018-01-30 17:58:49 -0800

    Add PasswordRecovery selectors

--- src/connectors/scene/PasswordRecoveryConnector.js
+++ src/connectors/scene/PasswordRecoveryConnector.js
@@ -1,14 +1,16 @@
 // @flow
+
 import {connect} from 'react-redux'
-import * as CORE_SELECTORS from '../../modules/Core/selectors.js'
 import {Actions} from 'react-native-router-flux'
+
+import {getContext, getAccount} from '../../modules/Core/selectors.js'
 import PasswordRecoveryComponent from '../../modules/UI/scenes/PasswordRecovery/PasswordRecoveryComponent.ui.js'
 
 import type {State} from '../../modules/ReduxTypes'
 
 export const mapStateToProps = (state: State) => ({
-  context: CORE_SELECTORS.getContext(state),
-  account: CORE_SELECTORS.getAccount(state),
+  context: getContext(state),
+  account: getAccount(state),
   showHeader: false
 })
 
