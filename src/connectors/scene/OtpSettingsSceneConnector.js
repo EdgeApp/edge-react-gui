999b8b8a3f7d21a7214bb445dfe1d5f8445bfb53
Kevin Sullivan <kevin@airbitz.co>
2018-01-30 17:33:22 -0800

    Add OTP selectors

--- src/connectors/scene/OtpSettingsSceneConnector.js
+++ src/connectors/scene/OtpSettingsSceneConnector.js
@@ -1,14 +1,16 @@
 // @flow
 import {connect} from 'react-redux'
+
 import type {Dispatch, State} from '../../modules/ReduxTypes'
-import * as SETTINGS_SELECTORS from '../../modules/UI/Settings/selectors.js'
+import {getIsOtpEnabled, getOtpKey, getOtpResetDate} from '../../modules/UI/Settings/selectors.js'
 import OtpSettingsSceneComponent from '../../modules/UI/scenes/Otp/OtpSettingsSceneComponent.js'
 // import * as Constants from '../../constants/indexConstants.js'
 import {enableOtp, disableOtp, keepOtp} from '../../actions/indexActions.js'
+
 export const mapStateToProps = (state: State) => ({
-  isOtpEnabled: SETTINGS_SELECTORS.getIsOtpEnabled(state),
-  otpKey: SETTINGS_SELECTORS.getOtpKey(state),
-  otpResetDate: SETTINGS_SELECTORS.getOtpResetDate(state)
+  isOtpEnabled: getIsOtpEnabled(state),
+  otpKey: getOtpKey(state),
+  otpResetDate: getOtpResetDate(state)
 })
 
 export const mapDispatchToProps = (dispatch: Dispatch) => ({
