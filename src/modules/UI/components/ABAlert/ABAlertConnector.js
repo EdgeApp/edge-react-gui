bda2a640fa282620bdd953010f2af170a21eb69b
Kevin Sullivan <kevin@airbitz.co>
2018-01-30 17:35:06 -0800

    Add ABAlert selectors

--- src/modules/UI/components/ABAlert/ABAlertConnector.js
+++ src/modules/UI/components/ABAlert/ABAlertConnector.js
@@ -1,22 +1,33 @@
 // @flow
-import ABAlert from './ABAlert.ui'
+
 import {connect} from 'react-redux'
+
+import ABAlert from './ABAlert.ui'
 import * as actions from '../../../../actions/indexActions'
 import * as Constants from '../../../../constants/indexConstants'
 import type {
+  Dispatch,
   State
 } from '../../../ReduxTypes.js' // Dispatch was left out beacuse it causes a cascade
 // of problems
 
+import {
+  getView,
+  getTitle,
+  getMessage,
+  getButtons,
+  getRoute
+} from './selectors.js'
+
 const mapStateToProps = (state: State) => ({
-  view: state.ui.scenes.ABAlert.view,
-  title: state.ui.scenes.ABAlert.syntax.title,
-  message: state.ui.scenes.ABAlert.syntax.message,
-  buttons: state.ui.scenes.ABAlert.syntax.buttons,
-  route: state.ui.scenes.ABAlert.route
+  view: getView(state),
+  title: getTitle(state),
+  message: getMessage(state),
+  buttons: getButtons(state),
+  route: getRoute(state)
 })
 
-const mapDispatchToProps = (dispatch: any) => ({
+const mapDispatchToProps = (dispatch: Dispatch) => ({
   closeAlert: () => dispatch(actions.dispatchAction(Constants.CLOSE_AB_ALERT))
 })
 
