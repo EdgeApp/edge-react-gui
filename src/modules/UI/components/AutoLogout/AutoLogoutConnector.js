f8e294053d35cda888f8e045fdc57f4965b2d20f
Kevin Sullivan <kevin@airbitz.co>
2018-01-30 17:36:17 -0800

    Add autologout selectors

--- src/modules/UI/components/AutoLogout/AutoLogoutConnector.js
+++ src/modules/UI/components/AutoLogout/AutoLogoutConnector.js
@@ -2,15 +2,15 @@
 
 import type {State, Dispatch} from '../../../ReduxTypes'
 
-import * as SETTINGS_SELECTORS from '../../Settings/selectors'
+import {getLoginStatus, getAutoLogoutTimeInSeconds} from '../../Settings/selectors'
 import {logoutRequest} from '../../../Login/action'
 
 import {connect} from 'react-redux'
 import AutoLogout from './AutoLogout.ui.js'
 
 const mapStateToProps = (state: State) => ({
-  loginStatus: SETTINGS_SELECTORS.getLoginStatus(state),
-  autoLogoutTimeInSeconds: SETTINGS_SELECTORS.getAutoLogoutTimeInSeconds(state)
+  loginStatus: getLoginStatus(state),
+  autoLogoutTimeInSeconds: getAutoLogoutTimeInSeconds(state)
 })
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
