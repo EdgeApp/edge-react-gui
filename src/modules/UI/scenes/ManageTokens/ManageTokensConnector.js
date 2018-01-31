69aec932911d54935f85ca4d005960ee66e9aaa4
Kevin Sullivan <kevin@airbitz.co>
2018-01-30 17:47:03 -0800

    Add ManageTokens selectors

--- src/modules/UI/scenes/ManageTokens/ManageTokensConnector.js
+++ src/modules/UI/scenes/ManageTokens/ManageTokensConnector.js
@@ -8,6 +8,9 @@ import {getEnabledTokens, setEnabledTokens} from '../../Wallets/action.js'
 import type {GuiWallet, CustomTokenInfo} from '../../../../types'
 import type {State} from '../../../ReduxTypes'
 
+import {getManageTokensPending} from '../../Wallets/selectors.js'
+import {getCustomTokens} from '../../Settings/selectors.js'
+
 export type StateProps = {
   guiWallet: GuiWallet,
   manageTokensPending: boolean,
@@ -20,9 +23,9 @@ export type DispatchProps = {
 export type OwnProps = {guiWallet: GuiWallet}
 
 const mapStateToProps = (state: State, ownProps: OwnProps): StateProps => ({
-  manageTokensPending: state.ui.wallets.manageTokensPending,
+  manageTokensPending: getManageTokensPending(state),
   guiWallet: ownProps.guiWallet,
-  settingsCustomTokens: state.ui.settings.customTokens
+  settingsCustomTokens: getCustomTokens(state)
 })
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
   getEnabledTokensList: (walletId: string) => dispatch(getEnabledTokens(walletId)),
