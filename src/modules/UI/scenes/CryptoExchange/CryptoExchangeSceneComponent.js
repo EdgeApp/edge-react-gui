82ba927021d7d9fe902e4582c621e1e72868ce54
Kylan Hurt <kylan.hurt@gmail.com>
2018-02-14 17:20:27 -0800

    Fix some visual styling bugs for exchange scene

--- src/modules/UI/scenes/CryptoExchange/CryptoExchangeSceneComponent.js
+++ src/modules/UI/scenes/CryptoExchange/CryptoExchangeSceneComponent.js
@@ -152,6 +152,7 @@ export class CryptoExchangeSceneComponent extends Component<Props, State> {
             <CryptoExchangeConnector style={style.exchangeRateBanner} />
             <View style={style.shim} />
             <CryptoExchangeFlipInputWrapperComponent
+              walletDirectionString={s.strings.fragment_exchange_from}
               style={style.flipWrapper}
               guiWallet={this.props.fromWallet}
               fee={this.props.fee}
@@ -170,6 +171,7 @@ export class CryptoExchangeSceneComponent extends Component<Props, State> {
             <View style={style.shim} />
             <CryptoExchangeFlipInputWrapperComponent
               style={style.flipWrapper}
+              walletDirectionString={s.strings.fragment_exchange_to}
               guiWallet={this.props.toWallet}
               fee={null}
               buttonText={this.props.toButtonText}
