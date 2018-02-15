8f57918ec20765618baf933bc496d58180baa597
Kevin Sullivan <kevin@airbitz.co>
2018-02-14 18:39:13 -0800

    Formatting

--- src/modules/UI/components/MenuDropDown/MenuDropDown.ui.js
+++ src/modules/UI/components/MenuDropDown/MenuDropDown.ui.js
@@ -79,11 +79,11 @@ export default class MenuDropDown extends Component<Props, State> {
           <MenuTrigger style={[style.menuTrigger]}>
             {this.renderMenuIcon(style)}
           </MenuTrigger>
-            <MenuOptions ref='menuInteriorParent' optionsContainerStyle={[optionsStyle]}>
-              <View ref='menuInterior' onLayout={this._onInternalLayout}>
-                {this.renderMenuOptions(style)}
-              </View>
-            </MenuOptions>
+          <MenuOptions ref='menuInteriorParent' optionsContainerStyle={[optionsStyle]}>
+            <View ref='menuInterior' onLayout={this._onInternalLayout}>
+              {this.renderMenuOptions(style)}
+            </View>
+          </MenuOptions>
         </Menu>
       </View>
     )
