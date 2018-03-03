fc4dc89867e1d2d9455f7d9f2f37fad9efacfbe0
Allen Hujsak <allenhujsak@Allens-MacBook-Air.local>
2018-03-03 08:19:49 -0500

    Fixed any , and got permission working

--- src/reducers/permissions/actions.js
+++ src/reducers/permissions/actions.js
@@ -12,12 +12,12 @@ export const UPDATE_PERMISSIONS = PREFIX + 'UPDATE_PERMISSIONS'
 export const requestPermission = (permission: Permission) => (dispatch: Dispatch, getState: GetState) => {
   RNPermissions.check(permission).then(permissionStatus => {
     request(permission)
-      .then(status => dispatch(updatePermissions({ [permission]: status })))
+      .then(status => dispatch(updatePermissions({ [permission]: status }))) // eslint-disable-line no-undef
       .catch(console.log)
   })
 }
 // $flowFixMe
-export const updatePermissions = (permissions: { [Permission]: any }) => ({
+export const updatePermissions = (permissions: Object) => ({
   type: UPDATE_PERMISSIONS,
   data: permissions
 })
