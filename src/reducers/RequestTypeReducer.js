a17ec52e06f5074635b114c5f50ea26c48a1e857
Allen Hujsak <allenhujsak@Allens-MacBook-Air.local>
2018-03-08 10:36:10 -0500

    flow's files

--- src/reducers/RequestTypeReducer.js
+++ src/reducers/RequestTypeReducer.js
@@ -1,12 +1,19 @@
+// @flow
 import * as Constants from '../constants/indexConstants.js'
+import type { Action } from '../modules/ReduxTypes.js'
 
-const initialState = {
+type RequestType = {
+  useLegacyAddress: boolean,
+  uniqueLegacyAddress: boolean
+}
+const initialState: RequestType = {
   useLegacyAddress: false,
   uniqueLegacyAddress: false
 }
-const requestType = (state = initialState, action) => {
+const requestType = (state: RequestType = initialState, action: Action): RequestType => {
   switch (action.type) {
     case Constants.UPDATE_RECEIVE_ADDRESS_SUCCESS:
+      if (!action.data) return state
       let uniqueLegacy = true
       if (action.data.receiveAddress.legacyAddress) {
         uniqueLegacy = action.data.receiveAddress.publicAddress === action.data.receiveAddress.legacyAddress
