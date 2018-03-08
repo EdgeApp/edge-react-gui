a713e64951b4454a9c97398c2bcdca31af061d80
Kevin Sullivan <kevin@airbitz.co>
2018-03-08 15:51:34 -0800

    Simplifiy password component props

--- src/modules/UI/components/PasswordReminderModal/components/PasswordInput.ui.js
+++ src/modules/UI/components/PasswordReminderModal/components/PasswordInput.ui.js
@@ -9,7 +9,8 @@ import s from '../../../../../locales/strings.js'
 const DEFAULTS = {
   secureTextEntry: true,
   tintColor: THEME.COLORS.GRAY_2,
-  baseColor: THEME.COLORS.GRAY_2
+  baseColor: THEME.COLORS.GRAY_2,
+  label: s.strings.password
 }
 
 export type Props = {
@@ -53,81 +54,7 @@ export type State = {}
 
 export class PasswordInput extends Component<Props, State> {
   render () {
-    const {
-      secureTextEntry,
-      tintColor,
-      baseColor,
-      label,
-      inputContainerStyle,
-      textColor,
-      fontSize,
-      titleFontSize,
-      labelFontSize,
-      labelHeight,
-      labelPadding,
-      inputContainerPadding,
-      lineWidth,
-      activeLineWidth,
-      disabledLineWidth,
-      title,
-      prefix,
-      suffix,
-      error,
-      errorColor,
-      disabledLineType,
-      animationDuration,
-      characterRestriction,
-      disabled,
-      editable,
-      multiline,
-      containerStyle,
-      labelTextStyle,
-      titleTextStyle,
-      affixTextStyle,
-      renderAccessory,
-      onChangeText,
-      onFocus,
-      onBlur,
-      value
-    } = this.props
-    return (
-      <TextField
-        secureTextEntry={secureTextEntry || DEFAULTS.secureTextEntry}
-        tintColor={tintColor || DEFAULTS.tintColor}
-        baseColor={baseColor || DEFAULTS.baseColor}
-        label={label || s.strings.password}
-        inputContainerStyle={inputContainerStyle}
-        textColor={textColor}
-        fontSize={fontSize}
-        titleFontSize={titleFontSize}
-        labelFontSize={labelFontSize}
-        labelHeight={labelHeight}
-        labelPadding={labelPadding}
-        inputContainerPadding={inputContainerPadding}
-        lineWidth={lineWidth}
-        activeLineWidth={activeLineWidth}
-        disabledLineWidth={disabledLineWidth}
-        title={title}
-        prefix={prefix}
-        suffix={suffix}
-        error={error}
-        errorColor={errorColor}
-        disabledLineType={disabledLineType}
-        animationDuration={animationDuration}
-        characterRestriction={characterRestriction}
-        disabled={disabled}
-        editable={editable}
-        multiline={multiline}
-        containerStyle={containerStyle}
-        labelTextStyle={labelTextStyle}
-        titleTextStyle={titleTextStyle}
-        affixTextStyle={affixTextStyle}
-        renderAccessory={renderAccessory}
-        onChangeText={onChangeText}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        value={value}
-      />
-    )
+    const props = { ...DEFAULTS, ...this.props }
+    return <TextField {...props} />
   }
 }
