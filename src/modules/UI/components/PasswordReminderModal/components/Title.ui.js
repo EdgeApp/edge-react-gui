c36245e530d27d208cc8a1e672980f98895f4b2d
Kevin Sullivan <kevin@airbitz.co>
2018-03-08 16:07:58 -0800

    Upgrade Title to use Stylesheet for styles

--- src/modules/UI/components/PasswordReminderModal/components/Title.ui.js
+++ src/modules/UI/components/PasswordReminderModal/components/Title.ui.js
@@ -2,18 +2,18 @@
 
 import React, { Component } from 'react'
 import {
-  // StyleSheet,
+  StyleSheet,
   Text
 } from 'react-native'
-import PropTypes from 'prop-types'
 
 import { THEME } from '../../../../../theme/variables/airbitz.js'
 
-const rawStyles = {
-  fontFamily: THEME.FONTS.DEFAULT,
-  fontSize: 18
-}
-// const style = StyleSheet.create(rawStyles) // unknown error
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: THEME.FONTS.DEFAULT,
+    fontSize: 18
+  }
+})
 
 export type Props = {
   children: string,
@@ -23,11 +23,6 @@ export type State = {}
 
 export class Title extends Component<Props, State> {
   render () {
-    return <Text style={[rawStyles, this.props.style]}>{this.props.children}</Text>
+    return <Text style={[styles.title, this.props.style]}>{this.props.children}</Text>
   }
-}
-
-Title.propTypes = {
-  children: PropTypes.node.isRequired,
-  style: PropTypes.object
 }
