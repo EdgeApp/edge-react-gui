16507e7fc78687ae905176072a6ee78947f89862
Paul Puey <paul@edgesecure.co>
2018-03-22 12:24:19 -0600

    Fix sed command to be both Linux and MacOS compatible

--- postinstall.sh
+++ postinstall.sh
@@ -6,7 +6,7 @@ rm -rf ./node_modules/bccoin/.babelrc ./node_modules/edge-currency-bitcoin/node_
 rm -rf ./node_modules/lcoin/.babelrc ./node_modules/edge-currency-bitcoin/node_modules/lcoin/.babelrc
 
 # Remove inclusion of c++_shared.so library since we are using jsc-android which already includes it
-sed -e "s/, \'-DANDROID_STL=c++_shared\'//g" ./node_modules/react-native-fast-crypto/android/build.gradle > build.gradle
+sed "s/\,[[:space:]]'-DANDROID_STL=c++_shared'//g" ./node_modules/react-native-fast-crypto/android/build.gradle > build.gradle
 mv build.gradle ./node_modules/react-native-fast-crypto/android/build.gradle
 
 # Disable minification
