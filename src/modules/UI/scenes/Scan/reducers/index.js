892cdc60137784e76ec87b28eeeefbacf3634dbe
Kevin Sullivan <kevin@airbitz.co>
2018-04-13 13:15:56 -0700

    Compose scan scene reducers, add flow

---
+++ src/modules/UI/scenes/Scan/reducers/index.js
@@ -0,0 +1,17 @@
+// @flow
+
+import { addressModalVisible } from './addressModalVisible.js'
+import { recipientAddress } from './recipientAddress.js'
+import { torchEnabled } from './torchEnabled.js'
+import { scanToWalletListModalVisibility } from './scanToWalletListModalVisibility.js'
+import { selectedWalletListModalVisibility } from './selectedWalletListModalVisibility.js'
+import { scanEnabled } from './scanEnabled.js'
+
+export {
+  addressModalVisible,
+  recipientAddress,
+  scanEnabled,
+  scanToWalletListModalVisibility,
+  selectedWalletListModalVisibility,
+  torchEnabled
+}
---
+++ src/modules/UI/scenes/Scan/reducers/torchEnabled.js
@@ -0,0 +1,16 @@
+// @flow
+
+import type { Action } from '../../../../ReduxTypes.js'
+
+import * as ACTION from '../action'
+
+export const initialState = false
+export type State = boolean
+export const torchEnabled = (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case ACTION.TOGGLE_ENABLE_TORCH:
+      return !state
+    default:
+      return state
+  }
+}
