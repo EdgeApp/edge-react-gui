22e22032a41cdb943f026f962d5a52f795256659
Kevin Sullivan <kevin@airbitz.co>
2018-04-24 17:52:22 -0700

    Remove unused files

--- src/modules/UI/components/ControlPanel/reactotron.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable flowtype/require-valid-file-annotation */
-
-import Reactotron from 'reactotron-react-native'
-
-Reactotron.configure() // we can use plugins here -- more on this later
-  .connect() // let's connect!rea
