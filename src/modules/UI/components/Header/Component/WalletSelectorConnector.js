6c79dde813d1312d5eeaf95671f8bfd58dc5ece7
Paul Puey <paul@edgesecure.co>
2018-05-11 04:05:34 -0400

    Do not return an inline declared function from mapStateToProps in WalletSelectorConnector
    Declare the function inside the component itself. Since the connector will validate props, the connector won't be called unless props change so the inline function is only used when we really have to render

--- src/modules/UI/components/Header/Component/WalletSelectorConnector.js
+++ src/modules/UI/components/Header/Component/WalletSelectorConnector.js
@@ -1,25 +1,20 @@
 // @flow
 
-import React from 'react'
 import { connect } from 'react-redux'
 
-import s from '../../../../../locales/strings.js'
 import type { Dispatch, State } from '../../../../ReduxTypes'
 import * as UI_SELECTORS from '../../../selectors'
 import { toggleScanToWalletListModal, toggleSelectedWalletListModal } from '../../WalletListModal/action'
-import { WalletNameHeader } from './WalletNameHeader.ui'
 import WalletSelector from './WalletSelector.ui'
 import type { DispatchProps, StateProps } from './WalletSelector.ui'
 
 const mapStateToProps = (state: State): StateProps => {
   const selectedWallet = UI_SELECTORS.getSelectedWallet(state)
   const selectedWalletCurrencyCode = UI_SELECTORS.getSelectedCurrencyCode(state)
-  const title = selectedWallet
-    ? function HeaderComp (styles) {
-      return <WalletNameHeader name={selectedWallet.name} denomination={selectedWalletCurrencyCode} styles={styles} />
-    }
-    : s.strings.loading
-  return { title }
+  return {
+    selectedWalletName: selectedWallet ? selectedWallet.name : null,
+    selectedWalletCurrencyCode
+  }
 }
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
   onPress: () => {
