a5c07e26b8c99873bccaca3d3e11e160b824736d
Kevin Sullivan <kevin@airbitz.co>
2018-06-06 08:06:37 -0700

    Add sidenav buttons

--- src/modules/UI/components/ControlPanel/Component/Main.js
+++ src/modules/UI/components/ControlPanel/Component/Main.js
@@ -1,65 +1,145 @@
 // @flow
 
 import React, { Component } from 'react'
-import { Image, TouchableHighlight, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import { Actions } from 'react-native-router-flux'
-import { sprintf } from 'sprintf-js'
+
+import exchangeIcon from '../../../../../assets/images/sidenav/exchange.png'
+import receiveIcon from '../../../../../assets/images/sidenav/receive.png'
+import scanIcon from '../../../../../assets/images/sidenav/scan.png'
+import walletIcon from '../../../../../assets/images/sidenav/wallets.png'
 
 import logoutImage from '../../../../../assets/images/sidenav/logout.png'
 import settings from '../../../../../assets/images/sidenav/settings.png'
+
 import s from '../../../../../locales/strings.js'
-import T from '../../../components/FormattedText'
 import styles from '../style'
 import UserList from './UserListConnector'
+import { Button } from './Button/Button.ui.js'
+import { Separator } from './Separator/Separator.ui.js'
 
-const LOGOUT_TEXT = sprintf(s.strings.settings_button_logout)
-const SETTINGS_TEXT = sprintf(s.strings.settings_title)
+const WALLETS_TEXT = s.strings.drawer_wallets
+const SCAN_TEXT = s.strings.drawer_scan_qr_send
+const SWEEP_PRIVATE_KEY_TEXT = s.strings.drawer_sweep_private_key
+const REQUEST_TEXT = s.strings.drawer_request
+const EXCHANGE_TEXT = s.strings.drawer_exchange
+const LOGOUT_TEXT = s.strings.settings_button_logout
+const SETTINGS_TEXT = s.strings.settings_title
 
-type Props = {
+export type Props = {
   logout: (username?: string) => void,
-  onPressOption: () => void
+  usersView: boolean
 }
-
 export default class Main extends Component<Props> {
-  onLogout = () => {
-    this.props.logout()
-  }
-
   render () {
-    return this.props.usersView ? (
+    const { logout, usersView } = this.props
+
+    return usersView ? (
       <UserList />
     ) : (
-      <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-        <View style={styles.others.container}>
-          <TouchableHighlight
-            style={styles.others.iosTouchableHighlight}
-            underlayColor={styles.main.iosTouchableHighlightUnderlayColor}
-            onPress={this.onLogout}
-          >
-            <View style={[styles.others.link, styles.others.borderVertical, { flex: 1 }]}>
-              <View style={styles.iconImageContainer}>
-                <Image style={styles.iconImage} source={logoutImage} />
-              </View>
-              <View style={styles.others.textContainer}>
-                <T style={styles.others.text}>{LOGOUT_TEXT}</T>
-              </View>
-            </View>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={styles.others.iosTouchableHighlight}
-            underlayColor={styles.main.iosTouchableHighlightUnderlayColor}
-            onPress={Actions.settingsOverviewTab}
-          >
-            <View style={[styles.others.link, styles.others.borderBottom, { flex: 1 }]}>
-              <View style={styles.iconImageContainer}>
-                <Image style={styles.iconImage} source={settings} />
-              </View>
-              <View style={styles.others.textContainer}>
-                <T style={styles.others.text}>{SETTINGS_TEXT}</T>
-              </View>
-            </View>
-          </TouchableHighlight>
+      <View style={{ flex: 1, justifyContent: 'space-between' }}>
+        <View>
+          <Button onPress={Actions.walletList}>
+            <Button.Left>
+              <Image source={walletIcon} />
+            </Button.Left>
+
+            <Button.Center>
+              <Button.Text>
+                <Text>{WALLETS_TEXT}</Text>
+              </Button.Text>
+            </Button.Center>
+          </Button>
+
+          <Separator />
+
+          <Button onPress={Actions.scan}>
+            <Button.Left>
+              <Image source={scanIcon} />
+            </Button.Left>
+
+            <Button.Center>
+              <Button.Text>
+                <Text>{SCAN_TEXT}</Text>
+              </Button.Text>
+            </Button.Center>
+          </Button>
+
+          <Separator />
+
+          <Button onPress={Actions.scan}>
+            <Button.Left>
+              <Image source={scanIcon} />
+            </Button.Left>
+
+            <Button.Center>
+              <Button.Text>
+                <Text>{SWEEP_PRIVATE_KEY_TEXT}</Text>
+              </Button.Text>
+            </Button.Center>
+          </Button>
+
+          <Separator />
+
+          <Button onPress={Actions.request}>
+            <Button.Left>
+              <Image source={receiveIcon} />
+            </Button.Left>
+
+            <Button.Center>
+              <Button.Text>
+                <Text>{REQUEST_TEXT}</Text>
+              </Button.Text>
+            </Button.Center>
+          </Button>
+
+          <Separator />
+
+          <Button onPress={Actions.exchange}>
+            <Button.Left>
+              <Image source={exchangeIcon} />
+            </Button.Left>
+
+            <Button.Center>
+              <Button.Text>
+                <Text>{EXCHANGE_TEXT}</Text>
+              </Button.Text>
+            </Button.Center>
+          </Button>
+
+          <Separator />
         </View>
+
+        <View>
+          <Separator />
+
+          <Button onPress={logout}>
+            <Button.Left>
+              <Image style={styles.iconImage} source={logoutImage} />
+            </Button.Left>
+
+            <Button.Center>
+              <Button.Text>
+                <Text>{LOGOUT_TEXT}</Text>
+              </Button.Text>
+            </Button.Center>
+          </Button>
+
+          <Separator />
+
+          <Button onPress={Actions.settingsOverviewTab}>
+            <Button.Left>
+              <Image style={styles.iconImage} source={settings} />
+            </Button.Left>
+
+            <Button.Center>
+              <Button.Text>
+                <Text>{SETTINGS_TEXT}</Text>
+              </Button.Text>
+            </Button.Center>
+          </Button>
+        </View>
+
       </View>
     )
   }
