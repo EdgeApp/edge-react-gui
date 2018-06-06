5634bb13f934058966b5dafecc09680f0d186a8e
Kevin Sullivan <kevin@airbitz.co>
2018-06-06 08:22:17 -0700

    Use theme colors

--- src/modules/UI/components/ControlPanel/Component/Button/styles.js
+++ src/modules/UI/components/ControlPanel/Component/Button/styles.js
@@ -9,36 +9,36 @@ export const rawStyles = {
     flexDirection: 'row',
     width: '100%',
     height: 50,
-    backgroundColor: 'transparent',
+    backgroundColor: THEME.COLORS.TRANSPARENT,
     alignItems: 'center',
     justifyContent: 'space-between'
   },
   left: {
     flexDirection: 'row',
     width: 50,
-    backgroundColor: 'transparent',
+    backgroundColor: THEME.COLORS.TRANSPARENT,
     alignItems: 'center',
     justifyContent: 'center'
   },
   center: {
     flex: 1,
     flexDirection: 'row',
-    backgroundColor: 'transparent',
+    backgroundColor: THEME.COLORS.TRANSPARENT,
     alignItems: 'center',
     alignSelf: 'stretch'
   },
   right: {
     flexDirection: 'row',
     width: 50,
-    backgroundColor: 'transparent',
+    backgroundColor: THEME.COLORS.TRANSPARENT,
     alignItems: 'center',
     justifyContent: 'center'
   },
   text: {
     fontSize: 16,
     fontFamily: THEME.FONTS.DEFAULT,
-    color: 'white',
-    backgroundColor: 'transparent',
+    color: THEME.COLORS.WHITE,
+    backgroundColor: THEME.COLORS.TRANSPARENT,
     alignItems: 'center',
     justifyContent: 'center'
   },
