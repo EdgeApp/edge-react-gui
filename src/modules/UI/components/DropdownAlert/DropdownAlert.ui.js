664e6b04f22f9df5b95041369e6b38ea39f82539
Art Manalili <artmanalili@gmail.com>
2018-06-29 21:47:16 +0800

    Dropdown Alert adjustment when on iPhoneX

--- src/modules/UI/components/DropdownAlert/DropdownAlert.ui.js
+++ src/modules/UI/components/DropdownAlert/DropdownAlert.ui.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react'
 import type { Node } from 'react'
 import RNDropdownAlert from 'react-native-dropdownalert'
+import { isIphoneX } from '../.././../../lib/isIphoneX.js'
 
 type Props = {
   visible: boolean,
@@ -30,7 +31,7 @@ export default class DropdownAlert extends Component<Props> {
         onPress={onPress}
         panResponderEnabled={false}
         updateStatusBar={false}
-        endDelta={20}
+        endDelta={isIphoneX ? 45 : 20}
         onClose={onClose}
       >
         {children}
