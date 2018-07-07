c7607d31eb30e816560140141e1c0e1adc26394c
Kylan Hurt <kylan.hurt@gmail.com>
2018-07-07 12:42:54 -0700

    Initial set up of DEX scene

--- src/components/indexComponents.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable flowtype/require-valid-file-annotation */
-
-export * from './FormField'
-export * from './modals/StaticModalComponent.js'
-export * from './modals/TwoButtonTextModalComponent.js'
-export * from './ExpandableBoxComponent.js'
