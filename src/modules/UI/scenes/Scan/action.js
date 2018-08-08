c15896eedf9082afee28e27cd941832665c7289b
Eliran Zach <thehobbit85@gmail.com>
2018-08-07 20:24:07 -0700

    Update action.js
    Timeout not needed for dispatching paymentProtocolUriReceived since it's not a modal

--- src/modules/UI/scenes/Scan/action.js
+++ src/modules/UI/scenes/Scan/action.js
@@ -111,7 +111,7 @@ export const parseUri = (data: string) => (dispatch: Dispatch, getState: GetStat
       if (isPaymentProtocolUri(parsedUri)) {
         // BIP70 URI
         // $FlowFixMe
-        return setTimeout(() => dispatch(paymentProtocolUriReceived()), 500)
+        return dispatch(paymentProtocolUriReceived())
       }
 
       // PUBLIC ADDRESS URI
