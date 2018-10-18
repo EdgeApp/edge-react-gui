6ef1a128dece6b328881fc96006571c36afddb7c
Art Manalili <artmanalili@gmail.com>
2018-10-19 01:54:36 +0800

    Scale OTP screen

--- src/styles/scenes/OtpSettingsScreenStyles.js
+++ src/styles/scenes/OtpSettingsScreenStyles.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { scale } from '../../lib/scaling.js'
 import THEME from '../../theme/variables/airbitz'
 import * as Styles from '../indexStyles.js'
 // import {Image} from 'react-native'
@@ -14,58 +15,58 @@ const OtpSettingsScreenStyles = {
     height: THEME.HEADER
   },
   body: {
-    padding: 18
+    padding: scale(18)
   },
   hero: {
     container: {
       width: '100%',
-      height: 120,
+      height: scale(120),
       alignItems: 'center',
       backgroundColor: THEME.COLORS.TRANSPARENT
     },
     icon: {},
     shim: {
-      height: 10
+      height: scale(10)
     },
-    imageSize: 50,
+    imageSize: scale(50),
     bodyText: {
       width: '100%',
       textAlign: 'center',
-      fontSize: 21,
+      fontSize: scale(21),
       color: THEME.COLORS.GRAY_1
     }
   },
   shim: {
-    height: 10
+    height: scale(10)
   },
   middle: {
     width: '100%',
-    minHeight: 200
+    minHeight: scale(200)
   },
   middleText: {
     width: '100%',
-    fontSize: 18,
+    fontSize: scale(18),
     textAlign: 'center',
     fontFamily: THEME.FONTS.DEFAULT,
     color: THEME.COLORS.GRAY_2
   },
   keyText: {
     width: '100%',
-    fontSize: 18,
+    fontSize: scale(18),
     textAlign: 'center',
     fontFamily: THEME.FONTS.DEFAULT,
     color: THEME.COLORS.GRAY_1
   },
   buttonContainer: {
     width: '100%',
-    height: THEME.BUTTONS.HEIGHT
+    height: scale(THEME.BUTTONS.HEIGHT)
   },
   keyBox: Styles.ExpandableBoxStyle,
   showConfirmationModal: Styles.TwoButtonModalStyle,
   icon: {
     color: THEME.COLORS.WHITE
   },
-  iconSize: 36
+  iconSize: scale(36)
 }
 
 export { OtpSettingsScreenStyles }
