12ca5221e5463a03c3007beb60b2cc57b58cfbc4
ahujask <allen@missionvi.com>
2018-10-24 16:07:58 -0700

    Sign in With Google user agent • Fix for Webview on Android call

--- src/locales/en_US.js
+++ src/locales/en_US.js
@@ -394,7 +394,9 @@ const strings = {
   mining_fee: 'Mining Fee',
   confirm_to_complete_exchange: 'Confirm to complete your exchange.',
   kyc_title: 'Customer Information Needed',
-  kyc_message: 'The available exchange for this transaction requires a ShapeShift account. Please create an account to continue.'
+  kyc_message: 'The available exchange for this transaction requires a ShapeShift account. Please create an account to continue.',
+  kyc_something_wrong: 'Something went wrong',
+  kyc_something_wrong_message: 'Please try again later'
 }
 
 // export default strings
