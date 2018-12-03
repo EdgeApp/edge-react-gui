c35200e33c15009cf8a5144cb674c3b28351a480
Paul Puey <paul@edge.app>
2018-12-03 00:06:44 -0500

    Fix UniqueIdentifierModal to not auto-populate default value when deleted
    Fixes bug where fully deleting the uniqueIdentifier in the modal will cause the default scanned value to suddenly show.

--- src/connectors/UniqueIdentifierModalConnector.js
+++ src/connectors/UniqueIdentifierModalConnector.js
@@ -6,10 +6,16 @@ import { deactivated, reset, uniqueIdentifierChanged } from '../actions/UniqueId
 import { UniqueIdentifierModal } from '../components/modals/UniqueIdentifierModal.js'
 import type { Dispatch, State } from '../modules/ReduxTypes.js'
 
-export const mapStateToProps = (state: State) => ({
-  isActive: state.ui.scenes.uniqueIdentifierModal.isActive,
-  uniqueIdentifier: state.ui.scenes.uniqueIdentifierModal.uniqueIdentifier || state.ui.scenes.sendConfirmation.guiMakeSpendInfo.uniqueIdentifier
-})
+export const mapStateToProps = (state: State) => {
+  let uniqueIdentifier = state.ui.scenes.uniqueIdentifierModal.uniqueIdentifier
+  if (uniqueIdentifier === undefined) {
+    uniqueIdentifier = state.ui.scenes.sendConfirmation.guiMakeSpendInfo.uniqueIdentifier
+  }
+  return {
+    isActive: state.ui.scenes.uniqueIdentifierModal.isActive,
+    uniqueIdentifier
+  }
+}
 export const mapDispatchToProps = (dispatch: Dispatch, ownProps: Object) => ({
   uniqueIdentifierChanged: (uniqueIdentifier: string) => dispatch(uniqueIdentifierChanged(uniqueIdentifier)),
   onConfirm: (uniqueIdentifier: string) => {
