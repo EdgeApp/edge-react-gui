ea2a300c6317968e6cf4d7702646163c357094be
James Risberg <james.risberg@gmail.com>
2019-01-09 17:40:34 -0800

    Show alert when user taps a currency link for which no wallet exists

--- src/modules/DeepLinkingManager.js
+++ src/modules/DeepLinkingManager.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react'
+import { Alert } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
 
@@ -9,13 +10,15 @@ import { SCAN } from '../constants/indexConstants.js'
 import type { Dispatch } from './ReduxTypes.js'
 
 type DeepLinkingManagerStateProps = {
+  totalWalletCount: number,
   wallets: Object,
   addressDeepLinkData: Object,
   deepLinkPending: boolean
 }
 
 type DeepLinkingManagerDispatchProps = {
-  selectWallet: (walletId: string, currencyCode: string) => any
+  selectWallet: (walletId: string, currencyCode: string) => any,
+  markAddressDeepLinkDone: () => any
 }
 
 type Props = DeepLinkingManagerStateProps & DeepLinkingManagerDispatchProps
@@ -44,14 +47,42 @@ class DeepLinkingManager extends React.Component<Props> {
 
         Actions[SCAN]()
 
-        break
+        return
       }
+    }
+
+    if (Object.keys(this.props.wallets).length === this.props.totalWalletCount) {
+      this.props.markAddressDeepLinkDone()
+
+      const currency = this.convertCurrencyStringFromCurrencyCode(currencyCode)
+      const article = currency === 'Ethereum' ? 'an ' : 'a '
+      const noWalletMessage =
+        'No wallets exist that support ' + currency + '. You can create ' + article + currency + ' wallet by tapping + on the Wallet List screen.'
+      Alert.alert(noWalletMessage)
+    }
+  }
+
+  convertCurrencyStringFromCurrencyCode (code: string) {
+    switch (code) {
+      case 'BTC':
+        return 'Bitcoin'
+      case 'BCH':
+        return 'Bitcoin Cash'
+      case 'ETH':
+        return 'Ethereum'
+      case 'LTC':
+        return 'Litecoin'
+      case 'DASH':
+        return 'Dash'
+      default:
+        return ''
     }
   }
 }
 
 const mapStateToProps = (state): DeepLinkingManagerStateProps => {
   return {
+    totalWalletCount: state.ui.wallets.activeWalletIds.length,
     wallets: state.ui.wallets.byId,
     addressDeepLinkData: state.core.deepLinking.addressDeepLinkData,
     deepLinkPending: state.core.deepLinking.deepLinkPending
@@ -60,7 +91,11 @@ const mapStateToProps = (state): DeepLinkingManagerStateProps => {
 
 const mapDispatchToProps = (dispatch: Dispatch): DeepLinkingManagerDispatchProps => {
   return {
-    selectWallet: (walletId: string, currencyCode: string) => dispatch(selectWallet(walletId, currencyCode))
+    selectWallet: (walletId: string, currencyCode: string) => dispatch(selectWallet(walletId, currencyCode)),
+    markAddressDeepLinkDone: () =>
+      dispatch({
+        type: 'ADDRESS_DEEP_LINK_COMPLETE'
+      })
   }
 }
 
