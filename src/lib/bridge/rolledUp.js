9c6fcca6d86f79a75fcf2def7c95dcc0ccd66064
William Swanson <swansontec@gmail.com>
2019-02-14 15:30:54 -0800

    Upgrade to edge-core-js v0.14.0

--- src/lib/bridge/rolledUp.js
+++ src/lib/bridge/rolledUp.js
@@ -1171,12 +1171,12 @@ hack.on = onMethod;
 hack.watch = watchMethod;
 bridgifyClass(Bridgeable);
 
+// @flow
 setTimeout(function () {
   window.bridge = new Bridge({
-    sendMessage: message => window.postMessage(JSON.stringify(message)),
+    sendMessage: message => window.postMessage(JSON.stringify(message))
   });
-  window.bridge.getRoot()
-    .then(api => {
-      window.edgeProvider = api;
-    });
+  window.bridge.getRoot().then(api => {
+    window.edgeProvider = api;
+  });
 }, 1);
