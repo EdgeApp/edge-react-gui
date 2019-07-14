2d9c9337fbee253b8e03d53a64145e4ec77ee1a5
kylanhurt <kylan.hurt@gmail.com>
2019-07-13 21:22:25 -0700

    Fix swap quote logo (Totle) and text over flow

--- src/assets/images/exchange/index.js
+++ src/assets/images/exchange/index.js
@@ -1,4 +1,10 @@
 // @flow
+
+import changellyFullLogo from './exchange_logo_changelly.png'
+import changenowFullLogo from './exchange_logo_changenow.png'
+import faastFullLogo from './exchange_logo_faast.png'
+import shapeshiftFullLogo from './exchange_logo_shapeshift.png'
+import totleFullLogo from './exchange_logo_totle.png'
 import changellyLogo from './settingsExchangeChangelly.png'
 import changenowLogo from './settingsExchangeChangenow.png'
 import defaultLogo from './settingsExchangeDefault.png'
@@ -6,4 +12,16 @@ import faastLogo from './settingsExchangeFaast.png'
 import shapeshiftLogo from './settingsExchangeShapeshift.png'
 import totleLogo from './settingsExchangeTotle.png'
 
-export { changellyLogo, changenowLogo, defaultLogo, faastLogo, shapeshiftLogo, totleLogo }
+export {
+  changellyLogo,
+  changenowLogo,
+  defaultLogo,
+  faastLogo,
+  shapeshiftLogo,
+  totleLogo,
+  changellyFullLogo,
+  changenowFullLogo,
+  faastFullLogo,
+  shapeshiftFullLogo,
+  totleFullLogo
+}
