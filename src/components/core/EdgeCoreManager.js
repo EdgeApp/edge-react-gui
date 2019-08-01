e2301c288a4ddf50d15fc8f3203aee35b05eda39
peachbits <41451710+peachbits@users.noreply.github.com>
2019-07-31 20:24:49 -0700

    disable shapeshift

--- src/components/core/EdgeCoreManager.js
+++ src/components/core/EdgeCoreManager.js
@@ -56,7 +56,7 @@ export const currencyPlugins = {
 }
 
 export const ratePlugins = {
-  'shapeshift-rate': true,
+  'shapeshift-rate': false,
   coinbase: true,
   coincap: true,
   coincapLegacy: true,
