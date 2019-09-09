eb7e963da7a2246677a8aaafb742d37a287f760a
kylanhurt <kylan.hurt@gmail.com>
2019-09-09 12:39:00 -0500

    Adjustments to RSK implementation

--- src/constants/SceneKeys.js
+++ src/constants/SceneKeys.js
@@ -120,10 +120,10 @@ export const CURRENCY_SETTINGS = {
     pluginName: 'eboost',
     currencyCode: 'EBST'
   },
-  /* rskSettings: {
-     pluginName: 'rsk',
-     currencyCode: 'RBTC'
-  }, */
+  rskSettings: {
+    pluginName: 'rsk',
+    currencyCode: 'RBTC'
+  },
   ufoSettings: {
     pluginName: 'ufo',
     currencyCode: 'UFO'
