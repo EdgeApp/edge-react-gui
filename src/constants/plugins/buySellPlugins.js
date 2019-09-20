03e8c1773d911f78fe4980ab2fd9201a8ba24f85
Allen Hujsak <allen@missionvi.com>
2019-09-20 14:40:49 -0500

    change domain reversal

--- src/constants/plugins/buySellPlugins.js
+++ src/constants/plugins/buySellPlugins.js
@@ -66,7 +66,7 @@ export const allPlugins: Array<BuySellPlugin> = [
     permissions: ['camera']
   },
   {
-    pluginId: 'co.edgesecure.bity',
+    pluginId: 'com.bity',
     uri: hostedUri + 'co.edgesecure.bity/index.html',
     name: 'Bity',
     subtitle: 'ACH bank transfer\nBTC\nFee: 1% / Settlement: 1-5 days\nBuy Limit: $2500 / Unlimited Sell',
