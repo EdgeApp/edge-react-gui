fdfbde3fc0c7a19561315eb6d49e6a6cb7eb232b
Allen Hujsak <allen@missionvi.com>
2019-10-07 07:23:57 -0600

    remove dummy data and call actual server

--- src/util/notificationUtil.js
+++ src/util/notificationUtil.js
@@ -4,78 +4,52 @@ import { AsyncStorage, Platform } from 'react-native'
 
 import { version } from '../../package.json'
 import { LOCAL_STORAGE_PREVIOUS_NOTIFICATIONS } from '../constants/indexConstants'
-import type { Notification } from '../types//types'
-
-const getNotifications = (): Array<Notification> => {
-  return [
-    {
-      subject: 'first one',
-      message: 'iOS is true', // HTML
-      apiKeys: { none: true },
-      minVersion: '1.9.0',
-      maxVersion: '1.9.7',
-      android: true,
-      ios: true,
-      isUpgrade: false
-    },
-    {
-      subject: 'Second One',
-      message: 'iOS is false',
-      apiKeys: { none: true },
-      minVersion: '1.9.0',
-      maxVersion: '1.9.7',
-      android: true,
-      ios: true,
-      isUpgrade: false
-    },
-    {
-      subject: 'Upgrade Me Now',
-      message: 'Doing the right thing',
-      apiKeys: { none: true },
-      minVersion: '1.9.0',
-      maxVersion: '1.9.7',
-      android: true,
-      ios: true,
-      isUpgrade: true
-    }
-  ]
-}
 
 export async function checkForNotifications () {
-  // fetch from server
-  const validNotifications = []
-  const notifications = getNotifications()
-  const pn = await AsyncStorage.getItem(LOCAL_STORAGE_PREVIOUS_NOTIFICATIONS)
-  const previousNotifications = pn ? JSON.parse(pn) : {}
-  for (let i = 0; i < notifications.length; i++) {
-    const notif = notifications[i]
-    const versionCompareMin = compareVersion(version, notif.minVersion)
-    const versionCompareMax = compareVersion(version, notif.maxVersion)
-    if (Number(versionCompareMin) >= 0 && Number(versionCompareMax) <= 0) {
-      // check to see if we have seen it before and it is not an upgrade. All upgrades get seen.
-      if (!previousNotifications[notif.subject] || notif.isUpgrade) {
-        // final filter, is this for this os?
-        if (Platform.OS === 'ios') {
-          if (notif.ios) {
-            validNotifications.push(notif)
-          }
-        } else {
-          if (notif.android) {
-            validNotifications.push(notif)
+  try {
+    const url = 'https://info1.edgesecure.co:8444/v1/notifications'
+    const notifications = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    const validNotifications = []
+    const pn = await AsyncStorage.getItem(LOCAL_STORAGE_PREVIOUS_NOTIFICATIONS)
+    const previousNotifications = pn ? JSON.parse(pn) : {}
+    for (let i = 0; i < notifications.length; i++) {
+      const notif = notifications[i]
+      const versionCompareMin = compareVersion(version, notif.minVersion)
+      const versionCompareMax = compareVersion(version, notif.maxVersion)
+      if (Number(versionCompareMin) >= 0 && Number(versionCompareMax) <= 0) {
+        // check to see if we have seen it before and it is not an upgrade. All upgrades get seen.
+        if (!previousNotifications[notif.subject] || notif.isUpgrade) {
+          // final filter, is this for this os?
+          if (Platform.OS === 'ios') {
+            if (notif.ios) {
+              validNotifications.push(notif)
+            }
+          } else {
+            if (notif.android) {
+              validNotifications.push(notif)
+            }
           }
         }
       }
     }
+    // if we have any valid notifications send back the first one
+    if (validNotifications.length > 0) {
+      const key = validNotifications[0].subject
+      previousNotifications[key] = 'sent'
+      // record the subject of what we sent so we don't send again.
+      await AsyncStorage.setItem(LOCAL_STORAGE_PREVIOUS_NOTIFICATIONS, JSON.stringify(previousNotifications))
+      return validNotifications[0]
+    }
+    return null
+  } catch (error) {
+    return null
   }
-  // if we have any valid notifications send back the first one
-  if (validNotifications.length > 0) {
-    const key = validNotifications[0].subject
-    previousNotifications[key] = 'sent'
-    // record the subject of what we sent so we don't send again.
-    await AsyncStorage.setItem(LOCAL_STORAGE_PREVIOUS_NOTIFICATIONS, JSON.stringify(previousNotifications))
-    return validNotifications[0]
-  }
-  return null
 }
 
 const compareVersion = (v1: string, v2: string) => {
