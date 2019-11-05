3d6497da45c9934cbfdff789dc8d9b310aed46ef
William Swanson <swansontec@gmail.com>
2019-11-05 13:44:00 -0800

    Move Firebase tracking into its own function

--- src/util/tracking.js
+++ src/util/tracking.js
@@ -7,8 +7,6 @@ import firebase from 'react-native-firebase'
 import ENV from '../../env.json'
 import { loadCreationReason } from './installReason.js'
 
-// Feel free to add new events at any time!
-// This type is here so we know all the possible values:
 type TrackingEvent =
   | 'Activate_Wallet_Cancel'
   | 'Activate_Wallet_Done'
@@ -22,8 +20,6 @@ type TrackingEvent =
   | 'Signup_Wallets_Created'
   | 'Start_App'
 
-// Feel free to add new parameters at any time!
-// This type is here so we know all the possible values:
 export type TrackingValues = {
   accountDate?: string, // Account creation date
   currencyCode?: string, // Wallet currency code
@@ -36,33 +32,6 @@ export type TrackingValues = {
 if (ENV.USE_FIREBASE && !firebase.isMock) {
   firebase.analytics().setUserId(DeviceInfo.getUniqueID())
   global.firebase = firebase
-}
-
-/**
- * Tracks a user event, like navigating or logging in.
- */
-export async function trackEvent (event: TrackingEvent, opts?: TrackingValues = {}) {
-  if (global.firebase) {
-    const { accountDate, currencyCode, dollarValue, installerId, pluginId } = opts
-
-    const params: Object = {}
-    if (accountDate != null) params.adate = accountDate
-    if (currencyCode != null) params.currency = currencyCode
-    if (dollarValue != null) {
-      params.currency = 'USD'
-      params.value = Number(dollarValue.toFixed(2))
-    }
-    if (installerId != null) params.aid = installerId
-    if (pluginId != null) params.plugin = pluginId
-    global.firebase.analytics().logEvent(event, params)
-
-    // If we get passed a dollarValue, translate the event into a purchase:
-    if (dollarValue != null) {
-      params.items = event
-      global.firebase.analytics().logEvent('purchase', params)
-      global.firebase.analytics().logEvent('ecommerce_purchase', params)
-    }
-  }
 }
 
 /**
@@ -82,10 +51,59 @@ export async function trackConversion (
   // Look up the dollar value:
   const dollarValue: number = await account.exchangeCache.convertCurrency(currencyCode, 'iso:USD', exchangeAmount)
 
+  // Record the event:
   const values: TrackingValues = {
     dollarValue,
     pluginId,
     ...(await loadCreationReason(account))
   }
-  return trackEvent(event, values)
+  return logToFirebase(event, values)
+}
+
+/**
+ * Tracks a user event, like navigating or logging in.
+ */
+export async function trackEvent (
+  event: TrackingEvent,
+  opts?: {
+    account?: EdgeAccount,
+    currencyCode?: string // For wallet creation events
+  } = {}
+) {
+  const { account, currencyCode } = opts
+
+  // Record the event:
+  const values: TrackingValues = {
+    currencyCode,
+    ...(await loadCreationReason(account))
+  }
+  return logToFirebase(event, values)
+}
+
+/**
+ * Send a raw event to Firebase.
+ */
+async function logToFirebase (event: TrackingEvent, values: TrackingValues) {
+  const { accountDate, currencyCode, dollarValue, installerId, pluginId } = values
+
+  if (!global.firebase) return
+
+  // Adjust params:
+  const params: Object = {}
+  if (accountDate != null) params.adate = accountDate
+  if (currencyCode != null) params.currency = currencyCode
+  if (dollarValue != null) {
+    params.currency = 'USD'
+    params.value = Number(dollarValue.toFixed(2))
+  }
+  if (installerId != null) params.aid = installerId
+  if (pluginId != null) params.plugin = pluginId
+  global.firebase.analytics().logEvent(event, params)
+
+  // If we get passed a dollarValue, translate the event into a purchase:
+  if (dollarValue != null) {
+    params.items = event
+    global.firebase.analytics().logEvent('purchase', params)
+    global.firebase.analytics().logEvent('ecommerce_purchase', params)
+  }
 }
