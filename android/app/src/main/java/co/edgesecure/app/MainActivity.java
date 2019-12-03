4420f92596b4d8944b82c9d403d08558820cdb86
William Swanson <swansontec@gmail.com>
2019-12-03 09:17:46 -0800

    Run google-java-format on Android sources

--- android/app/src/main/java/co/edgesecure/app/MainActivity.java
+++ android/app/src/main/java/co/edgesecure/app/MainActivity.java
@@ -2,30 +2,29 @@ package co.edgesecure.app;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
 import com.reactnativecomponent.splashscreen.RCTSplashScreen;
 
 public class MainActivity extends ReactActivity {
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "edge";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "edge";
+  }
 
-    protected void onCreate(Bundle savedInstanceState) {
-        RCTSplashScreen.openSplashScreen(this);
-        super.onCreate(savedInstanceState);
-        if (getResources().getBoolean(R.bool.portrait_only)) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        }
+  protected void onCreate(Bundle savedInstanceState) {
+    RCTSplashScreen.openSplashScreen(this);
+    super.onCreate(savedInstanceState);
+    if (getResources().getBoolean(R.bool.portrait_only)) {
+      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
+  }
 
-    @Override
-    public void invokeDefaultOnBackPressed() {
-        moveTaskToBack(true);
-    }
+  @Override
+  public void invokeDefaultOnBackPressed() {
+    moveTaskToBack(true);
+  }
 }
--- android/app/src/main/java/co/edgesecure/app/MainApplication.java
+++ android/app/src/main/java/co/edgesecure/app/MainApplication.java
@@ -2,98 +2,95 @@ package co.edgesecure.app;
 
 import android.app.Application;
 import android.webkit.WebView;
-
+import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
+import cl.json.RNSharePackage;
+import co.airbitz.AbcCoreJsUi.AbcCoreJsUiPackage;
+import co.airbitz.fastcrypto.RNFastCryptoPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.beefe.picker.PickerViewPackage;
+import com.bitgo.randombytes.RandomBytesPackage;
+import com.bugsnag.BugsnagReactNative;
+import com.chirag.RNMail.RNMail;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.art.ARTPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.reactlibrary.DiskletPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.psykar.cookiemanager.CookieManagerPackage;
-import com.krazylabs.OpenAppSettingsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.bugsnag.BugsnagReactNative;
-import org.reactnative.camera.RNCameraPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.krazylabs.OpenAppSettingsPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lynxit.contactswrapper.ContactsWrapperPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.peel.react.TcpSocketsModule;
+import com.psykar.cookiemanager.CookieManagerPackage;
+import com.reactlibrary.DiskletPackage;
+import com.reactnativecommunity.art.ARTPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
+import com.rnfs.RNFSPackage;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+import com.zmxv.RNSound.RNSoundPackage;
+import io.fixd.rctlocale.RCTLocalePackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
-import com.chirag.RNMail.RNMail;
-import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
-import com.beefe.picker.PickerViewPackage;
-import co.airbitz.AbcCoreJsUi.AbcCoreJsUiPackage;
-import com.zmxv.RNSound.RNSoundPackage;
-import cl.json.RNSharePackage;
-import co.airbitz.fastcrypto.RNFastCryptoPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.peel.react.TcpSocketsModule;
-import com.bitgo.randombytes.RandomBytesPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.rnfs.RNFSPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.lynxit.contactswrapper.ContactsWrapperPackage;
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
-import io.fixd.rctlocale.RCTLocalePackage;
-import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
-import com.facebook.react.modules.i18nmanager.I18nUtil;
-
 import java.util.Arrays;
 import java.util.List;
+import org.reactnative.camera.RNCameraPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new ARTPackage(),
-            new AsyncStoragePackage(),
-            new DiskletPackage(),
-            new RNCWebViewPackage(),
-            new CookieManagerPackage(),
-            new OpenAppSettingsPackage(),
-            BugsnagReactNative.getPackage(),
-            new RNFirebasePackage(),
-            new RNFirebaseAnalyticsPackage(),
-            new RNFirebaseDatabasePackage(),
-            new RNMail(),
-            new RCTSplashScreenPackage(),
-            new PickerViewPackage(),
-            new AbcCoreJsUiPackage(),
-            new RNSoundPackage(),
-            new RNSharePackage(),
-            new RandomBytesPackage(),
-            new RNFastCryptoPackage(),
-            new VectorIconsPackage(),
-            new TcpSocketsModule(),
-            new ReactMaterialKitPackage(),
-            new LinearGradientPackage(),
-            new ImagePickerPackage(),
-            new RNFSPackage(),
-            new RNDeviceInfo(),
-            new ContactsWrapperPackage(),
-            new ReactNativeContacts(),
-            new RNCameraPackage(),
-            new RCTLocalePackage(),
-            new ExtraDimensionsPackage()
-            );
-    }
+        @Override
+        protected List<ReactPackage> getPackages() {
+          return Arrays.<ReactPackage>asList(
+              new MainReactPackage(),
+              new ARTPackage(),
+              new AsyncStoragePackage(),
+              new DiskletPackage(),
+              new RNCWebViewPackage(),
+              new CookieManagerPackage(),
+              new OpenAppSettingsPackage(),
+              BugsnagReactNative.getPackage(),
+              new RNFirebasePackage(),
+              new RNFirebaseAnalyticsPackage(),
+              new RNFirebaseDatabasePackage(),
+              new RNMail(),
+              new RCTSplashScreenPackage(),
+              new PickerViewPackage(),
+              new AbcCoreJsUiPackage(),
+              new RNSoundPackage(),
+              new RNSharePackage(),
+              new RandomBytesPackage(),
+              new RNFastCryptoPackage(),
+              new VectorIconsPackage(),
+              new TcpSocketsModule(),
+              new ReactMaterialKitPackage(),
+              new LinearGradientPackage(),
+              new ImagePickerPackage(),
+              new RNFSPackage(),
+              new RNDeviceInfo(),
+              new ContactsWrapperPackage(),
+              new ReactNativeContacts(),
+              new RNCameraPackage(),
+              new RCTLocalePackage(),
+              new ExtraDimensionsPackage());
+        }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -104,7 +101,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
 
-    //Disable RTL
+    // Disable RTL
     I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
     sharedI18nUtilInstance.allowRTL(getApplicationContext(), false);
 
