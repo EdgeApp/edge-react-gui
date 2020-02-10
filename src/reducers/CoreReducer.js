21733d41e142e045c39b665be50c144afe7e97d3
William Swanson <swansontec@gmail.com>
2020-02-10 13:10:02 -0800

    Flatten away the account reducer

--- src/reducers/CoreReducer.js
+++ src/reducers/CoreReducer.js
@@ -1,31 +1,44 @@
 // @flow
 
 import { type Disklet } from 'disklet'
-import { type EdgeContext } from 'edge-core-js/types'
+import { type EdgeAccount, type EdgeContext } from 'edge-core-js/types'
 import { type Reducer, combineReducers } from 'redux'
 
 import { type Action } from '../types/reduxTypes.js'
-import { type AccountState, account } from './CoreAccountReducer.js'
 import { type WalletsState, wallets } from './CoreWalletsReducer.js'
 import { type DeepLinkingState, deepLinking } from './DeepLinkingReducer.js'
 import { type EdgeLoginState, edgeLogin } from './EdgeLoginReducer.js'
 
 export type CoreState = {
+  +account: EdgeAccount,
   +context: EdgeContext,
   +disklet: Disklet,
 
   // Nested reducers:
-  +account: AccountState,
   +wallets: WalletsState,
   +edgeLogin: EdgeLoginState,
   +deepLinking: DeepLinkingState
 }
 
 const flowHack: any = {}
+const defaultAccount: EdgeAccount = flowHack
 const defaultContext: EdgeContext = flowHack
 const defaultDisklet: Disklet = flowHack
 
 export const core: Reducer<CoreState, Action> = combineReducers({
+  account (state: EdgeAccount = defaultAccount, action: Action): EdgeAccount {
+    switch (action.type) {
+      case 'ACCOUNT/LOGGED_IN': {
+        if (!action.data) throw new Error('Invalid Action')
+        return action.data.account
+      }
+      case 'LOGOUT':
+      case 'DEEP_LINK_RECEIVED':
+        return defaultAccount
+    }
+    return state
+  },
+
   context (state: EdgeContext = defaultContext, action: Action): EdgeContext {
     return action.type === 'CORE/CONTEXT/ADD_CONTEXT' ? action.data.context : state
   },
@@ -35,7 +48,6 @@ export const core: Reducer<CoreState, Action> = combineReducers({
   },
 
   // Nested reducers:
-  account,
   deepLinking,
   edgeLogin,
   wallets
