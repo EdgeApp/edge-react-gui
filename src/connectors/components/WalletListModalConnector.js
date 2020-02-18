9e36c7d9230f4dcaff235d20a25b05bfadbaebb0
Anthony Bruscantini <ajbruscantini@gmail.com>
2020-02-18 21:25:21 +0800

    hide wallets from WalletListModal

--- src/connectors/components/WalletListModalConnector.js
+++ src/connectors/components/WalletListModalConnector.js
@@ -4,12 +4,15 @@ import { connect } from 'react-redux'
 
 import type { StateProps } from '../../components/modals/WalletListModal.js'
 import { WalletListModal } from '../../components/modals/WalletListModal.js'
-import { getActiveWalletIds } from '../../modules/UI/selectors.js'
+import { getActiveWalletIds, getHiddenWalletIds } from '../../modules/UI/selectors.js'
 import type { State } from '../../types/reduxTypes.js'
 
 export const mapStateToProps = (state: State): StateProps => {
   const wallets = state.ui.wallets.byId
-  const activeWalletIds = getActiveWalletIds(state).filter(id => !(wallets[id] != null && wallets[id].type === 'wallet:fio'))
+  const hiddenWalletIds = getHiddenWalletIds(state)
+  const activeWalletIds = getActiveWalletIds(state)
+    .filter(id => !(wallets[id] != null && wallets[id].type === 'wallet:fio'))
+    .filter(id => !hiddenWalletIds.includes(id))
   const { mostRecentWallets } = state.ui.settings
   return {
     activeWalletIds,
