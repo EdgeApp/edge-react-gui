0272e020cf7026b1ad0d6edb879d479a41ebdb47
William Swanson <swansontec@gmail.com>
2020-02-21 13:12:05 -0800

    Stop re-exporting `isIphoneX` from platform.js

--- src/util/isIphoneX.js
+++ src/util/isIphoneX.js
@@ -3,6 +3,4 @@
 import { Dimensions, Platform } from 'react-native'
 
 const { height, width } = Dimensions.get('window')
-const isIphoneX: boolean = Platform.OS === 'ios' && (height === 812 || width === 812)
-
-export { isIphoneX }
+export const isIphoneX: boolean = Platform.OS === 'ios' && (height === 812 || width === 812)
