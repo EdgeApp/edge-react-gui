c147c4cde1ea40562f7f48ed5990511b218c0ac1
William Swanson <swansontec@gmail.com>
2020-05-25 16:47:52 -0700

    Remove more unused source files

--- src/components/materialWrappers/indexMaterial.js
+++ src/components/materialWrappers/indexMaterial.js
@@ -1,6 +1,5 @@
 // @flow
 
-export * from './PasswordComponent'
 export * from './Input'
 export * from './InputWithAutoFocus'
 export * from './Select'
--- src/modules/UI/components/Fees/index.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-import Fees from './Fees.ui.js'
-
-export default Fees
