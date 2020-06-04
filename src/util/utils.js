b8548fc5393fdc89699df82156affb57f4febf32
kylanhurt <kylan.hurt@gmail.com>
2020-06-03 21:29:02 -0700

    Add debounce utility function

--- src/util/utils.js
+++ src/util/utils.js
@@ -628,3 +628,25 @@ export const getYesterdayDateRoundDownHour = () => {
   const yesterday = date.setDate(date.getDate() - 1)
   return new Date(yesterday).toISOString()
 }
+
+export function debounce (func: Function, wait: number, immediate: boolean): any {
+  let timeout
+
+  return function executedFunction() {
+    const context = this
+    const args = arguments
+
+    const later = function() {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+
+    const callNow = immediate && !timeout
+
+    clearTimeout(timeout)
+
+    timeout = setTimeout(later, wait)
+
+    if (callNow) func.apply(context, args)
+  }
+}
