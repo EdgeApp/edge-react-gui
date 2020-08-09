2fe224bb6bc29d46aa59b5a77259c0f26c163180
kylanhurt <kylan.hurt@gmail.com>
2020-08-08 20:52:53 -0700

    Add conditional for debounce to prevent flow error, and format

--- src/util/utils.js
+++ src/util/utils.js
@@ -559,21 +559,21 @@ export function splitTransactionCategory(
   }
 }
 
-export function debounce (func: Function, wait: number, immediate: boolean): any {
+export function debounce(func: Function, wait: number, immediate: boolean): any {
   let timeout
 
   return function executedFunction() {
     const context = this
     const args = arguments
 
-    const later = function() {
+    const later = function () {
       timeout = null
       if (!immediate) func.apply(context, args)
     }
 
     const callNow = immediate && !timeout
 
-    clearTimeout(timeout)
+    if (timeout) clearTimeout(timeout)
 
     timeout = setTimeout(later, wait)
 
