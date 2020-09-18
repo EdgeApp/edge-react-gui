cb487b6fac9f99b74d3785c100eb311f62f9ab1b
Matthew <matthew@edge.app>
2020-09-18 16:12:40 -0700

    Upgrade edge-currency-accountbased to v0.7.25 and edge-exchange-plugins to v0.11.7
    Add rule for node-hid in postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# The `usb` module doesn't properly install on some boxes:
+# The `usb` and 'node-hid' modules doesn't properly install on some boxes:
 mkdir -p node_modules/usb
 touch node_modules/usb/index.js
+
+mkdir -p node_modules/node-hid
+touch node_modules/node-hid/index.js
 
 # Set up CocoaPods on iOS:
 unamestr=`uname`
