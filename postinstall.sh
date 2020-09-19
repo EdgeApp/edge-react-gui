e64283afbffdbdd91894a15b417306b287504b0c
William Swanson <swansontec@gmail.com>
2020-09-18 20:39:19 -0700

    Upgrade to edge-login-ui-rn v0.6.17

--- postinstall.sh
+++ postinstall.sh
@@ -3,6 +3,8 @@
 # The `usb` and 'node-hid' modules doesn't properly install on some boxes:
 mkdir -p node_modules/usb
 touch node_modules/usb/index.js
+mkdir -p node_modules/node-hid
+touch node_modules/node-hid/index.js
 
 mkdir -p node_modules/node-hid
 touch node_modules/node-hid/index.js
