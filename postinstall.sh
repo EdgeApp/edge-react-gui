f4c43a0285511a62dd7ded3ab1858e108c1ebccf
Matthew <matthew@edge.app>
2020-10-05 16:00:42 -0700

    Update ONE

--- postinstall.sh
+++ postinstall.sh
@@ -3,6 +3,8 @@
 # The `usb` module doesn't properly install on some boxes:
 mkdir -p node_modules/usb
 touch node_modules/usb/index.js
+mkdir -p node_modules/node-hid
+touch node_modules/node-hid/index.js
 
 # Set up CocoaPods on iOS:
 unamestr=`uname`
