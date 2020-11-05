1d40bbc4e9ed924f2f25f3fe307211ea44379a60
Art Manalili <artmanalili@gmail.com>
2020-11-06 04:31:01 +0800

    Promo Card Refactor - Separate the Promo Card from parent Wallet List Scene to its new themed component - Promo Card functions into separate class variable - Replace Promo Card TouchableOpacity to Touchable Highlight

---
+++ src/components/themed/PromoCard.js
@@ -0,0 +1,107 @@
+// @flow
+
+import * as React from 'react'
+import { Image, TouchableHighlight, TouchableOpacity, View } from 'react-native'
+import AntDesignIcon from 'react-native-vector-icons/AntDesign'
+import { connect } from 'react-redux'
+
+import { hideMessageTweak } from '../../actions/AccountReferralActions.js'
+import { linkReferralWithCurrencies } from '../../actions/WalletListActions.js'
+import { type Dispatch, type RootState } from '../../types/reduxTypes.js'
+import { type AccountReferral } from '../../types/ReferralTypes.js'
+import { type MessageTweak } from '../../types/TweakTypes.js'
+import { type TweakSource, bestOfMessages } from '../../util/ReferralHelpers.js'
+import { type Theme, type ThemeProps, cacheStyles, withTheme } from '../services/ThemeContext.js'
+import { EdgeText } from '../themed/EdgeText.js'
+
+type StateProps = {
+  accountMessages: MessageTweak[],
+  accountReferral: AccountReferral
+}
+
+type DispatchProps = {
+  hideMessageTweak(messageId: string, source: TweakSource): void,
+  linkReferralWithCurrencies(string): void
+}
+
+type Props = StateProps & DispatchProps & ThemeProps
+
+class PromoCardComponent extends React.PureComponent<Props> {
+  handlePress = () => {
+    const { accountMessages, accountReferral, linkReferralWithCurrencies } = this.props
+    const messageSummary = bestOfMessages(accountMessages, accountReferral)
+    const uri = messageSummary ? messageSummary.message.uri : null
+    if (uri != null) linkReferralWithCurrencies(uri)
+  }
+
+  handleClose = () => {
+    const { accountMessages, accountReferral, hideMessageTweak } = this.props
+    const messageSummary = bestOfMessages(accountMessages, accountReferral)
+    if (messageSummary != null) {
+      hideMessageTweak(messageSummary.messageId, messageSummary.messageSource)
+    }
+  }
+
+  render() {
+    const { accountMessages, accountReferral, theme } = this.props
+    const styles = getStyles(theme)
+    const messageSummary = bestOfMessages(accountMessages, accountReferral)
+    if (messageSummary == null) return null
+    const { message } = messageSummary
+    return (
+      <View style={styles.rootContainer}>
+        <TouchableHighlight activeOpacity={0.95} underlayColor={theme.underlayColor} onPress={this.handlePress}>
+          <View style={styles.container}>
+            {message.iconUri != null ? <Image resizeMode="contain" source={{ uri: message.iconUri }} style={styles.icon} /> : null}
+            <EdgeText numberOfLines={0} style={styles.text}>
+              {message.message}
+            </EdgeText>
+            <TouchableOpacity onPress={this.handleClose}>
+              <AntDesignIcon name="close" color={theme.iconTappable} size={theme.rem(1)} style={styles.close} />
+            </TouchableOpacity>
+          </View>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+const getStyles = cacheStyles((theme: Theme) => ({
+  // Promo area:
+  rootContainer: {
+    margin: theme.rem(1)
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: theme.tileBackground,
+    padding: theme.rem(0.5)
+  },
+  icon: {
+    width: theme.rem(2),
+    height: theme.rem(2),
+    margin: theme.rem(0.5)
+  },
+  text: {
+    flex: 1,
+    margin: theme.rem(0.5)
+  },
+  close: {
+    padding: theme.rem(0.5)
+  }
+}))
+
+export const PromoCard = connect(
+  (state: RootState): StateProps => ({
+    accountMessages: state.account.referralCache.accountMessages,
+    accountReferral: state.account.accountReferral
+  }),
+  (dispatch: Dispatch): DispatchProps => ({
+    hideMessageTweak(messageId: string, source: TweakSource) {
+      dispatch(hideMessageTweak(messageId, source))
+    },
+    linkReferralWithCurrencies(uri) {
+      dispatch(linkReferralWithCurrencies(uri))
+    }
+  })
+)(withTheme(PromoCardComponent))
