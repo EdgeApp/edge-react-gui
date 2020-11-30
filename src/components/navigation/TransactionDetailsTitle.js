43cb38a1bab0bf3909f08ddb273b3778b9a9f8d4
Art Manalili <artmanalili@gmail.com>
2020-11-30 17:10:39 +0800

    Fix Transaction Deatils Header and Settings Header.

--- src/components/navigation/TransactionDetailsTitle.js
+++ src/components/navigation/TransactionDetailsTitle.js
@@ -2,10 +2,10 @@
 
 import { type EdgeTransaction } from 'edge-core-js'
 import * as React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { View } from 'react-native'
 
-import T from '../../modules/UI/components/FormattedText/FormattedText.ui.js'
-import { THEME } from '../../theme/variables/airbitz.js'
+import { type Theme, cacheStyles, useTheme } from '../services/ThemeContext.js'
+import { EdgeText } from '../themed/EdgeText.js'
 
 type Props = {
   edgeTransaction: EdgeTransaction
@@ -14,34 +14,32 @@ type Props = {
 export function TransactionDetailsTitle(props: Props) {
   if (props.edgeTransaction == null) return null // Should never happen!?
 
+  const styles = getStyles(useTheme())
   const { date } = props.edgeTransaction
   const txDate = new Date(date * 1000)
   const dateString = txDate.toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' })
   const time = txDate.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })
 
   return (
-    <View style={styles.transactionDetailsDateTimeContainer}>
-      <T style={styles.transactionDetailsDate}>{dateString}</T>
-      <T style={styles.transactionDetailsTime}>{time}</T>
+    <View style={styles.container}>
+      <EdgeText style={styles.date}>{dateString}</EdgeText>
+      <EdgeText style={styles.time}>{time}</EdgeText>
     </View>
   )
 }
 
-const rawStyles = {
-  transactionDetailsDateTimeContainer: {
+const getStyles = cacheStyles((theme: Theme) => ({
+  container: {
+    flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    width: '100%'
+    paddingHorizontal: theme.rem(1.5)
   },
-  transactionDetailsDate: {
-    fontSize: THEME.rem(1),
-    color: THEME.COLORS.WHITE,
-    fontFamily: THEME.FONTS.DEFAULT
+  date: {
+    fontFamily: theme.fontFaceBold
   },
-  transactionDetailsTime: {
-    fontSize: THEME.rem(0.6875),
-    color: THEME.COLORS.HEADER_TEXT_SECONDARY,
-    fontFamily: THEME.FONTS.DEFAULT
+  time: {
+    fontSize: theme.rem(0.75),
+    color: theme.secondaryText
   }
-}
-const styles: typeof rawStyles = StyleSheet.create(rawStyles)
+}))
