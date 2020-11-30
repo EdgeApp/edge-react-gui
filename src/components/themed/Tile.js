a985f358c1fb609f41d14222250e80e8f6af570b
Art Manalili <artmanalili@gmail.com>
2020-11-30 19:21:08 +0800

    Fix adjustsFontSizeToFit on iOS text to be disabled when the tile is for copying text

--- src/components/themed/Tile.js
+++ src/components/themed/Tile.js
@@ -40,7 +40,7 @@ class TileComponent extends React.PureComponent<Props> {
             {type === 'copy' && <FontAwesomeIcon name="copy" style={styles.editIcon} />}
             <EdgeText style={error ? styles.textHeaderError : styles.textHeader}>{title}</EdgeText>
             {typeof body === 'string' && (
-              <EdgeText style={styles.textBody} numberOfLines={3}>
+              <EdgeText style={styles.textBody} numberOfLines={3} adjustsFontSizeToFit={false}>
                 {body}
               </EdgeText>
             )}
