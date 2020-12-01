706d2ad903fd0c4c0b0b6ad9657b2933409320e5
Art Manalili <artmanalili@gmail.com>
2020-12-01 23:53:14 +0800

    All logic to BuyCrypto components and themed BuyCrypto

--- src/components/common/BuyCrypto.js
+++ src/components/common/BuyCrypto.js
@@ -1,81 +1,99 @@
 // @flow
 
 import * as React from 'react'
-import { Image, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
+import { Image, View } from 'react-native'
 import { Actions } from 'react-native-router-flux'
+import { connect } from 'react-redux'
 import { sprintf } from 'sprintf-js'
 
-import * as Constants from '../../constants/indexConstants.js'
 import s from '../../locales/strings.js'
-import T from '../../modules/UI/components/FormattedText/FormattedText.ui.js'
-import { THEME } from '../../theme/variables/airbitz.js'
-import { PLATFORM } from '../../theme/variables/platform.js'
-import type { GuiWallet } from '../../types/types.js'
-import { scale } from '../../util/scaling.js'
+import { type RootState } from '../../types/reduxTypes.js'
+import { type Theme, type ThemeProps, cacheStyles, withTheme } from '../services/ThemeContext.js'
+import { EdgeText } from '../themed/EdgeText.js'
+import { ButtonBox } from '../themed/ThemedButtons.js'
 
-export type Props = {
-  wallet: GuiWallet
+const allowedCurrencies = { BTC: true, BCH: true, ETH: true, LTC: true, XRP: true, BSV: true }
+
+type OwnProps = {
+  walletId: string,
+  currencyCode: string
 }
 
-export default class BuyCrypto extends React.Component<Props> {
-  getCurrencyName = () => {
-    const { wallet } = this.props
-    return wallet.currencyNames[wallet.currencyCode]
-  }
+type StateProps = {
+  currencyName: string,
+  currencyImage?: string
+}
 
+type Props = OwnProps & StateProps & ThemeProps
+
+class BuyCryptoComponent extends React.PureComponent<Props> {
   render() {
+    const { currencyCode, currencyImage, currencyName, theme } = this.props
+    const styles = getStyles(theme)
+
     return (
-      <TouchableWithoutFeedback onPress={Actions[Constants.PLUGIN_BUY]}>
-        <View style={styles.buyCryptoContainer}>
-          <View style={styles.buyCryptoBox}>
-            <Image style={styles.buyCryptoBoxImage} source={{ uri: this.props.wallet.symbolImage }} resizeMode="cover" />
-            <T style={styles.buyCryptoBoxText}>{sprintf(s.strings.transaction_list_buy_crypto_message, this.getCurrencyName)}</T>
-          </View>
-          <View style={styles.buyCryptoNoTransactionBox}>
-            <T style={styles.buyCryptoNoTransactionText}>{s.strings.transaction_list_no_tx_yet}</T>
-          </View>
+      <>
+        {allowedCurrencies[currencyCode] && (
+          <ButtonBox onPress={Actions.pluginBuy} paddingRem={1}>
+            <View style={styles.container}>
+              <View style={styles.buyCrypto}>
+                <Image style={styles.buyCryptImage} source={{ uri: currencyImage }} resizeMode="cover" />
+                <EdgeText style={styles.buyCryptoText}>{sprintf(s.strings.transaction_list_buy_crypto_message, currencyName)}</EdgeText>
+              </View>
+            </View>
+          </ButtonBox>
+        )}
+        <View style={styles.noTransactionContainer}>
+          <EdgeText style={styles.noTransactionText}>{s.strings.transaction_list_no_tx_yet}</EdgeText>
         </View>
-      </TouchableWithoutFeedback>
+      </>
     )
   }
 }
 
-const rawStyles = {
-  buyCryptoContainer: {
-    backgroundColor: THEME.COLORS.GRAY_4,
-    width: PLATFORM.deviceWidth,
-    height: scale(220),
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: scale(15)
-  },
-  buyCryptoBox: {
-    flex: 2,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    backgroundColor: THEME.COLORS.WHITE
-  },
-  buyCryptoBoxImage: {
-    width: scale(40),
-    height: scale(40)
-  },
-  buyCryptoBoxText: {
-    marginTop: scale(10),
-    fontSize: scale(17),
-    color: THEME.COLORS.GRAY_1
-  },
-  buyCryptoNoTransactionBox: {
+const getStyles = cacheStyles((theme: Theme) => ({
+  container: {
     flex: 1,
-    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.rem(1),
+    backgroundColor: theme.tileBackground
+  },
+  buyCrypto: {
     alignItems: 'center',
     justifyContent: 'center'
   },
-  buyCryptoNoTransactionText: {
-    fontSize: scale(17),
-    color: THEME.COLORS.ACCENT_BLUE
+  buyCryptImage: {
+    width: theme.rem(2.25),
+    height: theme.rem(2.25),
+    marginVertical: theme.rem(0.25)
+  },
+  buyCryptoText: {
+    fontFamily: theme.fontFaceBold,
+    marginVertical: theme.rem(0.25)
+  },
+  noTransactionContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: theme.rem(0.5)
+  },
+  noTransactionBigContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: theme.rem(15)
+  },
+  noTransactionText: {
+    fontSize: theme.rem(1.25)
   }
-}
-const styles: typeof rawStyles = StyleSheet.create(rawStyles)
+}))
+
+export const BuyCrypto = connect((state: RootState, ownProps: OwnProps): StateProps => {
+  const guiWallet = state.ui.wallets.byId[ownProps.walletId]
+
+  return {
+    currencyName: guiWallet.currencyNames[ownProps.currencyCode],
+    currencyImage: guiWallet.symbolImage
+  }
+})(withTheme(BuyCryptoComponent))
