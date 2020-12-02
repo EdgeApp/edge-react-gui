9bfee070ce19b8790739f7be0a23da0db3de9eb1
William Swanson <swansontec@gmail.com>
2020-12-01 16:17:45 -0800

    Remove various reat-native-material-input wrappers

--- src/components/common/FormField.js
+++ src/components/common/FormField.js
@@ -1,11 +1,10 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
 import * as React from 'react'
+import { TextField } from 'react-native-material-textfield'
 
 import { THEME } from '../../theme/variables/airbitz.js'
 import { PLATFORM } from '../../theme/variables/platform.js'
-import { Input } from '../materialWrappers/Input.js'
-import { InputWithAutoFocus } from '../materialWrappers/InputWithAutoFocus.js'
 
 /*
 type Props = {
@@ -19,7 +18,6 @@ type Props = {
  placeholder?: string,
  autoCorrect: boolean,
  autoFocus: boolean,
- forceFocus: boolean,
  autoCapitalize?: string,
  secureTextEntry?: boolean,
  showSecureCheckbox?: boolean,
@@ -31,106 +29,61 @@ type Props = {
  onChangeText(string):void,
 }
 
-type State = {
- secure: boolean
-}
-
-class FormField extends React.Component<Props, State> {
+class FormField extends React.Component<Props> {
 */
 export class FormField extends React.Component {
   static defaultProps = {
     autoCapitalize: 'none',
     autoCorrect: false,
     autoFocus: false,
-    forceFocus: false,
     returnKeyType: 'go',
     label: '',
     keyboardType: 'default',
     multiline: false
   }
 
-  constructor(props) {
-    super(props)
-    const { secureTextEntry = false } = props
-    this.state = {
-      secure: secureTextEntry
+  inputRef = React.createRef()
+
+  componentDidMount() {
+    if (this.props.autoFocus && this.inputRef.current != null) {
+      this.inputRef.current.focus()
     }
   }
 
   render() {
     const { style = {} } = this.props
     const { container, baseColor, tintColor, textColor, errorColor, titleTextStyle } = style
-    if (this.props.autoFocus) {
-      return (
-        <InputWithAutoFocus
-          label={this.props.label}
-          onChangeText={this.props.onChangeText}
-          error={this.props.error}
-          containerStyle={container}
-          secureTextEntry={this.state.secure}
-          returnKeyType={this.props.returnKeyType}
-          fontSize={this.props.fontSize}
-          titleFontSize={this.props.titleFontSize}
-          labelFontSize={this.props.labelFontSize}
-          baseColor={baseColor}
-          tintColor={tintColor}
-          textColor={textColor}
-          errorColor={errorColor}
-          titleTextStyle={titleTextStyle}
-          forceFocus={this.props.forceFocus}
-          onFocus={this.props.onFocus}
-          onBlur={this.props.onBlur}
-          autoCapitalize={this.props.autoCapitalize}
-          onSubmitEditing={this.onSubmitEditing.bind(this)}
-          value={this.props.value}
-          keyboardType={this.props.keyboardType}
-          maxLength={this.props.maxLength}
-          autoCorrect={this.props.autoCorrect || false}
-          autoFocus={this.state.autoFocus}
-          multiline={this.props.multiline}
-          suffix={this.props.suffix}
-          prefix={this.props.prefix}
-        />
-      )
-    } else {
-      return (
-        <Input
-          label={this.props.label}
-          onChangeText={this.props.onChangeText}
-          error={this.props.error}
-          containerStyle={container}
-          secureTextEntry={this.state.secure}
-          returnKeyType={this.props.returnKeyType}
-          fontSize={this.props.fontSize}
-          titleFontSize={this.props.titleFontSize}
-          labelFontSize={this.props.labelFontSize}
-          baseColor={baseColor}
-          tintColor={tintColor}
-          textColor={textColor}
-          errorColor={errorColor}
-          titleTextStyle={titleTextStyle}
-          autoFocus={this.state.autoFocus}
-          forceFocus={this.props.forceFocus}
-          onFocus={this.props.onFocus}
-          onBlur={this.props.onBlur}
-          autoCapitalize={this.props.autoCapitalize}
-          onSubmitEditing={this.onSubmitEditing.bind(this)}
-          value={this.props.value}
-          keyboardType={this.props.keyboardType}
-          maxLength={this.props.maxLength}
-          autoCorrect={this.props.autoCorrect || false}
-          multiline={this.props.multiline}
-          suffix={this.props.suffix}
-          prefix={this.props.prefix}
-        />
-      )
-    }
-  }
 
-  onSubmitEditing = () => {
-    if (this.props.onSubmitEditing) {
-      this.props.onSubmitEditing()
-    }
+    return (
+      <TextField
+        ref={this.inputRef}
+        label={this.props.label}
+        onChangeText={this.props.onChangeText}
+        error={this.props.error}
+        containerStyle={container}
+        secureTextEntry={this.props.secureTextEntry}
+        returnKeyType={this.props.returnKeyType}
+        fontSize={this.props.fontSize}
+        titleFontSize={this.props.titleFontSize}
+        labelFontSize={this.props.labelFontSize}
+        baseColor={baseColor}
+        tintColor={tintColor}
+        textColor={textColor}
+        errorColor={errorColor}
+        titleTextStyle={titleTextStyle}
+        onFocus={this.props.onFocus}
+        onBlur={this.props.onBlur}
+        autoCapitalize={this.props.autoCapitalize}
+        onSubmitEditing={this.props.onSubmitEditing}
+        value={this.props.value}
+        keyboardType={this.props.keyboardType}
+        maxLength={this.props.maxLength}
+        autoCorrect={this.props.autoCorrect}
+        multiline={this.props.multiline}
+        suffix={this.props.suffix}
+        prefix={this.props.prefix}
+      />
+    )
   }
 }
 
