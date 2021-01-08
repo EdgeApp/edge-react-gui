4b38471b743d69e2891bf96e3032e616c7b4d869
Art Manalili <artmanalili@gmail.com>
2021-01-09 03:09:32 +0800

    Password Reminder Modal Fixes - Fix the logic to revert back to the old way - Fix comments on the PR - Added new util function matchJson that is reusable

--- src/actions/PasswordReminderActions.js
+++ src/actions/PasswordReminderActions.js
@@ -4,12 +4,16 @@ import { showError } from '../components/services/AirshipInstance.js'
 import { setPasswordReminderRequest } from '../modules/Core/Account/settings.js'
 import type { Dispatch, GetState } from '../types/reduxTypes.js'
 
+export const passwordReminderSuccess = () => ({
+  type: 'PASSWORD_REMINDER_MODAL/CHECK_PASSWORD_SUCCESS'
+})
+
 export const requestChangePassword = () => ({
   type: 'PASSWORD_REMINDER_MODAL/REQUEST_CHANGE_PASSWORD'
 })
 
 export const postponePasswordReminder = () => ({
-  type: 'PASSWORD_REMINDER_MODAL/PASSWORD_REMINDER_POSTPONED'
+  type: 'PASSWORD_REMINDER/PASSWORD_REMINDER_POSTPONED'
 })
 
 // Saving data to account local folder
