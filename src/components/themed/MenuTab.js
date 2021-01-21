5002e27d0ca179bbe3dbfc987fed1687866bbb8a
Art Manalili <artmanalili@gmail.com>
2021-01-21 17:28:05 +0800

    Tapping the Wallet List on the menu will always go to the Wallet List Scene

--- src/components/themed/MenuTab.js
+++ src/components/themed/MenuTab.js
@@ -26,6 +26,13 @@ const title = {
 }
 
 class MenuTabComponent extends React.PureComponent<Props> {
+  handleOnPress = (route: string) => {
+    if (route === Constants.WALLET_LIST) {
+      return Actions.jump(Constants.WALLET_LIST_SCENE)
+    }
+    Actions.jump(route)
+  }
+
   render() {
     const { theme } = this.props
     const styles = getStyles(theme)
@@ -42,7 +49,7 @@ class MenuTabComponent extends React.PureComponent<Props> {
             [Constants.EXCHANGE]: <Ionicon name="swap-horizontal" size={theme.rem(1.25)} color={color} />
           }
           return (
-            <TouchableOpacity style={styles.content} key={element.key} onPress={() => Actions.jump(element.key)}>
+            <TouchableOpacity style={styles.content} key={element.key} onPress={() => this.handleOnPress(element.key)}>
               {icon[element.key]}
               <EdgeText style={{ ...styles.text, color: color }}>{title[element.key]}</EdgeText>
             </TouchableOpacity>
