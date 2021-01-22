90fde6e32f3860435a8e71b27a71973f617028af
Jacob Burrell <Jacobburrell@pm.me>
2021-01-21 21:56:57 -0800

    Created simplified util function to get multiplier from setting object and used in rendering exchange data to calculate the correct denomination for the payout currency.

--- src/components/scenes/TransactionDetailsScene.js
+++ src/components/scenes/TransactionDetailsScene.js
@@ -47,7 +47,8 @@ type StateProps = {
   destinationWallet?: GuiWallet,
   guiWallet: GuiWallet,
   subcategoriesList: string[],
-  walletDefaultDenomProps: EdgeDenomination
+  walletDefaultDenomProps: EdgeDenomination,
+  settings: Object
 }
 type DispatchProps = {
   getSubcategories(): void,
@@ -252,15 +253,19 @@ export class TransactionDetailsComponent extends React.Component<Props, State> {
   }
 
   renderExchangeData = (symbolString: string) => {
-    const { destinationDenomination, destinationWallet, edgeTransaction, guiWallet, walletDefaultDenomProps, theme } = this.props
+    const { destinationDenomination, destinationWallet, edgeTransaction, guiWallet, walletDefaultDenomProps, theme, settings } = this.props
     const { swapData, spendTargets } = edgeTransaction
     const styles = getStyles(theme)
 
     if (!swapData || !spendTargets || !destinationDenomination) return null
 
+    const payoutMultiplier = UTILS.getMultiplierFromSettings(swapData.payoutCurrencyCode.toUpperCase(), settings) // "ETH", Settings object
+
     const { plugin, isEstimate, orderId, payoutAddress, refundAddress } = swapData
     const sourceAmount = UTILS.convertNativeToDisplay(walletDefaultDenomProps.multiplier)(spendTargets[0].nativeAmount)
-    const destinationAmount = UTILS.convertNativeToDisplay(destinationDenomination.multiplier)(swapData.payoutNativeAmount)
+    // const sourceCurrencyCode = spendTargets[0].currencyCode
+    // Remove ending 0s
+    const destinationAmount = UTILS.convertNativeToDisplay(payoutMultiplier)(swapData.payoutNativeAmount)
     const destinationCurrencyCode = swapData.payoutCurrencyCode
 
     const createExchangeDataString = (newline: string = '\n') => {
@@ -623,7 +628,8 @@ export const TransactionDetailsScene = connect(
       destinationWallet,
       guiWallet: wallet,
       subcategoriesList,
-      walletDefaultDenomProps
+      walletDefaultDenomProps,
+      settings
     }
   },
   (dispatch: Dispatch): DispatchProps => ({
