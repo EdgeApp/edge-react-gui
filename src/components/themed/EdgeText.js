7fa10bb816b32d373ff07719d1ce43f914ebedae
Art Manalili <artmanalili@gmail.com>
2021-01-22 13:24:34 +0800

    Make EdgeText component add \n characters to numberOfLines. This will fix the bug not seeing the limits on plugin descriptions

--- src/components/themed/EdgeText.js
+++ src/components/themed/EdgeText.js
@@ -16,12 +16,16 @@ class EdgeTextComponent extends React.PureComponent<OwnProps & ThemeProps> {
   render() {
     const { children, style, theme, ...props } = this.props
     const { text } = getStyles(theme)
+    let numberOfLines = this.props.numberOfLines || 1
+    if (typeof children === 'string' && children.includes('\n')) {
+      numberOfLines = numberOfLines + (children.match(/\n/g) || []).length
+    }
     return Platform.OS === 'ios' ? (
-      <Text style={[text, style]} numberOfLines={this.props.numberOfLines || 1} adjustsFontSizeToFit {...props}>
+      <Text style={[text, style]} numberOfLines={numberOfLines} adjustsFontSizeToFit {...props}>
         {children}
       </Text>
     ) : (
-      <Text style={[text, style]} numberOfLines={this.props.numberOfLines || 1} ellipsizeMode={this.props.ellipsizeMode || 'middle'} {...props}>
+      <Text style={[text, style]} numberOfLines={numberOfLines} ellipsizeMode={this.props.ellipsizeMode || 'middle'} {...props}>
         {children}
       </Text>
     )
