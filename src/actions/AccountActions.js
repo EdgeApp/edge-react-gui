18445ba804244dd412ac2778ac78f014fd1575fb
William Swanson <swansontec@gmail.com>
2021-02-11 13:20:36 -0800

    Add a 2fa repair screen

---
+++ src/actions/AccountActions.js
@@ -0,0 +1,17 @@
+// @flow
+
+import { OtpError } from 'edge-core-js'
+import { Actions } from 'react-native-router-flux'
+
+import { OTP_REPAIR_SCENE } from '../constants/SceneKeys.js'
+import { type Dispatch, type GetState } from '../types/reduxTypes.js'
+
+export const handleOtpError = (otpError: OtpError) => (dispatch: Dispatch, getState: GetState) => {
+  const state = getState()
+  const { account, otpErrorShown } = state.core
+
+  if (account.loggedIn && !otpErrorShown) {
+    dispatch({ type: 'OTP_ERROR_SHOWN' })
+    Actions.push(OTP_REPAIR_SCENE, { otpError })
+  }
+}
