525ec91bda0b4b093d05ed41151c75e184a9e942
Art Manalili <artmanalili@gmail.com>
2021-03-11 17:49:32 +0800

    Make a header row where have a clickable text Send Max Amount

--- src/components/modals/FlipInputModal.js
+++ src/components/modals/FlipInputModal.js
@@ -3,7 +3,7 @@
 import { bns } from 'biggystring'
 import { errorNames } from 'edge-core-js'
 import * as React from 'react'
-import { ScrollView, View } from 'react-native'
+import { ScrollView, TouchableOpacity, View } from 'react-native'
 import { type AirshipBridge } from 'react-native-airship'
 import { MenuProvider } from 'react-native-popup-menu'
 import { connect } from 'react-redux'
@@ -22,7 +22,6 @@ import { ExchangeRate } from '../common/ExchangeRate.js'
 import { type Theme, type ThemeProps, cacheStyles, withTheme } from '../services/ThemeContext.js'
 import { EdgeText } from '../themed/EdgeText.js'
 import { ModalCloseArrow, ModalTitle } from '../themed/ModalParts.js'
-import { PrimaryButton } from '../themed/ThemedButtons.js'
 import { ThemedModal } from '../themed/ThemedModal.js'
 
 type OwnProps = {
@@ -91,6 +90,19 @@ class FlipInputModalComponent extends React.PureComponent<Props, State> {
   }
 
   handleSendMaxAmount = () => this.props.updateMaxSpend(this.props.walletId, this.props.currencyCode)
+
+  renderTitle = () => {
+    const styles = getStyles(this.props.theme)
+    return (
+      <View style={styles.headerContainer}>
+        <ModalTitle>{s.strings.string_enter_amount}</ModalTitle>
+        <View style={styles.spacer} />
+        <TouchableOpacity onPress={this.handleSendMaxAmount}>
+          <EdgeText style={styles.headerMaxAmountText}>{s.strings.send_confirmation_max_button_title}</EdgeText>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 
   renderExchangeRates = () => {
     const { primaryInfo, secondaryInfo, fiatPerCrypto, errorMessage, theme } = this.props
@@ -164,12 +176,13 @@ class FlipInputModalComponent extends React.PureComponent<Props, State> {
       <MenuProvider style={styles.menuProvider}>
         <ThemedModal bridge={this.props.bridge} onCancel={this.handleCloseModal}>
           <ScrollView style={{ maxHeight: this.props.theme.rem(23) }} keyboardShouldPersistTaps="handled">
-            <ModalTitle>{s.strings.string_enter_amount}</ModalTitle>
+            {this.renderTitle()}
             {this.renderExchangeRates()}
             {this.renderBalance()}
             {this.renderFlipInput()}
             {this.renderFees()}
             <PrimaryButton label={s.strings.send_confirmation_max_button_title} marginRem={1} onPress={this.handleSendMaxAmount} />
+            <ModalCloseArrow onPress={this.handleCloseModal} />
           </ScrollView>
           <ModalCloseArrow onPress={this.handleCloseModal} />
         </ThemedModal>
@@ -181,6 +194,13 @@ class FlipInputModalComponent extends React.PureComponent<Props, State> {
 const getStyles = cacheStyles((theme: Theme) => ({
   menuProvider: {
     flexDirection: 'row'
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  headerMaxAmountText: {
+    color: theme.textLink
   },
   balanceContainer: {
     flexDirection: 'row',
@@ -219,6 +239,9 @@ const getStyles = cacheStyles((theme: Theme) => ({
   },
   feesSyntaxDanger: {
     color: theme.dangerText
+  },
+  spacer: {
+    flex: 1
   }
 }))
 
