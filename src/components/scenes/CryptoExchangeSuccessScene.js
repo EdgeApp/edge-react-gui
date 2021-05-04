95984edb486679a5f5b21b349e4a1606edae8bf6
andreyvEze <andreyv@eze.tech>
2021-05-04 22:01:15 +0300

    Show confetti logic.

--- src/components/scenes/CryptoExchangeSuccessScene.js
+++ src/components/scenes/CryptoExchangeSuccessScene.js
@@ -1,21 +1,33 @@
 // @flow
 
+import type { Disklet } from 'disklet'
 import * as React from 'react'
 import { View } from 'react-native'
 import ConfettiCannon from 'react-native-confetti-cannon'
 import { Actions } from 'react-native-router-flux'
+import { connect } from 'react-redux'
 
 import * as Constants from '../../constants/indexConstants'
 import s from '../../locales/strings.js'
+import type { RootState } from '../../reducers/RootReducer'
+import { needToShowConfetti } from '../../util/show-confetti'
 import { SceneWrapper } from '../common/SceneWrapper.js'
 import { type Theme, type ThemeProps, cacheStyles, withTheme } from '../services/ThemeContext.js'
 import { EdgeText } from '../themed/EdgeText'
 import { Fade } from '../themed/Fade'
 import { SecondaryButton } from '../themed/ThemedButtons'
 
-type LocalState = {
-  showButton: boolean
+type StateProps = {
+  userId: string,
+  disklet: Disklet
 }
+
+type LocalState = {
+  showButton: boolean,
+  showConfetti: boolean
+}
+
+type Props = StateProps & ThemeProps
 
 const confettiProps = {
   count: 250,
@@ -23,16 +35,14 @@ const confettiProps = {
   fallSpeed: 4000
 }
 
-class CryptoExchangeSuccessComponent extends React.PureComponent<ThemeProps, LocalState> {
+class CryptoExchangeSuccessComponent extends React.PureComponent<Props, LocalState> {
   constructor() {
     super()
-    this.state = { showButton: false }
+    this.state = { showButton: false, showConfetti: false }
   }
 
-  componentDidMount(): * {
-    setTimeout(() => {
-      this.setState({ showButton: true })
-    }, 4500)
+  componentDidMount(): void {
+    this.showConfetti()
   }
 
   done = () => {
@@ -40,7 +50,22 @@ class CryptoExchangeSuccessComponent extends React.PureComponent<ThemeProps, Loc
     Actions.popTo(Constants.EXCHANGE_SCENE)
   }
 
+  showConfetti = async () => {
+    const { userId, disklet } = this.props
+    const show: boolean = await needToShowConfetti(userId, disklet)
+
+    if (show) {
+      this.setState({ showConfetti: true })
+      setTimeout(() => {
+        this.setState({ showButton: true })
+      }, 4500)
+    } else {
+      this.setState({ showButton: true })
+    }
+  }
+
   renderConfetti() {
+    if (!this.state.showConfetti) return null
     return <ConfettiCannon {...confettiProps} />
   }
 
@@ -88,4 +113,7 @@ const getStyles = cacheStyles((theme: Theme) => ({
   }
 }))
 
-export const CryptoExchangeSuccessScene = withTheme(CryptoExchangeSuccessComponent)
+export const CryptoExchangeSuccessScene = connect((state: RootState): StateProps => ({
+  userId: state.core.account.id,
+  disklet: state.core.disklet
+}))(withTheme(CryptoExchangeSuccessComponent))
