fae744b67a98a731635cba3a630f4f50cdbeb3f2
Paul Puey <paul@edge.app>
2021-05-21 14:01:24 -0700

    Test new bitcoin plugin

--- src/util/corePluginBundle.js
+++ src/util/corePluginBundle.js
@@ -3,6 +3,11 @@
 
 import 'core-js'
 import 'edge-currency-accountbased/lib/index.js'
-import 'edge-currency-bitcoin/lib/react-native.js'
+// import 'edge-currency-bitcoin/lib/react-native.js'
 import 'edge-currency-monero'
+import 'edge-currency-plugins/lib/btc.js'
+// import 'edge-currency-plugins/lib/bch.js'
+// import 'edge-currency-plugins/lib/bsv.js'
+// import 'edge-currency-plugins/lib/ftc.js'
+// import 'edge-currency-plugins/lib/ltc.js'
 import 'edge-exchange-plugins'
