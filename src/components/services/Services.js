ffa3eaf298d3e5f9be87b3ca112d616fc0fcfab3
alex0735070005 <alex0735070005@gmail.com>
2021-05-24 10:23:49 +0300

    Implemented use redux store from global for dev

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -84,7 +84,10 @@ export class Services extends React.PureComponent<Props> {
   }
 
   render() {
-    global.db = this.store.getState
+    if (global.__DEV__) {
+      global.reduxStore = this.store.getState
+    }
+
     return (
       <Provider store={this.store}>
         <>
