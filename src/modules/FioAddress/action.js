c8432182f464496dda4d91535b11a7a0c8f4ea8e
andreyvEze <andreyv@eze.tech>
2021-06-23 21:08:08 +0300

    Use refresh fio names util in fio names refresh action

--- src/modules/FioAddress/action.js
+++ src/modules/FioAddress/action.js
@@ -6,9 +6,8 @@ import { checkExpiredFioNames } from '../../actions/FioActions'
 import * as Constants from '../../constants/indexConstants'
 import s from '../../locales/strings'
 import type { Dispatch, GetState } from '../../types/reduxTypes'
-import type { FioAddress, FioDomain } from '../../types/types'
 import { getDefaultIsoFiat } from '../Settings/selectors'
-import { findWalletByFioAddress, refreshConnectedWalletsForFioAddress } from './util'
+import { refreshConnectedWalletsForFioAddress, refreshFioNames } from './util'
 
 export const createFioWallet =
   () =>
@@ -26,20 +25,8 @@ export const refreshAllFioAddresses =
     const state = getState()
     const { currencyWallets } = state.core.account
     const fioWallets: EdgeCurrencyWallet[] = checkExpiredWallets.length !== 0 ? checkExpiredWallets : state.ui.wallets.fioWallets
-    const fioWalletsById: { [string]: EdgeCurrencyWallet } = {}
-    let fioAddresses: FioAddress[] = []
-    let fioDomains: FioDomain[] = []
 
-    if (fioWallets != null) {
-      for (const wallet of fioWallets) {
-        const walletId = wallet.id
-        const walletFioAddresses = await wallet.otherMethods.getFioAddresses()
-        fioAddresses = [...fioAddresses, ...walletFioAddresses.map(({ name, expiration }) => ({ name, expiration, walletId }))]
-        const walletFioDomains = await wallet.otherMethods.getFioDomains()
-        fioDomains = [...fioDomains, ...walletFioDomains.map(({ name, expiration, isPublic }) => ({ name, expiration, isPublic, walletId }))]
-        fioWalletsById[walletId] = wallet
-      }
-    }
+    const { fioAddresses, fioDomains, fioWalletsById } = await refreshFioNames(fioWallets)
 
     window.requestAnimationFrame(() => {
       dispatch({
@@ -51,14 +38,13 @@ export const refreshAllFioAddresses =
         data: { fioDomains }
       })
     })
-
     if (checkExpiredWallets.length !== 0) return dispatch(checkExpiredFioNames([...fioAddresses, ...fioDomains], fioWalletsById))
 
     const { connectedWalletsByFioAddress } = state.ui.fio
     const wallets = Object.keys(currencyWallets).map(walletKey => currencyWallets[walletKey])
-    for (const { name } of fioAddresses) {
+    for (const { name, walletId } of fioAddresses) {
       if (!connectedWalletsByFioAddress[name]) {
-        const fioWallet = await findWalletByFioAddress(fioWallets, name)
+        const fioWallet = fioWalletsById[walletId]
         if (!fioWallet) continue
         const ccWalletMap = await refreshConnectedWalletsForFioAddress(name, fioWallet, wallets)
         dispatch({
