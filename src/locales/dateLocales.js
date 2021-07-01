47eeb5c67b9f73d643d7ec9889a42c5bbfa1a9f0
andreyvEze <andreyv@eze.tech>
2021-07-01 17:41:19 +0300

    Add date fns locales

---
+++ src/locales/dateLocales.js
@@ -0,0 +1,29 @@
+import de from 'date-fns/locale/de'
+import enGB from 'date-fns/locale/en-GB'
+import enUS from 'date-fns/locale/en-US'
+import es from 'date-fns/locale/es'
+import fr from 'date-fns/locale/fr'
+import it from 'date-fns/locale/it'
+import ja from 'date-fns/locale/ja'
+import ko from 'date-fns/locale/ko'
+import pt from 'date-fns/locale/pt'
+import ru from 'date-fns/locale/ru'
+import vi from 'date-fns/locale/vi'
+import zhCN from 'date-fns/locale/zh-CN'
+import zhTW from 'date-fns/locale/zh-TW'
+
+export const locales = {
+  de,
+  'en-US': enUS,
+  'en-GB': enGB,
+  es,
+  fr,
+  it,
+  ja,
+  ko,
+  pt,
+  ru,
+  vi,
+  'zh-CN': zhCN,
+  'zh-TW': zhTW
+}
