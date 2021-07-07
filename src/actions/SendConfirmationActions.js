0144f503271299825073fcab040a3dd3a146aa10
andreyvEze <andreyv@eze.tech>
2021-07-07 16:16:21 +0300

    Check only for empty string amount

--- src/actions/SendConfirmationActions.js
+++ src/actions/SendConfirmationActions.js
@@ -78,9 +78,7 @@ export const sendConfirmationUpdateTx =
       data: { spendInfo, authRequired }
     })
 
-    if (guiMakeSpendInfo.nativeAmount == null || guiMakeSpendInfo.nativeAmount === '') {
-      return
-    }
+    if (guiMakeSpendInfo.nativeAmount === '') return
 
     if (maxSpendSet && isFeeChanged) {
       return dispatch(updateMaxSpend(walletId, selectedCurrencyCode || state.ui.wallets.selectedCurrencyCode, guiMakeSpendInfoClone))
@@ -446,7 +444,7 @@ export const updateTransactionAmount =
       data: false
     })
 
-    if (guiMakeSpendInfo.nativeAmount == null || guiMakeSpendInfo.nativeAmount === '') {
+    if (guiMakeSpendInfo.nativeAmount === '') {
       return
     }
 
