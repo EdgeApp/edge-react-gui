70bac0c0989f7d09809a29053ef6e07bc4b0ed5a
Denis Boldyrev <denisboldyrev@Deniss-MacBook-Pro.local>
2021-07-21 14:35:07 +0300

    Update Fade component to use fade animation hook

--- src/components/themed/Fade.js
+++ src/components/themed/Fade.js
@@ -1,55 +1,25 @@
 // @flow
 
 import * as React from 'react'
-import Animated, { interpolate, runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+import Animated from 'react-native-reanimated'
 
-import { useEffect, useRef, useState } from '../../types/reactHooks.js'
+import { useFadeAnimation } from '../../hooks/animations/useFadeAnimation'
 
 type Props = {
-  visible: boolean,
   children: React.Node,
+
+  // True to make the contents visible:
+  visible: boolean,
+
+  // Animation duration, in ms:
+  duration?: number,
+
+  // No fade in animation on first render:
   noFadeIn?: boolean
 }
 
-export const Fade = ({ visible: propsVisible, children, noFadeIn }: Props) => {
-  const firstRender = useRef(true)
-  const opacity = useSharedValue(noFadeIn ? 0.5 : 0)
-  const [visible, setVisible] = useState<boolean>(propsVisible)
-  const [prevVisible, setPrevVisible] = useState<boolean>(propsVisible)
-
-  const animate = (toValue: number) => {
-    if (toValue === 0.5) setVisible(true)
-
-    opacity.value = withTiming(
-      toValue,
-      {
-        duration: 500
-      },
-      (isComplete: boolean) => {
-        if (!isComplete) return
-
-        runOnJS(setVisible)(toValue === 0.5)
-      }
-    )
-  }
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => animate(propsVisible ? 0.5 : 0), [])
-
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false
-      return
-    }
-
-    if (propsVisible !== prevVisible) {
-      animate(propsVisible ? 0.5 : 1)
-      setPrevVisible(propsVisible)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [propsVisible])
-
-  const style = useAnimatedStyle(() => ({ opacity: interpolate(opacity.value, [0, 0.5, 1], [0, 1, 0]) }))
+export const Fade = ({ children, duration, visible, noFadeIn }: Props) => {
+  const style = useFadeAnimation(visible, { noFadeIn, duration })
 
   return (
     <Animated.View style={style} pointerEvents={visible ? 'auto' : 'none'}>
