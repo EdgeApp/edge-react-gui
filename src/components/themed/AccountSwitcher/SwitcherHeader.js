03bbc4fde3931e9e5f0e3287940f32532d08843a
Alexandr Axenov <alex0735070005@gmail.com>
2021-08-03 06:55:35 +0300

    Implemented switcher account components

---
+++ src/components/themed/AccountSwitcher/SwitcherHeader.js
@@ -0,0 +1,49 @@
+// @flow
+
+import * as React from 'react'
+import { View } from 'react-native'
+import Feather from 'react-native-vector-icons/Feather'
+
+import { Fontello } from '../../../assets/vector'
+import { type Theme, cacheStyles, useTheme } from '../../services/ThemeContext'
+import { EdgeText } from '../EdgeText'
+
+export type Props = {
+  username: string
+}
+
+export const SwitcherHeader = (props: Props) => {
+  const theme = useTheme()
+  const styles = getStyles(theme)
+
+  return (
+    <View style={styles.header}>
+      <View style={styles.iconUser}>
+        <Fontello name="edge.logo" size={theme.rem(1.5)} color={theme.iconPanel} />
+      </View>
+      <View style={styles.textContainer}>
+        <EdgeText style={styles.text}>{props.username}</EdgeText>
+      </View>
+      <Feather name="chevron-down" color={theme.iconPanel} size={theme.rem(1.5)} />
+    </View>
+  )
+}
+
+const getStyles = cacheStyles((theme: Theme) => ({
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: theme.rem(2)
+  },
+  iconUser: {
+    marginRight: theme.rem(1.5)
+  },
+  textContainer: {
+    marginRight: 'auto'
+  },
+  text: {
+    fontFamily: theme.fontFaceBold
+  }
+}))
---
+++ src/components/themed/AccountSwitcher/SwitcherList.js
@@ -0,0 +1,55 @@
+// @flow
+
+import * as React from 'react'
+import { ScrollView, TouchableHighlight, View } from 'react-native'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+
+import { type Theme, cacheStyles, useTheme } from '../../services/ThemeContext'
+import { EdgeText } from '../EdgeText'
+export type Props = {
+  onSwitchAccount: (username: string) => void,
+  deleteLocalAccount: (username: string) => void,
+  usernames: string[]
+}
+
+export const SwitcherList = (props: Props) => {
+  const { onSwitchAccount, deleteLocalAccount, usernames } = props
+
+  const theme = useTheme()
+  const styles = getStyles(theme)
+  return (
+    <ScrollView style={styles.container}>
+      {usernames.map((username: string) => (
+        <View key={username} style={styles.row}>
+          <TouchableHighlight style={styles.textContainer} onPress={() => onSwitchAccount(username)}>
+            <EdgeText style={styles.text}>{username}</EdgeText>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => deleteLocalAccount(username)}>
+            <View /* Hack, do not remove */>
+              <MaterialIcon size={theme.rem(1.5)} name="close" color={theme.iconPanel} />
+            </View>
+          </TouchableHighlight>
+        </View>
+      ))}
+    </ScrollView>
+  )
+}
+
+const getStyles = cacheStyles((theme: Theme) => ({
+  container: {
+    alignSelf: 'stretch'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: theme.rem(2.5),
+    paddingLeft: theme.rem(2)
+  },
+  textContainer: {
+    marginRight: theme.rem(0.5)
+  },
+  text: {
+    fontFamily: theme.fontFaceBold
+  }
+}))
