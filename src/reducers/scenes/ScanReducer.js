cf63672f198044bbcaf334673ccd89fb189478ba
William Swanson <swansontec@gmail.com>
2021-08-05 16:46:08 -0700

    Flatten away other scan reducers

--- src/reducers/scenes/ScanReducer.js
+++ src/reducers/scenes/ScanReducer.js
@@ -4,8 +4,6 @@ import { type Reducer, combineReducers } from 'redux'
 
 import { type Action } from '../../types/reduxTypes.js'
 import { type PrivateKeyModalState, privateKeyModal } from '../PrivateKeyModalReducer.js'
-import { scanEnabled } from '../ScanEnabledReducer.js'
-import { torchEnabled } from '../TorchEnabledReducer.js'
 
 export type ScanState = {
   +torchEnabled: boolean,
@@ -15,6 +13,20 @@ export type ScanState = {
 
 export const scan: Reducer<ScanState, Action> = combineReducers({
   privateKeyModal,
-  scanEnabled,
-  torchEnabled
+
+  scanEnabled(state = false, action: Action): boolean {
+    switch (action.type) {
+      case 'ENABLE_SCAN':
+      case 'PRIVATE_KEY_MODAL/SWEEP_PRIVATE_KEY_FAIL':
+      case 'PRIVATE_KEY_MODAL/SWEEP_PRIVATE_KEY_SUCCESS':
+        return true
+      case 'DISABLE_SCAN':
+        return false
+    }
+    return state
+  },
+
+  torchEnabled(state = false, action: Action): boolean {
+    return action.type === 'TOGGLE_ENABLE_TORCH' ? !state : state
+  }
 })
