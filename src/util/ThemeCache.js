877e2215f2470e6e49f1a14da4ab5af3b5a6b36a
Matthew <matthew@edge.app>
2021-08-28 00:35:45 -0700

    Always return local file to use as the background image.
    In the background, downloadFile will determine if the local file matches the remote file and will download a new copy if necessary.

--- src/util/ThemeCache.js
+++ src/util/ThemeCache.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { asMap, asNumber, asObject } from 'cleaners'
+import { asMap, asNumber, asObject, uncleaner } from 'cleaners'
 import { Disklet } from 'disklet'
 import { ImageSourcePropType, Platform } from 'react-native'
 import RNFS from 'react-native-fs'
@@ -11,6 +11,7 @@ const THEME_CACHE_FILE_NAME = 'themeCache.json'
 const EDGE_CONTENT_SERVER = 'https://content.edge.app'
 
 const asThemeCache = asMap(asObject({ start: asNumber, expiration: asNumber }))
+const wasThemeCache = uncleaner(asThemeCache)
 type ThemeCache = $Call<typeof asThemeCache>
 
 type ItemTimestamps = { start: number, expiration: number }
@@ -20,7 +21,10 @@ async function getThemeCache(disklet: Disklet): Promise<ThemeCache> {
   return asThemeCache(JSON.parse(data))
 }
 
-const setThemeCache = async (disklet: Disklet, data: ThemeCache): Promise<void> => await disklet.setText(THEME_CACHE_FILE_NAME, JSON.stringify(data))
+async function setThemeCache(disklet: Disklet, data: ThemeCache): Promise<void> {
+  const text = JSON.stringify(wasThemeCache(data))
+  await disklet.setText(THEME_CACHE_FILE_NAME, text)
+}
 
 const getThemeItemTimestamps = async (url: string): Promise<ItemTimestamps> => {
   const response = await fetch(url, { method: 'HEAD' })
@@ -30,37 +34,40 @@ const getThemeItemTimestamps = async (url: string): Promise<ItemTimestamps> => {
   return { start: Date.parse(start), expiration: Date.parse(expiration) }
 }
 
-export async function getBackgroundImageFromCDN(disklet: Disklet): Promise<ImageSourcePropType> {
+const downloadFile = async (disklet: Disklet, fromUrl: string, toFile: string): Promise<void> => {
+  // See if the server even has an image for us in the first place:
+  const cache: ThemeCache = await getThemeCache(disklet).catch(() => ({}))
+  const { start, expiration } = await getThemeItemTimestamps(fromUrl)
+  if (expiration < Date.now()) return
+  if (cache[fromUrl] != null && cache[fromUrl].start === start && cache[fromUrl].expiration === expiration) return
+
+  // Download file whenever local file timestamps do not equal remote file timestamps
+  const download = RNFS.downloadFile({ fromUrl, toFile })
+  const status = await download.promise
+  if (status.statusCode !== 200) throw new Error('Failed to download')
+  cache[fromUrl] = { start, expiration }
+  await setThemeCache(disklet, cache)
+}
+
+export async function getBackgroundImage(disklet: Disklet, fallback: ImageSourcePropType): Promise<ImageSourcePropType> {
   const BACKGROUND_IMAGE_FILE_NAME = 'login_bg.gif'
   const BACKGROUND_IMAGE_URL = `${EDGE_CONTENT_SERVER}/${BACKGROUND_IMAGE_FILE_NAME}`
   const BACKGROUND_IMAGE_LOCAL_URI = `file://${directory}/${BACKGROUND_IMAGE_FILE_NAME}`
   const now = Date.now()
+  let image = fallback
 
   const cache: ThemeCache = await getThemeCache(disklet).catch(() => ({}))
-  if (cache[BACKGROUND_IMAGE_URL] == null) {
-    cache[BACKGROUND_IMAGE_URL] = { cachedTimestamp: now }
+  if (
+    cache[BACKGROUND_IMAGE_URL] != null &&
+    cache[BACKGROUND_IMAGE_URL].start < now &&
+    cache[BACKGROUND_IMAGE_URL].expiration > now &&
+    (await RNFS.exists(BACKGROUND_IMAGE_LOCAL_URI))
+  ) {
+    image = { uri: BACKGROUND_IMAGE_LOCAL_URI }
   }
-
-  try {
-    const timestamps = await getThemeItemTimestamps(BACKGROUND_IMAGE_URL)
-    if (now > timestamps.start && now < timestamps.expiration) {
-      if (
-        cache[BACKGROUND_IMAGE_URL].cachedTimestamp > timestamps.start &&
-        cache[BACKGROUND_IMAGE_URL].cachedTimestamp < timestamps.expiration &&
-        (await RNFS.exists(BACKGROUND_IMAGE_LOCAL_URI))
-      ) {
-        return { uri: BACKGROUND_IMAGE_LOCAL_URI }
-      }
-
-      await RNFS.downloadFile({ fromUrl: BACKGROUND_IMAGE_URL, toFile: BACKGROUND_IMAGE_LOCAL_URI })
-      cache[BACKGROUND_IMAGE_URL].cachedTimestamp = now
-      await setThemeCache(disklet, cache)
-      return { uri: BACKGROUND_IMAGE_LOCAL_URI }
-    } else {
-      throw new Error('CDN file expired.')
-    }
-  } catch (e) {
-    console.warn(`Error accessing CDN at ${BACKGROUND_IMAGE_URL} or local cache`)
-    throw e
-  }
+  // Always return existing local file but query and download new remote file in the background
+  downloadFile(disklet, BACKGROUND_IMAGE_URL, BACKGROUND_IMAGE_LOCAL_URI).catch(() => {
+    console.warn(`Error downloading ${BACKGROUND_IMAGE_LOCAL_URI}`)
+  })
+  return image
 }
