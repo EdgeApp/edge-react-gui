dd098e7ebdda3db0c399705e49c271aa78d8cec5
Art Manalili <artmanalili@gmail.com>
2021-11-11 19:14:56 +0800

    Added flags to show the scan modal buttons

--- src/components/modals/ScanModal.js
+++ src/components/modals/ScanModal.js
@@ -16,7 +16,7 @@ import type { PermissionStatus } from '../../reducers/PermissionsReducer'
 import { useEffect } from '../../types/reactHooks.js'
 import { connect } from '../../types/reactRedux.js'
 import { QrPeephole } from '../common/QrPeephole.js'
-import { showError, showWarning } from '../services/AirshipInstance.js'
+import { showError, showWarning } from '../services/AirshipInstance'
 import { requestPermission } from '../services/PermissionsManager'
 import { type Theme, type ThemeProps, cacheStyles, withTheme } from '../services/ThemeContext.js'
 import { EdgeText } from '../themed/EdgeText.js'
@@ -26,7 +26,9 @@ import { SceneHeader } from '../themed/SceneHeader.js'
 
 type OwnProps = {
   bridge: AirshipBridge<string | void>,
-  title: string
+  title: string,
+  isFlash?: boolean,
+  isAlbum?: boolean
 }
 
 type StateProps = {
@@ -43,7 +45,19 @@ type DispatchProps = {
 type Props = OwnProps & StateProps & DispatchProps & ThemeProps
 
 const Component = (props: Props) => {
-  const { bridge, theme, title, enableScan, disableScan, scanEnabled, toggleEnableTorch, torchEnabled, cameraPermission } = props
+  const {
+    bridge,
+    theme,
+    title,
+    enableScan,
+    disableScan,
+    scanEnabled,
+    toggleEnableTorch,
+    torchEnabled,
+    cameraPermission,
+    isAlbum = true,
+    isFlash = true
+  } = props
   const styles = getStyles(theme)
 
   const { width: windowWidth, height: windowHeight } = useWindowSize()
@@ -170,14 +184,18 @@ const Component = (props: Props) => {
             <View style={[styles.inner, { flexDirection: isLandscape ? 'row' : 'column' }]}>
               <View style={styles.peepholeSpace} onLayout={handleLayoutPeepholeSpace} />
               <View style={[styles.buttonsContainer, { flexDirection: isLandscape ? 'column-reverse' : 'row' }]}>
-                <TouchableOpacity style={styles.iconButton} onPress={handleFlash}>
-                  <Ionicon style={styles.icon} name="flash-outline" size={theme.rem(1.5)} />
-                  <EdgeText>{s.strings.fragment_send_flash}</EdgeText>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.iconButton} onPress={handleAlbum}>
-                  <Ionicon style={styles.icon} name="albums-outline" size={theme.rem(1.5)} />
-                  <EdgeText>{s.strings.fragment_send_album}</EdgeText>
-                </TouchableOpacity>
+                {isFlash ? (
+                  <TouchableOpacity style={styles.iconButton} onPress={handleFlash}>
+                    <Ionicon style={styles.icon} name="flash-outline" size={theme.rem(1.5)} />
+                    <EdgeText>{s.strings.fragment_send_flash}</EdgeText>
+                  </TouchableOpacity>
+                ) : null}
+                {isAlbum ? (
+                  <TouchableOpacity style={styles.iconButton} onPress={handleAlbum}>
+                    <Ionicon style={styles.icon} name="albums-outline" size={theme.rem(1.5)} />
+                    <EdgeText>{s.strings.fragment_send_album}</EdgeText>
+                  </TouchableOpacity>
+                ) : null}
               </View>
             </View>
           </View>
