63e2947e2322ab42f14c462b769e6b4486b787ad
Matthew <matthew@edge.app>
2021-11-12 16:09:12 -0800

    Only use native crypto amounts in WcSmartContractModal
    The fiat and crypto display components need denomination settings so it's better that they handle the conversions

--- src/components/themed/CryptoFiatAmountTile.js
+++ src/components/themed/CryptoFiatAmountTile.js
@@ -1,15 +1,19 @@
 // @flow
+import { bns } from 'biggystring'
 import * as React from 'react'
 
 import { MAX_CRYPTO_AMOUNT_CHARACTERS } from '../../constants/WalletAndCurrencyConstants.js'
 import { formatNumber } from '../../locales/intl.js'
+import { getDisplayDenomination } from '../../selectors/DenominationSelectors'
+import { useSelector } from '../../types/reactRedux'
+import { DECIMAL_PRECISION } from '../../util/utils'
 import { type Theme, cacheStyles, useTheme } from '../services/ThemeContext.js'
 import { EdgeText } from './EdgeText'
 import { FiatText } from './FiatText'
 import { Tile } from './Tile.js'
 
 type Props = {
-  cryptoAmount: string | number,
+  cryptoAmount: string,
   cryptoCurrencyCode: string,
   isoFiatCurrencyCode: string,
   maxCryptoChars?: number,
@@ -19,11 +23,17 @@ type Props = {
 export const CryptoFiatAmountTile = (props: Props) => {
   const { title, cryptoAmount, cryptoCurrencyCode, isoFiatCurrencyCode, maxCryptoChars } = props
   const styles = getStyles(useTheme())
+
+  const { cryptoName, cryptoMultiplier } = useSelector(state => {
+    const { name, multiplier } = getDisplayDenomination(state, cryptoCurrencyCode)
+    return { cryptoName: name, cryptoMultiplier: multiplier }
+  })
   // Default to 10 displayed chars for crypto amount
-  const fmtCryptoAmount = formatNumber(cryptoAmount, { toFixed: maxCryptoChars === undefined ? MAX_CRYPTO_AMOUNT_CHARACTERS : maxCryptoChars })
-  const cryptoAmountText = `${fmtCryptoAmount} ${cryptoCurrencyCode} `
+  const cryptoAmountDenom = bns.div(cryptoAmount, cryptoMultiplier, DECIMAL_PRECISION)
+  const fmtCryptoAmount = formatNumber(cryptoAmountDenom, { toFixed: maxCryptoChars === undefined ? MAX_CRYPTO_AMOUNT_CHARACTERS : maxCryptoChars })
+  const cryptoAmountText = `${fmtCryptoAmount} ${cryptoName} `
   // Fiat amount is always positive for this specific tile
-  const absCryptoAmount = Math.abs(parseFloat(cryptoAmount))
+  const absCryptoAmount = bns.abs(cryptoAmount)
   const fiatAmount = (
     <FiatText cryptoAmount={absCryptoAmount} cryptoCurrencyCode={cryptoCurrencyCode} isoFiatCurrencyCode={isoFiatCurrencyCode} parenthesisEnclosed />
   )
