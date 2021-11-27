8ce7d05462de4eca6d102ddab0f1463a7c9b0b9a
Jon Tzeng <jon@edge.capp>
2021-11-26 16:14:57 -0800

    Remove legacy ControlPanel

--- src/modules/UI/components/ControlPanel/action.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import { showError } from '../../../../components/services/AirshipInstance.js'
-import type { Dispatch, GetState } from '../../../../types/reduxTypes.js'
-
-export const deleteLocalAccount = (username: string) => (dispatch: Dispatch, getState: GetState) => {
-  const state = getState()
-  return state.core.context.deleteLocalAccount(username).catch(showError)
-}
