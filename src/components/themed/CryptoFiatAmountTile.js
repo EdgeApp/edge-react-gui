ff7595ab0ce81c75abf58ef4da4b973480cb2d52
eliran zach <thehobbit85@gmail.com>
2021-11-30 21:07:09 -0800

    Refactor the FiatText Component

--- src/components/themed/CryptoFiatAmountTile.js
+++ src/components/themed/CryptoFiatAmountTile.js
@@ -4,7 +4,7 @@ import * as React from 'react'
 
 import { MAX_CRYPTO_AMOUNT_CHARACTERS } from '../../constants/WalletAndCurrencyConstants.js'
 import { formatNumber } from '../../locales/intl.js'
-import { getDisplayDenomination } from '../../selectors/DenominationSelectors'
+import { getExchangeDenomination } from '../../selectors/DenominationSelectors'
 import { useSelector } from '../../types/reactRedux'
 import { DECIMAL_PRECISION } from '../../util/utils'
 import { type Theme, cacheStyles, useTheme } from '../services/ThemeContext.js'
@@ -24,14 +24,22 @@ export const CryptoFiatAmountTile = (props: Props) => {
   const { title, nativeCryptoAmount, cryptoCurrencyCode, isoFiatCurrencyCode, maxCryptoChars } = props
   const styles = getStyles(useTheme())
 
-  const { cryptoName, cryptoMultiplier } = useSelector(state => {
-    const { name, multiplier } = getDisplayDenomination(state, cryptoCurrencyCode)
-    return { cryptoName: name, cryptoMultiplier: multiplier }
+  const { cryptoName, cryptoDenomMult } = useSelector(state => {
+    const { name, multiplier } = getExchangeDenomination(state, cryptoCurrencyCode)
+
+    return { cryptoName: name, cryptoDenomMult: multiplier }
   })
+
+  // Convert wallet native denomination to exchange denomination
+  // Does NOT take into account display denomination settings here,
+  // i.e. sats, bits, etc.
+  const cryptoAmountDenom = bns.div(nativeCryptoAmount, cryptoDenomMult, DECIMAL_PRECISION)
+
   // Default to 10 displayed chars for crypto amount
-  const cryptoAmountDenom = bns.div(nativeCryptoAmount, cryptoMultiplier, DECIMAL_PRECISION)
   const fmtCryptoAmount = formatNumber(cryptoAmountDenom, { toFixed: maxCryptoChars === undefined ? MAX_CRYPTO_AMOUNT_CHARACTERS : maxCryptoChars })
+
   const cryptoAmountText = `${fmtCryptoAmount} ${cryptoName} `
+
   // Fiat amount is always positive for this specific tile
   const absCryptoAmount = bns.abs(nativeCryptoAmount)
   const fiatAmount = (
