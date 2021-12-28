b91b07d67872341f555040852f1ae7bafc6b994d
Matthew <matthew@edge.app>
2021-12-27 20:23:03 -0800

    Disable Exolix

--- src/util/corePlugins.js
+++ src/util/corePlugins.js
@@ -88,7 +88,7 @@ export const ratePlugins = {
 export const swapPlugins = {
   changelly: ENV.CHANGELLY_INIT,
   changenow: ENV.CHANGE_NOW_INIT,
-  exolix: ENV.EXOLIX_INIT,
+  exolix: false,
   foxExchange: ENV.FOX_INIT,
   godex: ENV.GODEX_INIT,
   // shapeshift: ENV.SHAPESHIFT_INIT,
