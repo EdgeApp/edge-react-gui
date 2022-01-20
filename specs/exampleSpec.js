18e7815acca4b2c831ba162a71fa2a56c45219ff
eliran zach <thehobbit85@gmail.com>
2022-01-20 02:40:41 -0800

    Example of a simple login test

--- specs/exampleSpec.js
+++ specs/exampleSpec.js
@@ -1,5 +1,10 @@
+import { fastLogin } from './helpers.js'
+
 export default function (spec) {
   spec.describe('Logging in', function () {
-    spec.it('Login', async function () {})
+    spec.it('Login', async function () {
+      const login = await spec.findComponent('Login.Login')
+      await fastLogin(login)
+    })
   })
 }
