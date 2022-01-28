43ee74a5622521513cb0e5f8fb3e6bcac9b1b23a
Samuel Holmes <sam@edge.app>
2022-01-27 22:23:52 -0800

    Integrate stake/unstake transactions

--- src/components/scenes/StakingChangeScene.js
+++ src/components/scenes/StakingChangeScene.js
@@ -1,18 +1,28 @@
 // @flow
 
+import { bns } from 'biggystring'
+import type { EdgeCurrencyWallet, EdgeDenomination } from 'edge-core-js'
 import * as React from 'react'
 import { Image, View } from 'react-native'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { sprintf } from 'sprintf-js'
 
+import { refreshAllFioAddresses } from '../../actions/FioAddressActions'
 import fioLogo from '../../assets/images/fio/fio_logo.png'
+import { SPECIAL_CURRENCY_INFO, STAKING_BALANCES } from '../../constants/WalletAndCurrencyConstants'
+import { formatDate, formatNumber } from '../../locales/intl'
 import s from '../../locales/strings.js'
 import { Slider } from '../../modules/UI/components/Slider/Slider'
-import { useState } from '../../types/reactHooks'
-import type { RouteProp } from '../../types/routerTypes'
+import { getDisplayDenomination, getExchangeDenomination } from '../../selectors/DenominationSelectors'
+import { convertCurrency } from '../../selectors/WalletSelectors'
+import { useEffect, useState } from '../../types/reactHooks'
+import { connect } from '../../types/reactRedux'
+import type { NavigationProp, RouteProp } from '../../types/routerTypes'
+import type { FioAddress } from '../../types/types'
+import { convertNativeToDenomination } from '../../util/utils'
 import { SceneWrapper } from '../common/SceneWrapper.js'
 import { FlipInputModal } from '../modals/FlipInputModal'
-import { Airship } from '../services/AirshipInstance'
+import { Airship, showToast } from '../services/AirshipInstance'
 import { type ThemeProps, cacheStyles, withTheme } from '../services/ThemeContext.js'
 import { EdgeText } from '../themed/EdgeText'
 import { ModalCloseArrow, ModalMessage, ModalTitle } from '../themed/ModalParts'
@@ -21,31 +31,75 @@ import { ThemedModal } from '../themed/ThemedModal'
 import { Tile } from '../themed/Tile.js'
 
 type OwnProps = {
-  route: RouteProp<'stakingChange'>
+  route: RouteProp<'stakingChange'>,
+  navigation: NavigationProp<'stakingChange'>
 }
-type Props = OwnProps & ThemeProps
+
+type StateProps = {
+  stakingBalances: {
+    [cCode: string]: {
+      native: string,
+      crypto: string,
+      fiat: string
+    }
+  },
+  currencyWallet: EdgeCurrencyWallet,
+  currencyDenomination: EdgeDenomination,
+  fioAddresses: FioAddress[]
+}
+type DispatchProps = {
+  refreshAllFioAddresses: () => void
+}
+type Props = StateProps & OwnProps & DispatchProps & ThemeProps
 
 export const StakingChangeSceneComponent = (props: Props) => {
   const {
     theme,
     route: {
       params: { change, currencyCode, walletId }
-    }
+    },
+    currencyWallet,
+    currencyDenomination,
+    stakingBalances,
+    navigation,
+    fioAddresses
   } = props
   const styles = getStyles(theme)
 
+  const [nativeAmount, setNativeAmount] = useState('0')
   const [exchangeAmount, setExchangeAmount] = useState('0')
   const [error, setError] = useState(null)
-  const [loading] = useState(false)
-  const [tx] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [tx, setTx] = useState(null)
   const sliderDisabled = tx == null || exchangeAmount === '0' || error != null
 
   const onAmountChanged = (nativeAmount: string, exchangeAmount: string) => {
     setExchangeAmount(exchangeAmount)
+    setNativeAmount(nativeAmount)
   }
 
   const onMaxSet = () => {
-    // todo
+    switch (change) {
+      case 'add': {
+        currencyWallet
+          .getMaxSpendable({
+            currencyCode,
+            spendTargets: [{ nativeAmount: '', otherParams: {}, publicAddress: '' }]
+          })
+          .then(nativeAmount => {
+            onAmountChanged(nativeAmount, bns.add(convertNativeToDenomination(currencyDenomination.multiplier)(nativeAmount), '0'))
+          })
+        break
+      }
+      case 'remove': {
+        onAmountChanged(
+          stakingBalances[`${currencyCode}${STAKING_BALANCES.staked}`].native,
+          stakingBalances[`${currencyCode}${STAKING_BALANCES.staked}`].crypto
+        )
+        break
+      }
+      default:
+    }
   }
 
   const onFeesChange = () => {
@@ -53,7 +107,21 @@ export const StakingChangeSceneComponent = (props: Props) => {
   }
 
   const handleSubmit = async () => {
-    // todo
+    if (tx == null) return setError(s.strings.create_wallet_account_error_sending_transaction)
+    setLoading(true)
+    try {
+      const signedTx = await currencyWallet.signTx(tx)
+      await currencyWallet.broadcastTx(signedTx)
+      const messages = {
+        add: s.strings.staking_success,
+        remove: s.strings.staking_unstake_success
+      }
+      showToast(messages[change])
+      navigation.goBack()
+    } catch (e) {
+      setError(e.message)
+      setLoading(false)
+    }
   }
 
   const handleAmount = () => {
@@ -85,6 +153,48 @@ export const StakingChangeSceneComponent = (props: Props) => {
     })
   }
 
+  useEffect(() => {
+    props.refreshAllFioAddresses()
+  }, [])
+
+  // Make spend transaction after amount change
+  useEffect(() => {
+    if (nativeAmount === '0') return
+
+    let abort = false
+
+    const actionName = SPECIAL_CURRENCY_INFO[currencyCode].stakeActions != null ? SPECIAL_CURRENCY_INFO[currencyCode].stakeActions[change] : ''
+    currencyWallet
+      .makeSpend({
+        spendTargets: [
+          {
+            nativeAmount,
+            publicAddress: ''
+          }
+        ],
+        otherParams: {
+          action: {
+            name: actionName,
+            params: {
+              fioAddress: fioAddresses.length > 0 ? fioAddresses[0].name : ''
+            }
+          }
+        }
+      })
+      .then(tx => {
+        if (abort) return
+        setError(null)
+        setTx(tx)
+      })
+      .catch(error => {
+        if (abort) return
+        setError(error)
+      })
+    return () => {
+      abort = true
+    }
+  }, [nativeAmount])
+
   const renderAdd = () => {
     return (
       <>
@@ -103,7 +213,16 @@ export const StakingChangeSceneComponent = (props: Props) => {
   }
 
   const renderRemove = () => {
-    const unlockDate = 'Aug 17, 2021'
+    let unlockDate = ''
+    if (SPECIAL_CURRENCY_INFO[currencyCode].stakeLockPeriod != null) {
+      const DAY_INTERVAL = 1000 * 60 * 60 * 24
+      const beginingOfGmtDay = Math.floor(Date.now() / DAY_INTERVAL) * DAY_INTERVAL
+      unlockDate = formatDate(new Date(beginingOfGmtDay + SPECIAL_CURRENCY_INFO[currencyCode].stakeLockPeriod), true)
+    }
+    let estReward = '0'
+    if (tx != null && tx.otherParams != null && tx.otherParams.ui != null && tx.otherParams.ui.estReward != null) {
+      estReward = bns.add(convertNativeToDenomination(currencyDenomination.multiplier)(tx.otherParams.ui.estReward), '0')
+    }
     return (
       <>
         <SceneHeader style={styles.sceneHeader} title={sprintf(s.strings.staking_change_remove_header, currencyCode)} underline withTopMargin>
@@ -112,6 +231,11 @@ export const StakingChangeSceneComponent = (props: Props) => {
         <Tile type="editable" title={s.strings.staking_change_remove_amount_title} onPress={handleAmount}>
           <EdgeText style={styles.amountText}>{exchangeAmount}</EdgeText>
         </Tile>
+        {estReward !== '0' && (
+          <Tile type="static" title={s.strings.staking_estimated_rewards}>
+            <EdgeText style={styles.amountText}>{estReward}</EdgeText>
+          </Tile>
+        )}
         <Tile type="questionable" title={s.strings.staking_change_remove_unlock_date} onPress={handleUnlockDate}>
           <EdgeText>{unlockDate}</EdgeText>
         </Tile>
@@ -177,4 +301,49 @@ const getStyles = cacheStyles(theme => ({
   }
 }))
 
-export const StakingChangeScene = withTheme(StakingChangeSceneComponent)
+export const StakingChangeScene = connect<StateProps, DispatchProps, OwnProps>(
+  (state, ownProps) => {
+    const {
+      route: {
+        params: { walletId, currencyCode }
+      }
+    } = ownProps
+    const currencyWallet = state.core.account.currencyWallets[walletId]
+    const guiWallet = state.ui.wallets.byId[walletId]
+    const stakingBalances = {}
+
+    const currencyDenomination = getDisplayDenomination(state, currencyWallet.currencyInfo.pluginId, currencyCode)
+    const defaultDenomination = getExchangeDenomination(state, currencyWallet.currencyInfo.pluginId, currencyCode)
+
+    if (SPECIAL_CURRENCY_INFO[currencyCode]?.isStakingSupported) {
+      for (const cCodeKey in STAKING_BALANCES) {
+        const stakingCurrencyCode = `${currencyCode}${STAKING_BALANCES[cCodeKey]}`
+
+        const stakingNativeAmount = guiWallet.nativeBalances[stakingCurrencyCode] || '0'
+        const stakingCryptoAmount: string = convertNativeToDenomination(currencyDenomination.multiplier)(stakingNativeAmount)
+        const stakingCryptoAmountFormat = formatNumber(bns.add(stakingCryptoAmount, '0'))
+
+        const stakingDefaultCryptoAmount = convertNativeToDenomination(defaultDenomination.multiplier)(stakingNativeAmount)
+        const stakingFiatBalance = convertCurrency(state, currencyCode, guiWallet.isoFiatCurrencyCode, stakingDefaultCryptoAmount)
+        const stakingFiatBalanceFormat = formatNumber(stakingFiatBalance && bns.gt(stakingFiatBalance, '0.000001') ? stakingFiatBalance : 0, { toFixed: 2 })
+
+        stakingBalances[stakingCurrencyCode] = {
+          native: stakingNativeAmount,
+          crypto: stakingCryptoAmountFormat,
+          fiat: stakingFiatBalanceFormat
+        }
+      }
+    }
+    return {
+      stakingBalances,
+      currencyWallet,
+      currencyDenomination,
+      fioAddresses: state.ui.scenes.fioAddress.fioAddresses
+    }
+  },
+  dispatch => ({
+    refreshAllFioAddresses() {
+      dispatch(refreshAllFioAddresses())
+    }
+  })
+)(withTheme(StakingChangeSceneComponent))
