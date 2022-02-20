8e9ea9c924e0e3c3d101d3c71277eed7ab80484b
Paul V Puey <paul@airbitz.co>
2022-02-20 13:57:07 -0800

    Add nonce/counter to ensure correct makeSpend is used
    This fixes a bug caused by the race-condition between concurrent
    `makeSpend` invocations.

--- src/actions/SendConfirmationActions.js
+++ src/actions/SendConfirmationActions.js
@@ -426,6 +426,8 @@ export const displayFeeAlert = async (currency: string, fee: string) => {
   return resolveValue === 'confirm'
 }
 
+let lastUpdateTransactionAmountNonce = 0
+
 export const updateTransactionAmount =
   (nativeAmount: string, exchangeAmount: string, walletId: string, currencyCode: string) => (dispatch: Dispatch, getState: GetState) => {
     const state = getState()
@@ -458,9 +460,14 @@ export const updateTransactionAmount =
       return
     }
 
+    // Fixes race-condition caused by concurrent makeSpend calls from each
+    // key stroke from user input
+    const nonce = ++lastUpdateTransactionAmountNonce
+
     coreWallet
       .makeSpend(spendInfo)
       .then(edgeTransaction => {
+        if (nonce < lastUpdateTransactionAmountNonce) return
         dispatch({
           type: 'UI/SEND_CONFIRMATION/UPDATE_TRANSACTION',
           data: {
@@ -472,6 +479,7 @@ export const updateTransactionAmount =
         })
       })
       .catch(error => {
+        if (nonce < lastUpdateTransactionAmountNonce) return
         let customError
 
         if (error.labelCode && coreWallet.currencyInfo?.defaultSettings?.errorCodes[error.labelCode] != null) {
