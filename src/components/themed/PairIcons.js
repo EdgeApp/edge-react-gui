6f20cb55522ab8003e062ceaa7e2b7d74c24dc2c
Art Manalili <artmanalili@gmail.com>
2022-03-16 10:31:22 +0800

    PairIcons Component

---
+++ src/components/themed/PairIcons.js
@@ -0,0 +1,42 @@
+// @flow
+
+import * as React from 'react'
+import { View } from 'react-native'
+import FastImage from 'react-native-fast-image'
+
+import rightArrow from '../../assets/images/rightArrow.png'
+import { type Theme, cacheStyles, useTheme } from '../services/ThemeContext.js'
+
+export function PairIcons({ icons }: { icons: string[] }): React.Node {
+  const styles = getStyles(useTheme())
+  const lastIcon = icons.pop()
+
+  return (
+    <View style={styles.container}>
+      {icons.map((icon, index) => (
+        <FastImage style={[styles.icon, index > 0 ? styles.consecutiveIcon : undefined]} source={{ uri: icon }} key={index} />
+      ))}
+      <FastImage style={styles.arrow} source={rightArrow} resizeMode="contain" />
+      <FastImage style={styles.icon} source={{ uri: lastIcon }} />
+    </View>
+  )
+}
+
+const getStyles = cacheStyles((theme: Theme) => ({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  icon: {
+    width: theme.rem(3),
+    height: theme.rem(3)
+  },
+  arrow: {
+    width: theme.rem(4),
+    height: theme.rem(1),
+    marginHorizontal: theme.rem(0.5)
+  },
+  consecutiveIcon: {
+    marginLeft: theme.rem(-0.5)
+  }
+}))
