1bd9c35ab6a5957820b23e1772172e0aa50c3867
Samuel Holmes <sam@edge.app>
2022-03-17 11:40:11 -0700

    Add stake-plugin types and stubs

---
+++ src/plugins/stake-plugins/types.js
@@ -0,0 +1,99 @@
+// @flow
+import type { EdgeCurrencyWallet, EdgeToken, JsonObject } from 'edge-core-js'
+
+// -----------------------------------------------------------------------------
+// Stake Policy
+// -----------------------------------------------------------------------------
+
+// Not sure if this is going be enough because how "LP-staking" works;
+// Need to figure out how we deal with LP pool asset-ratios.
+export type StakePolicy = {
+  // Perhaps we need a UUID for each policy?
+  stakePolicyId: string,
+
+  // A percentage number representing the yield per year
+  apy: number,
+
+  // The assets which must be staked
+  stakeAssets: {
+    [pluginId: string]: {
+      [tokenId: string]: boolean
+    }
+  },
+
+  // The assets which can be earned
+  rewardAssets: {
+    [pluginId: string]: {
+      [tokenId: string]: boolean
+    }
+  },
+
+  // Whether claim action is required to obtain reward
+  mustClaimRewards: boolean
+}
+
+// -----------------------------------------------------------------------------
+// Change Quote
+// -----------------------------------------------------------------------------
+
+export type ChangeQuoteRequest = {
+  action: 'stake' | 'unstake' | 'claim',
+  stakePolicyId: string,
+  tokenId: string,
+  nativeAmount: string,
+  wallet: EdgeCurrencyWallet
+}
+
+export type ChangeQuote = {
+  allocations: Array<{
+    allocationType: 'stake' | 'unstake' | 'claim' | 'fee',
+    tokenId: string,
+    nativeAmount: string
+  }>,
+
+  approve: () => Promise<void>
+}
+
+// -----------------------------------------------------------------------------
+// Stake Details
+// -----------------------------------------------------------------------------
+
+export type StakeDetailRequest = {
+  stakePolicyId: string,
+  wallet: EdgeCurrencyWallet
+}
+
+export type StakeDetails = {
+  allocations: Array<{
+    // The type of asset for this allocation
+    token: EdgeToken, // Possibly not needed
+
+    // The type of the allocation
+    allocationType: 'staked' | 'unstaked' | 'earned',
+    // Amount of the asset allocated
+    nativeAmount: string,
+
+    /*
+    A date/time when the allocation is available.
+    Example: earned allocations with a future date are not available,
+    but earned allocations with a past date are available to be earned.
+    For some tokens (e.g. FIO), there is no earned allocation; rather there is
+    an unstaked allocation which is locked until the date.
+    */
+    locktime?: Date,
+
+    // Feel free to add other weird coin states here.
+    // We can standardize them later if they are common:
+    otherParams?: JsonObject
+  }>
+}
+
+// -----------------------------------------------------------------------------
+// Stake Plugin
+// -----------------------------------------------------------------------------
+
+export type StakePlugin = {
+  getStakePolicies: () => Promise<StakePolicy[]>,
+  fetchChangeQuote: (request: ChangeQuoteRequest) => Promise<ChangeQuote>,
+  fetchStakeDetails: (request: StakeDetailRequest) => Promise<StakeDetails>
+}
