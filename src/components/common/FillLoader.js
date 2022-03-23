edea551b83b73da4e79270980d0546e26c14061f
Samuel Holmes <sam@edge.app>
2022-03-23 16:40:36 -0700

    Add FillLoader component
    This component is like `FullScreenLoader` accept it is embeddable and
    fills up the area where it is embedded.

---
+++ src/components/common/FillLoader.js
@@ -0,0 +1,33 @@
+// @flow
+
+import * as React from 'react'
+import { ActivityIndicator, StyleSheet, View, ViewPropTypes } from 'react-native'
+
+import { THEME } from '../../theme/variables/airbitz.js'
+
+type Props = {
+  indicatorStyles?: ViewPropTypes.style,
+  size?: 'large' | 'small'
+}
+
+export class FillLoader extends React.Component<Props> {
+  render() {
+    const { size, indicatorStyles } = this.props
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator color={THEME.COLORS.ACCENT_MINT} style={[styles.indicator, indicatorStyles]} size={size || 'large'} />
+      </View>
+    )
+  }
+}
+
+const rawStyles = {
+  loadingContainer: {
+    flex: 1
+  },
+  indicator: {
+    flex: 1,
+    alignSelf: 'center'
+  }
+}
+const styles: typeof rawStyles = StyleSheet.create(rawStyles)
