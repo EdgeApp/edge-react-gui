1b126a5554327cdad08f9346e542f51a86af54d6
Kurtis Casperson <kcasperson7@gmail.com>
2022-03-23 16:16:49 -0700

    Add cavy wrap function to native components

--- index.test.js
+++ index.test.js
@@ -1,24 +1,31 @@
 import 'react-native-gesture-handler'
 import './src/app.js'
 
-import { Tester, TestHookStore } from 'cavy'
+import { Tester, TestHookStore, wrap } from 'cavy'
 import React, { Component } from 'react'
 import * as ReactNative from 'react-native'
 import Animated from 'react-native-reanimated'
 
 import { name as appName } from './app.json'
 import ExampleSpec from './specs/exampleSpec'
+import MenuTabSpec from './specs/MenuTabSpec'
 import { App } from './src/components/App.js'
 
 // See https://github.com/software-mansion/react-native-reanimated/issues/1794#issuecomment-898393331
 Animated.addWhitelistedNativeProps({})
+
+// eslint-disable-next-line no-import-assign
+ReactNative.TouchableOpacity = wrap(ReactNative.TouchableOpacity)
+
+// eslint-disable-next-line no-import-assign
+ReactNative.Pressable = wrap(ReactNative.Pressable)
 
 const testHookStore = new TestHookStore()
 
 class AppWrapper extends Component {
   render() {
     return (
-      <Tester specs={[ExampleSpec]} store={testHookStore}>
+      <Tester specs={[ExampleSpec, MenuTabSpec]} store={testHookStore}>
         <App />
       </Tester>
     )
