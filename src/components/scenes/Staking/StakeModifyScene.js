c1fa176fafee200793aa4a1c18e60ed1925d6b5a
Jon Tzeng <jon@edge.app>
2022-03-23 19:23:15 -0700

    StakeModifyScene Updates
    -Remove unused imports
    -Misc refactoring/bugfixes
    -Implement FlipInput max button for unstake (not implemented for stake)
    -Map EditableAmountTile rendering
    -Fix scene title string

--- src/components/scenes/Staking/StakeModifyScene.js
+++ src/components/scenes/Staking/StakeModifyScene.js
@@ -6,13 +6,14 @@ import { sprintf } from 'sprintf-js'
 import s from '../../../locales/strings.js'
 import { Slider } from '../../../modules/UI/components/Slider/Slider.js'
 import { type ChangeQuote, type StakeDetails, makeStakePlugin } from '../../../plugins/stake-plugins'
+import { type AllocationType } from '../../../plugins/stake-plugins/types'
 import { getDisplayDenomination, getExchangeDenomination } from '../../../selectors/DenominationSelectors.js'
 import { useEffect, useState } from '../../../types/reactHooks.js'
 import { useSelector } from '../../../types/reactRedux'
 import type { RouteProp } from '../../../types/routerTypes'
 import { getCurrencyIcon } from '../../../util/CurrencyInfoHelpers.js'
 import { getWalletFiat } from '../../../util/CurrencyWalletHelpers.js'
-import { getAllocations, getRewardAllocation, getRewardAssetsName, getStakeAllocation, getStakeAssetsName, getStakeDetails } from '../../../util/stakeUtils.js'
+import { getRewardAllocation, getRewardAssetsName, getStakeAssetsName, getStakeDetails } from '../../../util/stakeUtils.js'
 import { SceneWrapper } from '../../common/SceneWrapper.js'
 import { FlipInputModal } from '../../modals/FlipInputModal.js'
 import { Airship } from '../../services/AirshipInstance.js'
@@ -23,7 +24,6 @@ import { EdgeText } from '../../themed/EdgeText.js'
 import { EditableAmountTile } from '../../themed/EditableAmountTile.js'
 import { IconTile } from '../../themed/IconTile'
 import { SceneHeader } from '../../themed/SceneHeader.js'
-import { Tile } from '../../themed/Tile.js'
 
 type Props = {
   route: RouteProp<'stakeModify'>
@@ -80,30 +80,24 @@ export const StakeModifyScene = (props: Props) => {
   })
 
   // Current Allocation Info
-  const [nativeAllocationAmount, setNativeAllocationAmount] = useState()
-  const [nativeRewardAmount, setNativeRewardAmount] = useState('0')
-  const [stakeDetails: StakeDetails, setStakeDetails] = useState()
+  const [stakeDetails: StakeDetails, setStakeDetails] = useState({
+    allocations: []
+  })
+  const [rewardAmount: string, setRewardAmount] = useState('0')
   useEffect(() => {
-    getStakeDetails(stakePlugin, stakePolicyId, currencyWallet)
-      .then(stakeDetails => {
-        setStakeDetails(stakeDetails)
-        const modificationMap = {
-          stake: ['stake'],
-          claim: ['earned'],
-          unstake: ['stake', 'earned']
-        }
-        return modificationMap.map(allocationType => getAllocations(stakeDetails, modificationMap[allocationType]))
+    getStakeDetails(stakePlugin, stakePolicyId, currencyWallet).then(stakeDetails => {
+      setStakeDetails(stakeDetails)
+      getRewardAllocation(stakeDetails).then(rewardAllocation => {
+        setRewardAmount(rewardAllocation === null ? '0' : rewardAllocation.nativeAmount)
       })
-      .then(detailAllocations => {
-        setNativeAllocationAmount(detailAllocations[0])
-      })
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currencyWallet, modification, stakePolicyId])
 
   // Handlers
-  const [pendingChangeQuote, setPendingChangeQuote] = useState()
+  const [pendingChangeQuote: ChangeQuote, setPendingChangeQuote] = useState({ allocations: [], approve: () => {} })
   const [nativeModAmount, setNativeModAmount] = useState('0')
   const [nativeFeeAmount, setNativeFeeAmount] = useState('0')
-
   const onAmountEdited = (flipNativeAmount: string, displayModAmount: string) => {
     if (!bns.eq(nativeModAmount, flipNativeAmount)) {
       setNativeModAmount(flipNativeAmount)
@@ -119,17 +113,21 @@ export const StakeModifyScene = (props: Props) => {
         })
         .then(changeQuote => {
           setPendingChangeQuote(changeQuote)
-          setNativeFeeAmount(changeQuote.allocations.find(allocation => allocation.allocationType === 'fee')?.nativeAmount)
+          setNativeFeeAmount(changeQuote.allocations.find(allocation => allocation.allocationType === 'fee')?.nativeAmount ?? '0')
         })
     }
   }
 
   const onMaxButtonPress = () => {
     // Allocation to modify is in native currency
-    if (currencyWallet.currencyInfo.currencyCode === stakeAssetsName) {
-      // TODO: V2
+    if (currencyWallet.currencyInfo.currencyCode === allocationToMod?.tokenId) {
+      // TODO: (V2) Set max amount minus fees if specifying native asset amount
     } else {
-      // TODO: V1
+      if (modification === 'unstake') {
+        setNativeModAmount(allocationToMod?.nativeAmount ?? '0')
+      }
+      // TODO: Get token amounts from currencyWallet
+      // else if (modification === 'stake')
     }
   }
 
@@ -144,33 +142,53 @@ export const StakeModifyScene = (props: Props) => {
   }
 
   const isClaim = modification === 'claim'
-  const renderEditableAmount = (modToRender: string, stakeDetails: StakeDetails) => {
+  const renderEditableAmount = (allocationType: AllocationType, stakeDetails: StakeDetails) => {
+    console.log('\x1b[34m\x1b[43m' + `allocationType: ${JSON.stringify(allocationType, null, 2)}` + '\x1b[0m')
     if (stakeDetails !== null) {
       const titleMap = {
         stake: s.strings.stake_amount_stake,
         claim: s.strings.stake_amount_claim,
-        unstake: s.strings.stake_amount_unstake
+        unstake: s.strings.stake_amount_unstake,
+        fee: ''
       }
 
       const nativeAmountMap = {
         stake: nativeModAmount,
-        claim: stakeDetails === null ? '0' : getRewardAllocation(stakeDetails),
-        unstake: nativeModAmount
+        claim: rewardAmount,
+        unstake: nativeModAmount,
+        fee: ''
       }
 
-      const amountCurrencyCode = isClaim ? rewardAssetsName : stakeAssetsName
-      const exchangeDenomination = isClaim ? rewardExchangeDenom : stakeExchangeDenom
-      const displayDenomination = isClaim ? rewardDisplayDenom : stakeDisplayDenom
+      const amountCurrencyCodeMap = {
+        stake: stakeAssetsName,
+        claim: rewardAssetsName,
+        unstake: stakeAssetsName,
+        fee: ''
+      }
+
+      const exchangeDenomMap = {
+        stake: stakeExchangeDenom,
+        claim: rewardExchangeDenom,
+        unstake: stakeExchangeDenom,
+        fee: stakeExchangeDenom
+      }
+
+      const displayDenomMap = {
+        stake: stakeDisplayDenom,
+        claim: rewardDisplayDenom,
+        unstake: stakeDisplayDenom,
+        fee: stakeDisplayDenom
+      }
 
       return (
         <EditableAmountTile
-          title={titleMap[modToRender]}
+          title={titleMap[allocationType]}
           exchangeRates={guiExchangeRates}
-          nativeAmount={nativeAmountMap[modToRender]}
+          nativeAmount={nativeAmountMap[allocationType]}
           currencyWallet={currencyWallet}
-          currencyCode={amountCurrencyCode}
-          exchangeDenomination={exchangeDenomination}
-          displayDenomination={displayDenomination}
+          currencyCode={amountCurrencyCodeMap[allocationType]}
+          exchangeDenomination={exchangeDenomMap[allocationType]}
+          displayDenomination={displayDenomMap[allocationType]}
           lockInputs={isClaim}
           onPress={showFlipInputModal}
         />
@@ -178,28 +196,23 @@ export const StakeModifyScene = (props: Props) => {
     }
   }
 
-  // Stake/Claim/Unstake-Mapped vars
-  const sceneTitleMap = {
-    stake: sprintf(s.strings.stake_x_to_earn_y, stakeAssetsName, rewardAssetsName),
-    claim: s.strings.stake_claim_rewards,
-    unstake: s.strings.stake_claim_unstake
-  }
-
   const renderWarning = () => {
     if (!isClaim) return null
     return <Alert marginTop={0.5} title={s.strings.wc_smartcontract_warning_title} message={s.strings.stake_warning_claim} numberOfLines={0} type="warning" />
   }
 
-  const amountTiles = stakeDetails !== null ? renderEditableAmount(modification, stakeDetails) : null
+  const displayAllocationTypesMap = {
+    stake: ['stake'],
+    claim: ['claim'],
+    unstake: ['claim', 'stake']
+  }
   const renderAmountTiles = (modAllocation, nativeAmount, modification) => {
     return (
       <>
-        <Tile type="static" title={sceneTitleMap[modification]}>
-          <IconTile title={s.strings.wc_smartcontract_wallet} iconUri={getCurrencyIcon(currencyWallet.currencyInfo.pluginId).symbolImage}>
-            <EdgeText>{currencyWallet.name}</EdgeText>
-          </IconTile>
-        </Tile>
-        {amountTiles}
+        <IconTile title={s.strings.wc_smartcontract_wallet} iconUri={getCurrencyIcon(currencyWallet.currencyInfo.pluginId).symbolImage}>
+          <EdgeText>{currencyWallet.name}</EdgeText>
+        </IconTile>
+        {displayAllocationTypesMap[modification].map(allocationType => renderEditableAmount(allocationType, stakeDetails))}
         <CryptoFiatAmountTile
           title={s.strings.wc_smartcontract_network_fee}
           nativeCryptoAmount={nativeFeeAmount}
@@ -221,9 +234,14 @@ export const StakeModifyScene = (props: Props) => {
     )
   }
 
+  const sceneTitleMap = {
+    stake: sprintf(s.strings.stake_x_to_earn_y, stakeAssetsName, rewardAssetsName),
+    claim: s.strings.stake_claim_rewards,
+    unstake: s.strings.stake_claim_unstake
+  }
   return (
     <SceneWrapper background="theme">
-      <SceneHeader style={styles.sceneHeader} title={`Stake ${stakeAssetsName} to earn ${rewardAssetsName}`} underline withTopMargin />
+      <SceneHeader style={styles.sceneHeader} title={sceneTitleMap[modification]} underline withTopMargin />
       {renderAmountTiles(allocationToMod, nativeModAmount, modification)}
     </SceneWrapper>
   )
