e56f34831d5eba776c14bb261027a06ed3d5193f
Samuel Holmes <sam@edge.app>
2022-03-25 15:00:19 -0700

    Change StakePolicy asset field types
    The nested map data structure is inappropriate for most use-cases.
    Instead opt for a simpler, more obvious data structure (an array of
    objects).

--- src/util/stakeUtils.js
+++ src/util/stakeUtils.js
@@ -27,16 +27,14 @@ export const getAllocationLocktimeMessage = (allocation: DetailAllocation) => {
   return allocation.locktime != null ? ` (${sprintf(s.strings.stake_lock_message, formatTimeDate(allocation.locktime))})` : ''
 }
 
-export const getStakeAssetsName = (stakePolicy: StakePolicy) => {
-  const stakeChainsArr = Object.keys(stakePolicy.stakeAssets)
-  const stakeTokensArr = stakeChainsArr.map(chain => Object.keys(stakePolicy.stakeAssets[chain]))[0]
-  const stakeAssetsName = stakeTokensArr.length > 1 ? `${stakeTokensArr.join('-')}-LP` : stakeTokensArr[0]
+export const getStakeAssetsName = (stakePolicy: StakePolicy): string => {
+  const stakeAssetCurrencyCodes = stakePolicy.stakeAssets.map(asset => asset.tokenId)
+  const stakeAssetsName = stakeAssetCurrencyCodes.length > 1 ? `${stakeAssetCurrencyCodes.join('-')}-LP` : stakeAssetCurrencyCodes[0]
   return stakeAssetsName
 }
 
-export const getRewardAssetsName = (stakePolicy: StakePolicy) => {
-  const rewardChainsArr = Object.keys(stakePolicy.rewardAssets)
-  const rewardTokensArr = rewardChainsArr.map(chain => Object.keys(stakePolicy.rewardAssets[chain]))[0]
-  const rewardAssetsName = rewardTokensArr.length > 1 ? `${rewardTokensArr.join(', ')}` : rewardTokensArr[0]
+export const getRewardAssetsName = (stakePolicy: StakePolicy): string => {
+  const rewardAssetCurrencyCodes = stakePolicy.rewardAssets.map(asset => asset.tokenId)
+  const rewardAssetsName = rewardAssetCurrencyCodes.length > 1 ? `${rewardAssetCurrencyCodes.join('-')}-LP` : rewardAssetCurrencyCodes[0]
   return rewardAssetsName
 }
