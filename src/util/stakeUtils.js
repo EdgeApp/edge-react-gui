cf921cf74c0df2d4b3bfd6fbbd9f0eda4a217fbc
Jon Tzeng <jon@edge.app>
2022-03-29 13:35:10 -0700

    Rename `fetchStakeDetails` to `fetchStakePostion`

--- src/util/stakeUtils.js
+++ src/util/stakeUtils.js
@@ -5,25 +5,25 @@ import { sprintf } from 'sprintf-js'
 
 import { formatTimeDate } from '../locales/intl'
 import s from '../locales/strings'
-import type { DetailAllocation, StakeDetails, StakePlugin, StakePolicy } from '../plugins/stake-plugins'
+import type { PositionAllocation, StakePlugin, StakePolicy, StakePosition } from '../plugins/stake-plugins'
 import { makeStakePlugin } from '../plugins/stake-plugins'
 
 // TODO: Get the plugin instance from the core context when the plugin is loaded into the core
 export const stakePlugin = makeStakePlugin()
 
-export const getStakeDetails = async (stakePlugin: StakePlugin, stakePolicyId: string, currencyWallet: EdgeCurrencyWallet) => {
-  return stakePlugin.fetchStakeDetails({ stakePolicyId, wallet: currencyWallet })
+export const getStakePosition = async (stakePlugin: StakePlugin, stakePolicyId: string, currencyWallet: EdgeCurrencyWallet) => {
+  return stakePlugin.fetchStakePosition({ stakePolicyId, wallet: currencyWallet })
 }
 
-export const getRewardAllocation = (stakeDetails: StakeDetails) => {
-  return stakeDetails.allocations.filter(stakeDetail => stakeDetail.allocationType === 'earned')[0]
+export const getRewardAllocation = (stakePosition: StakePosition) => {
+  return stakePosition.allocations.filter(stakeDetail => stakeDetail.allocationType === 'earned')[0]
 }
 
-export const getStakeAllocation = (stakeDetails: StakeDetails) => {
-  return stakeDetails.allocations.filter(stakeDetail => stakeDetail.allocationType === 'staked')[0]
+export const getStakeAllocation = (stakePosition: StakePosition) => {
+  return stakePosition.allocations.filter(stakeDetail => stakeDetail.allocationType === 'staked')[0]
 }
 
-export const getAllocationLocktimeMessage = (allocation: DetailAllocation) => {
+export const getAllocationLocktimeMessage = (allocation: PositionAllocation) => {
   return allocation.locktime != null ? ` (${sprintf(s.strings.stake_lock_message, formatTimeDate(allocation.locktime))})` : ''
 }
 
