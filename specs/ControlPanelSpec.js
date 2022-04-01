ba5da693f55fc0fb869bcef26b7481f641a916ae
Kurtis Casperson <kcasperson7@gmail.com>
2022-04-01 15:30:16 -0700

    Sidemenu cavy test

---
+++ specs/ControlPanelSpec.js
@@ -0,0 +1,28 @@
+export default function (spec) {
+  spec.describe('ControlPanel', function () {
+    spec.it('option navigation', async function () {
+      await spec.pause(5000)
+      await spec.press('WalletList.SideMenuButton')
+      await spec.pause(1000)
+      await spec.press('SideMenu.fio-names')
+      await spec.pause(1000)
+      await spec.press('SideMenu.fio')
+      await spec.pause(1000)
+      await spec.press('SideMenu.wallet-connect')
+      await spec.pause(1000)
+      await spec.press('SideMenu.scan-qr')
+      await spec.pause(1000)
+      await spec.press('ScanModal.Close')
+      await spec.pause(1000)
+      await spec.press('SideMenu.sweep')
+      await spec.pause(1000)
+      await spec.press('WalletListModal.Close')
+      await spec.pause(1000)
+      await spec.press('SideMenu.tos')
+      await spec.pause(1000)
+      await spec.press('SideMenu.settings')
+      await spec.pause(1000)
+      await spec.press('SideMenu.logout')
+    })
+  })
+}
