144f061545dbfa2f8a7ea7449bf5de399262f9fd
eliran zach <thehobbit85@gmail.com>
2022-04-08 06:01:41 -0700

    Use the new CurrencyIcon in the ExchangeQuoteComponent

--- src/components/themed/ExchangeQuoteComponent.js
+++ src/components/themed/ExchangeQuoteComponent.js
@@ -1,18 +1,17 @@
 // @flow
 
 import * as React from 'react'
-import FastImage from 'react-native-fast-image'
 
 import s from '../../locales/strings.js'
 import { type Theme, type ThemeProps, cacheStyles, withTheme } from '../services/ThemeContext.js'
 import { Card } from './Card'
 import { CardContent } from './CardContent'
+import { CurrencyIcon } from './CurrencyIcon.js'
 import { DataRow } from './DataRow'
 import { EdgeText } from './EdgeText'
 
 type Props = {
   isTop?: boolean | null,
-  walletIcon: string,
   cryptoAmount: string,
   currency: string,
   currencyCode: string,
@@ -47,11 +46,10 @@ export class ExchangeQuoteComponent extends React.PureComponent<Props & ThemePro
   }
 
   render() {
-    const styles = getStyles(this.props.theme)
     return (
       <Card>
         <CardContent
-          image={<FastImage style={styles.currencyIcon} source={{ uri: this.props.walletIcon }} />}
+          image={<CurrencyIcon currencyCode={this.props.currencyCode} sizeRem={1.5} />}
           title={this.props.currency}
           subTitle={this.props.walletName}
           value={`${this.props.cryptoAmount} ${this.props.currencyCode}`}
@@ -67,11 +65,6 @@ const getStyles = cacheStyles((theme: Theme) => ({
   bottomText: {
     fontWeight: '600',
     fontSize: theme.rem(0.75)
-  },
-  currencyIcon: {
-    height: theme.rem(1.5),
-    width: theme.rem(1.5),
-    resizeMode: 'contain'
   }
 }))
 
