fc8aae44bdd8771e3303ee979f69ad87f69d51b2
eliran zach <thehobbit85@gmail.com>
2022-04-14 14:53:24 -0700

    Use tokenId and pluginId instead of currencyCode when using the walletListRow in the manage token scene

--- src/components/themed/ManageTokensRow.js
+++ src/components/themed/ManageTokensRow.js
@@ -5,6 +5,8 @@ import * as React from 'react'
 import { Switch, View } from 'react-native'
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
 
+import { useSelector } from '../../types/reactRedux.js'
+import { getTokenId } from '../../util/CurrencyInfoHelpers.js'
 import { type Theme, cacheStyles, useTheme } from '../services/ThemeContext.js'
 import { WalletListRow } from './WalletListRow'
 
@@ -13,6 +15,7 @@ export type Props = {
   metaToken: EdgeMetaToken & {
     item: any
   },
+  pluginId: string,
   enabledList: string[],
   goToEditTokenScene: string => void,
   metaTokens: EdgeMetaToken[]
@@ -23,17 +26,19 @@ export function ManageTokensRow(props: Props) {
   const styles = getStyles(theme)
 
   const { currencyCode, currencyName } = props.metaToken.item
-  const { enabledList, toggleToken, goToEditTokenScene, metaTokens } = props
+  const { pluginId, enabledList, toggleToken, goToEditTokenScene, metaTokens } = props
   const enabled = enabledList.indexOf(currencyCode) >= 0
   // disable editing if token is native to the app
   const isEditable = metaTokens.every(token => token.currencyCode !== currencyCode)
 
+  const account = useSelector(state => state.core.account)
+  const tokenId = getTokenId(account, pluginId, currencyCode)
   const onPress = () => {
     if (isEditable) goToEditTokenScene(currencyCode)
   }
 
   return (
-    <WalletListRow onPress={onPress} gradient currencyCode={currencyCode} walletName={currencyName}>
+    <WalletListRow pluginId={pluginId} tokenId={tokenId} onPress={onPress} gradient walletName={currencyName}>
       <View style={styles.touchableCheckboxInterior}>
         {isEditable ? (
           <View style={styles.editIcon}>
