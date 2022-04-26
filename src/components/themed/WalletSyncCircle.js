63f6a3a63c5a8d1b68dc1165f0bfd38a147e8ecd
William Swanson <swansontec@gmail.com>
2022-04-26 00:55:23 -0700

    Fix bugs in the wallet sync circle
    - Fix the outer radius calculation.
    - Never compare floating-point values using `===`.
    - Do not use `theme.primaryText` for non-text visuals.
    - Use a smaller minimum percentage, since the rounded end caps make this look better.
    - Use a numerically-stable path, instead of hacking a dashed line around a circle.
    - Store our `done` flag in a ref, as it is not used in any worklets.

--- src/components/themed/WalletSyncCircle.js
+++ src/components/themed/WalletSyncCircle.js
@@ -2,94 +2,94 @@
 
 import { type EdgeCurrencyWallet } from 'edge-core-js'
 import * as React from 'react'
+import { ViewStyle } from 'react-native'
 import Animated, { useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated'
-import Svg, { Circle } from 'react-native-svg'
+import Svg, { Path } from 'react-native-svg'
 
-import { useEffect, useMemo } from '../../types/reactHooks.js'
+import { useEffect, useRef } from '../../types/reactHooks.js'
 import { useTheme } from '../services/ThemeContext.js'
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle)
-const BASE_RATIO = 0.05
-const MAX_RATIO = 0.95
+const AnimatedPath = Animated.createAnimatedComponent(Path)
+
 type Props = {
   // The diameter of the inner currency icon:
   size: number,
-  edgeWallet: EdgeCurrencyWallet
+
+  wallet: EdgeCurrencyWallet
 }
 
 /**
  * Renders the sync progress ratio as part of the `CurrencyIcon` component.
  */
 export const WalletSyncCircle = (props: Props) => {
+  const { size, wallet } = props
   const theme = useTheme()
-  const { size = theme.rem(2), edgeWallet } = props
-  // Animation shared state
-  const syncRatio = useSharedValue(edgeWallet.syncRatio < 0.05 ? 0.05 : edgeWallet.syncRatio)
-  const isDone = useSharedValue(false)
-  const stroke = useSharedValue(theme.walletProgressIconFill)
 
   // Subscribe to the sync ratio:
-  useEffect(
-    () =>
-      edgeWallet.watch('syncRatio', (ratio: number) => {
-        // If already done but needs to resync reset the flags and animations
-        if (isDone.value && ratio < BASE_RATIO) {
-          isDone.value = false
-          stroke.value = theme.walletProgressIconFill
-          syncRatio.value = BASE_RATIO
+  const done = useRef(wallet.syncRatio > 0.99)
+  const opacity = useSharedValue(done.current ? 0 : 1)
+  const syncRatio = useSharedValue(wallet.syncRatio)
+  useEffect(() => {
+    return wallet.watch('syncRatio', ratio => {
+      if (!done.current) {
+        // We are not done, so track the ratio:
+        syncRatio.value = withTiming(ratio, { duration: 1000 })
+        if (ratio > 0.99) {
+          // We have reached the end, so fade away:
+          done.current = true
+          opacity.value = withTiming(0, { duration: 2000 })
         }
-        // If the wallet hasn't fully synced show progress animation
-        if (!isDone.value) {
-          if (ratio === 1) {
-            isDone.value = true
-            stroke.value = theme.primaryText
-          }
-          if (ratio < BASE_RATIO) syncRatio.value = withTiming(BASE_RATIO, { duration: 1000 })
-          else if (ratio > MAX_RATIO && ratio < 1) syncRatio.value = withTiming(MAX_RATIO, { duration: 1000 })
-          else syncRatio.value = withTiming(ratio, { duration: 1000 })
-        }
-      }),
-    [edgeWallet, isDone, isDone.value, stroke, syncRatio, theme.primaryText, theme.walletProgressIconFill]
-  )
+      } else if (ratio < 0.05) {
+        // We were already done, but a resync took place:
+        done.current = false
+        opacity.value = withTiming(1, { duration: 1000 })
+        syncRatio.value = ratio
+      }
+    })
+  }, [opacity, syncRatio, wallet])
 
-  // Calculate the sync circle "Thickness" to make sure it's proportional to the size
-  const strokeWidth = 2 * Math.floor(size / theme.rem(1)) - theme.rem(1 / 16) // Make sure to always end with a round number
-  // Calculate circle params based on size
-  const radius = Math.floor((size + strokeWidth) / 2)
-  const circumference = 2 * Math.PI * radius
-  const center = size / 2
-  // Calculate the final size of the SVG
-  const svgSize = size + 4 * strokeWidth
-  const viewBox = `${-strokeWidth * 2} ${-strokeWidth * 2} ${svgSize} ${svgSize}`
+  // Animate the SVG path:
+  const animatedProps = useAnimatedProps(() => {
+    const ratio = Math.max(syncRatio.value, 0.02)
+    const theta = 2 * Math.PI * ratio
+    const arc = 'A 1 1 0 0 1' // Clockwise arc with radius 1
 
-  // Animated Params
-  const animatedProps = useAnimatedProps(() => ({
-    strokeDashoffset: circumference * (1 - syncRatio.value),
-    opacity: withTiming(syncRatio.value === 1 ? 0 : 1, { duration: 500 }),
-    stroke: withTiming(stroke.value)
-  }))
+    let path = `M 0 -1 `
+    if (ratio > 0.25) path += `${arc} 1 0 `
+    if (ratio > 0.5) path += `${arc} 0 1 `
+    if (ratio > 0.75) path += `${arc} -1 0 `
+    path += `${arc} ${Math.sin(theta)} ${-Math.cos(theta)}`
 
-  // Memoized SvgStyle to reduce rerenders
-  const svgStyle = useMemo(
-    () => ({
-      transform: [{ rotateZ: '-90deg' }],
-      position: 'absolute',
-      top: -2 * strokeWidth,
-      left: -2 * strokeWidth
-    }),
-    [strokeWidth]
-  )
+    return { d: path, strokeOpacity: opacity.value }
+  })
+
+  // Calculate the final size of the SVG:
+  const strokeWidth = theme.rem(3 / 16)
+  const svgSize = size + 2 * strokeWidth
+  const svgStyle: ViewStyle = {
+    position: 'absolute',
+    top: -strokeWidth,
+    left: -strokeWidth
+  }
+
+  // Scale the coordinate system to make the circle radius exactly 1:
+  const r = (size + strokeWidth) / 2 // The on-screen radius
+  const vSize = svgSize / r // The virtual viewport width
 
   return (
-    <Svg height={svgSize} width={svgSize} viewBox={viewBox} style={svgStyle}>
-      <AnimatedCircle
-        cx={center}
-        cy={center}
-        r={radius}
-        strokeLinecap="round"
-        strokeDasharray={circumference}
-        strokeWidth={strokeWidth}
+    <Svg
+      height={svgSize}
+      width={svgSize}
+      style={svgStyle}
+      // Put the origin in the center of the box:
+      viewBox={`${-vSize / 2} ${-vSize / 2} ${vSize} ${vSize}`}
+    >
+      <AnimatedPath
         animatedProps={animatedProps}
+        stroke={theme.walletProgressIconFill}
+        strokeLinecap="round"
+        // Scale the stroke width into SVG coordinates:
+        strokeWidth={strokeWidth / r}
       />
     </Svg>
   )
