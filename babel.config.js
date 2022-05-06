0ecd64117762261f8035efc3538a5499508ff476
thehobit85 <thehobbit85@gmail.com>
2022-05-06 04:11:35 -0700

    Use 'react-native-vision-camera' instead of the deprecated `react-native-camera`

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,12 @@
 
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin']
+  plugins: [
+    [
+      'react-native-reanimated/plugin',
+      {
+        globals: ['__scanCodes']
+      }
+    ]
+  ]
 }
