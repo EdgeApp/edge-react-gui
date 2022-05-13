9953556adeaca19e5b6283707786444ddfcc11fb
Paul V Puey <paul@edge.app>
2022-05-12 20:13:47 -0700

    Exclusively use the rates2 server for all exchange rate queries.
    This mostly fixes performance issues in Android with RN69

--- src/actions/ExchangeRateActions.js
+++ src/actions/ExchangeRateActions.js
@@ -1,10 +1,25 @@
 // @flow
 
-import { div } from 'biggystring'
+import { div, eq } from 'biggystring'
+import { asArray, asEither, asNull, asObject, asString } from 'cleaners'
 
 import { type Dispatch, type GetState, type RootState } from '../types/reduxTypes.js'
 import { type GuiExchangeRates } from '../types/types.js'
-import { DECIMAL_PRECISION, getYesterdayDateRoundDownHour } from '../util/utils.js'
+import { DECIMAL_PRECISION, getYesterdayDateRoundDownHour, pickRandom } from '../util/utils.js'
+
+const RATES_SERVERS = ['https://rates2.edge.app']
+const RATES_SERVER_MAX_QUERY_SIZE = 100
+const HOUR_MS = 1000 * 60 * 60
+
+const asRatesResponse = asObject({
+  data: asArray(
+    asObject({
+      currency_pair: asString,
+      date: asString,
+      exchangeRate: asEither(asString, asNull)
+    })
+  )
+})
 
 export const updateExchangeRates = () => async (dispatch: Dispatch, getState: GetState) => {
   const state = getState()
@@ -17,79 +32,77 @@ export const updateExchangeRates = () => async (dispatch: Dispatch, getState: Ge
 
 async function buildExchangeRates(state: RootState): GuiExchangeRates {
   const { account } = state.core
-  const { currencyWallets, rateCache } = account
+  const { currencyWallets } = account
+
   const accountIsoFiat = state.ui.settings.defaultIsoFiat
 
-  const exchangeRates: { [pair: string]: Promise<number> } = {}
-  const finalExchangeRates: GuiExchangeRates = {}
+  const exchangeRates = []
   const yesterdayDate = getYesterdayDateRoundDownHour()
   if (accountIsoFiat !== 'iso:USD') {
-    exchangeRates[`iso:USD_${accountIsoFiat}`] = rateCache.convertCurrency('iso:USD', accountIsoFiat)
+    exchangeRates.push({ currency_pair: `iso:USD_${accountIsoFiat}` })
   }
   for (const id of Object.keys(currencyWallets)) {
     const wallet = currencyWallets[id]
     const walletIsoFiat = wallet.fiatCurrencyCode
-    const currencyCode = wallet.currencyInfo.currencyCode // should get GUI or core versions?
+    const currencyCode = wallet.currencyInfo.currencyCode
     // need to get both forward and backwards exchange rates for wallets & account fiats, for each parent currency AND each token
-    exchangeRates[`${currencyCode}_${walletIsoFiat}`] = rateCache.convertCurrency(currencyCode, walletIsoFiat)
-    exchangeRates[`${currencyCode}_${accountIsoFiat}`] = rateCache.convertCurrency(currencyCode, accountIsoFiat)
-    exchangeRates[`${currencyCode}_iso:USD_${yesterdayDate}`] = fetchExchangeRateHistory(currencyCode, yesterdayDate)
-    // add them to the list of promises to resolve
-    // keep track of the exchange rates
+    exchangeRates.push({ currency_pair: `${currencyCode}_${walletIsoFiat}` })
+    exchangeRates.push({ currency_pair: `${currencyCode}_${accountIsoFiat}` })
+    exchangeRates.push({ currency_pair: `${currencyCode}_iso:USD`, date: `${yesterdayDate}` })
     // now add tokens, if they exist
     if (walletIsoFiat !== 'iso:USD') {
-      exchangeRates[`iso:USD_${walletIsoFiat}`] = rateCache.convertCurrency('iso:USD', walletIsoFiat)
+      exchangeRates.push({ currency_pair: `iso:USD_${walletIsoFiat}` })
     }
     for (const tokenCode of Object.keys(wallet.balances)) {
       if (tokenCode !== currencyCode) {
-        exchangeRates[`${tokenCode}_${walletIsoFiat}`] = rateCache.convertCurrency(tokenCode, walletIsoFiat)
-        exchangeRates[`${tokenCode}_${accountIsoFiat}`] = rateCache.convertCurrency(tokenCode, accountIsoFiat)
-        exchangeRates[`${tokenCode}_iso:USD_${yesterdayDate}`] = fetchExchangeRateHistory(tokenCode, yesterdayDate)
+        exchangeRates.push({ currency_pair: `${tokenCode}_${walletIsoFiat}` })
+        exchangeRates.push({ currency_pair: `${tokenCode}_${accountIsoFiat}` })
+        exchangeRates.push({ currency_pair: `${tokenCode}_iso:USD`, date: `${yesterdayDate}` })
       }
     }
   }
-  const exchangeRateKeys = Object.keys(exchangeRates)
-  const exchangeRatePromises = Object.values(exchangeRates)
-  // Promise.allSettled() is the correct function for this but somehow not included in Promise
-  const rates = await Promise.all(
-    exchangeRatePromises.map(promise =>
-      // $FlowExpectedError - Object.values() always produce mixed type so .catch will produce error
-      promise.catch(e => {
-        console.log(e)
-        return 0
-      })
-    )
-  )
 
-  for (let i = 0; i < exchangeRateKeys.length; i++) {
-    const key = exchangeRateKeys[i]
-    const codes = key.split('_')
-    const reverseExchangeRateKey = `${codes[1]}_${codes[0]}${codes[2] ? '_' + codes[2] : ''}`
-    const rate = rates[i]
-    finalExchangeRates[key] = '0'
-    finalExchangeRates[reverseExchangeRateKey] = '0'
+  const serverRates: GuiExchangeRates = {}
+  while (exchangeRates.length > 0) {
+    const query = exchangeRates.splice(0, RATES_SERVER_MAX_QUERY_SIZE)
+    let tries = 5
+    do {
+      const url = pickRandom(RATES_SERVERS) ?? ''
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ data: query })
+      }
+      try {
+        const response = await fetch(url + '/v2/exchangeRates', options)
+        if (response.ok) {
+          const json = await response.json()
+          const cleanedRates = asRatesResponse(json)
+          for (const rate of cleanedRates.data) {
+            const { currency_pair: currencyPair, exchangeRate, date } = rate
+            const newDate = new Date(date).valueOf()
+            const now = Date.now()
 
-    if (rate != null && !isNaN(rate) && rate !== 0) {
-      const rateStr = rate.toFixed(DECIMAL_PRECISION)
-      finalExchangeRates[key] = rateStr
-      finalExchangeRates[reverseExchangeRateKey] = div('1', rateStr, DECIMAL_PRECISION)
-    }
+            const key = now - newDate > HOUR_MS ? `${currencyPair}_${date}` : currencyPair
+            serverRates[key] = exchangeRate ?? '0'
+
+            const codes = key.split('_')
+            const reverseExchangeRateKey = `${codes[1]}_${codes[0]}${codes[2] ? '_' + codes[2] : ''}`
+            if (serverRates[reverseExchangeRateKey] == null) {
+              if (eq(serverRates[key], '0')) {
+                serverRates[reverseExchangeRateKey] = '0'
+              } else {
+                serverRates[reverseExchangeRateKey] = div('1', serverRates[key], DECIMAL_PRECISION)
+              }
+            }
+          }
+          break
+        }
+      } catch (e) {
+        console.log(`buildExchangeRates error querying ${url} ${e.message}`)
+      }
+    } while (--tries > 0)
   }
 
-  return finalExchangeRates
-}
-
-async function fetchExchangeRateHistory(currency: string, date: string): Promise<number> {
-  try {
-    const currencyHistory = await fetch(`https://rates1.edge.app/v1/exchangeRate?currency_pair=${currency}_USD&date=${date}`).catch(e => {
-      console.log('Error fetching fetchExchangeRateHistory', e)
-    })
-    if (currencyHistory != null) {
-      const result = await currencyHistory.json()
-      return parseFloat(result.exchangeRate)
-    }
-  } catch (e) {
-    console.log('fetchExchangeRateHistory', e)
-  }
-  return 0
+  return serverRates
 }
