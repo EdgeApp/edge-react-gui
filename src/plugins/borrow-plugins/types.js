dd3c73fdc17481e7c3537ef87d5631030570fb7e
Samuel Holmes <sam@edge.app>
2022-05-23 17:38:11 -0700

    Fix comments in types.js to follow conventions

--- src/plugins/borrow-plugins/types.js
+++ src/plugins/borrow-plugins/types.js
@@ -1,5 +1,10 @@
 // @flow
+
 import { type EdgeCurrencyWallet, type EdgeNetworkFee } from 'edge-core-js'
+
+// -----------------------------------------------------------------------------
+// Request Method Types
+// -----------------------------------------------------------------------------
 
 // Borrow more:
 export type BorrowRequest = {
@@ -43,9 +48,9 @@ export type ApprovableAction = {
   +approve: () => Promise<void>
 }
 
-//
+// -----------------------------------------------------------------------------
 // Engine
-//
+// -----------------------------------------------------------------------------
 
 export type BorrowCollateral = {
   tokenId?: string,
@@ -82,9 +87,9 @@ export type BorrowEngine = {
   close: () => Promise<ApprovableAction>
 }
 
-//
+// -----------------------------------------------------------------------------
 // Plugin Info
-//
+// -----------------------------------------------------------------------------
 
 export type BorrowPluginInfo = {
   pluginId: string,
@@ -97,9 +102,9 @@ export type BorrowPluginInfo = {
   maxLtvRatio: number
 }
 
-//
+// -----------------------------------------------------------------------------
 // Plugin
-//
+// -----------------------------------------------------------------------------
 
 export type BorrowPlugin = {
   borrowInfo: BorrowPluginInfo,
