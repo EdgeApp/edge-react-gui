fb58a18915a27fa56902c69228d7b6663d0a36c6
eliran zach <thehobbit85@gmail.com>
2022-06-06 22:43:10 -0700

    Fix manual wallet sorting

--- src/components/themed/WalletListSortable.js
+++ src/components/themed/WalletListSortable.js
@@ -5,7 +5,9 @@ import * as React from 'react'
 import { StyleSheet } from 'react-native'
 import SortableListView from 'react-native-sortable-listview'
 
+import { useHandler } from '../../hooks/useHandler.js'
 import { useWatchAccount } from '../../hooks/useWatch.js'
+import { useEffect, useState } from '../../types/reactHooks.js'
 import { useSelector } from '../../types/reactRedux.js'
 import { showError } from '../services/AirshipInstance.js'
 import { WalletListSortableRow } from './WalletListSortableRow.js'
@@ -20,25 +22,28 @@ export function WalletListSortable(props: Props) {
   const account = useSelector(state => state.core.account)
 
   const currencyWallets = useWatchAccount(account, 'currencyWallets')
-  const activeWalletIds = useWatchAccount(account, 'activeWalletIds')
+  const [walletOrder, setWalletOrder] = useState(account.activeWalletIds)
 
-  const handleRowMoved = (action: { from: number, to: number }) => {
-    const newOrder = [...activeWalletIds]
+  const handleRowMoved = useHandler((action: { from: number, to: number }) => {
+    const newOrder = [...walletOrder]
     newOrder.splice(action.to, 0, newOrder.splice(action.from, 1)[0])
+    setWalletOrder(newOrder)
+  })
 
+  useEffect(() => () => {
     const keyStates: EdgeWalletStates = {}
-    for (let i = 0; i < newOrder.length; ++i) {
-      const walletId = newOrder[i]
+    for (let i = 0; i < walletOrder.length; ++i) {
+      const walletId = walletOrder[i]
       keyStates[walletId] = { sortIndex: i }
     }
     account.changeWalletStates(keyStates).catch(showError)
-  }
+  })
 
   return (
     <SortableListView
       style={StyleSheet.absoltueFill}
       data={currencyWallets}
-      order={activeWalletIds}
+      order={walletOrder}
       onRowMoved={handleRowMoved}
       renderRow={(wallet: EdgeCurrencyWallet | void) => <WalletListSortableRow wallet={wallet} />}
     />
