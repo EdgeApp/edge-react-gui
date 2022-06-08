50b50912f1cd38b013d94a55006fbb73142f33e5
eliran zach <thehobbit85@gmail.com>
2022-06-08 01:13:25 -0700

    Upgrade to the new react-native Architecture and enable TurboModules on iOS

--- ios/edge/AppDelegate.mm
+++ ios/edge/AppDelegate.mm
@@ -1,6 +1,19 @@
 #import "AppDelegate.h"
 #import "edgeApiKey.h"
 
+#import <reacthermes/HermesExecutorFactory.h>
+#import <React/RCTCxxBridgeDelegate.h>
+#import <React/RCTJSIExecutorRuntimeInstaller.h>
+#import <ReactCommon/RCTTurboModuleManager.h>
+#import <React/CoreModulesPlugins.h>
+#import <React/RCTDataRequestHandler.h>
+#import <React/RCTHTTPRequestHandler.h>
+#import <React/RCTFileRequestHandler.h>
+#import <React/RCTNetworking.h>
+#import <React/RCTImageLoader.h>
+#import <React/RCTGIFImageDecoder.h>
+#import <React/RCTLocalAssetImageLoader.h>
+
 #import "RCTSplashScreen.h"
 #import <Bugsnag/Bugsnag.h>
 #import <Firebase.h>
@@ -31,6 +44,8 @@ static void InitializeFlipper(UIApplication *application) {
 #endif
 
 @implementation AppDelegate
+
+RCTTurboModuleManager *_turboModuleManager;
 
 - (BOOL)application:(UIApplication *)application
             openURL:(NSURL *)url
@@ -67,6 +82,7 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  RCTEnableTurboModule(YES);
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"edge"
@@ -278,6 +294,92 @@ typedef void (^MessagesHandler)(NSArray<NSString *> *problemUsers, NSError *erro
 -(NSError *)makeReplyError
 {
   return [NSError errorWithDomain:NSPOSIXErrorDomain code:EBADMSG userInfo:nil];
+}
+
+#pragma mark - RCTCxxBridgeDelegate
+
+- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
+{
+  // Add these lines to create a TurboModuleManager
+  if (RCTTurboModuleEnabled()) {
+    _turboModuleManager =
+        [[RCTTurboModuleManager alloc] initWithBridge:bridge
+                                             delegate:self
+                                            jsInvoker:bridge.jsCallInvoker];
+
+    // Necessary to allow NativeModules to lookup TurboModules
+    [bridge setRCTTurboModuleRegistry:_turboModuleManager];
+
+    if (!RCTTurboModuleEagerInitEnabled()) {
+      /**
+       * Instantiating DevMenu has the side-effect of registering
+       * shortcuts for CMD + d, CMD + i,  and CMD + n via RCTDevMenu.
+       * Therefore, when TurboModules are enabled, we must manually create this
+       * NativeModule.
+       */
+       [_turboModuleManager moduleForName:"DevMenu"];
+    }
+  }
+
+  // Add this line...
+  __weak __typeof(self) weakSelf = self;
+
+  // If you want to use the `JSCExecutorFactory`, remember to add the `#import <React/JSCExecutorFactory.h>`
+  // import statement on top.
+  return std::make_unique<facebook::react::HermesExecutorFactory>(
+    facebook::react::RCTJSIExecutorRuntimeInstaller([weakSelf, bridge](facebook::jsi::Runtime &runtime) {
+      if (!bridge) {
+        return;
+      }
+
+      // And add these lines to install the bindings...
+      __typeof(self) strongSelf = weakSelf;
+      if (strongSelf) {
+        facebook::react::RuntimeExecutor syncRuntimeExecutor =
+            [&](std::function<void(facebook::jsi::Runtime & runtime_)> &&callback) { callback(runtime); };
+        [strongSelf->_turboModuleManager installJSBindingWithRuntimeExecutor:syncRuntimeExecutor];
+      }
+    }));
+}
+
+
+#pragma mark RCTTurboModuleManagerDelegate
+
+- (Class)getModuleClassFromName:(const char *)name
+{
+  return RCTCoreModulesClassProvider(name);
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)
+    getTurboModule:(const std::string &)name
+         jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker {
+  return nullptr;
+}
+
+- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
+{
+  // Set up the default RCTImageLoader and RCTNetworking modules.
+  if (moduleClass == RCTImageLoader.class) {
+    return [[moduleClass alloc] initWithRedirectDelegate:nil
+        loadersProvider:^NSArray<id<RCTImageURLLoader>> *(RCTModuleRegistry * moduleRegistry) {
+          return @ [[RCTLocalAssetImageLoader new]];
+        }
+        decodersProvider:^NSArray<id<RCTImageDataDecoder>> *(RCTModuleRegistry * moduleRegistry) {
+          return @ [[RCTGIFImageDecoder new]];
+        }];
+  } else if (moduleClass == RCTNetworking.class) {
+     return [[moduleClass alloc]
+        initWithHandlersProvider:^NSArray<id<RCTURLRequestHandler>> *(
+            RCTModuleRegistry *moduleRegistry) {
+          return @[
+            [RCTHTTPRequestHandler new],
+            [RCTDataRequestHandler new],
+            [RCTFileRequestHandler new],
+          ];
+        }];
+  }
+  // No custom initializer here.
+  return [moduleClass new];
 }
 
 @end
