eb3792ffb32cc6fc72dd8b9c354ae6ca6bc91dd0
eliran zach <thehobbit85@gmail.com>
2022-06-14 07:20:12 -0700

    Add Dash to default wallets and reorg based on Market-Cap

--- src/theme/edgeConfig.js
+++ src/theme/edgeConfig.js
@@ -20,5 +20,5 @@ export const edgeConfig: AppConfig = {
   website: 'https://edge.app',
   termsOfServiceSite: 'https://edge.app/tos/',
   appStore: 'https://itunes.apple.com/app/id1344400091',
-  defaultWallets: ['BTC', 'LTC', 'BCH', 'ETH']
+  defaultWallets: ['BTC', 'ETH', 'LTC', 'BCH', 'DASH']
 }
