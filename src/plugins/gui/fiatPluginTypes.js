e00b38c127dbe3a5e39d277379cd041c972405e7
Paul V Puey <paul@edge.app>
2022-06-18 18:09:58 -0700

    Implement FiatPlugin interface

--- src/plugins/gui/fiatPluginTypes.js
+++ src/plugins/gui/fiatPluginTypes.js
@@ -1,3 +1,43 @@
 // @flow
+import { type EdgeAccount } from 'edge-core-js'
+
+import { type EdgeTokenId } from '../../types/types.js'
+
+export type FiatPluginEnterAmountParams = {
+  headerTitle: string,
+  label1: string,
+  label2: string,
+  convertValue: (sourceFieldNum: number, value: string) => Promise<string | void>,
+  initialAmount1?: string,
+  headerIconUri?: string
+}
 
 export type FiatPluginEnterAmountResponse = { lastUsed: number, value1: string, value2: string }
+export type FiatPluginOpenWebViewParams = { url: string }
+export type FiatPluginUi = {
+  openWebView: FiatPluginOpenWebViewParams => Promise<void>,
+  walletPicker: (params: { headerTitle: string, allowedAssets?: EdgeTokenId[], showCreateWallet?: boolean }) => Promise<{
+    walletId: string | void,
+    currencyCode: string | void
+  }>,
+  errorDropdown: (error: Error) => Promise<void>,
+  enterAmount: (params: FiatPluginEnterAmountParams) => Promise<FiatPluginEnterAmountResponse>,
+  popScene: () => {}
+  // showWebView: (params: { webviewUrl: string }) => Promise<void>
+}
+
+export type FiatPluginFactoryArgs = {
+  // TODO:
+  // io: {
+  //   log: EdgeLog, // scoped logs
+  // }
+  showUi: FiatPluginUi,
+  account: EdgeAccount
+}
+
+export type FiatPlugin = {
+  pluginId: string,
+  startPlugin: () => Promise<void>
+}
+
+export type FiatPluginFactory = (params: FiatPluginFactoryArgs) => Promise<FiatPlugin>
