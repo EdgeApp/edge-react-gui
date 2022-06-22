64712d9f712ce8cdc34d8f6353263521f10ed65e
Jon Tzeng <jon@edge.app>
2022-06-22 15:38:56 -0700

    fixup! Add LoanDashboardScene

--- src/components/scenes/Loans/LoanDashboardScene.js
+++ src/components/scenes/Loans/LoanDashboardScene.js
@@ -1,27 +1,24 @@
 // @flow
-import { add, div, mul } from 'biggystring'
 import { type EdgeCurrencyWallet } from 'edge-core-js'
 import * as React from 'react'
-import { FlatList, TouchableOpacity, View } from 'react-native'
-import FastImage from 'react-native-fast-image'
+import { FlatList, TouchableOpacity } from 'react-native'
 import Ionicon from 'react-native-vector-icons/Ionicons'
 
-import { getSymbolFromCurrency } from '../../../constants/WalletAndCurrencyConstants'
-import { formatFiatString } from '../../../hooks/useFiatText'
+import { useAsyncEffect } from '../../../hooks/useAsyncEffect'
 import { useHandler } from '../../../hooks/useHandler'
 import { useWatchAccount } from '../../../hooks/useWatch'
 import s from '../../../locales/strings'
 import { makeAaveBorrowPlugin, makeAaveKovanBorrowPlugin } from '../../../plugins/borrow-plugins/plugins/aave/index'
 import { type TempBorrowInfo, filterActiveBorrowInfos, getAaveBorrowInfo, getAaveBorrowInfos } from '../../../plugins/helpers/getAaveBorrowPlugins'
-import { memo, useEffect, useState } from '../../../types/reactHooks'
+import { memo, useState } from '../../../types/reactHooks'
 import { useSelector } from '../../../types/reactRedux'
 import { type Theme } from '../../../types/Theme'
-import { type FlatListItem, type GuiExchangeRates } from '../../../types/types'
+import { type FlatListItem } from '../../../types/types'
 import { getCurrencyIconUris } from '../../../util/CdnUris'
 import { guessFromCurrencyCode } from '../../../util/CurrencyInfoHelpers'
 import { fixSides, mapSides, sidesToMargin } from '../../../util/sides'
-import { DECIMAL_PRECISION, truncateDecimals, zeroString } from '../../../util/utils'
-import { TappableCard } from '../../cards/TappableCard'
+import { Card } from '../../cards/Card'
+import { LoanSummaryCard } from '../../cards/LoanSummaryCard'
 import { FillLoader } from '../../common/FillLoader'
 import { SceneWrapper } from '../../common/SceneWrapper'
 import { WalletListModal } from '../../modals/WalletListModal'
@@ -47,22 +44,20 @@ export const LoanDashboardSceneComponent = () => {
   ).symbolImage
 
   const isWalletsLoaded = sortedWalletList.every(walletListItem => walletListItem.wallet != null)
-  const [isLoading, setIsLoading] = useState(true)
   const [borrowInfos, setBorrowInfos] = useState([])
+  const [isSceneLoading, setIsSceneLoading] = useState(true)
+  const [isNewLoanLoading, setIsNewLoanLoading] = useState(false)
 
-  useEffect(() => {
+  useAsyncEffect(async () => {
     // Initialize AAVE ETH and ETH Kovan borrow engines
-    if (isWalletsLoaded && isLoading) {
-      getAaveBorrowInfos([makeAaveKovanBorrowPlugin(), makeAaveBorrowPlugin()], account)
-        .then(filterActiveBorrowInfos)
-        .then(borrowInfos => {
-          setBorrowInfos(borrowInfos)
-          setIsLoading(false)
-        })
+    if (isWalletsLoaded && isSceneLoading) {
+      const borrowInfos = await getAaveBorrowInfos([makeAaveKovanBorrowPlugin(), makeAaveBorrowPlugin()], account)
+      setBorrowInfos(filterActiveBorrowInfos(borrowInfos))
+      setIsSceneLoading(false)
     }
-  }, [account, isLoading, isWalletsLoaded, wallets])
+  }, [account, isSceneLoading, isWalletsLoaded, wallets])
 
-  // TODO: The spec does not allow for a way to specify which dApp/plugin to create a loan from...
+  // TODO: When new loan dApps are added, we will need a way to specify a way to select which dApp to add a new loan for.
   const handleAddLoan = useHandler(() => {
     Airship.show(bridge => (
       <WalletListModal
@@ -74,95 +69,64 @@ export const LoanDashboardSceneComponent = () => {
     )).then(({ walletId }) => {
       if (walletId != null) {
         const wallet = wallets[walletId]
+        setIsNewLoanLoading(true)
+
         getAaveBorrowInfo(wallet.currencyInfo.pluginId === hardWalletPluginId ? makeAaveBorrowPlugin() : makeAaveKovanBorrowPlugin(), wallet)
           .then((borrowInfo: TempBorrowInfo) => {
+            setIsNewLoanLoading(false)
             // navigation.navigate('loanDetails', { borrowEngine: borrowInfo.borrowEngine, borrowPlugin: borrowInfo.borrowPlugin })
           })
-          .catch(err => redText(err.message))
+          .catch(err => {
+            redText(err.message)
+          })
+          .finally(() => setIsNewLoanLoading(false))
       }
     })
   })
 
-  const renderLoanCard = useHandler((item: FlatListItem<any>) => {
-    const { borrowEngine } = item.item
-    const { currencyWallet: wallet, collaterals, debts } = borrowEngine
-    const isoFiatCurrencyCode = wallet.fiatCurrencyCode
-    const fiatCurrencyCode = isoFiatCurrencyCode.replace('iso:', '')
-
-    try {
-      const styles = getStyles(theme)
-      const fiatSymbol = getSymbolFromCurrency(isoFiatCurrencyCode)
-
-      // Calculate fiat totals
-      const collateralTotal = collaterals.reduce((accumulator, collateral) => {
-        return add(accumulator, calculateFiatValue(wallet, collateral.tokenId, isoFiatCurrencyCode, collateral.nativeAmount, exchangeRates) ?? '0')
-      }, '0')
-      const displayCollateralTotal = `${fiatSymbol}${formatFiatString({ autoPrecision: true, fiatAmount: collateralTotal, noGrouping: true })}`
-      const displayBorrowTotal = formatFiatString({
-        autoPrecision: true,
-        fiatAmount: debts.reduce((accumulator, debt) => {
-          return add(accumulator, calculateFiatValue(wallet, debt.tokenId, isoFiatCurrencyCode, debt.nativeAmount, exchangeRates))
-        }, '0'),
-        noGrouping: true
-      })
-
-      // TODO: Calculate amount-adjusted cumulative interest
-      const displayInterestTotal = `${truncateDecimals((debts[0].apr * 100).toString(), 1)}%`
-
-      return (
-        <TappableCard
-          onPress={() => {
-            // TODO: After LoanDetailsScene implementation
-            // navigation.navigate('loanDetails', { borrowEngine, borrowPlugin })
-          }}
-        >
-          <View style={styles.cardContainer}>
-            <View style={styles.row}>
-              <FastImage style={styles.icon} source={{ uri: iconUri }} />
-              <EdgeText style={styles.textMain}>{displayBorrowTotal}</EdgeText>
-              <EdgeText>{fiatCurrencyCode}</EdgeText>
-            </View>
-            <View style={styles.spacedRow}>
-              <View style={styles.column}>
-                <EdgeText style={styles.textSecondary}>{s.strings.loan_collateral_value}</EdgeText>
-                <EdgeText style={styles.textPrimary}>{displayCollateralTotal}</EdgeText>
-              </View>
-              <View style={styles.column}>
-                <EdgeText style={styles.textSecondary}>{s.strings.loan_interest_rate}</EdgeText>
-                <EdgeText style={styles.textPrimary}>{displayInterestTotal}</EdgeText>
-              </View>
-            </View>
-          </View>
-        </TappableCard>
-      )
-    } catch (err) {
-      showError(err.message)
-      return null
+  const renderLoanCard = useHandler((item: FlatListItem<TempBorrowInfo>) => {
+    const borrowInfo: TempBorrowInfo = item.item
+    const handleLoanPress = () => {
+      // navigation.navigate('loanDetails', { borrowEngine: borrowInfo.borrowEngine, borrowPlugin: borrowInfo.borrowPlugin })
     }
+    return <LoanSummaryCard onPress={handleLoanPress} borrowEngine={borrowInfo.borrowEngine} iconUri={iconUri} exchangeRates={exchangeRates} />
   })
 
-  const footer = (
+  const footer = isNewLoanLoading ? (
+    // Render a loading card in place of the "New Loan" button while initializing a new loan
+    <Card>
+      <FillLoader />
+    </Card>
+  ) : (
     <TouchableOpacity onPress={handleAddLoan} style={styles.addButtonsContainer}>
       <Ionicon name="md-add" style={styles.addItem} size={theme.rem(1.5)} color={theme.iconTappable} />
       <EdgeText style={[styles.addItem, styles.addItemText]}>{s.strings.loan_dashboard_new_loan}</EdgeText>
     </TouchableOpacity>
   )
 
-  if (isLoading)
+  if (isSceneLoading) {
     return (
       <SceneWrapper background="theme" hasTabs={false}>
         <SceneHeader underline title={s.strings.loan_dashboard_title} />
         <FillLoader />
       </SceneWrapper>
     )
-  else
+  } else {
     return (
       <SceneWrapper background="theme" hasTabs={false}>
         <SceneHeader underline title={s.strings.loan_dashboard_title} />
         <EdgeText style={styles.textSectionHeader}>{s.strings.loan_dashboard_active_loans_title}</EdgeText>
-        <FlatList data={borrowInfos} keyboardShouldPersistTaps="handled" renderItem={renderLoanCard} style={margin} ListFooterComponent={footer} />
+        <FlatList
+          data={borrowInfos}
+          keyboardShouldPersistTaps="handled"
+          renderItem={renderLoanCard}
+          style={margin}
+          ListFooterComponent={footer}
+          keyExtractor={(borrowInfo: TempBorrowInfo) => borrowInfo.borrowEngine.currencyWallet.id ?? '0'}
+        />
       </SceneWrapper>
     )
+  }
 }
 
 export const getToken = (wallet: EdgeCurrencyWallet, tokenIdStr: string) => {
@@ -172,25 +136,6 @@ export const getToken = (wallet: EdgeCurrencyWallet, tokenIdStr: string) => {
   } else {
     return allTokens[tokenIdStr]
   }
-}
-
-export const calculateFiatValue = (
-  wallet: EdgeCurrencyWallet,
-  tokenIdStr: string,
-  isoFiatCurrencyCode: string,
-  nativeAmount: string,
-  exchangeRates: GuiExchangeRates
-) => {
-  const token = getToken(wallet, tokenIdStr)
-  if (token == null) return '0'
-  const { currencyCode, denominations } = token
-  const [denomination] = denominations
-  const cryptoAmount = div(nativeAmount, denomination.multiplier, DECIMAL_PRECISION)
-  const key = `${isoFiatCurrencyCode}_${currencyCode}`
-  const assetFiatPrice = Object.keys(exchangeRates).some(erKey => erKey === key) ? exchangeRates[key] : '0'
-
-  if (zeroString(assetFiatPrice)) showError(`No exchange rate for ${key}`)
-  return mul(cryptoAmount, assetFiatPrice)
 }
 
 const getStyles = cacheStyles((theme: Theme) => {
@@ -211,46 +156,15 @@ const getStyles = cacheStyles((theme: Theme) => {
     addItemText: {
       flexShrink: 1
     },
-    cardContainer: {
-      flex: 1
-    },
-    column: {
-      flexDirection: 'column',
-      alignItems: 'flex-start'
-    },
-    icon: {
-      alignSelf: 'center',
-      height: theme.rem(2),
-      width: theme.rem(2)
-    },
-    row: {
-      alignItems: 'flex-start',
-      flexDirection: 'row'
-    },
-    spacedRow: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignSelf: 'stretch',
-      marginTop: theme.rem(1.5)
+    cardEmptyContainer: {
+      marginLeft: theme.rem(0.5),
+      marginRight: theme.rem(0.5)
     },
     textSectionHeader: {
       fontFamily: theme.fontFaceBold,
       fontSize: theme.rem(0.75),
       marginTop: theme.rem(0.5),
       marginLeft: theme.rem(1)
-    },
-    textMain: {
-      fontFamily: theme.fontFaceMedium,
-      fontSize: theme.rem(2),
-      marginRight: theme.rem(0.5),
-      marginLeft: theme.rem(0.5)
-    },
-    textPrimary: {
-      fontFamily: theme.fontFaceBold,
-      fontSize: theme.rem(0.75)
-    },
-    textSecondary: {
-      fontSize: theme.rem(0.75)
     }
   }
 })
