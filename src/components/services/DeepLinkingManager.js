9312b24f51886c977df5c64311aea3be87fef555
Samuel Holmes <sam@edge.app>
2022-06-23 11:44:48 -0700

    Fix deep-linking for YOLO_DEEP_LINK

--- src/components/services/DeepLinkingManager.js
+++ src/components/services/DeepLinkingManager.js
@@ -6,7 +6,7 @@ import { Linking } from 'react-native'
 import ENV from '../../../env.json'
 import { launchDeepLink, retryPendingDeepLink } from '../../actions/DeepLinkingActions.js'
 import { useAsyncEffect } from '../../hooks/useAsyncEffect'
-import { useEffect } from '../../types/reactHooks.js'
+import { useEffect, useState } from '../../types/reactHooks'
 import { useDispatch, useSelector } from '../../types/reactRedux.js'
 import { parseDeepLink } from '../../util/DeepLinkParser.js'
 import { showError } from './AirshipInstance.js'
@@ -20,6 +20,7 @@ export function DeepLinkingManager(props: Props) {
   // We don't actually read these, but we need them to trigger updates:
   const accountReferralLoaded = useSelector(state => state.account.accountReferralLoaded)
   const wallets = useSelector(state => state.ui.wallets)
+  const [url, setUrl] = useState<string | null>(null)
 
   // Retry links that need a different app state:
   useEffect(() => {
@@ -29,23 +30,27 @@ export function DeepLinkingManager(props: Props) {
     requestAnimationFrame(() => dispatch(retryPendingDeepLink()))
   }, [accountReferralLoaded, dispatch, pendingDeepLink, wallets])
 
-  const handleUrl = url => {
+  useEffect(() => {
     try {
-      dispatch(launchDeepLink(parseDeepLink(url)))
+      if (url != null) dispatch(launchDeepLink(parseDeepLink(url)))
     } catch (error) {
       showError(error)
     }
-  }
+  }, [dispatch, url])
 
   // Startup tasks:
   useAsyncEffect(async () => {
-    const listener = Linking.addEventListener('url', event => handleUrl(event.url))
+    const listener = Linking.addEventListener('url', event => {
+      setUrl(event.url)
+    })
 
     let url = await Linking.getInitialURL()
     if (url == null && ENV.YOLO_DEEP_LINK != null) url = ENV.YOLO_DEEP_LINK
-    if (url != null) handleUrl(url)
+    if (url != null) setUrl(url)
 
-    return () => listener.remove()
+    return () => {
+      listener.remove()
+    }
   })
 
   return null
