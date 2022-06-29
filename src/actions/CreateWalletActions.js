3bec71a7d552086ec89e9b824f32696270acf89b
Xipu Li <lixipu98@gmail.com>
2022-06-29 11:52:46 -0700

    Replace Flux Actions with 'useNavigation' in functional components

--- src/actions/CreateWalletActions.js
+++ src/actions/CreateWalletActions.js
@@ -13,7 +13,7 @@ import s from '../locales/strings.js'
 import { getExchangeDenomination } from '../selectors/DenominationSelectors.js'
 import { config } from '../theme/appConfig.js'
 import type { Dispatch, GetState } from '../types/reduxTypes.js'
-import { Actions } from '../types/routerTypes.js'
+import { type NavigationProp, useNavigation } from '../types/routerTypes.js'
 import { logEvent } from '../util/tracking.js'
 
 export type CreateWalletOptions = {
@@ -132,6 +132,7 @@ export const createAccountTransaction =
     const { paymentAddress, amount, currencyCode } = state.ui.scenes.createWallet.walletAccountActivationPaymentInfo
     const handleAvailability = await currencyPlugin.otherMethods.validateAccount(accountName)
     const paymentDenom = getExchangeDenomination(state, paymentWallet.currencyInfo.pluginId, currencyCode)
+    const navigation: NavigationProp<any> = useNavigation()
     let nativeAmount = mul(amount, paymentDenom.multiplier)
     nativeAmount = toFixed(nativeAmount, 0, 0)
     if (handleAvailability.result === 'AccountAvailable') {
@@ -162,7 +163,7 @@ export const createAccountTransaction =
               notes: sprintf(s.strings.create_wallet_account_metadata_notes, createdWalletCurrencyCode, createdWalletCurrencyCode, config.supportEmail)
             }
             paymentWallet.saveTxMetadata(edgeTransaction.txid, currencyCode, edgeMetadata).then(() => {
-              Actions.popTo('walletListScene')
+              navigation.navigate('walletListScene')
               setTimeout(() => {
                 Alert.alert(s.strings.create_wallet_account_payment_sent_title, s.strings.create_wallet_account_payment_sent_message)
               }, 750)
@@ -172,7 +173,7 @@ export const createAccountTransaction =
         alternateBroadcast:
           createdCurrencyWallet.otherMethods.submitActivationPayment != null ? createdCurrencyWallet.otherMethods.submitActivationPayment : undefined
       }
-      Actions.push('send', {
+      navigation.push('send', {
         guiMakeSpendInfo,
         selectedWalletId: paymentWalletId,
         selectedCurrencyCode: currencyCode
@@ -186,6 +187,8 @@ export const createAccountTransaction =
 export const createHandleUnavailableModal = (newWalletId: string, accountName: string) => async (dispatch: Dispatch, getState: GetState) => {
   const state = getState()
   const { account } = state.core
+  const navigation: NavigationProp<'edge'> = useNavigation()
+
   account.changeWalletStates({
     [newWalletId]: {
       deleted: true
@@ -199,5 +202,5 @@ export const createHandleUnavailableModal = (newWalletId: string, accountName: s
       buttons={{ ok: { label: s.strings.string_ok } }}
     />
   ))
-  Actions.pop()
+  navigation.pop()
 }
--- src/actions/ScenesActions.js
+++ src/actions/ScenesActions.js
@@ -1,11 +1,13 @@
 // @flow
 import type { Dispatch, GetState } from '../types/reduxTypes.js'
-import { Actions } from '../types/routerTypes.js'
+import { type NavigationProp, useNavigation } from '../types/routerTypes.js'
 
 export const openDrawer = () => (dispatch: Dispatch, getState: GetState) => {
   const state = getState()
+
+  const navigation: NavigationProp<'edge'> = useNavigation()
   const selectedWallet = state.ui.wallets.selectedWalletId
-  if (selectedWallet.length > 0 && Actions.currentScene !== 'DrawerOpen') {
-    Actions.drawerOpen()
+  if (selectedWallet.length > 0 && navigation.currentScene !== 'DrawerOpen') {
+    navigation.openDrawer()
   }
 }
