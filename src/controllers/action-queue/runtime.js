4d39ce5d57e3f595e1cbf13f41fe2d2f5283a2b3
Samuel Holmes <sam@edge.app>
2022-07-26 15:34:04 -0700

    Handle all `actionOp.type` cases

--- src/controllers/action-queue/runtime.js
+++ src/controllers/action-queue/runtime.js
@@ -1,10 +1,13 @@
 // @flow
 
+import { add } from 'biggystring'
 import { type EdgeAccount } from 'edge-core-js'
 import * as React from 'react'
 
 import { AirshipToast } from '../../components/common/AirshipToast'
 import { Airship } from '../../components/services/AirshipInstance'
+import { queryBorrowPlugins } from '../../plugins/helpers/borrowPluginHelpers'
+import { getCurrencyCode } from '../../util/CurrencyInfoHelpers'
 import { snooze } from '../../util/utils'
 import { type ActionEffect, type ActionProgram, type ActionProgramState, type ExecutionResult, type ExecutionResults } from './types'
 
@@ -92,6 +95,214 @@ async function executeActionOp(account: EdgeAccount, program: ActionProgram, sta
         effect: {
           type: 'par',
           childEffects: childResults.map(result => result.effect)
+        }
+      }
+    }
+
+    case 'exchange-buy': {
+      const { exchangePluginId, nativeAmount, tokenId, walletId } = actionOp
+      const wallet = account.currencyWallets[walletId]
+      const currencyCode = getCurrencyCode(wallet, tokenId)
+
+      // TODO: Remove this
+      await Airship.show(bridge => <AirshipToast bridge={bridge} message={`Buy ${nativeAmount} ${currencyCode} on ${exchangePluginId}`} />)
+      return {
+        effect: { type: 'unixtime', timestamp: Date.now() + 3000 }
+      }
+
+      // TOOD: Use exchange plugin ID to do an exchange buy
+      // TODO: Return this effect
+      // return {
+      //   type: 'balance',
+      //   address: destAddress,
+      //   aboveAmount: expectedNativeBalance,
+      //   walletId: string,
+      //   tokenId
+      // }
+    }
+    case 'exchange-sell': {
+      const { exchangePluginId, nativeAmount, tokenId, walletId } = actionOp
+      const wallet = account.currencyWallets[walletId]
+      const currencyCode = getCurrencyCode(wallet, tokenId)
+
+      // TODO: Remove this
+      await Airship.show(bridge => <AirshipToast bridge={bridge} message={`Buy ${nativeAmount} ${currencyCode} on ${exchangePluginId}`} />)
+      return {
+        effect: { type: 'unixtime', timestamp: Date.now() + 3000 }
+      }
+
+      // TOOD: Use exchange plugin ID to do an exchange sell
+      // 1. Fetch address for currencyCode from https://api.sendwyre.com/v2/paymentMethods
+      // 2. Send native amount to address
+      // 3. Return this effect:
+      // return {
+      //   type: 'tx-confs',
+      //   txId,
+      //   walletId,
+      //   confirmations: 1
+      // }
+    }
+    case 'loan-borrow': {
+      const { borrowPluginId, nativeAmount, walletId, tokenId } = actionOp
+
+      const wallet = account.currencyWallets[walletId]
+      if (wallet == null) throw new Error(`Wallet '${walletId}' not found`)
+
+      // Get the borrow-plugin
+      const borrowPlugin = queryBorrowPlugins({ borrowPluginId })[0]
+
+      if (borrowPlugin == null) throw new Error(`Borrow plugin '${borrowPluginId}' not found`)
+
+      // Make borrow engine for wallet
+      const borrowEngine = await borrowPlugin.makeBorrowEngine(wallet)
+
+      // Do the thing
+      const approvableAction = await borrowEngine.borrow({ nativeAmount, tokenId })
+      const txs = await approvableAction.approve()
+
+      // Construct a tx-conf effect
+      const txId = txs[txs.length - 1].txid
+      return {
+        effect: {
+          type: 'tx-confs',
+          txId,
+          walletId,
+          confirmations: 1
+        }
+      }
+    }
+    case 'loan-deposit': {
+      const { borrowPluginId, nativeAmount, walletId, tokenId } = actionOp
+
+      const wallet = account.currencyWallets[walletId]
+      if (wallet == null) throw new Error(`Wallet '${walletId}' not found`)
+
+      // Get the borrow-plugin
+      const borrowPlugin = queryBorrowPlugins({ borrowPluginId })[0]
+
+      if (borrowPlugin == null) throw new Error(`Borrow plugin '${borrowPluginId}' not found`)
+
+      // Make borrow engine for wallet
+      const borrowEngine = await borrowPlugin.makeBorrowEngine(wallet)
+
+      // Do the thing
+      const approvableAction = await borrowEngine.deposit({ nativeAmount, tokenId })
+      const txs = await approvableAction.approve()
+
+      // Construct a tx-conf effect
+      const txId = txs[txs.length - 1].txid
+      return {
+        effect: {
+          type: 'tx-confs',
+          txId,
+          walletId,
+          confirmations: 1
+        }
+      }
+    }
+    case 'loan-repay': {
+      const { borrowPluginId, nativeAmount, walletId, tokenId } = actionOp
+
+      const wallet = account.currencyWallets[walletId]
+      if (wallet == null) throw new Error(`Wallet '${walletId}' not found`)
+
+      // Get the borrow-plugin
+      const borrowPlugin = queryBorrowPlugins({ borrowPluginId })[0]
+
+      if (borrowPlugin == null) throw new Error(`Borrow plugin '${borrowPluginId}' not found`)
+
+      // Make borrow engine for wallet
+      const borrowEngine = await borrowPlugin.makeBorrowEngine(wallet)
+
+      // Do the thing
+      const approvableAction = await borrowEngine.repay({ nativeAmount, tokenId })
+      const txs = await approvableAction.approve()
+
+      // Construct a tx-conf effect
+      const txId = txs[txs.length - 1].txid
+      return {
+        effect: {
+          type: 'tx-confs',
+          txId,
+          walletId,
+          confirmations: 1
+        }
+      }
+    }
+    case 'loan-withdraw': {
+      const { borrowPluginId, nativeAmount, walletId, tokenId } = actionOp
+
+      const wallet = account.currencyWallets[walletId]
+      if (wallet == null) throw new Error(`Wallet '${walletId}' not found`)
+
+      // Get the borrow-plugin
+      const borrowPlugin = queryBorrowPlugins({ borrowPluginId })[0]
+
+      if (borrowPlugin == null) throw new Error(`Borrow plugin '${borrowPluginId}' not found`)
+
+      // Make borrow engine for wallet
+      const borrowEngine = await borrowPlugin.makeBorrowEngine(wallet)
+
+      // Do the thing
+      const approvableAction = await borrowEngine.withdraw({ nativeAmount, tokenId })
+      const txs = await approvableAction.approve()
+
+      // Construct a tx-conf effect
+      const txId = txs[txs.length - 1].txid
+      return {
+        effect: {
+          type: 'tx-confs',
+          txId,
+          walletId,
+          confirmations: 1
+        }
+      }
+    }
+    case 'swap': {
+      const { fromTokenId, fromWalletId, nativeAmount, toTokenId, toWalletId } = actionOp
+
+      const fromWallet = account.currencyWallets[fromWalletId]
+      if (fromWallet == null) throw new Error(`Wallet '${fromWalletId}' not found for fromWalletId`)
+
+      const toWallet = account.currencyWallets[toWalletId]
+      if (toWallet == null) throw new Error(`Wallet '${toWalletId}' not found for toWalletId`)
+
+      const fromCurrencyCode = getCurrencyCode(fromWallet, fromTokenId)
+      const toCurrencyCode = getCurrencyCode(toWallet, toTokenId)
+
+      const swapQuote = await account.fetchSwapQuote({
+        fromWallet,
+        toWallet,
+        fromCurrencyCode,
+        toCurrencyCode,
+        nativeAmount,
+        quoteFor: 'from'
+      })
+      const swapResult = await swapQuote.approve()
+
+      // TOOD: Enable this when we can query wallet address balances
+      if (swapResult.destinationAddress) {
+        // const currentAddressBalance = (await toWallet.getReceiveAddress({ currencyCode: toCurrencyCode })).nativeAmount
+        // const aboveAmount = add(currentAddressBalance, swapQuote.toNativeAmount)
+        // return {
+        //   type: 'balance',
+        //   address: swapResult.destinationAddress,
+        //   aboveAmount,
+        //   walletId: toWalletId,
+        //   tokenId: toTokenId
+        // }
+      }
+
+      // Fallback to wallet balance:
+      const walletBalance = toWallet.balances[toCurrencyCode] ?? '0'
+      const aboveAmount = add(walletBalance, swapQuote.toNativeAmount)
+      return {
+        effect: {
+          type: 'address-balance',
+          address: '',
+          aboveAmount,
+          walletId: toWalletId,
+          tokenId: toTokenId
         }
       }
     }
