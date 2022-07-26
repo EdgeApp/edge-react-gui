25bce1c990ea3dc811edd8516b174ceddb7d709f
Samuel Holmes <sam@edge.app>
2022-07-26 14:40:20 -0700

    Add ActionQueue runtime

--- src/components/services/ActionQueue.js
+++ src/components/services/ActionQueue.js
@@ -3,13 +3,18 @@
 import { type EdgeAccount } from 'edge-core-js'
 
 import { makeActionQueueStore } from '../../controllers/action-queue/ActionQueueStore'
-import { loadActionQueueState } from '../../controllers/action-queue/redux/actions'
+import { loadActionQueueState, updateActionProgramState } from '../../controllers/action-queue/redux/actions'
+import { executeActionProgram } from '../../controllers/action-queue/runtime'
+import { type ActionQueueMap } from '../../controllers/action-queue/types'
 import { useAsyncEffect } from '../../hooks/useAsyncEffect'
+import { useRef } from '../../types/reactHooks'
 import { useDispatch, useSelector } from '../../types/reactRedux'
 
 export const ActionQueue = () => {
   const dispatch = useDispatch()
   const account: EdgeAccount = useSelector(state => state.core.account)
+  const queue: ActionQueueMap = useSelector(state => state.actionQueue.queue)
+  const executingRef = useRef<{ [programId: string]: boolean }>({})
 
   //
   // Initialization
@@ -22,6 +27,42 @@ export const ActionQueue = () => {
       dispatch(loadActionQueueState(queue))
     }
   }, [account, dispatch])
+
+  //
+  // Runtime
+  //
+
+  useAsyncEffect(async () => {
+    if (queue != null) {
+      const executing = executingRef.current
+      const promises = Object.keys(queue)
+        .filter(
+          programId =>
+            // Ignore running programs
+            !executing[programId]
+        )
+        .map(async programId => {
+          // Set program to running
+          executing[programId] = true
+
+          const { program, state } = queue[programId]
+          const { nextState } = await executeActionProgram(account, program, state).catch((error: Error) => ({
+            nextState: {
+              ...state,
+              effect: { type: 'done', error }
+            }
+          }))
+
+          // Update program state
+          dispatch(updateActionProgramState(nextState))
+
+          // Unset program to running
+          executing[programId] = false
+        })
+
+      await Promise.all(promises)
+    }
+  }, [queue])
 
   return null
 }
