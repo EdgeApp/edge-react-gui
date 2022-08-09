EdgeApp/edge-react-gui

commit 5d7ae1a172d0cc5e6508c0fb017f58c2006775cd

Merge goodluckh/replace-actions-w-navigation-components
Conflicts:
- Added navigation to AAVE scenes in Main.ui.js
- Integrated changes in ControlPanel
0982e0
- Pull test files from merging branch and update snapshots

src/components/services/AccountCallbackManager.js
@@ -12,7 +12,7 @@ import { useAsyncEffect } from '../../hooks/useAsyncEffect.js'
 import { useWalletsSubscriber } from '../../hooks/useWalletsSubscriber.js'
 import { useEffect, useState } from '../../types/reactHooks.js'
 import { useDispatch } from '../../types/reactRedux.js'
-import { Actions } from '../../types/routerTypes.js'
+import { type NavigationProp, useNavigation } from '../../types/routerTypes.js'
 import { isReceivedTransaction, snooze } from '../../util/utils.js'
 import { WcSmartContractModal } from '../modals/WcSmartContractModal.js'
 import { Airship } from './AirshipInstance.js'
@@ -38,6 +38,7 @@ export function AccountCallbackManager(props: Props) {
   const { account } = props
   const dispatch = useDispatch()
   const [dirty, setDirty] = useState<DirtyList>(notDirty)
+  const navigation: NavigationProp<'edge'> = useNavigation()
 
   // Helper for marking wallets dirty:
   function addWallet(wallet: EdgeCurrencyWallet) {
@@ -66,9 +67,7 @@ export function AccountCallbackManager(props: Props) {
       }),
 
       watchSecurityAlerts(account, hasAlerts => {
-        if (hasAlerts && Actions.currentScene !== 'securityAlerts') {
-          Actions.push('securityAlerts')
-        }
+        navigation.push('securityAlerts')
       }),
 
       account.rateCache.on('update', () =>

src/__tests__/TransactionDetails.ui.test.js
@@ -6,6 +6,7 @@ import ShallowRenderer from 'react-test-renderer/shallow'
 
 import { TransactionDetailsComponent } from '../components/scenes/TransactionDetailsScene.js'
 import { getTheme } from '../components/services/ThemeContext.js'
+import { type NavigationProp, useNavigation } from '../types/routerTypes.js'
 import { type GuiWallet } from '../types/types.js'
 
 const fakeGuiWallet: GuiWallet = {
@@ -61,6 +62,7 @@ describe('TransactionDetails.ui', () => {
           thumbnailPath: 'thumb/nail/path'
         }
       },
+
       contacts: [],
       subcategoriesList: [],
       currencyCode: 'BTC',
@@ -73,7 +75,8 @@ describe('TransactionDetails.ui', () => {
       displayDropdownAlert: jest.fn(),
       theme: getTheme()
     }
-    const actual = renderer.render(<TransactionDetailsComponent {...props} />)
+    const navigation: NavigationProp<'transactionDetails'> = useNavigation()
+    const actual = renderer.render(<TransactionDetailsComponent navigation={navigation} {...props} />)
 
     expect(actual).toMatchSnapshot()
   })
@@ -111,7 +114,9 @@ describe('TransactionDetails.ui', () => {
       displayDropdownAlert: jest.fn(),
       theme: getTheme()
     }
-    const actual = renderer.render(<TransactionDetailsComponent {...props} />)
+
+    const navigation: NavigationProp<'transactionDetails'> = useNavigation()
+    const actual = renderer.render(<TransactionDetailsComponent navigation={navigation} {...props} />)
 
     expect(actual).toMatchSnapshot()
   })
@@ -150,7 +155,8 @@ describe('TransactionDetails.ui', () => {
       displayDropdownAlert: jest.fn(),
       theme: getTheme()
     }
-    const actual = renderer.render(<TransactionDetailsComponent {...props} />)
+    const navigation: NavigationProp<'transactionDetails'> = useNavigation()
+    const actual = renderer.render(<TransactionDetailsComponent navigation={navigation} {...props} />)
 
     expect(actual).toMatchSnapshot()
   })
@@ -191,8 +197,8 @@ describe('TransactionDetails.ui', () => {
       displayDropdownAlert: jest.fn(),
       theme: getTheme()
     }
-    const actual = renderer.render(<TransactionDetailsComponent {...props} />)
-
+    const navigation: NavigationProp<'transactionDetails'> = useNavigation()
+    const actual = renderer.render(<TransactionDetailsComponent navigation={navigation} {...props} />)
     expect(actual).toMatchSnapshot()
   })
 })

src/__tests__/components/TransactionListRow.test.js
@@ -2,59 +2,31 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
 import * as React from 'react'
-import { Provider } from 'react-redux'
-import renderer from 'react-test-renderer'
-import { createStore } from 'redux'
+import ShallowRenderer from 'react-test-renderer/shallow'
 
-import { TransactionListRow } from '../../components/themed/TransactionListRow.js'
-import { rootReducer } from '../../reducers/RootReducer.js'
+import { getTheme } from '../../components/services/ThemeContext.js'
+import { TransactionListRowComponent } from '../../components/themed/TransactionListRow.js'
 
 describe('TransactionListRow', () => {
   it('should render with loading props', () => {
-    const currencyInfo = {
-      pluginId: 'bitcoin',
-      currencyCode: 'BTC',
-      displayName: 'Bitcoin',
-      requiredConfirmations: 1,
-      denominations: [
-        { name: 'BTC', multiplier: '100000000', symbol: '₿' },
-        { name: 'mBTC', multiplier: '100000', symbol: 'm₿' },
-        { name: 'bits', multiplier: '100', symbol: 'ƀ' },
-        { name: 'sats', multiplier: '1', symbol: 's' }
-      ]
-    }
-    const mockStore = {
-      core: {
-        account: {
-          currencyWallets: {
-            lmnop: {
-              pluginId: 'bitcoin',
-              watch: () => {},
-              currencyInfo,
-              fiatCurrencyCode: 'iso:USD'
-            }
-          },
-          currencyConfig: {
-            bitcoin: {
-              allTokens: [],
-              currencyInfo
-            }
-          }
-        }
-      }
-    }
-    const store = createStore(rootReducer, mockStore)
+    const renderer = new ShallowRenderer()
 
     const props = {
-      walletId: 'lmnop',
+      cryptoAmount: '1',
+      denominationSymbol: 'BTC',
+      fiatAmount: '1000000',
+      fiatSymbol: 'USD',
+      isSentTransaction: true,
+      requiredConfirmations: 11,
+      selectedCurrencyName: 'Bitcoin',
+      thumbnailPath: 'wyre.png',
+      walletBlockHeight: 11,
+      walletId: '3dfgbA347...',
       currencyCode: 'BTC',
-      transaction: '12ser4hh...'
+      transaction: '12ser4hh...',
+      theme: getTheme()
     }
-    const actual = renderer.create(
-      <Provider store={store}>
-        <TransactionListRow {...props} />
-      </Provider>
-    )
+    const actual = renderer.render(<TransactionListRowComponent {...props} />)
 
     expect(actual).toMatchSnapshot()
   })

src/__tests__/components/TransactionRow.test.js
@@ -1,54 +1,39 @@
 // @flow
 /* globals describe it expect */
 
+import { abs, toFixed } from 'biggystring'
 import { type EdgeTransaction } from 'edge-core-js'
 import * as React from 'react'
-import { Provider } from 'react-redux'
-import renderer from 'react-test-renderer'
-import { createStore } from 'redux'
+import ShallowRenderer from 'react-test-renderer/shallow'
 
-import { TransactionListRow } from '../../components/themed/TransactionListRow.js'
-import { rootReducer } from '../../reducers/RootReducer.js'
+import { getTheme } from '../../components/services/ThemeContext.js'
+import { TransactionListRowComponent } from '../../components/themed/TransactionListRow.js'
+import { getSymbolFromCurrency } from '../../constants/WalletAndCurrencyConstants.js'
+import { formatNumber } from '../../locales/intl.js'
+import { type GuiWallet } from '../../types/types.js'
+import { convertNativeToDisplay, decimalOrZero, isSentTransaction, truncateDecimals } from '../../util/utils'
 
 describe('Transaction List Row', () => {
   it('should render props', () => {
-    const currencyInfo = {
-      pluginId: 'bitcoin',
-      currencyCode: 'BTC',
-      displayName: 'Bitcoin',
-      requiredConfirmations: 1,
-      denominations: [
-        { name: 'BTC', multiplier: '100000000', symbol: '₿' },
-        { name: 'mBTC', multiplier: '100000', symbol: 'm₿' },
-        { name: 'bits', multiplier: '100', symbol: 'ƀ' },
-        { name: 'sats', multiplier: '1', symbol: 's' }
-      ]
+    const renderer = new ShallowRenderer()
+    const guiWallet: GuiWallet = {
+      id: 'SXq1f3x21H2e/h5A4ANvrMoK5xs+sQcDoFWHtCG25BA=',
+      type: 'wallet:monero',
+      name: 'Monero',
+      pluginId: 'monero',
+      primaryNativeBalance: '1492780012',
+      nativeBalances: { XMR: '1492780012' },
+      currencyNames: { XMR: 'Monero' },
+      currencyCode: 'XMR',
+      isoFiatCurrencyCode: 'iso:USD',
+      fiatCurrencyCode: 'USD',
+      metaTokens: [],
+      enabledTokens: [],
+      blockHeight: 1688551
     }
-    const mockStore: any = {
-      core: {
-        account: {
-          currencyWallets: {
-            lmnop: {
-              pluginId: 'bitcoin',
-              watch: () => {},
-              currencyInfo,
-              fiatCurrencyCode: 'iso:USD'
-            }
-          },
-          currencyConfig: {
-            bitcoin: {
-              allTokens: [],
-              currencyInfo
-            }
-          }
-        }
-      }
-    }
-    const store = createStore(rootReducer, mockStore)
-
     const amountFiat = 4424808418353299.5
 
-    const wallet: any = { id: 'qrstuv', type: 'wallet:monero' }
+    const wallet: any = { id: 'SXq1f3x21H…', type: 'wallet:monero' }
     const transaction: EdgeTransaction = {
       blockHeight: 1683022,
       date: 1539555412.068,
@@ -67,18 +52,31 @@ describe('Transaction List Row', () => {
       key: 0,
       metadata: { amountFiat }
     }
-    const props: any = {
-      walletId: 'lmnop',
-      currencyCode: 'BTC',
-      transaction: transaction
+    const displayDenomination = {
+      multiplier: '1000000000000',
+      name: 'XMR',
+      symbol: '‎ɱ'
     }
-
-    // TODO: Test TransactionRow component
-    const actual = renderer.create(
-      <Provider store={store}>
-        <TransactionListRow {...props} />
-      </Provider>
-    )
+    // CryptoAmount
+    const cryptoAmount = convertNativeToDisplay(displayDenomination.multiplier)(abs(transaction.nativeAmount ?? ''))
+    const cryptoAmountFormat = formatNumber(decimalOrZero(truncateDecimals(cryptoAmount), 6))
+    // FiatAmount
+    const fiatAmount = abs(amountFiat.toFixed(2))
+    const fiatAmountFormat = formatNumber(toFixed(fiatAmount, 2, 2), { toFixed: 2 })
+    const props: any = {
+      cryptoAmount: cryptoAmountFormat,
+      denominationSymbol: displayDenomination.symbol,
+      fiatAmount: fiatAmountFormat,
+      fiatSymbol: getSymbolFromCurrency(guiWallet.fiatCurrencyCode),
+      isSentTransaction: isSentTransaction(transaction),
+      requiredConfirmations: 15,
+      selectedCurrencyName: guiWallet.currencyNames[guiWallet.currencyCode],
+      thumbnailPath: '',
+      walletBlockHeight: guiWallet.blockHeight,
+      transaction: transaction,
+      theme: getTheme()
+    }
+    const actual = renderer.render(<TransactionListRowComponent {...props} />)
 
     expect(actual).toMatchSnapshot()
   })

src/__tests__/components/__snapshots__/TransactionListRow.test.js.snap
@@ -1,267 +1,16 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
 exports[`TransactionListRow should render with loading props 1`] = `
-<View
-  accessible={true}
-  collapsable={false}
-  focusable={true}
-  onClick={[Function]}
-  onResponderGrant={[Function]}
-  onResponderMove={[Function]}
-  onResponderRelease={[Function]}
-  onResponderTerminate={[Function]}
-  onResponderTerminationRequest={[Function]}
-  onStartShouldSetResponder={[Function]}
-  style={
-    Object {
-      "opacity": 1,
-    }
-  }
->
-  <View
-    style={
-      Array [
-        Object {
-          "alignItems": "center",
-          "flexDirection": "row",
-          "height": 96,
-          "justifyContent": "center",
-          "paddingHorizontal": 22,
-        },
-        Object {
-          "marginBottom": 0,
-          "marginLeft": 0,
-          "marginRight": 0,
-          "marginTop": 0,
-        },
-        Object {
-          "paddingBottom": 0,
-          "paddingLeft": 22,
-          "paddingRight": 22,
-          "paddingTop": 0,
-        },
-        null,
-        null,
-      ]
-    }
-  >
-    <View
-      style={
-        Object {
-          "marginRight": 22,
-        }
-      }
-    >
-      <View
-        style={
-          Array [
-            Object {
-              "alignItems": "center",
-              "borderRadius": 17,
-              "borderWidth": 2,
-              "height": 45,
-              "justifyContent": "center",
-              "width": 45,
-              "zIndex": 1,
-            },
-            Object {
-              "borderColor": "#77C513",
-              "elevation": 3,
-              "shadowColor": "#77C513",
-              "shadowOffset": Object {
-                "height": 0,
-                "width": 0,
-              },
-              "shadowOpacity": 0.3,
-              "shadowRadius": 11,
-            },
-            Object {
-              "backgroundColor": "#0C446A",
-            },
-          ]
-        }
-      >
-        <Text
-          allowFontScaling={false}
-          style={
-            Array [
-              Object {
-                "color": "#77C513",
-                "fontSize": 28,
-              },
-              Object {
-                "marginLeft": 1,
-              },
-              Object {
-                "fontFamily": "Ionicons",
-                "fontStyle": "normal",
-                "fontWeight": "normal",
-              },
-              Object {},
-            ]
-          }
-        >
-          
-        </Text>
-      </View>
-      <View
-        style={
-          Array [
-            Object {
-              "overflow": "hidden",
-            },
-            Object {
-              "borderRadius": 17,
-              "height": 45,
-              "position": "absolute",
-              "width": 45,
-            },
-          ]
-        }
-      >
-        <FastImageView
-          resizeMode="cover"
-          source={
-            Object {
-              "uri": undefined,
-            }
-          }
-          style={
-            Object {
-              "bottom": 0,
-              "left": 0,
-              "position": "absolute",
-              "right": 0,
-              "top": 0,
-            }
-          }
-        />
-      </View>
-    </View>
-    <View
-      style={
-        Object {
-          "flex": 1,
-          "justifyContent": "center",
-        }
-      }
-    >
-      <View
-        style={
-          Object {
-            "flexDirection": "row",
-          }
-        }
-      >
-        <Text
-          adjustsFontSizeToFit={true}
-          minimumFontScale={0.65}
-          numberOfLines={1}
-          style={
-            Array [
-              Object {
-                "color": "#FFFFFF",
-                "fontFamily": "Quicksand-Regular",
-                "fontSize": 22,
-                "includeFontPadding": false,
-              },
-              Object {
-                "flex": 1,
-                "fontFamily": "Quicksand-Medium",
-              },
-              null,
-            ]
-          }
-        >
-          Received Bitcoin
-        </Text>
-        <Text
-          adjustsFontSizeToFit={true}
-          minimumFontScale={0.65}
-          numberOfLines={1}
-          style={
-            Array [
-              Object {
-                "color": "#FFFFFF",
-                "fontFamily": "Quicksand-Regular",
-                "fontSize": 22,
-                "includeFontPadding": false,
-              },
-              Object {
-                "color": "#77C513",
-                "fontFamily": "Quicksand-Medium",
-                "marginLeft": 11,
-                "textAlign": "right",
-              },
-              null,
-            ]
-          }
-        >
-          + ₿ 0
-        </Text>
-      </View>
-      <View
-        style={
-          Object {
-            "flexDirection": "row",
-          }
-        }
-      >
-        <View
-          style={
-            Object {
-              "flex": 1,
-            }
-          }
-        >
-          <Text
-            adjustsFontSizeToFit={true}
-            minimumFontScale={0.65}
-            numberOfLines={1}
-            style={
-              Array [
-                Object {
-                  "color": "#FFFFFF",
-                  "fontFamily": "Quicksand-Regular",
-                  "fontSize": 22,
-                  "includeFontPadding": false,
-                },
-                Object {
-                  "color": "#F1AA19",
-                  "fontSize": 17,
-                },
-                null,
-              ]
-            }
-          >
-            Synchronizing
-          </Text>
-        </View>
-        <Text
-          adjustsFontSizeToFit={true}
-          minimumFontScale={0.65}
-          numberOfLines={1}
-          style={
-            Array [
-              Object {
-                "color": "#FFFFFF",
-                "fontFamily": "Quicksand-Regular",
-                "fontSize": 22,
-                "includeFontPadding": false,
-              },
-              Object {
-                "color": "#A4C7DF",
-                "fontSize": 17,
-                "textAlign": "right",
-              },
-              null,
-            ]
-          }
-        >
-          $ 0.00
-        </Text>
-      </View>
-    </View>
-  </View>
-</View>
+<WithTheme(TransactionRowComponent)
+  cryptoAmount="1"
+  denominationSymbol="BTC"
+  fiatAmount="1000000"
+  fiatSymbol="USD"
+  isSentTransaction={true}
+  onPress={[Function]}
+  requiredConfirmations={11}
+  selectedCurrencyName="Bitcoin"
+  thumbnailPath="wyre.png"
+  transaction="12ser4hh..."
+/>
 `;

src/__tests__/components/__snapshots__/TransactionRow.test.js.snap
@@ -1,267 +1,44 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
 exports[`Transaction List Row should render props 1`] = `
-<View
-  accessible={true}
-  collapsable={false}
-  focusable={true}
-  onClick={[Function]}
-  onResponderGrant={[Function]}
-  onResponderMove={[Function]}
-  onResponderRelease={[Function]}
-  onResponderTerminate={[Function]}
-  onResponderTerminationRequest={[Function]}
-  onStartShouldSetResponder={[Function]}
-  style={
+<WithTheme(TransactionRowComponent)
+  cryptoAmount="32.295514"
+  denominationSymbol="‎ɱ"
+  fiatAmount="4,424,808,418,353,299.50"
+  fiatSymbol="$"
+  isSentTransaction={true}
+  onPress={[Function]}
+  requiredConfirmations={15}
+  selectedCurrencyName="Monero"
+  thumbnailPath=""
+  transaction={
     Object {
-      "opacity": 1,
+      "SVGMetadataElement": Object {
+        "category": "",
+        "name": "ShapeShift",
+        "notes": "Exchanged …",
+      },
+      "amountSatoshi": -32295514330000,
+      "blockHeight": 1683022,
+      "currencyCode": "XMR",
+      "date": 1539555412.068,
+      "dateString": "Oct 14, 2018",
+      "key": 0,
+      "metadata": Object {
+        "amountFiat": 4424808418353299.5,
+      },
+      "nativeAmount": "-32295514330000",
+      "networkFee": "0",
+      "otherParams": Object {},
+      "ourReceiveAddresses": Array [],
+      "signedTx": "no_signature",
+      "time": "3:16 PM",
+      "txid": "4e92d23cff1714d52d48c0c5246adf4f6871d6d8d52d774b1b60cc4b28f8f296",
+      "wallet": Object {
+        "id": "SXq1f3x21H…",
+        "type": "wallet:monero",
+      },
     }
   }
->
-  <View
-    style={
-      Array [
-        Object {
-          "alignItems": "center",
-          "flexDirection": "row",
-          "height": 96,
-          "justifyContent": "center",
-          "paddingHorizontal": 22,
-        },
-        Object {
-          "marginBottom": 0,
-          "marginLeft": 0,
-          "marginRight": 0,
-          "marginTop": 0,
-        },
-        Object {
-          "paddingBottom": 0,
-          "paddingLeft": 22,
-          "paddingRight": 22,
-          "paddingTop": 0,
-        },
-        null,
-        null,
-      ]
-    }
-  >
-    <View
-      style={
-        Object {
-          "marginRight": 22,
-        }
-      }
-    >
-      <View
-        style={
-          Array [
-            Object {
-              "alignItems": "center",
-              "borderRadius": 17,
-              "borderWidth": 2,
-              "height": 45,
-              "justifyContent": "center",
-              "width": 45,
-              "zIndex": 1,
-            },
-            Object {
-              "borderColor": "#E85466",
-              "elevation": 3,
-              "shadowColor": "#E85466",
-              "shadowOffset": Object {
-                "height": 0,
-                "width": 0,
-              },
-              "shadowOpacity": 0.3,
-              "shadowRadius": 11,
-            },
-            Object {
-              "backgroundColor": "#0C446A",
-            },
-          ]
-        }
-      >
-        <Text
-          allowFontScaling={false}
-          style={
-            Array [
-              Object {
-                "color": "#E85466",
-                "fontSize": 28,
-              },
-              Object {
-                "marginLeft": 1,
-              },
-              Object {
-                "fontFamily": "Ionicons",
-                "fontStyle": "normal",
-                "fontWeight": "normal",
-              },
-              Object {},
-            ]
-          }
-        >
-          
-        </Text>
-      </View>
-      <View
-        style={
-          Array [
-            Object {
-              "overflow": "hidden",
-            },
-            Object {
-              "borderRadius": 17,
-              "height": 45,
-              "position": "absolute",
-              "width": 45,
-            },
-          ]
-        }
-      >
-        <FastImageView
-          resizeMode="cover"
-          source={
-            Object {
-              "uri": undefined,
-            }
-          }
-          style={
-            Object {
-              "bottom": 0,
-              "left": 0,
-              "position": "absolute",
-              "right": 0,
-              "top": 0,
-            }
-          }
-        />
-      </View>
-    </View>
-    <View
-      style={
-        Object {
-          "flex": 1,
-          "justifyContent": "center",
-        }
-      }
-    >
-      <View
-        style={
-          Object {
-            "flexDirection": "row",
-          }
-        }
-      >
-        <Text
-          adjustsFontSizeToFit={true}
-          minimumFontScale={0.65}
-          numberOfLines={1}
-          style={
-            Array [
-              Object {
-                "color": "#FFFFFF",
-                "fontFamily": "Quicksand-Regular",
-                "fontSize": 22,
-                "includeFontPadding": false,
-              },
-              Object {
-                "flex": 1,
-                "fontFamily": "Quicksand-Medium",
-              },
-              null,
-            ]
-          }
-        >
-          Sent Bitcoin
-        </Text>
-        <Text
-          adjustsFontSizeToFit={true}
-          minimumFontScale={0.65}
-          numberOfLines={1}
-          style={
-            Array [
-              Object {
-                "color": "#FFFFFF",
-                "fontFamily": "Quicksand-Regular",
-                "fontSize": 22,
-                "includeFontPadding": false,
-              },
-              Object {
-                "color": "#E85466",
-                "fontFamily": "Quicksand-Medium",
-                "marginLeft": 11,
-                "textAlign": "right",
-              },
-              null,
-            ]
-          }
-        >
-          - ₿ 322,955.1433
-        </Text>
-      </View>
-      <View
-        style={
-          Object {
-            "flexDirection": "row",
-          }
-        }
-      >
-        <View
-          style={
-            Object {
-              "flex": 1,
-            }
-          }
-        >
-          <Text
-            adjustsFontSizeToFit={true}
-            minimumFontScale={0.65}
-            numberOfLines={1}
-            style={
-              Array [
-                Object {
-                  "color": "#FFFFFF",
-                  "fontFamily": "Quicksand-Regular",
-                  "fontSize": 22,
-                  "includeFontPadding": false,
-                },
-                Object {
-                  "color": "#F1AA19",
-                  "fontSize": 17,
-                },
-                null,
-              ]
-            }
-          >
-            Synchronizing
-          </Text>
-        </View>
-        <Text
-          adjustsFontSizeToFit={true}
-          minimumFontScale={0.65}
-          numberOfLines={1}
-          style={
-            Array [
-              Object {
-                "color": "#FFFFFF",
-                "fontFamily": "Quicksand-Regular",
-                "fontSize": 22,
-                "includeFontPadding": false,
-              },
-              Object {
-                "color": "#A4C7DF",
-                "fontSize": 17,
-                "textAlign": "right",
-              },
-              null,
-            ]
-          }
-        >
-          $ 4424808418353299.50
-        </Text>
-      </View>
-    </View>
-  </View>
-</View>
+/>
 `;

commit 311254593951c3aff95eda786f17bd88414e1bdc

Fix unnecessary security alert popups
Commit 5d7ae1a1 broke this as it removed the check for hasAlerts and if the current scene is the SecurityAlerts scene.

src/components/services/AccountCallbackManager.js
@@ -12,7 +12,7 @@ import { useAsyncEffect } from '../../hooks/useAsyncEffect.js'
 import { useWalletsSubscriber } from '../../hooks/useWalletsSubscriber.js'
 import { useEffect, useState } from '../../types/reactHooks.js'
 import { useDispatch } from '../../types/reactRedux.js'
-import { type NavigationProp, useNavigation } from '../../types/routerTypes.js'
+import { type NavigationProp, Actions, useNavigation } from '../../types/routerTypes.js'
 import { isReceivedTransaction, snooze } from '../../util/utils.js'
 import { WcSmartContractModal } from '../modals/WcSmartContractModal.js'
 import { Airship } from './AirshipInstance.js'
@@ -67,7 +67,9 @@ export function AccountCallbackManager(props: Props) {
       }),
 
       watchSecurityAlerts(account, hasAlerts => {
-        navigation.push('securityAlerts')
+        if (hasAlerts && Actions.currentScene !== 'securityAlerts') {
+          navigation.push('securityAlerts')
+        }
       }),
 
       account.rateCache.on('update', () =>