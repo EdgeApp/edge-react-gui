33947a9a7ea9ac58ec05cfa7708cee64d5d52d36
Jon-edge <jon@edge.app>
2022-09-07 15:13:07 -0700

    Extend FiatAmountTile to accept a plain fiat amount

--- src/components/tiles/FiatAmountTile.js
+++ src/components/tiles/FiatAmountTile.js
@@ -2,30 +2,43 @@
 import { type EdgeCurrencyWallet } from 'edge-core-js'
 import * as React from 'react'
 
+import { formatFiatString } from '../../hooks/useFiatText.js'
+import { getDenomFromIsoCode } from '../../util/utils.js'
 import { type Theme, cacheStyles, useTheme } from '../services/ThemeContext.js'
 import { FiatText } from '../text/FiatText'
 import { EdgeText } from '../themed/EdgeText'
 import { Tile } from './Tile.js'
 
+// Either Fiat OR Crypto amount props must be provided.
 type Props = {
   title: string,
 
-  // The amount to show:
-  nativeCryptoAmount: string,
+  // Fiat amount props
+  fiatAmount?: string,
+
+  // Crypto amount props
+  nativeCryptoAmount?: string,
   tokenId?: string,
   wallet: EdgeCurrencyWallet
 }
 
 export const FiatAmountTile = (props: Props) => {
-  const { nativeCryptoAmount, title, tokenId, wallet } = props
+  const { fiatAmount, nativeCryptoAmount, title, tokenId, wallet } = props
+  if (fiatAmount == null && nativeCryptoAmount == null) throw new Error('Either fiat or crypto amount must be given to FiatAmountTile')
+
   const theme = useTheme()
   const styles = getStyles(theme)
 
+  const amountValue =
+    fiatAmount != null ? (
+      `${getDenomFromIsoCode(wallet.fiatCurrencyCode).symbol ?? ''}${formatFiatString({ fiatAmount })}`
+    ) : nativeCryptoAmount != null ? (
+      <FiatText tokenId={tokenId} nativeCryptoAmount={nativeCryptoAmount} wallet={wallet} />
+    ) : null
+
   return (
     <Tile type="static" title={title} contentPadding={false} style={styles.tileContainer}>
-      <EdgeText style={styles.tileBodyText}>
-        <FiatText tokenId={tokenId} nativeCryptoAmount={nativeCryptoAmount} wallet={wallet} />
-      </EdgeText>
+      <EdgeText>{amountValue}</EdgeText>
     </Tile>
   )
 }
@@ -34,8 +47,5 @@ const getStyles = cacheStyles((theme: Theme) => ({
   tileContainer: {
     flexDirection: 'row',
     alignItems: 'flex-start'
-  },
-  tileBodyText: {
-    fontSize: theme.rem(2)
   }
 }))
