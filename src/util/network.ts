64158ccf76d479812ec54e45cd4353a449937b98
Paul Puey <paul@edge.app>
2022-09-17 20:36:21 -0700

    Fix any and Object type usage

--- src/util/network.ts
+++ src/util/network.ts
@@ -1,4 +1,4 @@
-import { EdgeFetchFunction, EdgeFetchResponse } from 'edge-core-js'
+import { EdgeFetchFunction, EdgeFetchOptions, EdgeFetchResponse } from 'edge-core-js'
 
 import { config } from '../theme/appConfig'
 import { asyncWaterfall, shuffleArray } from './utils'
@@ -8,7 +8,7 @@ const RATES_SERVERS = ['https://rates1.edge.app', 'https://rates2.edge.app']
 export async function fetchWaterfall(
   servers: string[],
   path: string,
-  options?: any,
+  options?: EdgeFetchOptions,
   timeout: number = 5000,
   doFetch: EdgeFetchFunction = fetch
 ): Promise<EdgeFetchResponse> {
@@ -24,20 +24,25 @@ export async function fetchWaterfall(
   return asyncWaterfall(funcs, timeout)
 }
 
-// @ts-expect-error
-async function multiFetch(servers: string[], path: string, options?: any, timeout?: number = 5000, doFetch?: EdgeFetchFunction): Promise<any> {
+async function multiFetch(
+  servers: string[],
+  path: string,
+  options?: EdgeFetchOptions,
+  timeout: number = 5000,
+  doFetch?: EdgeFetchFunction
+): Promise<EdgeFetchResponse> {
   return fetchWaterfall(shuffleArray(servers), path, options, timeout, doFetch)
 }
 
-export const fetchInfo = async (path: string, options?: Object, timeout?: number, doFetch?: EdgeFetchFunction): Promise<any> => {
+export const fetchInfo = async (path: string, options?: EdgeFetchOptions, timeout?: number, doFetch?: EdgeFetchFunction): Promise<EdgeFetchResponse> => {
   return multiFetch(INFO_SERVERS, path, options, timeout, doFetch)
 }
-export const fetchRates = async (path: string, options?: Object, timeout?: number, doFetch?: EdgeFetchFunction): Promise<any> => {
+export const fetchRates = async (path: string, options?: EdgeFetchOptions, timeout?: number, doFetch?: EdgeFetchFunction): Promise<EdgeFetchResponse> => {
   return multiFetch(RATES_SERVERS, path, options, timeout, doFetch)
 }
-export const fetchReferral = async (path: string, options?: Object, timeout?: number, doFetch?: EdgeFetchFunction): Promise<any> => {
+export const fetchReferral = async (path: string, options?: EdgeFetchOptions, timeout?: number, doFetch?: EdgeFetchFunction): Promise<EdgeFetchResponse> => {
   return multiFetch(config.referralServers ?? [], path, options, timeout, doFetch)
 }
-export const fetchPush = async (path: string, options?: Object, timeout?: number, doFetch?: EdgeFetchFunction): Promise<any> => {
+export const fetchPush = async (path: string, options?: EdgeFetchOptions, timeout?: number, doFetch?: EdgeFetchFunction): Promise<EdgeFetchResponse> => {
   return multiFetch(config.notificationServers, path, options, timeout, doFetch)
 }
