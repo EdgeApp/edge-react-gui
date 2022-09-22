75ec0aab1f75a1940aad4f83a22bfc999faded86
Samuel Holmes <sam@edge.app>
2022-09-22 16:36:33 -0700

    Check dryrunOutput length before continuing dryrun phase routine
    This fixes a bug which crashes the program if the next immediate action
    is not able to be pre-computed.

--- src/controllers/action-queue/runtime.ts
+++ src/controllers/action-queue/runtime.ts
@@ -33,34 +33,36 @@ export const executeActionProgram = async (context: ExecutionContext, program: A
     try {
       const dryrunOutputs = await dryrunActionProgram(context, program, state, true)
 
-      // Convert each dryrun result into an array of push events for the push-server.
-      const newPushEvents = await prepareNewPushEvents(context, program, effect, dryrunOutputs)
+      if (dryrunOutputs.length > 0) {
+        // Convert each dryrun result into an array of push events for the push-server.
+        const newPushEvents = await prepareNewPushEvents(context, program, effect, dryrunOutputs)
 
-      // Send PushEvents to the push server:
-      await uploadPushEvents(context, { createEvents: newPushEvents })
+        // Send PushEvents to the push server:
+        await uploadPushEvents(context, { createEvents: newPushEvents })
 
-      // Mutate the nextState accordingly; effect should be awaiting push events:
-      const nextChildEffects: PushEventEffect[] = newPushEvents.map(event => ({
-        type: 'push-event',
-        eventId: event.eventId
-      }))
-      let nextEffect: ActionEffect
-      if (effect.type === 'seq') {
-        // Drop the last effect because it is to be replaced by the first push-event effect
-        const prevOpIndex = effect.opIndex // Same opIndex because the first of nextChildEffects replaces the last or prevChildEffects
-        const prevChildEffects = effect.childEffects.slice(0, -1) // Slice to drop the last of prevChildEffects
-        nextEffect = {
-          type: 'seq',
-          opIndex: prevOpIndex,
-          childEffects: [...prevChildEffects, ...nextChildEffects]
+        // Mutate the nextState accordingly; effect should be awaiting push events:
+        const nextChildEffects: PushEventEffect[] = newPushEvents.map(event => ({
+          type: 'push-event',
+          eventId: event.eventId
+        }))
+        let nextEffect: ActionEffect
+        if (effect.type === 'seq') {
+          // Drop the last effect because it is to be replaced by the first push-event effect
+          const prevOpIndex = effect.opIndex // Same opIndex because the first of nextChildEffects replaces the last or prevChildEffects
+          const prevChildEffects = effect.childEffects.slice(0, -1) // Slice to drop the last of prevChildEffects
+          nextEffect = {
+            type: 'seq',
+            opIndex: prevOpIndex,
+            childEffects: [...prevChildEffects, ...nextChildEffects]
+          }
+        } else {
+          if (nextChildEffects.length > 1) throw new Error('Unexpected push events length for non-seq/par program')
+          nextEffect = nextChildEffects[0]
         }
-      } else {
-        if (nextChildEffects.length > 1) throw new Error('Unexpected push events length for non-seq/par program')
-        nextEffect = nextChildEffects[0]
-      }
 
-      // Exit early with dryrun results:
-      return { nextState: { ...state, effect: nextEffect } }
+        // Exit early with dryrun results:
+        return { nextState: { ...state, effect: nextEffect } }
+      }
     } catch (error: any) {
       // Silently fail dryrun
       console.error(error)
