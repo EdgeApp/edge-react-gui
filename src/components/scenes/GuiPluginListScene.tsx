6cfcdd8d53a76434cf685bb75f80e7733f83db9d
Paul Puey <paul@edge.app>
2022-09-24 14:14:03 -0400

    Allow buy/sell plugins to be configured by info server

--- src/components/scenes/GuiPluginListScene.tsx
+++ src/components/scenes/GuiPluginListScene.tsx
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { asObject, asString } from 'cleaners'
+import { Disklet } from 'disklet'
 import { EdgeAccount } from 'edge-core-js/types'
 import * as React from 'react'
 import { FlatList, Image, Platform, TouchableOpacity, View } from 'react-native'
@@ -15,13 +16,14 @@ import s from '../../locales/strings'
 import { getSyncedSettings, setSyncedSettings } from '../../modules/Core/Account/settings'
 import { executePlugin } from '../../plugins/gui/fiatPlugin'
 import { config } from '../../theme/appConfig'
-import { asGuiPluginJson, GuiPluginRow } from '../../types/GuiPluginTypes'
+import { asBuySellPlugins, asGuiPluginJson, BuySellPlugins, GuiPluginRow } from '../../types/GuiPluginTypes'
 import { connect } from '../../types/reactRedux'
 import { AccountReferral } from '../../types/ReferralTypes'
 import { NavigationProp, RouteProp } from '../../types/routerTypes'
 import { PluginTweak } from '../../types/TweakTypes'
 import { getPartnerIconUri } from '../../util/CdnUris'
 import { filterGuiPluginJson } from '../../util/GuiPluginTools'
+import { fetchInfo } from '../../util/network'
 import { bestOfPlugins } from '../../util/ReferralHelpers'
 import { SceneWrapper } from '../common/SceneWrapper'
 import { ButtonsModal } from '../modals/ButtonsModal'
@@ -32,8 +34,10 @@ import { cacheStyles, Theme, ThemeProps, withTheme } from '../services/ThemeCont
 import { EdgeText } from '../themed/EdgeText'
 import { SceneHeader } from '../themed/SceneHeader'
 
-const buyPluginJson = asGuiPluginJson(require('../../constants/plugins/buyPluginList.json'))
-const sellPluginJson = asGuiPluginJson(require('../../constants/plugins/sellPluginList.json'))
+const buySellPlugins: BuySellPlugins = {
+  buy: asGuiPluginJson(require('../../constants/plugins/buyPluginList.json')),
+  sell: asGuiPluginJson(require('../../constants/plugins/sellPluginList.json'))
+}
 
 const paymentTypeLogosById = {
   applepay: 'paymentTypeLogoApplePay',
@@ -70,6 +74,7 @@ type StateProps = {
   account: EdgeAccount
   accountPlugins: PluginTweak[]
   accountReferral: AccountReferral
+  coreDisklet: Disklet
   countryCode: string
   developerModeOn: boolean
 }
@@ -81,21 +86,30 @@ type DispatchProps = {
 type Props = OwnProps & StateProps & DispatchProps & ThemeProps
 type State = {
   developerUri: string
+  buySellPlugins: BuySellPlugins
 }
 
+const BUY_SELL_PLUGIN_REFRESH_INTERVAL = 60000
 const MODAL_DATA_FILE = 'pluginModalTracker.json'
 const DEVELOPER_PLUGIN_KEY = 'developerPlugin'
+const PLUGIN_LIST_FILE = 'buySellPlugins.json'
 const asDeveloperUri = asObject({ uri: asString })
 
 class GuiPluginList extends React.PureComponent<Props, State> {
+  componentMounted: boolean
+  timeoutId: ReturnType<typeof setTimeout> | undefined
+
   constructor(props: Props) {
     super(props)
     this.state = {
-      developerUri: ''
+      developerUri: '',
+      buySellPlugins
     }
+    this.componentMounted = true
   }
 
   async componentDidMount() {
+    this.updatePlugins()
     await this.checkDisclaimer()
     this.checkCountry()
     const text = await AsyncStorage.getItem(DEVELOPER_PLUGIN_KEY)
@@ -103,6 +117,56 @@ class GuiPluginList extends React.PureComponent<Props, State> {
       const clean = asDeveloperUri(JSON.parse(text))
       this.setState({ developerUri: clean.uri })
     }
+  }
+
+  componentWillUnmount() {
+    this.componentMounted = false
+    if (this.timeoutId != null) clearTimeout(this.timeoutId)
+  }
+
+  setPluginState(plugins: BuySellPlugins): void {
+    if (this.componentMounted && plugins != null) {
+      this.setState({
+        buySellPlugins: {
+          buy: plugins.buy ?? buySellPlugins.buy,
+          sell: plugins.sell ?? buySellPlugins.sell
+        }
+      })
+    }
+  }
+
+  async updatePlugins() {
+    const { coreDisklet } = this.props
+    let diskPlugins
+    try {
+      const fileText = await coreDisklet.getText(PLUGIN_LIST_FILE)
+      diskPlugins = asBuySellPlugins(JSON.parse(fileText))
+      this.setPluginState(diskPlugins)
+    } catch (e: any) {
+      console.log(e.message, `Error opening ${PLUGIN_LIST_FILE}. Trying network instead`)
+    }
+    this.updatePluginsNetwork(diskPlugins)
+  }
+
+  async updatePluginsNetwork(diskPlugins: BuySellPlugins | undefined) {
+    const { coreDisklet } = this.props
+    let networkPlugins: BuySellPlugins
+    try {
+      const response = await fetchInfo(`v1/buySellPlugins/${config.appId ?? 'edge'}`)
+      const reply = await response.json()
+      networkPlugins = asBuySellPlugins(reply)
+      if (JSON.stringify(networkPlugins) !== JSON.stringify(diskPlugins)) {
+        await coreDisklet
+          .setText(PLUGIN_LIST_FILE, JSON.stringify(networkPlugins))
+          .then(() => (diskPlugins = networkPlugins))
+          .catch(e => console.error(e.message))
+        this.setPluginState(networkPlugins)
+      }
+    } catch (e: any) {
+      console.log(e.message)
+      // This is ok. We just use default values
+    }
+    this.timeoutId = setTimeout(async () => this.updatePluginsNetwork(diskPlugins), BUY_SELL_PLUGIN_REFRESH_INTERVAL)
   }
 
   /**
@@ -257,11 +321,12 @@ class GuiPluginList extends React.PureComponent<Props, State> {
   render() {
     const { accountPlugins, accountReferral, countryCode, developerModeOn, theme, route } = this.props
     const { direction } = route.params ?? { direction: 'buy' }
+    const { buy = [], sell = [] } = this.state.buySellPlugins
     const styles = getStyles(theme)
     const countryData = COUNTRY_CODES.find(country => country['alpha-2'] === countryCode)
 
     // Pick a filter based on our direction:
-    let plugins = filterGuiPluginJson(direction === 'buy' ? buyPluginJson : sellPluginJson, Platform.OS, countryCode)
+    let plugins = filterGuiPluginJson(direction === 'buy' ? buy : sell, Platform.OS, countryCode)
 
     // Filter disabled plugins:
     const activePlugins = bestOfPlugins(accountPlugins, accountReferral, undefined)
@@ -377,6 +442,7 @@ export const GuiPluginListScene = connect<StateProps, DispatchProps, OwnProps>(
     account: state.core.account,
     accountPlugins: state.account.referralCache.accountPlugins,
     accountReferral: state.account.accountReferral,
+    coreDisklet: state.core.disklet,
     countryCode: state.ui.settings.countryCode,
     developerModeOn: state.ui.settings.developerModeOn
   }),
