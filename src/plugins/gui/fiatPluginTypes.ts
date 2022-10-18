1860a94452bec8c5688b77a18a6b619229c75fd5
Paul Puey <paul@edge.app>
2022-10-18 00:15:10 -0700

    Fix asFiatPaymentType cleaner and add iach

--- src/plugins/gui/fiatPluginTypes.ts
+++ src/plugins/gui/fiatPluginTypes.ts
@@ -1,11 +1,11 @@
-import { asValue } from 'cleaners'
+import { asEither, asValue } from 'cleaners'
 import { EdgeAccount } from 'edge-core-js'
 
 import { EdgeTokenId } from '../../types/types'
 import { EnterAmountPoweredBy } from './scenes/EnterAmountScene'
 
-export const asFiatPaymentTypes = asValue('credit', 'applepay', 'googlepay')
-export type FiatPaymentType = 'credit' | 'applepay' | 'googlepay'
+export const asFiatPaymentType = asEither(asValue('credit'), asValue('applepay'), asValue('googlepay'), asValue('iach'))
+export type FiatPaymentType = ReturnType<typeof asFiatPaymentType>
 export type FiatPaymentTypes = FiatPaymentType[]
 
 export type FiatPluginGetMethodsResponse = {
--- src/types/GuiPluginTypes.ts
+++ src/types/GuiPluginTypes.ts
@@ -1,6 +1,6 @@
 import { asArray, asEither, asMap, asNull, asNumber, asObject, asOptional, asString } from 'cleaners'
 
-import { asFiatPaymentTypes, FiatPaymentType, FiatPluginFactory } from '../plugins/gui/fiatPluginTypes'
+import { asFiatPaymentType, FiatPaymentType, FiatPluginFactory } from '../plugins/gui/fiatPluginTypes'
 import { Permission } from '../reducers/PermissionsReducer'
 import { EdgeTokenId } from '../types/types'
 import { UriQueryMap } from './WebTypes'
@@ -89,7 +89,7 @@ const asGuiPluginJsonRow = asObject({
   deepQuery: asOptional(asMap(asEither(asString, asNull))),
 
   // Optional params to sent to native Fiat Plugins
-  paymentType: asOptional(asFiatPaymentTypes),
+  paymentType: asOptional(asFiatPaymentType),
 
   // List display options:
   title: asOptional(asString),
