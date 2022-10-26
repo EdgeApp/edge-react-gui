b63da3e86b5eddf253dfa8e1595cff441f110151
William Swanson <swansontec@gmail.com>
2022-10-26 10:46:20 -0700

    Fix the CategoryModal test to render deeply

--- src/__tests__/modals/CategoryModal.test.tsx
+++ src/__tests__/modals/CategoryModal.test.tsx
@@ -1,13 +1,18 @@
 import { describe, expect, it } from '@jest/globals'
 import * as React from 'react'
-import { createRenderer } from 'react-test-renderer/shallow'
+import { Provider } from 'react-redux'
+import renderer from 'react-test-renderer'
+import { createStore } from 'redux'
 
 import { CategoryModal } from '../../components/modals/CategoryModal'
 import s from '../../locales/strings'
+import { rootReducer } from '../../reducers/RootReducer'
 import { defaultCategories } from '../../util/categories'
 import { fakeAirshipBridge } from '../../util/fake/fakeAirshipBridge'
 
 describe('CategoryModal', () => {
+  const store = createStore(rootReducer)
+
   const categories = {
     exchange: {
       syntax: s.strings.fragment_transaction_exchange,
@@ -28,34 +33,34 @@ describe('CategoryModal', () => {
   }
 
   it('should render with an empty subcategory', () => {
-    const renderer = createRenderer()
-
-    const actual = renderer.render(
-      <CategoryModal
-        bridge={fakeAirshipBridge}
-        categories={categories}
-        subCategories={defaultCategories.slice(0, 4)}
-        category="exchange"
-        subCategory=""
-        setNewSubcategory={(input: string, subCategories: string[]) => undefined}
-      />
+    const actual = renderer.create(
+      <Provider store={store}>
+        <CategoryModal
+          bridge={fakeAirshipBridge}
+          categories={categories}
+          subCategories={defaultCategories.slice(0, 4)}
+          category="exchange"
+          subCategory=""
+          setNewSubcategory={(input: string, subCategories: string[]) => undefined}
+        />
+      </Provider>
     )
 
     expect(actual).toMatchSnapshot()
   })
 
   it('should render with a subcategory', () => {
-    const renderer = createRenderer()
-
-    const actual = renderer.render(
-      <CategoryModal
-        bridge={fakeAirshipBridge}
-        categories={categories}
-        subCategories={defaultCategories.slice(0, 4)}
-        category="income"
-        subCategory="Paycheck"
-        setNewSubcategory={(input: string, subCategories: string[]) => undefined}
-      />
+    const actual = renderer.create(
+      <Provider store={store}>
+        <CategoryModal
+          bridge={fakeAirshipBridge}
+          categories={categories}
+          subCategories={defaultCategories.slice(0, 4)}
+          category="income"
+          subCategory="Paycheck"
+          setNewSubcategory={(input: string, subCategories: string[]) => undefined}
+        />
+      </Provider>
     )
 
     expect(actual).toMatchSnapshot()
