d8edbc5b3b6c6d6b0e6d6641ab229490b71df5f5
William Swanson <swansontec@gmail.com>
2022-10-26 10:56:26 -0700

    Update the CategoryModal testing props

--- src/__tests__/modals/CategoryModal.test.tsx
+++ src/__tests__/modals/CategoryModal.test.tsx
@@ -3,36 +3,57 @@ import * as React from 'react'
 import { createRenderer } from 'react-test-renderer/shallow'
 
 import { CategoryModal } from '../../components/modals/CategoryModal'
+import s from '../../locales/strings'
+import { defaultCategories } from '../../util/categories'
 import { fakeAirshipBridge } from '../../util/fake/fakeAirshipBridge'
 
 describe('CategoryModal', () => {
-  it('should render with loading props', () => {
+  const categories = {
+    exchange: {
+      syntax: s.strings.fragment_transaction_exchange,
+      key: 'exchange'
+    },
+    expense: {
+      syntax: s.strings.fragment_transaction_expense,
+      key: 'expense'
+    },
+    transfer: {
+      syntax: s.strings.fragment_transaction_transfer,
+      key: 'transfer'
+    },
+    income: {
+      syntax: s.strings.fragment_transaction_income,
+      key: 'income'
+    }
+  }
+
+  it('should render with an empty subcategory', () => {
     const renderer = createRenderer()
 
     const actual = renderer.render(
       <CategoryModal
         bridge={fakeAirshipBridge}
-        categories={{
-          exchange: {
-            syntax: 'Hello exchange',
-            key: 'exchange'
-          },
-          expense: {
-            syntax: 'Hello expense',
-            key: 'expense'
-          },
-          transfer: {
-            syntax: 'Hello transfer',
-            key: 'transfer'
-          },
-          income: {
-            syntax: 'Hello income',
-            key: 'income'
-          }
-        }}
-        subCategories={['exchange', 'income', 'expense']}
+        categories={categories}
+        subCategories={defaultCategories.slice(0, 4)}
         category="exchange"
         subCategory=""
+        setNewSubcategory={(input: string, subCategories: string[]) => undefined}
+      />
+    )
+
+    expect(actual).toMatchSnapshot()
+  })
+
+  it('should render with a subcategory', () => {
+    const renderer = createRenderer()
+
+    const actual = renderer.render(
+      <CategoryModal
+        bridge={fakeAirshipBridge}
+        categories={categories}
+        subCategories={defaultCategories.slice(0, 4)}
+        category="income"
+        subCategory="Paycheck"
         setNewSubcategory={(input: string, subCategories: string[]) => undefined}
       />
     )
