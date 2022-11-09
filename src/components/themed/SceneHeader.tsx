87b33578696b8e0d058088516bf25dea31ab13c1
Samuel Holmes <sam@edge.app>
2022-11-09 13:41:48 -0800

    Add `tertiary` prop to SceneHeader

--- src/components/themed/SceneHeader.tsx
+++ src/components/themed/SceneHeader.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { View, ViewStyle } from 'react-native'
 
+import { Space } from '../layout/Space'
 import { cacheStyles, Theme, useTheme } from '../services/ThemeContext'
 import { DividerLine } from './DividerLine'
 import { EdgeText } from './EdgeText'
@@ -8,13 +9,14 @@ import { EdgeText } from './EdgeText'
 interface Props {
   title?: string
   children?: React.ReactNode
+  tertiary?: React.ReactNode
   underline?: boolean
   withTopMargin?: boolean
   style?: ViewStyle
 }
 
 export const SceneHeaderComponent = (props: Props) => {
-  const { title, underline, withTopMargin, children, style } = props
+  const { title, underline, withTopMargin, children, tertiary = null, style } = props
   const theme = useTheme()
   const styles = getStyles(theme)
   return (
@@ -22,6 +24,7 @@ export const SceneHeaderComponent = (props: Props) => {
       <View style={[styles.container, withTopMargin ? styles.topMargin : null, style]}>
         {title ? <EdgeText style={styles.title}>{title}</EdgeText> : null}
         {children}
+        {tertiary ? <Space right>{tertiary}</Space> : null}
       </View>
       <View style={styles.dividerLine}>{underline ? <DividerLine /> : null}</View>
     </>
@@ -30,9 +33,11 @@ export const SceneHeaderComponent = (props: Props) => {
 
 const getStyles = cacheStyles((theme: Theme) => ({
   container: {
-    justifyContent: 'center',
     marginLeft: theme.rem(1),
-    paddingBottom: theme.rem(1)
+    paddingBottom: theme.rem(1),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   topMargin: {
     marginTop: theme.rem(1)
