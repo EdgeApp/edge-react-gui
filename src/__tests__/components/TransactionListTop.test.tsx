23b97b459358639c7da246d32ae3294d015a6fd5
Matthew <matthew@edge.app>
2022-11-10 14:48:22 -0800

    Deeply test `TransactionListTop`

--- src/__tests__/components/TransactionListTop.test.tsx
+++ src/__tests__/components/TransactionListTop.test.tsx
@@ -1,44 +1,89 @@
 import { describe, expect, it } from '@jest/globals'
+import { EdgeCurrencyInfo } from 'edge-core-js'
 import * as React from 'react'
-import { createRenderer } from 'react-test-renderer/shallow'
+import { Provider } from 'react-redux'
+import TestRenderer from 'react-test-renderer'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
 
-import { getTheme } from '../../components/services/ThemeContext'
-import { TransactionListTopComponent } from '../../components/themed/TransactionListTop'
+import { TransactionListTop } from '../../components/themed/TransactionListTop'
+import { rootReducer } from '../../reducers/RootReducer'
+import { makeGuiWallet } from '../../reducers/scenes/WalletsReducer'
 import { fakeNavigation } from '../../util/fake/fakeNavigation'
 
 describe('TransactionListTop', () => {
-  it('should render with loading props', () => {
-    const renderer = createRenderer()
+  const currencyInfo: EdgeCurrencyInfo = {
+    pluginId: 'bitcoin',
+    currencyCode: 'BTC',
+    displayName: 'Bitcoin',
+    walletType: 'bitcoin',
 
-    const actual = renderer.render(
-      <TransactionListTopComponent
-        cryptoAmount="string"
-        currencyCode="string"
-        pluginId="string"
-        denominationName="string"
-        fiatCurrencyCode="string"
-        fiatBalance="string"
-        fiatSymbol="string"
-        walletName="string"
-        isAccountBalanceVisible
-        stakingBalances={{
-          FIO: {
-            crypto: 'string',
-            fiat: 'string'
-          }
-        }}
-        onSelectWallet={() => undefined}
-        onChangeSortingState={() => undefined}
-        onSearchTransaction={() => undefined}
-        toggleBalanceVisibility={() => undefined}
-        walletId="string"
-        isEmpty={false}
-        searching={false}
-        navigation={fakeNavigation}
-        theme={getTheme()}
-      />
+    addressExplorer: '',
+    transactionExplorer: '',
+
+    defaultSettings: {},
+    metaTokens: [],
+    denominations: [
+      {
+        name: 'BTC',
+        multiplier: '100000000',
+        symbol: '₿'
+      }
+    ]
+  }
+
+  const fakeCurrencyConfig: any = {
+    currencyInfo,
+    allTokens: {},
+    builtinTokens: {},
+    customTokens: {}
+  }
+
+  const fakeWallet: any = {
+    balances: { BTC: '123123' },
+    blockHeight: 12345,
+    currencyConfig: fakeCurrencyConfig,
+    currencyInfo,
+    enabledTokenIds: [],
+    fiatCurrencyCode: 'USD',
+    id: '123',
+    name: 'wallet name',
+    type: 'wallet:bitcoin',
+    watch() {}
+  }
+
+  const fakeState: any = {
+    core: {
+      account: {
+        currencyWallets: { '123': fakeWallet },
+        currencyConfig: { bitcoin: fakeCurrencyConfig }
+      }
+    },
+    ui: {
+      wallets: {
+        byId: { '123': makeGuiWallet(fakeWallet) },
+        selectedWalletId: '123',
+        selectedCurrencyCode: 'BTC'
+      }
+    }
+  }
+
+  const store = createStore(rootReducer, fakeState, applyMiddleware(thunk))
+
+  it('should render', () => {
+    const renderer = TestRenderer.create(
+      <Provider store={store}>
+        <TransactionListTop
+          isEmpty={false}
+          navigation={fakeNavigation}
+          searching={false}
+          walletId="123"
+          onChangeSortingState={() => undefined}
+          onSearchTransaction={() => undefined}
+        />
+      </Provider>
     )
 
-    expect(actual).toMatchSnapshot()
+    expect(renderer.toJSON()).toMatchSnapshot()
   })
 })
