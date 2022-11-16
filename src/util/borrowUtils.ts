e57b4ff955def6fb793ab42bef3d7ebbb5ecf3e8
Jon Tzeng <jon@edge.app>
2022-11-15 18:39:13 -0800

    Implement getWalletPickerExcludeWalletIds First of a series of planned refactors. Future refactors will generate all wallet picker filtering props in a utility method.

--- src/util/borrowUtils.ts
+++ src/util/borrowUtils.ts
@@ -2,6 +2,8 @@ import { add, div, mul } from 'biggystring'
 import { EdgeCurrencyWallet } from 'edge-core-js'
 import * as React from 'react'
 
+import { LoanManageType } from '../components/scenes/Loans/LoanManageScene'
+import { showError } from '../components/services/AirshipInstance'
 import { BorrowCollateral, BorrowDebt } from '../plugins/borrow-plugins/types'
 import { useSelector } from '../types/reactRedux'
 import { mulToPrecision } from './utils'
@@ -25,4 +27,24 @@ export const useTotalFiatAmount = (wallet: EdgeCurrencyWallet, borrowArray: Borr
       return add(total, mul(div(obj.nativeAmount, multiplier, mulToPrecision(multiplier)), getExchangeRate(`${currencyCode}_${isoFiatCurrencyCode}`)))
     }, '0')
   }, [allTokens, borrowArray, currencyInfo, exchangeRates, isoFiatCurrencyCode])
+}
+
+export const getWalletPickerExcludeWalletIds = (
+  wallets: { [wallet: string]: EdgeCurrencyWallet },
+  loanManageType: LoanManageType,
+  borrowEngineWallet: EdgeCurrencyWallet
+): string[] => {
+  return Object.keys(wallets).filter(walletId => {
+    switch (loanManageType) {
+      case 'loan-manage-deposit':
+        return walletId !== borrowEngineWallet.id && wallets[walletId].currencyInfo.pluginId !== 'bitcoin'
+      case 'loan-manage-borrow':
+      case 'loan-manage-repay':
+      case 'loan-manage-withdraw':
+        return walletId !== borrowEngineWallet.id
+      default:
+        showError(`getWalletPickerExcludeWalletIds unhandled case: ${loanManageType}. Allowing all wallets.`)
+        return false
+    }
+  })
 }
