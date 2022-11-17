f9102cd11835ba93dabb36044c61486d401ac5b4
Jon Tzeng <jon@edge.app>
2022-11-16 18:05:05 -0800

    Fix sprintf call in FiatAmountInputCard

--- src/components/scenes/Loans/LoanCreateScene.tsx
+++ src/components/scenes/Loans/LoanCreateScene.tsx
@@ -348,7 +348,7 @@ export const LoanCreateScene = (props: Props) => {
           <FiatAmountInputCard
             wallet={destWallet == null ? borrowEngineWallet : destWallet}
             iconUri={iconUri}
-            inputModalMessage={sprintf(s.strings.loan_must_be_s_or_less, displayLtvLimit)}
+            inputModalMessage={sprintf(s.strings.loan_loan_amount_input_message_s, displayLtvLimit)}
             title={sprintf(s.strings.loan_enter_s_amount_s, s.strings.loan_fragment_loan, fiatCurrencyCode)}
             tokenId={destTokenId}
             onAmountChanged={handleBorrowAmountChanged}
