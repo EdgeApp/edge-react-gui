db545c2ff8854e01be042f5f26df98ed8bb173f7
William Swanson <swansontec@gmail.com>
2023-01-03 15:20:04 -0800

    Port core-related ObjectiveC code to Swift

--- ios/edge/AppDelegate.m
+++ ios/edge/AppDelegate.m
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-#import "edgeApiKey.h"
+#import "Edge-Swift.h"
 
 #import "RCTSplashScreen.h"
 #import <Bugsnag/Bugsnag.h>
@@ -102,8 +102,9 @@ static void InitializeFlipper(UIApplication *application) {
 - (void)application:(UIApplication *)application
   performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
-  [self fetchMessagesWithCompletion:^(NSArray *problemUsers, NSError *error) {
-    if (error) {
+  EdgeCore *core = [EdgeCore alloc];
+  [core fetchPendingLoginsWithCompletion:^(NSArray<NSString *> *problemUsers) {
+    if (!problemUsers) {
       return completionHandler(UIBackgroundFetchResultNoData);
     }
 
@@ -140,145 +141,6 @@ static void InitializeFlipper(UIApplication *application) {
            }];
     });
   }];
-}
-
-typedef void (^MessagesHandler)(NSArray<NSString *> *problemUsers, NSError *error);
-
-/**
- * Goes to the auth server and figures out which users have 2fa resets.
- */
-- (void)fetchMessagesWithCompletion:(MessagesHandler)completionHandler {
-  NSError *error = nil;
-
-  // Locate the `logins` folder:
-  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
-                                                       NSUserDomainMask, YES);
-  NSString *basePath = [paths firstObject];
-  NSString *loginsPath = [NSString pathWithComponents:@[ basePath, @"logins" ]];
-
-  // List the logins folder:
-  NSFileManager *fs = [NSFileManager defaultManager];
-  NSArray *files = [fs contentsOfDirectoryAtPath:loginsPath error:&error];
-  if (!files) {
-    return completionHandler(@[], nil);
-  }
-
-  // Load the files:
-  NSMutableDictionary<NSString *, NSString *> *loginIds =
-      [[NSMutableDictionary alloc] init];
-  for (NSUInteger i = 0; i < files.count; ++i) {
-    NSString *where = [NSString pathWithComponents:@[ loginsPath, files[i] ]];
-    NSData *data = [NSData dataWithContentsOfFile:where options:0 error:&error];
-    if (!data)
-      continue;
-    id json = [NSJSONSerialization JSONObjectWithData:data
-                                              options:0
-                                                error:&error];
-    if (!json || ![json isKindOfClass:[NSDictionary class]])
-      continue;
-
-    if (!json[@"loginAuthBox"]) continue;
-    id loginId = json[@"loginId"];
-    id username = json[@"username"];
-    if (!loginId || ![loginId isKindOfClass:[NSString class]])
-      continue;
-    if (!username || ![username isKindOfClass:[NSString class]])
-      continue;
-    loginIds[loginId] = username;
-  }
-
-  // Prepare our payload:
-  NSDictionary *payloadJson = @{@"loginIds" : [loginIds allKeys]};
-  NSData *payloadData = [NSJSONSerialization dataWithJSONObject:payloadJson
-                                                        options:0
-                                                          error:&error];
-  if (!payloadData) {
-    return completionHandler(nil, error);
-  }
-
-  // Prepare our headers:
-  NSURL *url = [NSURL URLWithString:@"https://auth.airbitz.co/api/v2/messages"];
-  NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
-  [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-  [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-  [request setValue:[@"Token " stringByAppendingString:@EDGE_API_KEY]
-      forHTTPHeaderField:@"Authorization"];
-  request.HTTPMethod = @"POST";
-  request.HTTPBody = payloadData;
-
-  // Prepare to handle the results:
-  typedef void (^DataHandler)(NSData *data, NSURLResponse *response, NSError *error);
-  DataHandler handler = ^(NSData *data, NSURLResponse *response,
-                          NSError *error) {
-    if (error) return completionHandler(nil, error);
-
-    // Check the HTTP status code:
-    if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
-      NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-      if (httpResponse.statusCode < 200 || httpResponse.statusCode >= 300) {
-        return completionHandler(nil, [self makeReplyError]);
-      }
-      NSLog(@"status %ld", httpResponse.statusCode);
-    } else {
-      return completionHandler(nil, [self makeReplyError]);
-    }
-
-    // Parse the reply:
-    id json = [NSJSONSerialization JSONObjectWithData:data
-                                               options:0
-                                                 error:&error];
-    if (!json) return completionHandler(nil, error);
-    if (![json isKindOfClass:[NSDictionary class]]) {
-      return completionHandler(nil, [self makeReplyError]);
-    }
-    id statusCode = json[@"status_code"];
-    if (!statusCode ||
-        ![statusCode isKindOfClass:[NSNumber class]] ||
-        [((NSNumber *)statusCode) integerValue] != 0) {
-      return completionHandler(nil, [self makeReplyError]);
-    }
-    id results = json[@"results"];
-    if (!results || ![results isKindOfClass:[NSArray class]]) {
-      return completionHandler(nil, [self makeReplyError]);
-    }
-    NSArray *messages = (NSArray *)results;
-
-    // Find messages with problems:
-    NSMutableArray *problemUsers = [[NSMutableArray alloc] init];
-    for (NSUInteger i = 0; i < messages.count; ++i) {
-      id message = messages[i];
-      if (![message isKindOfClass:[NSDictionary class]]) continue;
-      NSString *username = loginIds[message[@"loginId"]];
-      if (!username) continue;
-
-      id pendingVouchers = message[@"pendingVouchers"];
-      BOOL hasVoucher = pendingVouchers &&
-        [pendingVouchers isKindOfClass:[NSArray class]] &&
-        ((NSArray *)pendingVouchers).count > 0;
-
-      id otpResetPending = message[@"otpResetPending"];
-      BOOL hasReset = otpResetPending &&
-        [otpResetPending isKindOfClass:[NSNumber class]] &&
-        [((NSNumber *)otpResetPending) boolValue];
-
-      if (hasVoucher || hasReset) {
-        [problemUsers addObject:username];
-      }
-    }
-
-    completionHandler(problemUsers, nil);
-  };
-
-  // Do the fetch:
-  NSLog(@"fetching %@ %@", request.URL, [[NSString alloc]
-    initWithData:payloadData encoding:NSUTF8StringEncoding]);
-  NSURLSession *fetcher = [NSURLSession sharedSession];
-  [[fetcher dataTaskWithRequest:request completionHandler:handler] resume];
-}
-
--(NSError *)makeReplyError
-{
-  return [NSError errorWithDomain:NSPOSIXErrorDomain code:EBADMSG userInfo:nil];
 }
 
 @end
