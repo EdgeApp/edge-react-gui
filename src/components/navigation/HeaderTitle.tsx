6eed9bcc80f41c79bd7743e441091d0085d36fed
Samuel Holmes <sam@edge.app>
2023-01-30 09:30:49 -0800

    Convert HeaderTitle to functional component

--- src/components/navigation/HeaderTitle.tsx
+++ src/components/navigation/HeaderTitle.tsx
@@ -1,36 +1,22 @@
 import * as React from 'react'
 import { View } from 'react-native'
 
-import { getSelectedCurrencyWallet } from '../../selectors/WalletSelectors'
-import { connect } from '../../types/reactRedux'
-import { getWalletName } from '../../util/CurrencyWalletHelpers'
-import { cacheStyles, Theme, ThemeProps, withTheme } from '../services/ThemeContext'
+import { cacheStyles, Theme, useTheme } from '../services/ThemeContext'
 import { EdgeText } from '../themed/EdgeText'
 
-interface OwnProps {
-  showWalletNameOnly?: boolean
+interface Props {
   title?: string
 }
 
-interface StateProps {
-  selectedWalletName: string | null
-  selectedWalletCurrencyCode: string
-}
-
-interface DispatchProps {}
-
-type Props = OwnProps & StateProps & DispatchProps & ThemeProps
-
-class HeaderTitleComponent extends React.PureComponent<Props> {
-  render() {
-    const { title, theme } = this.props
-    const styles = getStyles(theme)
-    return (
-      <View style={styles.container}>
-        <EdgeText>{title}</EdgeText>
-      </View>
-    )
-  }
+export const HeaderTitle = (props: Props) => {
+  const { title } = props
+  const theme = useTheme()
+  const styles = getStyles(theme)
+  return (
+    <View style={styles.container}>
+      <EdgeText>{title}</EdgeText>
+    </View>
+  )
 }
 
 const getStyles = cacheStyles((theme: Theme) => ({
@@ -43,15 +29,3 @@ const getStyles = cacheStyles((theme: Theme) => ({
     paddingBottom: theme.rem(0.25)
   }
 }))
-
-export const HeaderTitle = connect<StateProps, DispatchProps, OwnProps>(
-  state => {
-    const selectedWallet = getSelectedCurrencyWallet(state)
-
-    return {
-      selectedWalletName: selectedWallet != null ? getWalletName(selectedWallet) : null,
-      selectedWalletCurrencyCode: state.ui.wallets.selectedCurrencyCode
-    }
-  },
-  _dispatch => ({})
-)(withTheme(HeaderTitleComponent))
