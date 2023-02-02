1f8e66907dc89647ea0c26c85bd41a05b0c42d70
William Swanson <swansontec@gmail.com>
2023-02-02 11:02:49 -0800

    Test the PromoCard deeply

--- src/__tests__/components/PromoCard.test.tsx
+++ src/__tests__/components/PromoCard.test.tsx
@@ -1,31 +1,41 @@
 import { describe, expect, it } from '@jest/globals'
 import * as React from 'react'
-import { createRenderer } from 'react-test-renderer/shallow'
+import { Provider } from 'react-redux'
+import renderer from 'react-test-renderer'
+import { createStore } from 'redux'
 
-import { PromoCardComponent } from '../../components/cards/PromoCard'
-import { getTheme } from '../../components/services/ThemeContext'
+import { PromoCard } from '../../components/cards/PromoCard'
+import { rootReducer } from '../../reducers/RootReducer'
+import { MessageTweak } from '../../types/TweakTypes'
 
 describe('PromoCard', () => {
-  it('should render with loading props', () => {
-    const renderer = createRenderer()
+  const fakeMessage: MessageTweak = {
+    message: 'Show me',
+    iconUri: 'https://edge.app/favicon.ico',
+    durationDays: 1
+  }
 
-    const fakeMessages: any[] = ['MessageTweak']
-    const fakeReferral: any = {
-      installerId: 'string',
-      currencyCodes: ['BTC'],
-      promotions: [],
-      ignoreAccountSwap: true,
-      hiddenAccountMessages: ['messageId']
+  const mockState: any = {
+    account: {
+      accountReferral: {
+        installerId: 'string',
+        currencyCodes: ['BTC'],
+        promotions: [],
+        ignoreAccountSwap: true,
+        hiddenAccountMessages: ['messageId']
+      },
+      referralCache: {
+        accountMessages: [fakeMessage]
+      }
     }
+  }
+  const store = createStore(rootReducer, mockState)
 
-    const actual = renderer.render(
-      <PromoCardComponent
-        accountMessages={fakeMessages}
-        accountReferral={fakeReferral}
-        hideMessageTweak={(messageId, source) => undefined}
-        linkReferralWithCurrencies={uri => undefined}
-        theme={getTheme()}
-      />
+  it('should render', () => {
+    const actual = renderer.create(
+      <Provider store={store}>
+        <PromoCard />
+      </Provider>
     )
 
     expect(actual).toMatchSnapshot()
