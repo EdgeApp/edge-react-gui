ab0cd9a67aa6a09b80f95bb0eb6071f647c0f40b
Samuel Holmes <sam@edge.app>
2023-02-03 12:22:09 -0800

    Convert BackButton to a function component

--- src/components/navigation/BackButton.tsx
+++ src/components/navigation/BackButton.tsx
@@ -2,56 +2,56 @@ import * as React from 'react'
 import { Platform, TouchableOpacity } from 'react-native'
 import IonIcon from 'react-native-vector-icons/Ionicons'
 
-import { cacheStyles, Theme, ThemeProps, withTheme } from '../../components/services/ThemeContext'
+import { cacheStyles, Theme, useTheme } from '../../components/services/ThemeContext'
 import { triggerHaptic } from '../../util/haptic'
 
 const isIos = Platform.OS === 'ios'
 
 export interface Props {
-  onPress: () => unknown
   isEmpty?: boolean
+  onPress?: () => void
 }
 
-class BackButtonComponent extends React.PureComponent<Props & ThemeProps> {
-  static defaultProps = {
-    withArrow: false,
-    onPress: () => {}
-  }
+export const BackButton = (props: Props) => {
+  const { isEmpty = false, onPress } = props
+  const theme = useTheme()
+  const styles = getBackButtonStyles(theme)
 
-  handlePress = () => {
+  const handlePress = () => {
     triggerHaptic('impactLight')
-    this.props.onPress()
+    if (onPress != null) {
+      onPress()
+    }
   }
 
-  renderIcon = () => {
-    const { isEmpty, theme } = this.props
-    const styles = getStyles(theme)
-    if (isEmpty) return null
-    return isIos ? (
-      <IonIcon size={theme.rem(1.5)} color={this.props.theme.icon} name="chevron-back-outline" style={styles.backIconStyle} />
-    ) : (
-      <IonIcon size={theme.rem(1.25)} color={this.props.theme.icon} name="md-arrow-back" style={styles.backIconAndroid} />
-    )
-  }
-
-  render() {
-    const { theme } = this.props
-    const styles = getStyles(theme)
-    return (
-      <TouchableOpacity style={styles.container} onPress={this.handlePress}>
-        {this.renderIcon()}
-      </TouchableOpacity>
-    )
-  }
+  return (
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
+      {isEmpty ? null : <Icon />}
+    </TouchableOpacity>
+  )
 }
 
-const getStyles = cacheStyles((theme: Theme) => ({
+const getBackButtonStyles = cacheStyles((theme: Theme) => ({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: theme.rem(1),
     height: 44 // This is a fixed height of the navigation header no matter what screen size. Default by router-flux
-  },
+  }
+}))
+
+const Icon = () => {
+  const theme = useTheme()
+  const styles = getIconStyles(theme)
+
+  return isIos ? (
+    <IonIcon size={theme.rem(1.5)} color={theme.icon} name="chevron-back-outline" style={styles.backIconStyle} />
+  ) : (
+    <IonIcon size={theme.rem(1.25)} color={theme.icon} name="md-arrow-back" style={styles.backIconAndroid} />
+  )
+}
+
+const getIconStyles = cacheStyles((theme: Theme) => ({
   backIconStyle: {
     marginLeft: theme.rem(-0.25),
     marginBottom: theme.rem(0.25),
@@ -61,5 +61,3 @@ const getStyles = cacheStyles((theme: Theme) => ({
     paddingRight: theme.rem(1)
   }
 }))
-
-export const BackButton = withTheme(BackButtonComponent)
