3b8c512b309d7a7fd914792bcb210ad2ad7fe0aa
Samuel Holmes <sam@edge.app>
2023-02-16 11:52:11 -0800

    Add styled HOC

---
+++ src/components/hoc/styled.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { ImageStyle, StyleProp, TextStyle, ViewStyle } from 'react-native'
+
+import { ThemeProps, useTheme } from '../services/ThemeContext'
+
+interface StyleProps {
+  style?: StyleProp<any>
+}
+type ValidStyles = ImageStyle | TextStyle | ViewStyle
+
+export function styled<BaseProps extends StyleProps>(Component: React.ComponentType<BaseProps>) {
+  function makeStyledComponent<Props extends object>(getStyles: (props: Omit<BaseProps, 'style'> & Props & ThemeProps) => ValidStyles) {
+    const StyledComponent: React.ComponentType<Omit<BaseProps, 'style'> & Props> = props => {
+      const theme = useTheme()
+      const styles = getStyles({ ...props, theme })
+      const allProps: BaseProps = { ...props, style: styles } as any
+      return <Component {...allProps} />
+    }
+    StyledComponent.displayName = Component.displayName != null ? `StyledComponent(${Component.displayName})` : `StyledComponent`
+    return StyledComponent
+  }
+  return makeStyledComponent
+}
