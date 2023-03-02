ffeb8059d989fe7c0e9b4a74a2c873e4a7bcd1a8
William Swanson <swansontec@gmail.com>
2023-03-01 16:12:35 -0800

    Render the MenuTabs test deeply

--- src/__tests__/components/MenuTabs.test.tsx
+++ src/__tests__/components/MenuTabs.test.tsx
@@ -1,23 +1,23 @@
 import { describe, expect, it } from '@jest/globals'
 import * as React from 'react'
-import { createRenderer } from 'react-test-renderer/shallow'
+import TestRenderer from 'react-test-renderer'
 
 import { MenuTabs } from '../../components/themed/MenuTabs'
 import { fakeNavigation } from '../../util/fake/fakeNavigation'
+import { FakeProviders } from '../../util/fake/FakeProviders'
 
 describe('MenuTabs', () => {
   it('should render with loading props', () => {
-    const renderer = createRenderer()
-
-    const actual = renderer.render(
-      <MenuTabs
-        // @ts-expect-error
-        navigation={fakeNavigation}
-        // @ts-expect-error
-        state={{ index: 0, routes: [] }}
-      />
+    const renderer = TestRenderer.create(
+      <FakeProviders>
+        <MenuTabs
+          // @ts-expect-error
+          navigation={fakeNavigation}
+          // @ts-expect-error
+          state={{ index: 0, routes: [] }}
+        />
+      </FakeProviders>
     )
-
-    expect(actual).toMatchSnapshot()
+    expect(renderer.toJSON()).toMatchSnapshot()
   })
 })
