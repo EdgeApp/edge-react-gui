9bd223ea0073ece2c36dbe81f144a6aa8a6492ca
Itay Plavin <4023066+itayplav@users.noreply.github.com>
2023-03-03 00:27:11 -0500

    [wip] login 2fa maestro flows w/ credential store

---
+++ maestro/env-sample.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-undef
+output.logins = {
+  C314: { USERNAME: '', PASSWORD: '', PIN: '', BACKUP_CODE: '' },
+  C753: { USERNAME: '', PASSWORD: '', PIN: '', BACKUP_CODE: '' }
+}
