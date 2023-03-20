a63c860cbfc67090b86fb534191316eb8e04ad0f
Samuel Holmes <sam@edge.app>
2023-03-20 16:12:54 -0700

    Add hasTabs to GuiPluignViewScene

--- src/components/scenes/GuiPluginViewScene.tsx
+++ src/components/scenes/GuiPluginViewScene.tsx
@@ -255,7 +255,7 @@ class GuiPluginView extends React.Component<Props, State> {
         : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
 
     return (
-      <SceneWrapper background="body">
+      <SceneWrapper background="body" hasTabs={route.name !== 'pluginView'}>
         <WebView
           allowFileAccess
           allowUniversalAccessFromFileURLs
