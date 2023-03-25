000263db11ca6d01ab15dc122d4349bffac50c36
Jon Tzeng <jon@edge.app>
2023-03-24 17:24:16 -0700

    Add FIO validation & registration fetches

--- src/components/scenes/Fio/FioCreateHandleScene.tsx
+++ src/components/scenes/Fio/FioCreateHandleScene.tsx
@@ -1,17 +1,21 @@
+import { asNumber, asObject, asString, asValue } from 'cleaners'
 import { EdgeCurrencyWallet } from 'edge-core-js'
 import * as React from 'react'
-import { ScrollView, TextInput, View } from 'react-native'
+import { NativeSyntheticEvent, ScrollView, TextInput, TextInputSelectionChangeEventData, View } from 'react-native'
 import FastImage from 'react-native-fast-image'
 import { cacheStyles } from 'react-native-patina'
+import { sprintf } from 'sprintf-js'
 
-import { createFioWallet } from '../../../actions/FioAddressActions'
+import { createFioWallet, refreshAllFioAddresses } from '../../../actions/FioAddressActions'
+import { FIO_ADDRESS_DELIMITER } from '../../../constants/WalletAndCurrencyConstants'
 import { useAsyncEffect } from '../../../hooks/useAsyncEffect'
 import { useHandler } from '../../../hooks/useHandler'
 import s from '../../../locales/strings'
-import { config } from '../../../theme/appConfig'
 import { useDispatch, useSelector } from '../../../types/reactRedux'
-import { NavigationProp } from '../../../types/routerTypes'
+import { NavigationProp, RouteProp } from '../../../types/routerTypes'
+import { getFioCustomizeHandleImage } from '../../../util/CdnUris'
 import { SceneWrapper } from '../../common/SceneWrapper'
+import { showError } from '../../services/AirshipInstance'
 import { Theme, useTheme } from '../../services/ThemeContext'
 import { EdgeText } from '../../themed/EdgeText'
 import { MainButton } from '../../themed/MainButton'
@@ -19,44 +23,115 @@ import { OutlinedTextInput } from '../../themed/OutlinedTextInput'
 
 interface Props {
   navigation: NavigationProp<'fioCreateHandle'>
+  route: RouteProp<'fioCreateHandle'>
 }
 
-export const FioCreateHandleScene = ({ navigation }: Props) => {
+export interface FioCreateHandleProps {
+  freeRegApiToken: string
+  freeRegRefCode: string
+}
+
+const asRegisterSuccessRes = asObject({
+  account_id: asNumber,
+  error: asValue(false)
+})
+
+const asRegisterFailedRes = asObject({
+  success: asValue(false),
+  error: asString
+})
+
+const asFreeFioDomain = asObject({
+  domain: asString,
+  free: asValue(true)
+})
+
+export const FioCreateHandleScene = ({ navigation, route }: Props) => {
+  const { freeRegApiToken, freeRegRefCode } = route.params
+
   const theme = useTheme()
   const styles = getStyles(theme)
   const dispatch = useDispatch()
+  const account = useSelector(state => state.core.account)
   const accountUserName = useSelector(state => state.core.account.username)
-  const domainStr = `@${config.appName.toLowerCase()}`
+  const fioPlugin = useSelector(state => state.core.account.currencyConfig.fio)
+  if (fioPlugin.otherMethods == null) {
+    showError(s.strings.fio_register_handle_error)
+    navigation.pop()
+  }
 
   const [wallet, setWallet] = React.useState<EdgeCurrencyWallet | undefined>()
+  const [domainStr, setDomainStr] = React.useState<string>('')
   const [fioHandle, setFioHandle] = React.useState<string>('')
   const [errorText, setErrorText] = React.useState<string>()
+
   const inputRef = React.useRef<TextInput>(null)
+  const mounted = React.useRef<boolean>(true)
 
   const handleChangeFioHandle = useHandler((userInput: string) => {
     // Clean the userInput:
     userInput = userInput.replace(domainStr, '').trim()
 
-    // ASCII a-z 0-9. Remove all non-alphanumeric characters, convert to
-    // lowercase
-    userInput = userInput.replace(/[^a-z0-9]/gi, '').toLowerCase()
-
     // Dash '-' allowed, but cannot be first or last character
-    if (userInput.length > 1) {
-      userInput = userInput.charAt(0) + userInput.slice(1, -1).replace('-', '') + userInput.slice(-1)
-    } else if (userInput === '-') {
-      userInput = ''
-    }
+    userInput = userInput.charAt(0).replace('-', '') + userInput.slice(1, -1) + userInput.slice(-1).replace('-', '')
 
-    setFioHandle(`${userInput}`)
+    // ASCII a-z 0-9. Remove all non-alphanumeric characters, convert to
+    // lowercase. Allow dashes as they were cleaned above
+    userInput = userInput.replace(/[^a-z0-9-]/gi, '').toLowerCase()
+
+    setFioHandle(userInput)
   })
 
   const handleRegisterPress = async () => {
-    // Register button is disabled if wallet creation isn't finished yet
+    // Register button is disabled if wallet ctreation isn't finished yet.
+    // Shouldn't happen.
     if (wallet == null) return
+    await account.waitForCurrencyWallet(wallet.id)
 
-    // TODO: Check if fioHandle is already registered, create the handle.
-    setErrorText('TODO')
+    // Check if the handle is already registered
+    const fioAccountName = `${fioHandle}${domainStr}`
+    if (!(await fioPlugin.otherMethods.validateAccount(fioAccountName))) {
+      if (!mounted.current) return
+      setErrorText(sprintf(s.strings.fio_register_handle_taken_error_s, fioAccountName))
+    }
+
+    // Register handle
+    try {
+      // TODO: Refactor fioPlugin.otherMethods.buyAddressRequest to support
+      // handling custom referralCode and apiToken
+      const regAddressRes = await fetch('https://reg.fioprotocol.io/public-api/buy-address', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          address: fioAccountName,
+          referralCode: freeRegRefCode,
+          publicKey: (await wallet.getReceiveAddress()).publicAddress,
+          redirectUrl: '',
+          apiToken: freeRegApiToken
+        })
+      }).then(async res => res.json())
+
+      // Check registration status
+      try {
+        asRegisterSuccessRes(regAddressRes)
+
+        dispatch(refreshAllFioAddresses())
+        navigation.pop()
+      } catch (e: any) {
+        // Rejected somehow, see if error is readable
+        const failedRes = asRegisterFailedRes(regAddressRes)
+        if (!mounted.current) return
+        setErrorText(failedRes.error)
+      }
+    } catch (e: any) {
+      // Registration fetch failed
+      console.error(JSON.stringify(e, null, 2))
+      if (!mounted.current) return
+      setErrorText(s.strings.fio_register_handle_error)
+    }
   }
 
   // Ensure that focus puts the cursor after the handle, but before the domain
@@ -68,19 +143,14 @@ export const FioCreateHandleScene = ({ navigation }: Props) => {
     }
   })
 
-  const handleInputBlur = useHandler(() => {
-    if (inputRef.current != null) {
-      inputRef.current.blur()
-    }
-  })
-
   const handleInputClear = useHandler(() => {
+    if (!mounted.current) return
     // TODO: BUG: Clearing the field twice consecutively will clear the domain.
     setFioHandle('')
   })
 
   // Ensure the cursor cannot be moved beyond the handle portion of the input
-  const handleSelectionChange = useHandler((event: any) => {
+  const handleSelectionChange = useHandler((event: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => {
     const start = event.nativeEvent.selection.start
     // Check if the cursor is within the handle name and before the domain
     if (start > fioHandle.length) {
@@ -96,39 +166,60 @@ export const FioCreateHandleScene = ({ navigation }: Props) => {
   // Create the new FIO wallet, default the handle to a cleaned version of the username
   useAsyncEffect(async () => {
     const wallet = await dispatch(createFioWallet())
+
+    if (!mounted.current) return
     setWallet(wallet)
 
     handleChangeFioHandle(accountUserName)
+
+    const domains = await fioPlugin.otherMethods.getDomains(freeRegRefCode)
+    if (domains.length === 1) {
+      if (!mounted.current) return
+      try {
+        setDomainStr(`${FIO_ADDRESS_DELIMITER}${asFreeFioDomain(domains[0]).domain}`)
+      } catch (e) {
+        setErrorText(s.strings.fio_register_handle_error)
+      }
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
   React.useEffect(() => {
     // Clear error, if there was one
     setErrorText(undefined)
   }, [fioHandle])
 
+  React.useEffect(() => {
+    return () => {
+      mounted.current = false
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <SceneWrapper background="theme">
       <ScrollView contentContainerStyle={styles.container}>
-        <FastImage source={{ uri: 'https://content.edge.app/currencyIconsV3/fio/fio_dark.png' }} style={styles.icon} />
+        <FastImage source={{ uri: getFioCustomizeHandleImage(theme) }} style={styles.icon} />
         <EdgeText style={styles.title}>{s.strings.personalize_wallet_title}</EdgeText>
         <View style={styles.inputContainer}>
           <OutlinedTextInput
             ref={inputRef}
-            error={errorText}
             value={`${fioHandle} ${domainStr}`}
             onChangeText={handleChangeFioHandle}
             autoCapitalize="none"
             autoCorrect={false}
             onFocus={handleInputFocus}
-            onBlur={handleInputBlur}
             onClear={handleInputClear}
             onSelectionChange={handleSelectionChange}
             // Actual limit is 64 total, but we added an extra space between domain and handle for prettiness
             maxLength={65}
+            showSpinner={domainStr === ''}
           />
+          <EdgeText style={styles.errorText} numberOfLines={5}>
+            {errorText ?? ''}
+          </EdgeText>
         </View>
         <View style={styles.buttonContainer}>
-          <MainButton type="secondary" label={s.strings.string_cancel_cap} onPress={handleCancelPress} marginRem={0.5} />
           <MainButton
             type="primary"
             label={s.strings.fio_register_handle_button}
@@ -136,6 +227,7 @@ export const FioCreateHandleScene = ({ navigation }: Props) => {
             marginRem={0.5}
             disabled={fioHandle.length < 3 || errorText != null || wallet == null}
           />
+          <MainButton type="escape" label={s.strings.string_cancel_cap} onPress={handleCancelPress} marginRem={0.5} />
         </View>
       </ScrollView>
     </SceneWrapper>
@@ -157,6 +249,10 @@ const getStyles = cacheStyles((theme: Theme) => ({
     alignItems: 'center',
     justifyContent: 'space-between',
     paddingVertical: theme.rem(1)
+  },
+  errorText: {
+    fontSize: theme.rem(0.75),
+    color: theme.dangerText
   },
   icon: {
     width: theme.rem(10),
