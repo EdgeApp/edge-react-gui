3f67779037b235e4f93830544e938b21ef7c66e4
William Swanson <swansontec@gmail.com>
2023-03-29 17:44:06 -0700

    Fix invalid conflict resolution
    This happened in 820dca1a10cf8957cc2a29b719dbd84d3edfabcc, bringing back a line deleted in 4692de88054713bcf3c6a0443903d594002b1d09.

--- src/controllers/edgeProvider/EdgeProviderServer.tsx
+++ src/controllers/edgeProvider/EdgeProviderServer.tsx
@@ -400,8 +400,6 @@ export class EdgeProviderServer implements EdgeProviderMethods {
           }
           // Do not expose the entire wallet to the plugin:
           resolve(cleanTx(transaction))
-          this._navigation.pop()
-
           wallet
             .nativeToDenomination(transaction.nativeAmount, transaction.currencyCode)
             .then(exchangeAmount => {
