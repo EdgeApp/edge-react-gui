b1e3672fcbca4bbe19884c6f844c1e3f20259b0a
Matthew <matthew@edge.app>
2023-04-04 14:28:03 -0700

    Add AVAX evmscan keys

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -71,6 +71,17 @@ export const asEnvConfig = asObject({
   AZTECO_API_KEY: asNullable(asString),
 
   // Core plugin options:
+  AVALANCHE_INIT: asCorePluginInit(
+    asObject({
+      alethioApiKey: asOptional(asString, ''),
+      amberdataApiKey: asOptional(asString, ''),
+      blockchairApiKey: asOptional(asString, ''),
+      evmScanApiKey: asOptional(asArray(asString), []),
+      gasStationApiKey: asOptional(asString, ''),
+      infuraProjectId: asOptional(asString, ''),
+      quiknodeApiKey: asOptional(asString, '')
+    }).withRest
+  ),
   BINANCE_SMART_CHAIN_INIT: asCorePluginInit(
     asObject({
       bscscanApiKey: asOptional(asArray(asString), []),
