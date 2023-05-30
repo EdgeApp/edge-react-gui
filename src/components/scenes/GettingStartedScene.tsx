d98c8544da3e49a39685b70b84648952271c0a80
Jon Tzeng <jon@edge.app>
2023-05-29 18:52:52 -0700

    Route USPs to new light account creation flows

--- src/components/scenes/GettingStartedScene.tsx
+++ src/components/scenes/GettingStartedScene.tsx
@@ -108,7 +108,7 @@ export const GettingStartedScene = (props: Props) => {
       if (localUsersLength > 0) {
         navigation.navigate('login', { loginUiInitialRoute: 'login-password' })
       } else {
-        navigation.navigate('login', { loginUiInitialRoute: 'new-account' })
+        navigation.navigate('login', { loginUiInitialRoute: 'new-light-account' })
       }
     }
   })
@@ -121,7 +121,7 @@ export const GettingStartedScene = (props: Props) => {
   })
   const handlePressSignUp = useHandler(() => {
     logEvent('Signup_Welcome', { variantId, variantParams: { doneMethod: 'click' } })
-    navigation.navigate('login', { loginUiInitialRoute: 'new-account' })
+    navigation.navigate('login', { loginUiInitialRoute: 'new-light-account' })
   })
   const handlePressSkip = useHandler(() => {
     navigation.navigate('login', {})
