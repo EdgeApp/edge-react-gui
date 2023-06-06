7514e0fc067bac5a8ffc75681e04f480b74109ae
Samuel Holmes <sam@edge.app>
2023-06-05 18:28:53 -0700

    Replace class with interface within `useStateManager.ts`

--- src/plugins/gui/hooks/useStateManager.ts
+++ src/plugins/gui/hooks/useStateManager.ts
@@ -3,29 +3,20 @@ import { useRef } from 'react'
 import { useHandler } from '../../../hooks/useHandler'
 import { useState } from '../../../types/reactHooks'
 
-export class StateManager<State extends object> {
-  private _state: State
-  private readonly onUpdate: (state: State) => void
-
-  constructor(state: State, onUpdate: (state: State) => void) {
-    this._state = state
-    this.onUpdate = onUpdate
-  }
-
-  get state(): State {
-    return this._state
-  }
-
-  update(state: Partial<State>): void {
-    this._state = { ...this._state, ...state }
-    this.onUpdate(this._state)
-  }
+export interface StateManager<State extends object> {
+  state: State
+  update: (state: Partial<State>) => void
 }
 
 export const useStateManager = <T extends object>(defaultState: T): StateManager<T> => {
   const [state, setState] = useState<T>(defaultState)
   const handleUpdate: StateManager<T>['update'] = useHandler((state: Partial<T>) => setState({ ...stateManagerRef.current.state, ...state }))
-  const stateManagerRef = useRef<StateManager<T>>(new StateManager(state, handleUpdate))
+  const stateManagerRef = useRef<StateManager<T>>({
+    state,
+    update: handleUpdate
+  })
+  // Always update the current ref's state
+  stateManagerRef.current.state = state
 
   return stateManagerRef.current
 }
