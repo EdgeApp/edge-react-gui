fe19aef2e660eafb50df9766701b99e02434a879
Paul Puey <paul@edge.app>
2023-06-09 14:25:53 -0700

    Add ENV.ENABLE_VISA_PROGRAM
    Disable all buttons to enter Visa Program if ENABLE_VISA_PROGRAM is false

--- src/components/themed/ControlPanel.tsx
+++ src/components/themed/ControlPanel.tsx
@@ -267,7 +267,7 @@ export function ControlPanel(props: DrawerContentComponentProps) {
     }
   ]
 
-  if (IONIA_SUPPORTED_FIATS.includes(defaultFiat)) {
+  if (ENV.ENABLE_VISA_PROGRAM && IONIA_SUPPORTED_FIATS.includes(defaultFiat)) {
     rowDatas.unshift({
       pressHandler: () => {
         dispatch(executePluginAction(navigation, 'rewardscard', 'sell'))
