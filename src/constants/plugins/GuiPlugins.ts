8f4e89e87830b8ca32ec56f66027b3f683b9c130
Paul Puey <paul@edge.app>
2023-06-13 15:34:35 -0700

    Enable Bitrefill with stablecoins

--- src/constants/plugins/GuiPlugins.ts
+++ src/constants/plugins/GuiPlugins.ts
@@ -107,7 +107,8 @@ export const guiPlugins: { [pluginId: string]: GuiPlugin } = {
   bitrefill: {
     pluginId: 'bitrefill',
     storeId: 'co.edgesecure.bitrefill',
-    baseUri: 'https://embed.bitrefill.com/?ref=nUqaI7Qe&theme=dark&paymentMethods=bitcoin,ethereum,dogecoin,litecoin,dash',
+    baseUri:
+      'https://embed.bitrefill.com/?ref=nUqaI7Qe&theme=dark&paymentMethods=bitcoin,ethereum,usdt_trc20,usdt_erc20,usdt_polygon,usdc_erc20,usdc_polygon,litecoin,dogecoin,dash',
     lockUriPath: true,
     displayName: 'Bitrefill'
   },
