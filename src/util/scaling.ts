44e44a2e66d81ea8b6152fb80a4e3c148dd7a8c7
Paul Puey <paul@edge.app>
2023-06-27 15:57:32 -0700

    Log scaling dimensions

--- src/util/scaling.ts
+++ src/util/scaling.ts
@@ -1,6 +1,8 @@
 import { Dimensions } from 'react-native'
 const { width, height } = Dimensions.get('window')
 
+console.log(`scaling.ts: Dimensions.get(window) width=${width} height=${height}`)
+
 // Guideline sizes are based on standard ~5" screen mobile device
 const guidelineBaseWidth = 320
 const guidelineBaseHeight = 568
@@ -8,6 +10,7 @@ const guidelineBaseHeight = 568
 // Differences between current sizes and guideline sizes:
 const ratioHorizontal = (width - guidelineBaseWidth) / guidelineBaseWidth
 const ratioVertical = (height - guidelineBaseHeight) / guidelineBaseHeight
+console.log(`scaling.ts: ratioHorizontal=${ratioHorizontal} ratioVertical=${ratioVertical}`)
 
 export const scaleH = (size: number, factor: number = 0.3) => {
   return size + size * factor * ratioHorizontal
