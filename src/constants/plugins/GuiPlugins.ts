2c93edfaf0d28c698649c87f8c62f75ea8da0663
Paul V Puey <paul@edge.app>
2023-08-29 13:58:40 -0700

    Coinhub files and patches v2

--- src/constants/plugins/GuiPlugins.ts
+++ src/constants/plugins/GuiPlugins.ts
@@ -292,7 +292,8 @@ export const guiPlugins: { [pluginId: string]: GuiPlugin } = {
     pluginId: 'coinhub',
     storeId: 'coinhub',
     baseUri: 'https://coinhubbitcoinwallet.app',
-    displayName: 'Coinhub ATMs'
+    displayName: 'Coinhub ATMs',
+    permissions: ['location']
   },
   custom: {
     pluginId: 'custom',
