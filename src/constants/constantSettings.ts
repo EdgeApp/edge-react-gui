51bcd56ad5b14af19e24e1f3ba79f9df0c92c70a
Jon Tzeng <jon@edge.app>
2023-08-29 14:48:27 -0700

    Sort constantSettings, removed unused tutorial const

--- src/constants/constantSettings.ts
+++ src/constants/constantSettings.ts
@@ -1,7 +1,6 @@
+export const AAVE_WELCOME = 'aaveWelcome.json'
+export const SCAM_WARNING = 'scamWarning.json'
 export const SETTINGS_PERMISSION_LIMITS = 'SETTINGS_PERMISSION_LIMIT'
 export const SETTINGS_PERMISSION_QUANTITY = 3
-export const TUTORIAL = 'tutorial.json'
-export const SCAM_WARNING = 'scamWarning.json'
-export const AAVE_WELCOME = 'aaveWelcome.json'
-export const TOKEN_TERMS_AGREEMENT = 'ttAgreement.json'
 export const STICKY_CONFIG = 'remoteConfigSticky.json'
+export const TOKEN_TERMS_AGREEMENT = 'ttAgreement.json'
