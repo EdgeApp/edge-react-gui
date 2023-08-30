c16422785bf35d5118a1255d3bff83e282b834a9
Jon Tzeng <jon@edge.app>
2023-08-29 18:11:13 -0700

    Implement "first open" tracking param
    Allow greater control over this tracking aspect compared to Firebase's/Google Analytics' built-in "first_open" param

--- src/constants/constantSettings.ts
+++ src/constants/constantSettings.ts
@@ -1,4 +1,5 @@
 export const AAVE_WELCOME = 'aaveWelcome.json'
+export const FIRST_OPEN = 'firstOpen.json'
 export const SCAM_WARNING = 'scamWarning.json'
 export const SETTINGS_PERMISSION_LIMITS = 'SETTINGS_PERMISSION_LIMIT'
 export const SETTINGS_PERMISSION_QUANTITY = 3
