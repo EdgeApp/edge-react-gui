5e69a4b5c7c6f95b6901d5cb883ebdaa2351aa4b
Jon Tzeng <jon@edge.app>
2023-09-15 17:02:37 -0700

    Implement TransferModal

---
+++ src/components/modals/TransferModal.tsx
@@ -0,0 +1,161 @@
+import { EdgeAccount } from 'edge-core-js'
+import React from 'react'
+import { View } from 'react-native'
+import { AirshipBridge } from 'react-native-airship'
+import { cacheStyles } from 'react-native-patina'
+import AntDesignIcon from 'react-native-vector-icons/AntDesign'
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
+import { sprintf } from 'sprintf-js'
+
+import { selectWalletToken } from '../../actions/WalletActions'
+import { useHandler } from '../../hooks/useHandler'
+import { lstrings } from '../../locales/strings'
+import { config } from '../../theme/appConfig'
+import { useDispatch } from '../../types/reactRedux'
+import { Airship } from '../services/AirshipInstance'
+import { Theme, useTheme } from '../services/ThemeContext'
+import { ModalTitle } from '../themed/ModalParts'
+import { SelectableRow } from '../themed/SelectableRow'
+import { ThemedModal } from '../themed/ThemedModal'
+import { WalletListModal, WalletListResult } from './WalletListModal'
+
+interface Props {
+  bridge: AirshipBridge<void>
+  account: EdgeAccount
+  depositOrSend: 'deposit' | 'send'
+  navigation: any
+}
+
+interface TransferOption {
+  icon: React.ReactNode
+  title: string
+  onPress: () => void
+}
+
+/**
+ * Renders a modal with descritive options for transferring crypto in/out of
+ * Edge, i.e.: buying/selling/sending/receiving crypto
+ */
+export const TransferModal = ({ account, bridge, depositOrSend, navigation }: Props) => {
+  const theme = useTheme()
+  const style = styles(theme)
+  const dispatch = useDispatch()
+
+  const handleSell = useHandler(() => {
+    navigation.navigate('sellTab', { screen: 'pluginListSell' })
+    Airship.clear()
+  })
+  const handleBuy = useHandler(() => {
+    navigation.navigate('buyTab', { screen: 'pluginListBuy' })
+    Airship.clear()
+  })
+  const handleSend = useHandler(async () => {
+    const { walletId, tokenId } = await Airship.show<WalletListResult>(bridge => (
+      <WalletListModal bridge={bridge} headerTitle={lstrings.select_wallet_to_send_from} navigation={navigation} />
+    ))
+    if (walletId != null && tokenId != null) {
+      navigation.push('send2', { walletId, tokenId, hiddenFeaturesMap: { scamWarning: false } })
+    }
+    Airship.clear()
+  })
+  const handleReceive = useHandler(async () => {
+    Airship.clear()
+    const { walletId, tokenId } = await Airship.show<WalletListResult>(bridge => (
+      <WalletListModal bridge={bridge} headerTitle={lstrings.select_receive_asset} navigation={navigation} showCreateWallet />
+    ))
+
+    if (walletId != null && tokenId != null) {
+      await dispatch(selectWalletToken({ navigation, walletId, tokenId }))
+      navigation.navigate('request', {})
+    }
+  })
+
+  const handleCancel = useHandler(() => {
+    bridge.resolve()
+  })
+
+  const iconProps = { size: theme.rem(1.25), color: theme.iconTappable }
+  const depositOptions: TransferOption[] = [
+    {
+      icon: (
+        <>
+          <FontAwesomeIcon name="bank" {...iconProps} />
+          <AntDesignIcon name="arrowright" {...iconProps} />
+        </>
+      ),
+      title: lstrings.transfer_from_bank_title,
+      onPress: handleBuy
+    },
+    {
+      icon: (
+        <>
+          <AntDesignIcon name="wallet" {...iconProps} />
+          <AntDesignIcon name="arrowright" {...iconProps} />
+        </>
+      ),
+      title: lstrings.transfer_from_wallet_title,
+      onPress: handleReceive
+    }
+  ]
+
+  const sendOptions: TransferOption[] = [
+    {
+      icon: (
+        <>
+          <AntDesignIcon name="arrowright" {...iconProps} />
+          <FontAwesomeIcon name="bank" {...iconProps} />
+        </>
+      ),
+      title: lstrings.transfer_to_bank_title,
+      onPress: handleSell
+    },
+    {
+      icon: (
+        <>
+          <FontAwesomeIcon name="bank" {...iconProps} />
+          <AntDesignIcon name="arrowright" {...iconProps} />
+        </>
+      ),
+      title: sprintf(lstrings.transfer_to_from_bank_title_1s, config.appName),
+      onPress: handleBuy
+    },
+    {
+      icon: (
+        <>
+          <AntDesignIcon name="arrowright" {...iconProps} />
+          <AntDesignIcon name="wallet" {...iconProps} />
+        </>
+      ),
+      title: lstrings.transfer_to_another_wallet_title,
+      onPress: handleSend
+    }
+  ]
+
+  const options = depositOrSend === 'deposit' ? depositOptions : sendOptions
+  return (
+    <ThemedModal bridge={bridge} onCancel={handleCancel}>
+      <ModalTitle>{depositOrSend === 'deposit' ? lstrings.loan_fragment_deposit : lstrings.fragment_send_subtitle}</ModalTitle>
+      {options.map((option, index) => {
+        const { title, icon, onPress } = option
+        return (
+          <SelectableRow
+            key={title}
+            title={title}
+            onPress={onPress}
+            icon={<View style={style.iconContainer}>{icon}</View>}
+            arrowTappable
+            autoHeight
+            underline={index !== options.length - 1}
+          />
+        )
+      })}
+    </ThemedModal>
+  )
+}
+
+export const styles = cacheStyles((theme: Theme) => ({
+  iconContainer: {
+    flexDirection: 'row',
+    paddingVertical: theme.rem(0.75)
+  }
+}))
--- src/components/themed/WalletListFooter.tsx
+++ src/components/themed/WalletListFooter.tsx
@@ -1,88 +1,57 @@
 import * as React from 'react'
-import { TouchableOpacity, View } from 'react-native'
-import Ionicon from 'react-native-vector-icons/Ionicons'
+import { View } from 'react-native'
 
-import { SPECIAL_CURRENCY_INFO } from '../../constants/WalletAndCurrencyConstants'
 import { useHandler } from '../../hooks/useHandler'
 import { lstrings } from '../../locales/strings'
 import { useSelector } from '../../types/reactRedux'
 import { NavigationProp } from '../../types/routerTypes'
-import { EdgeTokenId } from '../../types/types'
-import { WalletListModal, WalletListResult } from '../modals/WalletListModal'
-import { Airship, showError } from '../services/AirshipInstance'
+import { ButtonsContainer } from '../buttons/ButtonsContainer'
+import { TransferModal } from '../modals/TransferModal'
+import { Airship } from '../services/AirshipInstance'
 import { cacheStyles, Theme, useTheme } from '../services/ThemeContext'
-import { EdgeText } from './EdgeText'
 
 interface Props {
   navigation: NavigationProp<'walletList'>
 }
 
+/**
+ * Renders the footer component for the WalletList screen.
+ */
 export const WalletListFooter = (props: Props) => {
   const { navigation } = props
   const theme = useTheme()
   const styles = getStyles(theme)
   const account = useSelector(state => state.core.account)
 
-  const handleAddWallet = useHandler(() => {
-    navigation.navigate('createWalletSelectCrypto', {})
+  const handleSend = useHandler(() => {
+    Airship.show(bridge => <TransferModal depositOrSend="send" bridge={bridge} account={account} navigation={navigation} />).catch(() => {})
   })
 
-  const handleAddToken = useHandler(() => {
-    const allowedAssets: EdgeTokenId[] = Object.keys(account.currencyConfig)
-      .filter(pluginId => SPECIAL_CURRENCY_INFO[pluginId]?.isCustomTokensSupported)
-      .map(pluginId => ({ pluginId }))
-
-    Airship.show<WalletListResult>(bridge => (
-      <WalletListModal allowedAssets={allowedAssets} bridge={bridge} navigation={navigation} headerTitle={lstrings.select_wallet} showCreateWallet />
-    ))
-      .then(({ walletId, currencyCode }) => {
-        if (walletId != null && currencyCode != null) {
-          navigation.navigate('manageTokens', { walletId })
-        }
-      })
-      .catch(showError)
+  const handleDeposit = useHandler(() => {
+    Airship.show(bridge => <TransferModal depositOrSend="deposit" bridge={bridge} account={account} navigation={navigation} />).catch(() => {})
   })
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handleAddWallet} style={styles.addButtonsContainer}>
-        <Ionicon name="md-add" style={styles.addItem} size={theme.rem(1.5)} color={theme.iconTappable} />
-        <EdgeText style={[styles.addItem, styles.addItemText]}>{lstrings.wallet_list_add_wallet}</EdgeText>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleAddToken} style={styles.addButtonsContainer}>
-        <Ionicon name="md-add" style={styles.addItem} size={theme.rem(1.5)} color={theme.iconTappable} />
-        <EdgeText style={[styles.addItem, styles.addItemText]}>{lstrings.wallet_list_add_token}</EdgeText>
-      </TouchableOpacity>
+      <ButtonsContainer
+        layout="row"
+        secondary2={{
+          onPress: handleSend,
+          label: lstrings.fragment_send_subtitle
+        }}
+        secondary={{
+          onPress: handleDeposit,
+          label: lstrings.loan_fragment_deposit
+        }}
+      />
     </View>
   )
 }
 
 const getStyles = cacheStyles((theme: Theme) => ({
   container: {
-    alignItems: 'stretch',
     borderTopColor: theme.lineDivider,
     borderTopWidth: theme.thinLineWidth,
-    flex: 1,
-    flexDirection: 'row',
-    marginLeft: theme.rem(1),
-    marginVertical: theme.rem(1),
-    paddingRight: theme.rem(1),
-    paddingTop: theme.rem(0.75)
-  },
-  addButtonsContainer: {
-    alignItems: 'center',
-    backgroundColor: theme.tileBackground,
-    flex: 1,
-    flexDirection: 'row',
-    height: theme.rem(3.25),
-    justifyContent: 'center'
-  },
-  addItem: {
-    color: theme.textLink,
-    fontFamily: theme.addButtonFont,
-    margin: theme.rem(0.25)
-  },
-  addItemText: {
-    flexShrink: 1
+    paddingVertical: theme.rem(0.75)
   }
 }))
