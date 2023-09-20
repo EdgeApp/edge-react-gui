20540673c4970ea500a5a2a1cbe5d810ab9440e3
Jon Tzeng <jon@edge.app>
2023-09-20 16:46:38 -0700

    Use wallet.disklet for token warning

--- src/actions/TokenTermsActions.tsx
+++ src/actions/TokenTermsActions.tsx
@@ -1,22 +1,25 @@
-import { Disklet } from 'disklet'
+import { EdgeCurrencyWallet } from 'edge-core-js'
 import * as React from 'react'
 import { sprintf } from 'sprintf-js'
 
 import { ConfirmContinueModal } from '../components/modals/ConfirmContinueModal'
 import { Airship } from '../components/services/AirshipInstance'
-import { TOKEN_TERMS_AGREEMENT } from '../constants/constantSettings'
 import { lstrings } from '../locales/strings'
 import { config } from '../theme/appConfig'
 
-export const approveTokenTerms = async (disklet: Disklet, currencyCode: string) => {
+export const TOKEN_TERMS_AGREEMENT = 'ttAgreement.json'
+
+export const approveTokenTerms = async (wallet: EdgeCurrencyWallet) => {
+  const { disklet } = wallet
+  const { currencyCode } = wallet.currencyInfo
+
   const title = sprintf(lstrings.token_agreement_modal_title, currencyCode)
   const body = sprintf(lstrings.token_agreement_modal_message, currencyCode, config.appName)
-  const filePath = `${currencyCode}-${TOKEN_TERMS_AGREEMENT}`
 
   try {
-    await disklet.getText(filePath)
+    await disklet.getText(TOKEN_TERMS_AGREEMENT)
   } catch (error: any) {
     await Airship.show<boolean>(bridge => <ConfirmContinueModal bridge={bridge} title={title} body={body} />)
-    await disklet.setText(filePath, '')
+    await disklet.setText(TOKEN_TERMS_AGREEMENT, '')
   }
 }
