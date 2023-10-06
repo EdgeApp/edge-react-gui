cc5a6aa2ed33bc3040b62610d8d3e507caf5b2e3
Paul V Puey <paul@edge.app>
2023-10-05 21:10:03 -0700

    Change support domain for transition

--- src/theme/edgeConfig.ts
+++ src/theme/edgeConfig.ts
@@ -11,14 +11,14 @@ export const edgeConfig: AppConfig = {
   configName: 'edge',
   darkTheme: edgeDark,
   defaultWallets: ['BTC', 'ETH', 'LTC', 'BCH', 'DASH'],
-  knowledgeBase: 'https://support.edge.app/support/home',
+  knowledgeBase: 'https://help.edge.app/support/home',
   lightTheme: edgeLight,
   notificationServers: ['https://notif1.edge.app'],
   phoneNumber: '+1-619-777-5688',
   referralServers: ['https://referral1.edge.app'],
   supportsEdgeLogin: true,
   supportEmail: 'support@edge.app',
-  supportSite: 'https://support.edge.app/support/tickets/new',
+  supportSite: 'https://help.edge.app/support/tickets/new',
   termsOfServiceSite: 'https://edge.app/tos/',
   website: 'https://edge.app'
 }
