34937524307d082ad02df8fb803d65d04e256948
Jon Tzeng <jon@edge.app>
2023-11-17 10:35:08 -0800

    Use ButtonsContainer for Done button Also add more bottom margin. This allows the user to bring the button closer the middle of the screen. The middle of the screen has a higher tap accuracy than the bottom edge of the screen.

--- src/components/scenes/TransactionDetailsScene.tsx
+++ src/components/scenes/TransactionDetailsScene.tsx
@@ -22,6 +22,7 @@ import { useSelector } from '../../types/reactRedux'
 import { EdgeSceneProps } from '../../types/routerTypes'
 import { convertNativeToExchange } from '../../util/utils'
 import { getMemoTitle } from '../../util/validateMemos'
+import { ButtonsContainer } from '../buttons/ButtonsContainer'
 import { NotificationSceneWrapper } from '../common/SceneWrapper'
 import { AccelerateTxModal } from '../modals/AccelerateTxModal'
 import { CategoryModal } from '../modals/CategoryModal'
@@ -30,7 +31,6 @@ import { TextInputModal } from '../modals/TextInputModal'
 import { Airship, showError, showToast } from '../services/AirshipInstance'
 import { cacheStyles, Theme, useTheme } from '../services/ThemeContext'
 import { EdgeText } from '../themed/EdgeText'
-import { MainButton } from '../themed/MainButton'
 import { AdvancedDetailsCard } from '../ui4/AdvancedDetailsCard'
 import { CardUi4 } from '../ui4/CardUi4'
 import { RowUi4 } from '../ui4/RowUi4'
@@ -308,7 +308,14 @@ export const TransactionDetailsScene = (props: Props) => {
 
       <AdvancedDetailsCard transaction={transaction} url={sprintf(wallet.currencyInfo.transactionExplorer, transaction.txid)} />
 
-      <MainButton onPress={navigation.pop} label={lstrings.string_done_cap} marginRem={[0, 2, 2]} type="secondary" />
+      <ButtonsContainer
+        layout="column"
+        primary={{
+          onPress: navigation.pop,
+          label: lstrings.string_done_cap
+        }}
+        scrollMargin
+      />
     </NotificationSceneWrapper>
   )
 }
