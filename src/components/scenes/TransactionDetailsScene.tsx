ef1d55e91998c26c5d19017671b078f84d6eb461
Jon Tzeng <jon@edge.app>
2023-11-22 15:29:01 -0800

    Adjust icon styling

--- src/components/scenes/TransactionDetailsScene.tsx
+++ src/components/scenes/TransactionDetailsScene.tsx
@@ -257,15 +257,19 @@ const TransactionDetailsComponent = (props: Props) => {
   return (
     <NotificationSceneWrapper navigation={navigation} hasTabs scroll padding={theme.rem(0.5)}>
       <CardUi4>
-        <RowUi4 type="editable" title={personHeader} onPress={openPersonInput}>
-          <View style={styles.tileRow}>
-            {thumbnailPath ? (
+        <RowUi4
+          type="editable"
+          icon={
+            thumbnailPath ? (
               <FastImage style={styles.tileThumbnail} source={{ uri: thumbnailPath }} />
             ) : (
               <IonIcon style={styles.tileAvatarIcon} name="person" size={theme.rem(2)} />
-            )}
-            <EdgeText>{personName}</EdgeText>
-          </View>
+            )
+          }
+          title={personHeader}
+          onPress={openPersonInput}
+        >
+          <EdgeText>{personName}</EdgeText>
         </RowUi4>
       </CardUi4>
 
