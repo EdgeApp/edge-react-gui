53afac311fb9ca4aaca6a1153d65a2154584c963
Jon Tzeng <jon@edge.app>
2023-12-04 12:27:34 -0800

    Fix empty note placeholder, update string

--- src/components/scenes/TransactionDetailsScene.tsx
+++ src/components/scenes/TransactionDetailsScene.tsx
@@ -301,7 +301,12 @@ const TransactionDetailsComponent = (props: Props) => {
         <RowUi4 type="editable" title={lstrings.transaction_details_category_title} onPress={openCategoryInput}>
           <EdgeText style={styles.tileCategory}>{categoriesText}</EdgeText>
         </RowUi4>
-        <RowUi4 type="editable" title={lstrings.transaction_details_notes_title} body={notes ?? ''} onPress={openNotesInput} />
+        <RowUi4
+          type="editable"
+          title={lstrings.transaction_details_notes_title}
+          body={notes == null || notes.trim() === '' ? lstrings.transaction_details_empty_note_placeholder : notes}
+          onPress={openNotesInput}
+        />
         {transaction.memos?.map((memo, i) =>
           memo.hidden === true ? null : <RowUi4 body={memo.value} key={`memo${i}`} title={getMemoTitle(memo.memoName)} type="copy" />
         )}
