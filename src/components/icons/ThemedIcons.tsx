23f805d392402e30cbbcc189453f689896f0c20a
Samuel Holmes <sam@edge.app>
2023-12-11 16:15:32 -0800

    Use `theme.icon` default color for themed icons

--- src/components/icons/ThemedIcons.tsx
+++ src/components/icons/ThemedIcons.tsx
@@ -3,19 +3,21 @@ import Animated, { AnimatedProps, SharedValue, useAnimatedStyle } from 'react-na
 import AntDesignIcon from 'react-native-vector-icons/AntDesign'
 import { IconProps as VectorIconProps } from 'react-native-vector-icons/Icon'
 
+import { useTheme } from '../services/ThemeContext'
+
 //
 // Types
 
 //
 
 export interface AnimatedIconProps {
-  color: SharedValue<string>
+  color?: SharedValue<string>
   size?: SharedValue<number>
 }
 export type AnimatedIconComponent = React.FunctionComponent<AnimatedIconProps>
 
 export interface IconProps {
-  color: string
+  color?: string
   size?: number
 }
 export type IconComponent = React.FunctionComponent<IconProps>
@@ -27,17 +29,23 @@ export type IconComponent = React.FunctionComponent<IconProps>
 function makeAnimatedFontIcon(IconComponent: React.ComponentType<AnimatedProps<VectorIconProps>>, name: string): AnimatedIconComponent {
   return (props: AnimatedIconProps) => {
     const { color, size } = props
+    const { icon, rem } = useTheme()
+    const oneRem = rem(1)
+
     const style = useAnimatedStyle(() => ({
-      color: color.value,
-      fontSize: size?.value
+      color: color?.value ?? icon,
+      fontSize: size?.value ?? oneRem
     }))
+
     return <IconComponent name={name} adjustsFontSizeToFit style={style} />
   }
 }
 
 function makeFontIcon(IconComponent: React.ComponentType<VectorIconProps>, name: string): IconComponent {
   return (props: IconProps) => {
-    const { color, size } = props
+    const { icon, rem } = useTheme()
+    const { color = icon, size = rem(1) } = props
+
     const style = {
       color: color,
       fontSize: size
