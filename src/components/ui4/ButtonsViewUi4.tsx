584b3c8dcb6ac37bb82de7096815d75294d550c1
Jon Tzeng <jon@edge.app>
2024-01-03 17:48:50 -0800

    Update ButtonsViewUi4 to fully control inter-button spacing

--- src/components/ui4/ButtonsViewUi4.tsx
+++ src/components/ui4/ButtonsViewUi4.tsx
@@ -4,7 +4,10 @@ import Animated from 'react-native-reanimated'
 
 import { useFadeAnimation } from '../../hooks/animations/useFadeAnimation'
 import { styled } from '../hoc/styled'
+import { Space } from '../layout/Space'
 import { ButtonTypeUi4, ButtonUi4 } from './ButtonUi4'
+
+const INTER_BUTTON_SPACING_REM = 1
 
 export interface ButtonInfo {
   label: string
@@ -50,6 +53,8 @@ export const ButtonsViewUi4 = React.memo(({ absolute = false, fade, primary, sec
 
   const fadeStyle = useFadeAnimation(fadeVisibleHack, { noFadeIn: fade == null })
 
+  const spacing = <Space around={INTER_BUTTON_SPACING_REM / 2} />
+
   const renderButton = (type: ButtonTypeUi4, buttonProps?: ButtonInfo) => {
     if (buttonProps == null) return null
     const { label, onPress, disabled } = buttonProps
@@ -67,8 +72,11 @@ export const ButtonsViewUi4 = React.memo(({ absolute = false, fade, primary, sec
     <Animated.View style={fadeStyle}>
       <StyledButtonContainer absolute={absolute} layout={layout} sceneMargin={sceneMargin}>
         {renderButton('primary', primary)}
+        {primary != null && secondary != null ? spacing : null}
         {renderButton('secondary', secondary2)}
+        {secondary != null && secondary2 != null ? spacing : null}
         {renderButton('secondary', secondary)}
+        {tertiary != null ? spacing : null}
         {renderButton('tertiary', tertiary)}
       </StyledButtonContainer>
     </Animated.View>
@@ -108,8 +116,7 @@ const StyledButtonContainer = styled(View)<{ absolute: boolean; layout: 'row' | 
       ? {
           flex: 1,
           flexDirection: 'row-reverse',
-          justifyContent: 'center',
-          marginHorizontal: 0
+          justifyContent: 'center'
         }
       : {}
 
