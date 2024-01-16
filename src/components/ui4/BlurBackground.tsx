77b8d6e4f6a25a1ab94d04ef895c0869fadafc70
Samuel Holmes <sam@edge.app>
2024-01-16 13:26:15 -0800

    Add transparent black background to BlurHeader on Android
    We add a black transparent background on Android to BlurBackground for
    presentation consistency across platforms.

--- src/components/ui4/BlurBackground.tsx
+++ src/components/ui4/BlurBackground.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { Platform, StyleSheet } from 'react-native'
 import { BlurView } from 'rn-id-blurview'
 
-import { useTheme } from '../services/ThemeContext'
+import { cacheStyles, useTheme } from '../services/ThemeContext'
+
+const isAndroid = Platform.OS === 'android'
 
 export interface BlurBackgroundProps {
   overlayColor?: string
@@ -11,5 +13,14 @@ export interface BlurBackgroundProps {
 export const BlurBackground = (props: BlurBackgroundProps) => {
   const { overlayColor = 'rgba(0, 0, 0, 0)' } = props
   const theme = useTheme()
-  return <BlurView blurType={theme.isDark ? 'dark' : 'light'} style={StyleSheet.absoluteFill} overlayColor={overlayColor} />
+  const styles = getStyles(theme)
+
+  return <BlurView blurType={theme.isDark ? 'dark' : 'light'} style={styles.blurView} overlayColor={overlayColor} />
 }
+
+const getStyles = cacheStyles(() => ({
+  blurView: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: isAndroid ? '#00000055' : undefined
+  }
+}))
