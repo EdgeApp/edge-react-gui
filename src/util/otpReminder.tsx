99b4452d71d7bef3087597d02b479c67a6e41f07
Jon Tzeng <jon@edge.app>
2024-02-28 15:24:28 -0800

    Refactor showOtpReminder->getOtpReminderModal

--- src/util/otpReminder.tsx
+++ src/util/otpReminder.tsx
@@ -13,72 +13,78 @@ const OTP_REMINDER_KEY_NAME_LAST_OTP_CHECKED = 'lastOtpCheck'
 const OTP_REMINDER_KEY_NAME_DONT_ASK = 'OtpDontAsk'
 
 /**
- * Check and show the 2fa reminder on login.
+ * Check and return a potential 2fa reminder modal if needed to be shown onPress
+ * of its corresponding notification card.
  */
-export async function showOtpReminder(account: EdgeAccount): Promise<void> {
+export async function getOtpReminderModal(account: EdgeAccount): Promise<(() => Promise<void>) | undefined> {
   const { otpKey, dataStore, username, created } = account
-  if (username == null) return
-  if (otpKey != null) return
-
-  const [dontAsk, lastOtpCheckString]: [string | null, string | null] = (await Promise.all([
+  if (username == null || otpKey != null) return
+  const [dontAsk, lastOtpCheckString]: [string | null, string | null] = await Promise.all([
     dataStore.getItem(OTP_REMINDER_STORE_NAME, OTP_REMINDER_KEY_NAME_DONT_ASK).catch(() => null),
     dataStore.getItem(OTP_REMINDER_STORE_NAME, OTP_REMINDER_KEY_NAME_LAST_OTP_CHECKED).catch(() => null)
-  ])) as any
-
+  ])
   if (dontAsk) return
+
+  // Return a modal if we have never shown it before, and the account is old
+  // enough:
   const lastOtpCheck = lastOtpCheckString != null ? parseInt(lastOtpCheckString) : null
-
-  // Show the modal if we have never shown it before,
-  // and the account is old enough:
   if (lastOtpCheck == null && (created == null || Date.now() > created.valueOf() + OTP_REMINDER_MILLISECONDS)) {
-    Keyboard.dismiss()
-    const result = await Airship.show<'yes' | 'no' | undefined>(bridge => (
-      <ButtonsModal
-        bridge={bridge}
-        title={lstrings.otp_reset_modal_header}
-        message={lstrings.otp_reset_modal_message}
-        buttons={{
-          yes: { label: lstrings.otp_enable },
-          no: { label: lstrings.skip, type: 'secondary' }
-        }}
-      />
-    ))
-    if (result === 'yes') {
-      await enableOtp(account)
-    } else {
-      await account.dataStore.setItem(OTP_REMINDER_STORE_NAME, OTP_REMINDER_KEY_NAME_LAST_OTP_CHECKED, Date.now().toString())
+    return async () => {
+      Keyboard.dismiss()
+      const result = await Airship.show<'yes' | 'no' | undefined>(bridge => (
+        <ButtonsModal
+          bridge={bridge}
+          title={lstrings.otp_reset_modal_header}
+          message={lstrings.otp_reset_modal_message}
+          buttons={{
+            yes: { label: lstrings.otp_enable },
+            no: { label: lstrings.skip, type: 'secondary' }
+          }}
+        />
+      ))
+      if (result === 'yes') {
+        await enableOtp(account)
+      } else {
+        await account.dataStore.setItem(OTP_REMINDER_STORE_NAME, OTP_REMINDER_KEY_NAME_LAST_OTP_CHECKED, Date.now().toString())
+      }
     }
   }
 
-  // Show a modal with the "Don't ask again" button if we have waited
-  // long enough since the last time we showed a 2fa reminder modal:
+  // Return a modal with the "Don't ask again" button if we have waited long
+  // enough since the last time we showed a 2fa reminder modal:
   if (lastOtpCheck != null && Date.now() > lastOtpCheck + OTP_REMINDER_MILLISECONDS) {
-    Keyboard.dismiss()
-    const result = await Airship.show<'enable' | 'cancel' | 'dontAsk' | undefined>(bridge => (
-      <ButtonsModal
-        bridge={bridge}
-        title={lstrings.otp_reset_modal_header}
-        message={lstrings.otp_reset_modal_message}
-        buttons={{
-          enable: { label: lstrings.otp_enable, type: 'primary' },
-          cancel: { label: lstrings.skip, type: 'secondary' },
-          dontAsk: {
-            label: lstrings.otp_reset_modal_dont_ask,
-            type: 'secondary'
-          }
-        }}
-      />
-    ))
-    if (result === 'enable') {
-      await enableOtp(account)
-    } else if (result === 'dontAsk') {
-      await account.dataStore.setItem(OTP_REMINDER_STORE_NAME, OTP_REMINDER_KEY_NAME_DONT_ASK, 'true')
-    } else {
-      await account.dataStore.setItem(OTP_REMINDER_STORE_NAME, OTP_REMINDER_KEY_NAME_LAST_OTP_CHECKED, Date.now().toString())
+    return async () => {
+      Keyboard.dismiss()
+      const result = await Airship.show<'enable' | 'cancel' | 'dontAsk' | undefined>(bridge => (
+        <ButtonsModal
+          bridge={bridge}
+          title={lstrings.otp_reset_modal_header}
+          message={lstrings.otp_reset_modal_message}
+          buttons={{
+            enable: { label: lstrings.otp_enable, type: 'primary' },
+            cancel: { label: lstrings.skip, type: 'secondary' },
+            dontAsk: {
+              label: lstrings.otp_reset_modal_dont_ask,
+              type: 'secondary'
+            }
+          }}
+        />
+      ))
+      if (result === 'enable') {
+        await enableOtp(account)
+      } else if (result === 'dontAsk') {
+        await account.dataStore.setItem(OTP_REMINDER_STORE_NAME, OTP_REMINDER_KEY_NAME_DONT_ASK, 'true')
+      } else {
+        await account.dataStore.setItem(OTP_REMINDER_STORE_NAME, OTP_REMINDER_KEY_NAME_LAST_OTP_CHECKED, Date.now().toString())
+      }
     }
   }
+  return undefined
 }
 
+/**
+ * Enable OTP and show a success modal
+ */
 const enableOtp = async (account: EdgeAccount) => {
   await account.enableOtp()
   return await Airship.show<'ok' | undefined>(bridge => (
