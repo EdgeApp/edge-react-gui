e6a8b053a9887882c920e32a35cca25f1594b6c3
Jon Tzeng <jon@edge.app>
2024-03-11 18:47:59 -0700

    fixup! Implement CryptoAmount

--- src/util/CryptoAmount.ts
+++ src/util/CryptoAmount.ts
@@ -2,29 +2,30 @@ import { div, mul } from 'biggystring'
 import { EdgeCurrencyConfig, EdgeDenomination } from 'edge-core-js'
 
 import { RootState } from '../reducers/RootReducer'
-import { emptyEdgeDenomination } from '../selectors/DenominationSelectors'
 import { convertCurrency } from '../selectors/WalletSelectors'
 import { asBiggystring } from './cleaners'
 import { DECIMAL_PRECISION, mulToPrecision } from './utils'
 
-// Defines base properties for asset configuration.
+/**
+ * Defines base properties for the CryptoAmount asset configuration.
+ */
 interface AssetBaseArgs {
   currencyConfig: EdgeCurrencyConfig
 }
 
-// Specifies that one of tokenId or currencyCode must be provided, or neither.
+/**
+ * Specifies that one of tokenId or currencyCode must be provided. To describe a
+ * mainnet/parent/chain currency, an explicit null must be provided to tokenId
+ * or currencyCode.
+ */
 type TokenOrCurrencyCodeArgs =
   | {
-      tokenId: string
+      tokenId: string | null
       currencyCode?: never
     }
   | {
       tokenId?: never
       currencyCode: string
-    }
-  | {
-      tokenId?: never
-      currencyCode?: never
     }
 
 // Specifies that one of exchangeAmount or nativeAmount must be provided.
@@ -46,26 +47,33 @@ type CryptoAmountConstructorArgs = AssetBaseArgs & TokenOrCurrencyCodeArgs & Exc
  * Pass whatever you happen to have, get what you need.
  *
  * Usage:
- * const cryptoAmountWithExchange = CryptoAmount.createWithExchangeAmount({
- *   currencyConfig,
+ * const ethAmountWithExchange = new CryptoAmount({
+ *   currencyConfig, // 'ethereum'
+ *   tokenId: null,
  *   exchangeAmount: '100',
  * })
  *
- * const cryptoAmountWithNative = CryptoAmount.createWithNativeAmount({
+ * const cryptoAmountWithNative = new CryptoAmount({
+ *   currencyConfig,
  *   currencyCode: 'USDC',
  *   nativeAmount: '12345678',
  * })
+ *
+ * cryptoAmount.getDollarValue()
  */
 export class CryptoAmount {
   public readonly currencyConfig: EdgeCurrencyConfig
   public readonly nativeAmount: string
   public readonly tokenId: string | null
 
+  /**
+   * Must construct CryptoAmount with currencyConfig and one of either: tokenId or currencyCode
+   */
   public constructor(args: CryptoAmountConstructorArgs) {
     const { currencyCode, currencyConfig, exchangeAmount, nativeAmount, tokenId } = args
     this.currencyConfig = currencyConfig
 
-    // Populate tokenId
+    // Populate tokenId, derived from currencyCode
     if (currencyCode != null) {
       // Ensure currencyCode is recognized, if given as a constructor argument.
       const foundTokenId = Object.keys(currencyConfig.allTokens).find(edgeToken => currencyConfig.allTokens[edgeToken].currencyCode === currencyCode)
@@ -75,7 +83,7 @@ export class CryptoAmount {
         this.tokenId = foundTokenId
       }
     } else {
-      this.tokenId = tokenId ?? null
+      this.tokenId = tokenId
     }
 
     // Populate nativeAmount
@@ -99,20 +107,37 @@ export class CryptoAmount {
   // Getters:
   //
 
+  /**
+   * Mainnet, parent, network or chain currency code.
+   */
   get chainCode(): string {
     return this.currencyConfig.currencyInfo.currencyCode
   }
 
+  /**
+   * If this CryptoAmount is about a token, the currency code for that token.
+   * Else, the mainnet, parent, network or chain code.
+   */
   get currencyCode(): string {
     const { currencyCode } = this.tokenId == null ? this.currencyConfig.currencyInfo : this.currencyConfig.allTokens[this.tokenId]
     return currencyCode
   }
 
+  /**
+   * The decimal amount you would see on exchanges and pretty much anywhere
+   * else. Given in maximum decimal precision.
+   *
+   * If typical app precision or a specific precision is required, use
+   * getExchangeAmount() instead.
+   */
   get exchangeAmount(): string {
     const { multiplier } = this.getExchangeDenom()
     return div(this.nativeAmount, multiplier, DECIMAL_PRECISION)
   }
 
+  /**
+   * Core pluginId associated with this currency
+   */
   get pluginId(): string {
     return this.currencyConfig.currencyInfo.pluginId
   }
@@ -121,14 +146,25 @@ export class CryptoAmount {
   // Utilities:
   //
 
+  /**
+   * Automatically uses 2 decimal/cent places if unspecified.
+   */
   displayDollarValue(state: RootState, precision?: number): string {
-    return parseFloat(convertCurrency(state, this.currencyCode, 'iso:USD', this.exchangeAmount)).toFixed(precision ?? 2)
+    return this.displayFiatValue(state, 'iso:USD', precision)
   }
 
+  /**
+   * Automatically uses 2 decimal/cent places if unspecified.
+   */
   displayFiatValue(state: RootState, isoFiatCode: string, precision?: number) {
     return parseFloat(convertCurrency(state, this.currencyCode, isoFiatCode, this.exchangeAmount)).toFixed(precision ?? 2)
   }
 
+  /**
+   * The amount you would see on exchanges and pretty much anywhere else. If
+   * precision is unset, precision is dynamically set according to the asset's
+   * multiplier.
+   */
   getExchangeAmount(precision?: number): string {
     const { multiplier } = this.getExchangeDenom()
     return div(this.nativeAmount, multiplier, precision ?? mulToPrecision(multiplier))
@@ -136,13 +172,6 @@ export class CryptoAmount {
 
   getExchangeDenom(): EdgeDenomination {
     const { allTokens, currencyInfo } = this.currencyConfig
-
-    if (this.tokenId == null) return currencyInfo.denominations[0]
-    for (const tokenId of Object.keys(allTokens)) {
-      const token = allTokens[tokenId]
-      if (token.currencyCode === this.currencyCode) return token.denominations[0]
-    }
-
-    return emptyEdgeDenomination
+    return this.tokenId == null ? currencyInfo.denominations[0] : allTokens[this.tokenId].denominations[0]
   }
 }
