1d63f0298c222c7bd9c5b3661ca2216e02879722
Samuel Holmes <sam@edge.app>
2024-04-05 15:24:44 -0700

    Change supportEmail for bityProvider

--- src/plugins/gui/providers/bityProvider.ts
+++ src/plugins/gui/providers/bityProvider.ts
@@ -25,7 +25,7 @@ const storeId = 'com.bity'
 const partnerIcon = 'logoBity.png'
 const pluginDisplayName = 'Bity'
 const providerDisplayName = pluginDisplayName
-const supportEmail = 'support@bity.com'
+const supportEmail = 'support_edge@bity.com'
 const supportedPaymentType: FiatPaymentType = 'sepa'
 const partnerFee = 0.005
 
