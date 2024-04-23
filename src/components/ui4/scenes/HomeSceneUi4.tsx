8679fcfe30ab47289bd446aaeae0ac0bb9ad7dba
Jon Tzeng <jon@edge.app>
2024-04-23 15:52:08 -0700

    Move ip country code eval to HomeSceneUi4 Also, minor rearrangement of state and hook code in HomeSceneUi4

--- src/components/ui4/scenes/HomeSceneUi4.tsx
+++ src/components/ui4/scenes/HomeSceneUi4.tsx
@@ -5,7 +5,9 @@ import FastImage from 'react-native-fast-image'
 import Animated from 'react-native-reanimated'
 import { useSafeAreaFrame } from 'react-native-safe-area-context'
 
+import { getCountryCodeByIp } from '../../../actions/AccountReferralActions'
 import { SCROLL_INDICATOR_INSET_FIX } from '../../../constants/constantSettings'
+import { useAsyncEffect } from '../../../hooks/useAsyncEffect'
 import { useHandler } from '../../../hooks/useHandler'
 import { lstrings } from '../../../locales/strings'
 import { useSceneScrollHandler } from '../../../state/SceneScrollState'
@@ -41,6 +43,9 @@ export const HomeSceneUi4 = (props: Props) => {
   // Evenly distribute the home cards into 4 quadrants:
   const cardSize = screenWidth / 2 - theme.rem(TEMP_PADDING_REM)
 
+  const [blogPosts, setBlogPosts] = React.useState<BlogPost[]>([])
+  const [countryCode, setCountryCode] = React.useState<string | undefined>()
+
   //
   // Handlers
   //
@@ -62,7 +67,15 @@ export const HomeSceneUi4 = (props: Props) => {
   })
   const handleScroll = useSceneScrollHandler()
 
-  const [blogPosts, setBlogPosts] = React.useState<BlogPost[]>([])
+  // Set countryCode once
+  useAsyncEffect(
+    async () => {
+      const countryCode = await getCountryCodeByIp().catch(() => '')
+      setCountryCode(countryCode)
+    },
+    [],
+    'countryCode'
+  )
 
   // Check for BlogPosts from info server:
   React.useEffect(() => {
@@ -99,7 +112,7 @@ export const HomeSceneUi4 = (props: Props) => {
                   <BalanceCardUi4 onViewAssetsPress={handleViewAssetsPress} navigation={navigation} />
                 </EdgeAnim>
                 {/* Animation inside PromoCardsUi4 component */}
-                <PromoCardsUi4 navigation={navigation} screenWidth={screenWidth} />
+                <PromoCardsUi4 countryCode={countryCode} navigation={navigation} screenWidth={screenWidth} />
                 <EdgeAnim style={homeRowStyle} enter={fadeInUp80}>
                   <HomeCardUi4
                     title={lstrings.buy_crypto}
