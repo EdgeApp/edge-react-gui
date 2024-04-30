2bf934516d06380b2cc1761f7eeab2629afd9470
Jon Tzeng <jon@edge.app>
2024-04-29 18:25:12 -0700

    Add new asset setting to re-auto-enable tokens with balances

--- src/components/scenes/AssetSettingsScene.tsx
+++ src/components/scenes/AssetSettingsScene.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
 
 import { CURRENCY_SETTINGS_KEYS } from '../../constants/WalletAndCurrencyConstants'
+import { useHandler } from '../../hooks/useHandler'
+import { lstrings } from '../../locales/strings'
 import { useSelector } from '../../types/reactRedux'
 import { EdgeSceneProps } from '../../types/routerTypes'
 import { SceneWrapper } from '../common/SceneWrapper'
+import { showToast } from '../services/AirshipInstance'
+import { cacheStyles, Theme, useTheme } from '../services/ThemeContext'
 import { SettingsTappableRow } from '../settings/SettingsTappableRow'
 import { CryptoIconUi4 } from '../ui4/CryptoIconUi4'
 
@@ -11,10 +16,40 @@ interface Props extends EdgeSceneProps<'assetSettings'> {}
 
 export function AssetSettingsScene(props: Props) {
   const { navigation } = props
+  const theme = useTheme()
+  const styles = getStyles(theme)
+
   const account = useSelector(state => state.core.account)
+  const { currencyWallets } = account
+
+  /**
+   * Iterates through all the user's currency wallets, updates the enabled token
+   * IDs to include any newly detected tokens, and then displays a toast message
+   * indicating whether any detected tokens were enabled.
+   */
+  const handleRescanTokens = useHandler(async () => {
+    let isTokensDetected = false
+    for (const wallet of Object.values(currencyWallets)) {
+      const detectedDisabledTokens = wallet.detectedTokenIds.filter(dt => !wallet.enabledTokenIds.includes(dt))
+
+      if (detectedDisabledTokens.length > 0) {
+        await wallet.changeEnabledTokenIds([...wallet.enabledTokenIds, ...detectedDisabledTokens])
+        isTokensDetected = true
+      }
+    }
+
+    if (isTokensDetected) {
+      showToast(lstrings.settings_enable_detected_tokens_toast)
+    } else {
+      showToast(lstrings.settings_no_detected_tokens_toast)
+    }
+  })
 
   return (
     <SceneWrapper scroll>
+      <SettingsTappableRow key="detectTokens" label={lstrings.settings_detect_tokens} onPress={handleRescanTokens}>
+        <FontAwesomeIcon style={styles.icon} name="refresh" size={theme.rem(1.25)} color={theme.iconTappable} />
+      </SettingsTappableRow>
       {CURRENCY_SETTINGS_KEYS.map(pluginId => {
         if (account.currencyConfig[pluginId] == null) return null
         const { currencyInfo } = account.currencyConfig[pluginId]
@@ -33,3 +68,9 @@ export function AssetSettingsScene(props: Props) {
     </SceneWrapper>
   )
 }
+
+const getStyles = cacheStyles((theme: Theme) => ({
+  icon: {
+    marginLeft: theme.rem(0.6)
+  }
+}))
