5b69a5bf0748dd73f8de2bf066f03992f27acc99
Jon Tzeng <jon@edge.app>
2024-05-01 12:42:41 -0700

    Only call Animated.addWhitelistedNativeProps once No longer OK to call multiple times after expo and/or reanimated updates.

--- index.ts
+++ index.ts
@@ -10,6 +10,9 @@ import { App } from './src/components/App'
 if (typeof BigInt === 'undefined') global.BigInt = require('big-integer')
 
 // See https://github.com/software-mansion/react-native-reanimated/issues/1794#issuecomment-898393331
-Animated.addWhitelistedNativeProps({})
+// For ReText/SwipeChart support requirements, see:
+// https://github.com/software-mansion/react-native-reanimated/discussions/5621
+// https://github.com/software-mansion/react-native-reanimated/issues/5432
+Animated.addWhitelistedNativeProps({ text: true })
 
 AppRegistry.registerComponent(appName, () => App)
