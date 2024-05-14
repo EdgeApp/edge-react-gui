3cf003ebe4a71c1d244f35bcee3bfe88b6610888
Sam Holmes <sam@edge.app>
2024-05-14 09:56:48 -0700

    Upgrade edge-currency-plugins@^3.0.0

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -262,6 +262,7 @@ export const asEnvConfig = asObject({
   ALLOW_DEVELOPER_MODE: asOptional(asBoolean, true),
   DEV_TAB: asOptional(asBoolean, false),
   DEBUG_CORE: asOptional(asBoolean, false),
+  DEBUG_CURRENCY_PLUGINS: asOptional(asBoolean, false),
   DEBUG_PLUGINS: asOptional(asBoolean, false),
   DEBUG_ACCOUNTBASED: asOptional(asBoolean, false),
   DEBUG_EXCHANGES: asOptional(asBoolean, false),
