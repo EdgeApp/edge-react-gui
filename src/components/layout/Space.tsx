fa8de191281de161556a4c75bfc72de3c225d5dd
Sam Holmes <sam@edge.app>
2024-05-23 17:39:30 -0700

    Remove style prop from Space component

--- src/components/layout/Space.tsx
+++ src/components/layout/Space.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react'
-import { View, ViewStyle } from 'react-native'
+import { View } from 'react-native'
 
 import { SpaceProps, useSpaceStyle } from '../../hooks/useSpaceStyle'
 
 interface OwnProps {
   children?: React.ReactNode
-  style?: ViewStyle
 }
 type Props = OwnProps & SpaceProps
 
@@ -14,8 +13,8 @@ type Props = OwnProps & SpaceProps
  * allows for centering and alignment (see useSpaceStyle hook for details).
  */
 export const Space = React.memo((props: Props) => {
-  const { children, style } = props
+  const { children } = props
   const spaceStyle = useSpaceStyle(props)
 
-  return <View style={[spaceStyle, style]}>{children}</View>
+  return <View style={spaceStyle}>{children}</View>
 })
