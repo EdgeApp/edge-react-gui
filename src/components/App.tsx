6ef6ba47bb58368e2367342c62cbfe5397c44f5e
Sam Holmes <sam@edge.app>
2024-06-07 13:17:30 -0700

    Tag errors sent to sentry from crash ErrorBoundary as not handled

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,27 @@
 import '@ethersproject/shims'
 
-import { ErrorBoundary, wrap } from '@sentry/react-native'
+import { ErrorBoundary, Scope, wrap } from '@sentry/react-native'
 import * as React from 'react'
 import { StyleSheet } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+import { useHandler } from '../hooks/useHandler'
 import { CrashScene } from './scenes/CrashScene'
 import { EdgeCoreManager } from './services/EdgeCoreManager'
 import { StatusBarManager } from './services/StatusBarManager'
 import { ThemeProvider } from './services/ThemeContext'
 
 function MainApp() {
+  const handleBeforeCapture = useHandler((scope: Scope) => {
+    scope.setTag('handled', false)
+  })
+
   return (
     <SafeAreaProvider>
       <ThemeProvider>
         <GestureHandlerRootView style={StyleSheet.absoluteFill}>
-          <ErrorBoundary fallback={<CrashScene />}>
+          <ErrorBoundary beforeCapture={handleBeforeCapture} fallback={<CrashScene />}>
             <StatusBarManager />
             <EdgeCoreManager />
           </ErrorBoundary>
