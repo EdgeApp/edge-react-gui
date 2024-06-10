1f34e6c97962bb35965628e148fdded89b533428
Jon Tzeng <jon@edge.app>
2024-06-10 14:51:54 -0700

    Implement useAsyncNavigation

---
+++ src/hooks/useAsyncNavigation.tsx
@@ -0,0 +1,45 @@
+import { NavigationProp as NavigationCoreProp, StackActionHelpers } from '@react-navigation/native'
+import * as React from 'react'
+
+import { AppParamList, NavigationBase, RouteParamList } from '../types/routerTypes'
+
+/**
+ * Use this in place of NavigationProp/NavigationBase methods to prevent
+ * multiple navigations from rapid tapping. Navigation calls are only executed
+ * if there isn't already another one in flight
+ */
+export const useAsyncNavigation = <T extends keyof RouteParamList>(
+  navigation: NavigationBase | (NavigationCoreProp<AppParamList, T> & StackActionHelpers<AppParamList>)
+): NavigationBase & (NavigationCoreProp<AppParamList, T> & StackActionHelpers<AppParamList>) => {
+  const [isNavigating, setIsNavigating] = React.useState(false)
+
+  React.useEffect(() => {
+    const handleNavComplete = () => {
+      setIsNavigating(false)
+    }
+
+    const unsubscribeState = navigation.addListener('state', handleNavComplete)
+    const unsubscribeFocus = navigation.addListener('focus', handleNavComplete)
+
+    return () => {
+      unsubscribeState()
+      unsubscribeFocus()
+    }
+  }, [navigation])
+
+  const createDebouncedMethod = <U extends (...args: Parameters<U>) => void>(method: U): U => {
+    return ((...args: Parameters<U>) => {
+      if (isNavigating) return
+
+      setIsNavigating(true)
+      method(...args)
+    }) as U
+  }
+
+  return {
+    ...navigation,
+    push: createDebouncedMethod(navigation.push),
+    navigate: createDebouncedMethod(navigation.navigate)
+    // Add other navigation methods as needed
+  }
+}
