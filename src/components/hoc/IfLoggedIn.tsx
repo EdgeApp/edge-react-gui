9bf7ea449bc524314c0072724ccc1b9870e2f11f
William Swanson <swansontec@gmail.com>
2024-06-18 14:36:51 -0700

    Fix ifLoggedIn logic

--- src/components/hoc/IfLoggedIn.tsx
+++ src/components/hoc/IfLoggedIn.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 
+import { useWatch } from '../../hooks/useWatch'
 import { useSelector } from '../../types/reactRedux'
 import { LoadingScene } from '../scenes/LoadingScene'
 
 export function ifLoggedIn<Props extends {}>(Component: React.ComponentType<Props>): React.FunctionComponent<Props> {
   return function (props: Props) {
-    const settingsLoaded = useSelector(state => state.ui.settings.settingsLoaded ?? false)
-    return settingsLoaded ? <Component {...props} /> : <LoadingScene />
+    const account = useSelector(state => state.core.account)
+    const loggedIn = useWatch(account, 'loggedIn')
+    return loggedIn ? <Component {...props} /> : <LoadingScene />
   }
 }
