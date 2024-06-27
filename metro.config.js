1cdd5deb927376dfbe6570a1c89e6641f42ff23d
William Swanson <swansontec@gmail.com>
2024-06-27 11:25:47 -0700

    Downgrade to react-native 0.71.15

--- metro.config.js
+++ metro.config.js
@@ -1,29 +1,23 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
-
-const { createSentryMetroSerializer } = require('@sentry/react-native/dist/js/tools/sentryMetroSerializer')
-
-const defaultConfig = getDefaultConfig(__dirname)
-const { assetExts, sourceExts } = defaultConfig.resolver
-
 /**
- * Metro configuration
- * https://reactnative.dev/docs/metro
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
  *
- * @type {import('metro-config').MetroConfig}
+ * @format
  */
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer')
-  },
 
-  resolver: {
-    assetExts: assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg']
-  },
+const { getDefaultConfig } = require('metro-config')
 
-  serializer: {
-    customSerializer: createSentryMetroSerializer()
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig()
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer')
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg']
+    }
   }
-}
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+})()
