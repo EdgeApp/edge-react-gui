f2c4bcad299f4d4703bab5e3d7c324147ee996ed
Sam Holmes <sam@edge.app>
2024-06-27 16:33:04 -0700

    Add key back to SceneFooter

--- src/components/scenes/TransactionListScene.tsx
+++ src/components/scenes/TransactionListScene.tsx
@@ -260,6 +260,7 @@ function TransactionListComponent(props: Props) {
     sceneWrapperInfo => {
       return (
         <SearchFooter
+          key="TransactionListScene-SearchFooter"
           name="TransactionListScene-SearchFooter"
           placeholder={lstrings.transaction_list_search}
           isSearching={isSearching}
