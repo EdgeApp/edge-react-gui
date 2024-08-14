7206f35819357ea7fb4c23cb2e6a03aa5042fd8f
Jon Tzeng <jon@edge.app>
2024-08-14 14:31:08 -0700

    Convert SwapSuccessScene into a fn component

--- src/__tests__/scenes/SwapSuccessScene.test.tsx
+++ src/__tests__/scenes/SwapSuccessScene.test.tsx
@@ -1,19 +1,72 @@
 import { describe, expect, it } from '@jest/globals'
+import { EdgeCurrencyInfo } from 'edge-core-js'
 import * as React from 'react'
-import { createRenderer } from 'react-test-renderer/shallow'
+import TestRenderer from 'react-test-renderer'
 
-import { SwapSuccessSceneComponent } from '../../components/scenes/SwapSuccessScene'
-import { getTheme } from '../../components/services/ThemeContext'
+import { SwapSuccessScene } from '../../components/scenes/SwapSuccessScene'
+import { FakeProviders, FakeState } from '../../util/fake/FakeProviders'
 import { fakeSceneProps } from '../../util/fake/fakeSceneProps'
+
+const currencyInfo: EdgeCurrencyInfo = {
+  pluginId: 'bitcoin',
+  currencyCode: 'BTC',
+  displayName: 'Bitcoin',
+  walletType: 'bitcoin',
+
+  addressExplorer: '',
+  transactionExplorer: '',
+
+  defaultSettings: {},
+  metaTokens: [],
+  denominations: [
+    {
+      name: 'BTC',
+      multiplier: '100000000',
+      symbol: '₿'
+    }
+  ]
+}
+
+const fakeCurrencyConfig: any = {
+  currencyInfo,
+  allTokens: {},
+  builtinTokens: {},
+  customTokens: {}
+}
+
+const fakeCoreWallet: any = {
+  balanceMap: new Map([[null, '123123']]),
+  blockHeight: 12345,
+  currencyConfig: fakeCurrencyConfig,
+  currencyInfo,
+  enabledTokenIds: [],
+  fiatCurrencyCode: 'iso:USD',
+  id: '123',
+  name: 'wallet name',
+  type: 'wallet:bitcoin',
+  watch() {}
+}
 
 describe('SwapSuccessSceneComponent', () => {
   it('should render with loading props', () => {
-    const renderer = createRenderer()
+    const fakeState: FakeState = {
+      core: {
+        account: {
+          id: '',
+          currencyWallets: { '123': fakeCoreWallet },
+          currencyConfig: { bitcoin: fakeCurrencyConfig },
+          watch() {}
+        }
+      }
+    }
 
-    const fakeDisklet: any = {}
+    const renderer = TestRenderer.create(
+      <FakeProviders initialState={fakeState}>
+        <SwapSuccessScene {...fakeSceneProps('swapSuccess', {})} />
+      </FakeProviders>
+    )
 
-    const actual = renderer.render(<SwapSuccessSceneComponent {...fakeSceneProps('swapSuccess', {})} userId="" disklet={fakeDisklet} theme={getTheme()} />)
-
-    expect(actual).toMatchSnapshot()
+    expect(renderer.toJSON()).toMatchSnapshot()
+    renderer.unmount()
   })
 })
