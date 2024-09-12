d4946d15183062ec1e898ff17694fb02eac7f2fe
Sam Holmes <sam@edge.app>
2024-09-12 14:41:30 -0700

    Update GLIF Infinity Pool addresses

--- src/plugins/stake-plugins/generic/pluginInfo/filecoinGlifpool.ts
+++ src/plugins/stake-plugins/generic/pluginInfo/filecoinGlifpool.ts
@@ -17,8 +17,8 @@ const filecoinPolicyConfig: Array<StakePolicyConfig<GlifInfinityPoolAdapterConfi
     adapterConfig: {
       type: 'glif-infinity-pool',
       rpcProviderUrls: [`https://api.node.glif.io/rpc/v0`],
-      poolContractAddress: '0x43dAe5624445e7679D16a63211c5ff368681500c', // Pool 0
-      simpleRampContractAddress: '0x8147AccE69d711bcED176a0b7a029Ff54800d930'
+      poolContractAddress: '0xe764Acf02D8B7c21d2B6A8f0a96C78541e0DC3fd', // Pool 0
+      simpleRampContractAddress: '0xe764Acf02D8B7c21d2B6A8f0a96C78541e0DC3fd'
     },
     disableMaxStake: true,
     hideClaimAction: true,
