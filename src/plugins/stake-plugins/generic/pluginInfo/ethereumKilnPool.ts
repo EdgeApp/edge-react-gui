8886a081a97f39a7a54ca79c8bd518ac68e61fe1
peachbits <matthew@edge.app>
2024-09-26 12:41:07 -0700

    Add Ethereum mainnet staking

--- src/plugins/stake-plugins/generic/pluginInfo/ethereumKilnPool.ts
+++ src/plugins/stake-plugins/generic/pluginInfo/ethereumKilnPool.ts
@@ -24,6 +24,28 @@ const kilnPolicyConfig: Array<StakePolicyConfig<EthereumPooledKilnAdapterConfig>
     hideUnstakeAndClaimAction: true,
     stakeAssets: [{ pluginId: 'holesky', currencyCode: 'ETH' }],
     rewardAssets: [{ pluginId: 'holesky', currencyCode: 'ETH' }]
+  },
+  {
+    stakePolicyId: 'ethereum_kiln',
+    stakeProviderInfo: {
+      displayName: 'Ethereum Pooled Staking (Kiln)',
+      pluginId: 'ethereum',
+      stakeProviderId: 'ethereum_pooled_kiln'
+    },
+    parentPluginId: 'ethereum',
+    parentCurrencyCode: 'ETH',
+    adapterConfig: {
+      exitQueueAddress: '0x8d6Fd650500f82c7D978a440348e5a9b886943bF',
+      type: 'ethereum-pooled-kiln',
+      apiKey: ENV.KILN_MAINNET_API_KEY,
+      baseUrl: 'https://api.kiln.fi',
+      contractAddress: '0xEb4d67DBa18b3bE04484dFC7B7c2780E8D32A79d',
+      pluginId: 'ethereum',
+      rpcProviderUrls: [`https://ethereum-rpc.publicnode.com`, 'https://1rpc.io/eth']
+    },
+    hideUnstakeAndClaimAction: true,
+    stakeAssets: [{ pluginId: 'ethereum', currencyCode: 'ETH' }],
+    rewardAssets: [{ pluginId: 'ethereum', currencyCode: 'ETH' }]
   }
 ]
 
