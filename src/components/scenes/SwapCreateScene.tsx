5e386aea63c2e2ef65202b040ff078076239d300
Sam Holmes <sam@edge.app>
2024-11-06 14:41:00 -0800

    Fix amount bug after changing wallet within SwapInput on SwapCreateScene

--- src/components/scenes/SwapCreateScene.tsx
+++ src/components/scenes/SwapCreateScene.tsx
@@ -37,9 +37,8 @@ import { SwapVerticalIcon } from '../icons/ThemedIcons'
 import { WalletListModal, WalletListResult } from '../modals/WalletListModal'
 import { Airship, showToast, showWarning } from '../services/AirshipInstance'
 import { useTheme } from '../services/ThemeContext'
-import { ExchangedFlipInputAmounts, ExchangedFlipInputRef } from '../themed/ExchangedFlipInput2'
 import { LineTextDivider } from '../themed/LineTextDivider'
-import { SwapInput } from '../themed/SwapInput'
+import { SwapInput, SwapInputCardAmounts, SwapInputCardInputRef } from '../themed/SwapInput'
 import { ButtonBox } from '../themed/ThemedButtons'
 
 export interface SwapCreateParams {
@@ -72,8 +71,8 @@ export const SwapCreateScene = (props: Props) => {
   const [inputFiatAmount, setInputFiatAmount] = useState('0')
   const [inputNativeAmountFor, setInputNativeAmountFor] = useState<'from' | 'to'>('from')
 
-  const fromInputRef = React.useRef<ExchangedFlipInputRef>(null)
-  const toInputRef = React.useRef<ExchangedFlipInputRef>(null)
+  const fromInputRef = React.useRef<SwapInputCardInputRef>(null)
+  const toInputRef = React.useRef<SwapInputCardInputRef>(null)
 
   const swapRequestOptions = useSwapRequestOptions()
 
@@ -283,6 +282,15 @@ export const SwapCreateScene = (props: Props) => {
       // Update the error state:
       ...getNewErrorInfo('asset')
     })
+
+    // Make sure to update the values if the wallet change is for the input
+    // field that has a native amount:
+    if (direction === 'from' && inputNativeAmountFor === 'from') {
+      fromInputRef.current?.triggerConvertValue()
+    }
+    if (direction === 'to' && inputNativeAmountFor === 'to') {
+      toInputRef.current?.triggerConvertValue()
+    }
   })
 
   const handleMaxPress = useHandler(() => {
@@ -341,7 +349,7 @@ export const SwapCreateScene = (props: Props) => {
     await showWalletListModal('to')
   })
 
-  const handleFromAmountChange = useHandler((amounts: ExchangedFlipInputAmounts) => {
+  const handleFromAmountChange = useHandler((amounts: SwapInputCardAmounts) => {
     navigation.setParams({
       ...route.params,
       // Update the error state:
@@ -355,7 +363,7 @@ export const SwapCreateScene = (props: Props) => {
     toInputRef.current?.setAmount('crypto', '0')
   })
 
-  const handleToAmountChange = useHandler((amounts: ExchangedFlipInputAmounts) => {
+  const handleToAmountChange = useHandler((amounts: SwapInputCardAmounts) => {
     navigation.setParams({
       ...route.params,
       // Update the error state:
