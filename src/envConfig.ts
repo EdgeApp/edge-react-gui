6538b4bfc1bee5371e385b3348fe9993eb5c27d2
peachbits <matthew@edge.app>
2024-12-03 16:49:51 -0800

    Add Helius to Solana env config

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -268,6 +268,7 @@ export const asEnvConfig = asObject({
   SOLANA_INIT: asCorePluginInit(
     asObject({
       alchemyApiKey: asOptional(asString, ''),
+      heliusApiKey: asOptional(asString),
       poktPortalApiKey: asOptional(asString, '')
     }).withRest
   ),
