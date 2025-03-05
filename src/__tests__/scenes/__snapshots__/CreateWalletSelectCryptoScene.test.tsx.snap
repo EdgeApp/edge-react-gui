EdgeApp/edge-react-gui

commit 0fdff1cecd3d28a855323069c20a189dd3af8372

Disconnect CryptoIcon from redux

src/components/icons/CryptoIcon.tsx
@@ -4,87 +4,47 @@ import { StyleSheet, View } from 'react-native'
 import FastImage from 'react-native-fast-image'
 import { ShadowedView } from 'react-native-fast-shadow'
 
-import compromisedIcon from '../../assets/images/compromisedIcon.png'
 import { SPECIAL_CURRENCY_INFO } from '../../constants/WalletAndCurrencyConstants'
-import { useWatch } from '../../hooks/useWatch'
-import { useSelector } from '../../types/reactRedux'
 import { getCurrencyIconUris } from '../../util/CdnUris'
 import { fixSides, mapSides, sidesToMargin } from '../../util/sides'
-import { WalletSyncCircle } from '../progress-indicators/WalletSyncCircle'
 import { cacheStyles, Theme, useTheme } from '../services/ThemeContext'
 
-interface Props {
+export interface CryptoIconProps {
   // Main props - If non is specified, would just render an empty view
-  pluginId?: string // Needed when walletId is not supplied and we still want to get an icon
+  pluginId: string
   tokenId: EdgeTokenId // Needed when it's a token (not the plugin's native currency)
-  walletId?: string // To allow showing the progress ratio sync circle
 
   // Image props
   hideSecondary?: boolean // Only show the currency icon for token (no secondary icon for the network)
   mono?: boolean // To use the mono dark icon logo
+  secondaryCurrencyIconProp?: number | { uri: string }
 
   // Styling props
   marginRem?: number | number[]
   sizeRem?: number
 }
 
-export const CryptoIcon = (props: Props) => {
-  const { hideSecondary = false, marginRem, mono = false, sizeRem = 2, tokenId, walletId } = props
+export const CryptoIcon = (props: CryptoIconProps) => {
+  const { hideSecondary = false, marginRem, mono = false, secondaryCurrencyIconProp, sizeRem = 2, tokenId } = props
 
   const theme = useTheme()
   const styles = getStyles(theme)
   const size = theme.rem(sizeRem)
 
-  // Track wallets state from account and update the wallet when ready
-  const account = useSelector(state => state.core.account)
-  const currencyWallets = useWatch(account, 'currencyWallets')
-  const wallet = walletId != null ? currencyWallets[walletId] : null
-
-  const compromised = useSelector(state => {
-    if (walletId == null) return 0
-    const { modalShown = 0 } = state.ui?.settings?.securityCheckedWallets?.[walletId] ?? {}
-    return modalShown > 0
-  })
-
-  const { pluginId = wallet?.currencyInfo.pluginId } = props
-  const useChainIcon = pluginId == null ? false : SPECIAL_CURRENCY_INFO[pluginId]?.chainIcon ?? false
+  const { pluginId } = props
+  const useChainIcon = SPECIAL_CURRENCY_INFO[pluginId]?.chainIcon ?? false
 
   // Primary Currency icon
-  const primaryCurrencyIconUrl = React.useMemo(() => {
-    if (pluginId == null) return null
-
-    // Get Currency Icon URI
-    const icon = getCurrencyIconUris(pluginId, tokenId, useChainIcon)
-    return mono ? icon.symbolImageDarkMono : icon.symbolImage
-  }, [pluginId, tokenId, mono, useChainIcon])
-
-  const primaryCurrencyIcon = React.useMemo(() => {
-    if (primaryCurrencyIconUrl == null) return null
-
-    const source = { uri: primaryCurrencyIconUrl }
-
-    // Return Currency logo from the edge server
-    return source
-  }, [primaryCurrencyIconUrl])
+  const icon = getCurrencyIconUris(pluginId, tokenId, useChainIcon)
+  const primaryCurrencyIconUrl = mono ? icon.symbolImageDarkMono : icon.symbolImage
+  const primaryCurrencyIcon = { uri: primaryCurrencyIconUrl }
 
   // Secondary (parent) currency icon (if it's a token)
-  const secondaryCurrencyIcon = React.useMemo(() => {
-    if (compromised) {
-      return compromisedIcon
-    }
-
-    // Skip if this is not a token:
-    if (pluginId == null || (tokenId == null && !useChainIcon) || tokenId === pluginId) {
-      return null
-    }
-
-    // Get Parent Icon URI
+  let secondaryCurrencyIcon = secondaryCurrencyIconProp
+  if (tokenId != null && useChainIcon) {
     const icon = getCurrencyIconUris(pluginId, null)
-    const source = { uri: mono ? icon.symbolImageDarkMono : icon.symbolImage }
-
-    return source
-    // Return Parent logo from the edge server
-  }, [compromised, mono, pluginId, tokenId, useChainIcon])
+    secondaryCurrencyIcon = { uri: mono ? icon.symbolImageDarkMono : icon.symbolImage }
+  }
 
   // Main view styling
   const spacingStyle = React.useMemo(
@@ -109,14 +69,6 @@ export const CryptoIcon = (props: Props) => {
 
   return (
     <View style={spacingStyle}>
-      {wallet == null ? null : (
-        <WalletSyncCircle
-          /* key prevents component from being recycled and shared between wallets */
-          key={`${wallet.id}${String(tokenId)}`}
-          size={size}
-          wallet={wallet}
-        />
-      )}
       <ShadowedView style={shadowStyle}>
         {primaryCurrencyIcon != null ? <FastImage style={StyleSheet.absoluteFill} source={primaryCurrencyIcon} /> : null}
         {hideSecondary ? null : secondaryCurrencyIcon != null ? <FastImage style={styles.parentIcon} source={secondaryCurrencyIcon} /> : null}

src/components/icons/WalletIcon.tsx
@@ -1,0 +1,22 @@
+import * as React from 'react'
+
+import compromisedIcon from '../../assets/images/compromisedIcon.png'
+import { useSelector } from '../../types/reactRedux'
+import { CryptoIcon, CryptoIconProps } from './CryptoIcon'
+
+interface WalletIconProps extends CryptoIconProps {
+  walletId: string
+}
+
+export const WalletIcon = (props: WalletIconProps) => {
+  const compromised = useSelector(state => {
+    const { modalShown = 0 } = state.ui?.settings?.securityCheckedWallets?.[props.walletId] ?? {}
+    return modalShown > 0
+  })
+
+  if (compromised) {
+    return <CryptoIcon {...props} secondaryCurrencyIconProp={compromisedIcon} />
+  } else {
+    return <CryptoIcon {...props} />
+  }
+}

src/__tests__/components/__snapshots__/CurrencyIcon.test.tsx.snap
@@ -13,87 +13,6 @@ exports[`CryptoIcon should render with loading props 1`] = `
     }
   }
 >
-  <RNSVGSvgView
-    align="xMidYMid"
-    bbHeight={61}
-    bbWidth={61}
-    focusable={false}
-    height={61}
-    meetOrSlice={0}
-    minX={-8}
-    minY={-8}
-    style={
-      [
-        {
-          "backgroundColor": "transparent",
-          "borderWidth": 0,
-        },
-        {
-          "left": -8,
-          "position": "absolute",
-          "top": -8,
-          "transform": [
-            {
-              "rotateZ": "-90deg",
-            },
-          ],
-        },
-        {
-          "flex": 0,
-          "height": 61,
-          "width": 61,
-        },
-      ]
-    }
-    vbHeight={61}
-    vbWidth={61}
-    width={61}
-  >
-    <RNSVGGroup
-      fill={
-        {
-          "payload": 4278190080,
-          "type": 0,
-        }
-      }
-    >
-      <RNSVGCircle
-        cx={22.5}
-        cy={22.5}
-        fill={
-          {
-            "payload": 0,
-            "type": 0,
-          }
-        }
-        opacity={0}
-        propList={
-          [
-            "fill",
-            "stroke",
-            "strokeWidth",
-            "strokeDasharray",
-            "strokeLinecap",
-          ]
-        }
-        r={24}
-        stroke={
-          {
-            "payload": 4278251938,
-            "type": 0,
-          }
-        }
-        strokeDasharray={
-          [
-            150.79644737231007,
-            150.79644737231007,
-          ]
-        }
-        strokeLinecap={1}
-        strokeWidth={4}
-      />
-    </RNSVGGroup>
-  </RNSVGSvgView>
   <View
     style={
       {

src/__tests__/scenes/__snapshots__/CreateWalletEditNameScene.test.tsx.snap
@@ -897,40 +897,6 @@ exports[`CreateWalletEditNameComponent should render with loading props 1`] = `
                     }
                   />
                 </View>
-                <View
-                  style={
-                    [
-                      {
-                        "overflow": "hidden",
-                      },
-                      {
-                        "bottom": 0,
-                        "height": "50%",
-                        "position": "absolute",
-                        "right": 0,
-                        "width": "50%",
-                      },
-                    ]
-                  }
-                >
-                  <FastImageView
-                    resizeMode="cover"
-                    source={
-                      {
-                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
-                      }
-                    }
-                    style={
-                      {
-                        "bottom": 0,
-                        "left": 0,
-                        "position": "absolute",
-                        "right": 0,
-                        "top": 0,
-                      }
-                    }
-                  />
-                </View>
               </View>
             </View>
             <View

src/__tests__/scenes/__snapshots__/CreateWalletSelectCryptoScene.test.tsx.snap
@@ -1043,40 +1043,6 @@ exports[`CreateWalletSelectCrypto should render with loading props 1`] = `
                     }
                   />
                 </View>
-                <View
-                  style={
-                    [
-                      {
-                        "overflow": "hidden",
-                      },
-                      {
-                        "bottom": 0,
-                        "height": "50%",
-                        "position": "absolute",
-                        "right": 0,
-                        "width": "50%",
-                      },
-                    ]
-                  }
-                >
-                  <FastImageView
-                    resizeMode="cover"
-                    source={
-                      {
-                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
-                      }
-                    }
-                    style={
-                      {
-                        "bottom": 0,
-                        "left": 0,
-                        "position": "absolute",
-                        "right": 0,
-                        "top": 0,
-                      }
-                    }
-                  />
-                </View>
               </View>
             </View>
             <View
@@ -1268,40 +1234,6 @@ exports[`CreateWalletSelectCrypto should render with loading props 1`] = `
                     source={
                       {
                         "uri": "https://content.edge.app/currencyIconsV3/ethereum/221657776846890989a759ba2973e427dff5c9bb.png",
-                      }
-                    }
-                    style={
-                      {
-                        "bottom": 0,
-                        "left": 0,
-                        "position": "absolute",
-                        "right": 0,
-                        "top": 0,
-                      }
-                    }
-                  />
-                </View>
-                <View
-                  style={
-                    [
-                      {
-                        "overflow": "hidden",
-                      },
-                      {
-                        "bottom": 0,
-                        "height": "50%",
-                        "position": "absolute",
-                        "right": 0,
-                        "width": "50%",
-                      },
-                    ]
-                  }
-                >
-                  <FastImageView
-                    resizeMode="cover"
-                    source={
-                      {
-                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
                       }
                     }
                     style={
@@ -1519,40 +1451,6 @@ exports[`CreateWalletSelectCrypto should render with loading props 1`] = `
                     }
                   />
                 </View>
-                <View
-                  style={
-                    [
-                      {
-                        "overflow": "hidden",
-                      },
-                      {
-                        "bottom": 0,
-                        "height": "50%",
-                        "position": "absolute",
-                        "right": 0,
-                        "width": "50%",
-                      },
-                    ]
-                  }
-                >
-                  <FastImageView
-                    resizeMode="cover"
-                    source={
-                      {
-                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
-                      }
-                    }
-                    style={
-                      {
-                        "bottom": 0,
-                        "left": 0,
-                        "position": "absolute",
-                        "right": 0,
-                        "top": 0,
-                      }
-                    }
-                  />
-                </View>
               </View>
             </View>
             <View
@@ -1744,40 +1642,6 @@ exports[`CreateWalletSelectCrypto should render with loading props 1`] = `
                     source={
                       {
                         "uri": "https://content.edge.app/currencyIconsV3/ethereum/6b175474e89094c44da98b954eedeac495271d0f.png",
-                      }
-                    }
-                    style={
-                      {
-                        "bottom": 0,
-                        "left": 0,
-                        "position": "absolute",
-                        "right": 0,
-                        "top": 0,
-                      }
-                    }
-                  />
-                </View>
-                <View
-                  style={
-                    [
-                      {
-                        "overflow": "hidden",
-                      },
-                      {
-                        "bottom": 0,
-                        "height": "50%",
-                        "position": "absolute",
-                        "right": 0,
-                        "width": "50%",
-                      },
-                    ]
-                  }
-                >
-                  <FastImageView
-                    resizeMode="cover"
-                    source={
-                      {
-                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
                       }
                     }
                     style={

src/__tests__/components/CurrencyIcon.test.tsx
@@ -22,7 +22,7 @@ describe('CryptoIcon', () => {
   it('should render with loading props', () => {
     const renderer = TestRenderer.create(
       <FakeProviders initialState={mockState}>
-        <CryptoIcon pluginId="bitcoin" tokenId="bitcoin" walletId="332s0ds39f" marginRem={1} />
+        <CryptoIcon pluginId="bitcoin" tokenId="bitcoin" marginRem={1} />
       </FakeProviders>
     )
 

src/__tests__/components/__snapshots__/BuyCrypto.test.tsx.snap
@@ -92,7 +92,42 @@ exports[`BuyCrypto should render with some props 1`] = `
                 "width": 51,
               }
             }
-          />
+          >
+            <View
+              style={
+                [
+                  {
+                    "overflow": "hidden",
+                  },
+                  {
+                    "bottom": 0,
+                    "left": 0,
+                    "position": "absolute",
+                    "right": 0,
+                    "top": 0,
+                  },
+                ]
+              }
+            >
+              <FastImageView
+                resizeMode="cover"
+                source={
+                  {
+                    "uri": "https://content.edge.app/currencyIconsV3/bitcoin/bitcoin.png",
+                  }
+                }
+                style={
+                  {
+                    "bottom": 0,
+                    "left": 0,
+                    "position": "absolute",
+                    "right": 0,
+                    "top": 0,
+                  }
+                }
+              />
+            </View>
+          </View>
         </View>
         <Text
           adjustsFontSizeToFit={true}

src/__tests__/components/__snapshots__/ExchangeQuoteComponent.test.tsx.snap
@@ -67,87 +67,6 @@ exports[`ExchangeQuote should render with loading props 1`] = `
         }
       }
     >
-      <RNSVGSvgView
-        align="xMidYMid"
-        bbHeight={61}
-        bbWidth={61}
-        focusable={false}
-        height={61}
-        meetOrSlice={0}
-        minX={-8}
-        minY={-8}
-        style={
-          [
-            {
-              "backgroundColor": "transparent",
-              "borderWidth": 0,
-            },
-            {
-              "left": -8,
-              "position": "absolute",
-              "top": -8,
-              "transform": [
-                {
-                  "rotateZ": "-90deg",
-                },
-              ],
-            },
-            {
-              "flex": 0,
-              "height": 61,
-              "width": 61,
-            },
-          ]
-        }
-        vbHeight={61}
-        vbWidth={61}
-        width={61}
-      >
-        <RNSVGGroup
-          fill={
-            {
-              "payload": 4278190080,
-              "type": 0,
-            }
-          }
-        >
-          <RNSVGCircle
-            cx={22.5}
-            cy={22.5}
-            fill={
-              {
-                "payload": 0,
-                "type": 0,
-              }
-            }
-            opacity={0}
-            propList={
-              [
-                "fill",
-                "stroke",
-                "strokeWidth",
-                "strokeDasharray",
-                "strokeLinecap",
-              ]
-            }
-            r={24}
-            stroke={
-              {
-                "payload": 4278251938,
-                "type": 0,
-              }
-            }
-            strokeDasharray={
-              [
-                150.79644737231007,
-                150.79644737231007,
-              ]
-            }
-            strokeLinecap={1}
-            strokeWidth={4}
-          />
-        </RNSVGGroup>
-      </RNSVGSvgView>
       <View
         style={
           {

commit 6f56cc9879317a80bea9694c847fe64bce91095e

Fix `CryptoIcon's` secondary icon Regressed from 0fdff1c

src/components/icons/CryptoIcon.tsx
@@ -17,7 +17,7 @@ export interface CryptoIconProps {
   // Image props
   hideSecondary?: boolean // Only show the currency icon for token (no secondary icon for the network)
   mono?: boolean // To use the mono dark icon logo
-  secondaryCurrencyIconProp?: number | { uri: string }
+  secondaryIconOverride?: number | { uri: string }
 
   // Styling props
   marginRem?: number | number[]
@@ -25,7 +25,7 @@ export interface CryptoIconProps {
 }
 
 export const CryptoIcon = (props: CryptoIconProps) => {
-  const { hideSecondary = false, marginRem, mono = false, secondaryCurrencyIconProp, sizeRem = 2, tokenId } = props
+  const { hideSecondary = false, marginRem, mono = false, secondaryIconOverride, sizeRem = 2, tokenId } = props
 
   const theme = useTheme()
   const styles = getStyles(theme)
@@ -40,8 +40,8 @@ export const CryptoIcon = (props: CryptoIconProps) => {
   const primaryCurrencyIcon = { uri: primaryCurrencyIconUrl }
 
   // Secondary (parent) currency icon (if it's a token)
-  let secondaryCurrencyIcon = secondaryCurrencyIconProp
-  if (tokenId != null && useChainIcon) {
+  let secondaryCurrencyIcon = secondaryIconOverride
+  if (secondaryIconOverride == null && (tokenId != null || useChainIcon)) {
     const icon = getCurrencyIconUris(pluginId, null)
     secondaryCurrencyIcon = { uri: mono ? icon.symbolImageDarkMono : icon.symbolImage }
   }

src/components/icons/WalletIcon.tsx
@@ -31,7 +31,7 @@ export const WalletIcon = (props: WalletIconProps) => {
         size={size}
         wallet={wallet}
       />
-      <CryptoIcon {...props} pluginId={pluginId} secondaryCurrencyIconProp={compromised ? compromisedIcon : undefined} />
+      <CryptoIcon {...props} pluginId={pluginId} secondaryIconOverride={compromised ? compromisedIcon : undefined} />
     </View>
   )
 }

src/__tests__/components/__snapshots__/CurrencyIcon.test.tsx.snap
@@ -65,6 +65,40 @@ exports[`CryptoIcon should render with loading props 1`] = `
         }
       />
     </View>
+    <View
+      style={
+        [
+          {
+            "overflow": "hidden",
+          },
+          {
+            "bottom": 0,
+            "height": "50%",
+            "position": "absolute",
+            "right": 0,
+            "width": "50%",
+          },
+        ]
+      }
+    >
+      <FastImageView
+        resizeMode="cover"
+        source={
+          {
+            "uri": "https://content.edge.app/currencyIconsV3/bitcoin/bitcoin.png",
+          }
+        }
+        style={
+          {
+            "bottom": 0,
+            "left": 0,
+            "position": "absolute",
+            "right": 0,
+            "top": 0,
+          }
+        }
+      />
+    </View>
   </View>
 </View>
 `;

src/__tests__/scenes/__snapshots__/CreateWalletEditNameScene.test.tsx.snap
@@ -897,6 +897,40 @@ exports[`CreateWalletEditNameComponent should render with loading props 1`] = `
                     }
                   />
                 </View>
+                <View
+                  style={
+                    [
+                      {
+                        "overflow": "hidden",
+                      },
+                      {
+                        "bottom": 0,
+                        "height": "50%",
+                        "position": "absolute",
+                        "right": 0,
+                        "width": "50%",
+                      },
+                    ]
+                  }
+                >
+                  <FastImageView
+                    resizeMode="cover"
+                    source={
+                      {
+                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
+                      }
+                    }
+                    style={
+                      {
+                        "bottom": 0,
+                        "left": 0,
+                        "position": "absolute",
+                        "right": 0,
+                        "top": 0,
+                      }
+                    }
+                  />
+                </View>
               </View>
             </View>
             <View

src/__tests__/scenes/__snapshots__/CreateWalletSelectCryptoScene.test.tsx.snap
@@ -1043,6 +1043,40 @@ exports[`CreateWalletSelectCrypto should render with loading props 1`] = `
                     }
                   />
                 </View>
+                <View
+                  style={
+                    [
+                      {
+                        "overflow": "hidden",
+                      },
+                      {
+                        "bottom": 0,
+                        "height": "50%",
+                        "position": "absolute",
+                        "right": 0,
+                        "width": "50%",
+                      },
+                    ]
+                  }
+                >
+                  <FastImageView
+                    resizeMode="cover"
+                    source={
+                      {
+                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
+                      }
+                    }
+                    style={
+                      {
+                        "bottom": 0,
+                        "left": 0,
+                        "position": "absolute",
+                        "right": 0,
+                        "top": 0,
+                      }
+                    }
+                  />
+                </View>
               </View>
             </View>
             <View
@@ -1234,6 +1268,40 @@ exports[`CreateWalletSelectCrypto should render with loading props 1`] = `
                     source={
                       {
                         "uri": "https://content.edge.app/currencyIconsV3/ethereum/221657776846890989a759ba2973e427dff5c9bb.png",
+                      }
+                    }
+                    style={
+                      {
+                        "bottom": 0,
+                        "left": 0,
+                        "position": "absolute",
+                        "right": 0,
+                        "top": 0,
+                      }
+                    }
+                  />
+                </View>
+                <View
+                  style={
+                    [
+                      {
+                        "overflow": "hidden",
+                      },
+                      {
+                        "bottom": 0,
+                        "height": "50%",
+                        "position": "absolute",
+                        "right": 0,
+                        "width": "50%",
+                      },
+                    ]
+                  }
+                >
+                  <FastImageView
+                    resizeMode="cover"
+                    source={
+                      {
+                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
                       }
                     }
                     style={
@@ -1451,6 +1519,40 @@ exports[`CreateWalletSelectCrypto should render with loading props 1`] = `
                     }
                   />
                 </View>
+                <View
+                  style={
+                    [
+                      {
+                        "overflow": "hidden",
+                      },
+                      {
+                        "bottom": 0,
+                        "height": "50%",
+                        "position": "absolute",
+                        "right": 0,
+                        "width": "50%",
+                      },
+                    ]
+                  }
+                >
+                  <FastImageView
+                    resizeMode="cover"
+                    source={
+                      {
+                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
+                      }
+                    }
+                    style={
+                      {
+                        "bottom": 0,
+                        "left": 0,
+                        "position": "absolute",
+                        "right": 0,
+                        "top": 0,
+                      }
+                    }
+                  />
+                </View>
               </View>
             </View>
             <View
@@ -1642,6 +1744,40 @@ exports[`CreateWalletSelectCrypto should render with loading props 1`] = `
                     source={
                       {
                         "uri": "https://content.edge.app/currencyIconsV3/ethereum/6b175474e89094c44da98b954eedeac495271d0f.png",
+                      }
+                    }
+                    style={
+                      {
+                        "bottom": 0,
+                        "left": 0,
+                        "position": "absolute",
+                        "right": 0,
+                        "top": 0,
+                      }
+                    }
+                  />
+                </View>
+                <View
+                  style={
+                    [
+                      {
+                        "overflow": "hidden",
+                      },
+                      {
+                        "bottom": 0,
+                        "height": "50%",
+                        "position": "absolute",
+                        "right": 0,
+                        "width": "50%",
+                      },
+                    ]
+                  }
+                >
+                  <FastImageView
+                    resizeMode="cover"
+                    source={
+                      {
+                        "uri": "https://content.edge.app/currencyIconsV3/ethereum/ethereum.png",
                       }
                     }
                     style={