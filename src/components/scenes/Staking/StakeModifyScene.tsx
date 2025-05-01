a31bed6593a18cef8a6366de5d24ce87ba7a8946
peachbits <matthew@edge.app>
2025-05-01 16:04:00 -0700

    Add lockInputs to quote allocation

--- src/components/scenes/Staking/StakeModifyScene.tsx
+++ src/components/scenes/Staking/StakeModifyScene.tsx
@@ -347,7 +347,7 @@ const StakeModifySceneComponent = (props: Props) => {
   const styles = getStyles(theme)
 
   const renderEditableQuoteAmountRow = (quoteAllocation: QuoteAllocation) => {
-    const { currencyCode, pluginId, allocationType } = quoteAllocation
+    const { currencyCode, pluginId, allocationType, lockInputs = false } = quoteAllocation
     quoteAllocation =
       allocationType === 'unstake' && mustMaxUnstake
         ? {
@@ -383,7 +383,7 @@ const StakeModifySceneComponent = (props: Props) => {
           currencyCode={quoteCurrencyCode}
           exchangeDenomination={quoteDenom}
           displayDenomination={quoteDenom}
-          lockInputs={isClaim || (!!mustMaxUnstake && allocationType === 'unstake')}
+          lockInputs={lockInputs || isClaim || (!!mustMaxUnstake && allocationType === 'unstake')}
           onPress={handleShowFlipInputModal(currencyCode, tokenId)}
         />
       </EdgeCard>
