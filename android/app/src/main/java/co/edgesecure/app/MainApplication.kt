d15cab546421d58652de2a0b216bbf363d972055
William Swanson <swansontec@gmail.com>
2025-05-29 12:30:20 -0700

    Upgrade to react-native v0.79.2

--- android/app/src/main/java/co/edgesecure/app/MainApplication.kt
+++ android/app/src/main/java/co/edgesecure/app/MainApplication.kt
@@ -4,11 +4,14 @@ import android.app.Application
 import android.content.res.Configuration
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.modules.i18nmanager.I18nUtil
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import expo.modules.ApplicationLifecycleDispatcher.onApplicationCreate
 import expo.modules.ApplicationLifecycleDispatcher.onConfigurationChanged
@@ -16,18 +19,16 @@ import expo.modules.ReactNativeHostWrapper
 import io.sentry.Hint
 import io.sentry.SentryEvent
 import io.sentry.SentryLevel
-import io.sentry.SentryOptions.BeforeSendCallback
 import io.sentry.SentryOptions.BeforeBreadcrumbCallback
+import io.sentry.SentryOptions.BeforeSendCallback
 import io.sentry.android.core.SentryAndroid
 
 class MainApplication : Application(), ReactApplication {
-    val mReactNativeHost: ReactNativeHost =
+
+    override val reactNativeHost: ReactNativeHost =
         ReactNativeHostWrapper(
             this,
             object : DefaultReactNativeHost(this) {
-                override fun getUseDeveloperSupport(): Boolean {
-                    return BuildConfig.DEBUG
-                }
 
                 override fun getPackages(): List<ReactPackage> {
                     // Packages that cannot be autolinked yet can be added manually here, for
@@ -36,20 +37,17 @@ class MainApplication : Application(), ReactApplication {
                     return PackageList(this).packages
                 }
 
-                override fun getJSMainModuleName(): String {
-                    return "index"
-                }
+                override fun getJSMainModuleName(): String = "index"
 
-                override val isNewArchEnabled: Boolean
-                    protected get() = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-                override val isHermesEnabled: Boolean
-                    protected get() = BuildConfig.IS_HERMES_ENABLED
+                override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+                override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+                override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
             }
         )
 
-    override fun getReactNativeHost(): ReactNativeHost {
-        return mReactNativeHost
-    }
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
     override fun onCreate() {
         super.onCreate()
@@ -72,13 +70,16 @@ class MainApplication : Application(), ReactApplication {
                     options.environment = "production"
                 }
 
-                options.beforeBreadcrumb = BeforeBreadcrumbCallback { breadcrumb, hint ->
-                    if ("network.event" == breadcrumb.category || "http" == breadcrumb.category || "console" == breadcrumb.category) {
-                        null
-                    } else {
-                        breadcrumb
+                options.beforeBreadcrumb =
+                    BeforeBreadcrumbCallback { breadcrumb, hint ->
+                        if ("network.event" == breadcrumb.category ||
+                            "http" == breadcrumb.category || "console" == breadcrumb.category
+                        ) {
+                            null
+                        } else {
+                            breadcrumb
+                        }
                     }
-                }
                 // Add a callback that will be used before the event is sent to Sentry.
                 // With this callback, you can modify the event or, when returning null, also
                 // discard the event.
@@ -100,7 +101,7 @@ class MainApplication : Application(), ReactApplication {
         // Background task:
         MessagesWorker.ensureScheduled(context)
         // MessagesWorker.testRun(context);
-        SoLoader.init(this, /* native exopackage */ false)
+        SoLoader.init(this, OpenSourceMergedSoMapping)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this
             // app.
