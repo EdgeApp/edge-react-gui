2ebcb2d46368b3c564bf093de2f69229a8747a4c
William Swanson <swansontec@gmail.com>
2025-07-14 15:00:58 -0700

    Enable new architecture on Android

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,6 @@
 module.exports = function (api) {
-  const isAndroid = api.caller(c => c.platform === 'android')
-
   return {
     presets: ['module:@react-native/babel-preset'],
-    plugins: [
-      isAndroid
-        ? './node_modules/r3-hack/node_modules/react-native-reanimated/plugin'
-        : 'react-native-worklets/plugin'
-    ]
+    plugins: ['react-native-worklets/plugin']
   }
 }
