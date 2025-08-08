234d0349898a64c2e6fa83a03dff9914c3a4958d
William Swanson <swansontec@gmail.com>
2025-08-08 12:58:54 -0700

    Upgrade to reanimated 4

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['babel-preset-expo'],
-  plugins: ['react-native-reanimated/plugin']
+  plugins: ['react-native-worklets/plugin']
 }
--- index.ts
+++ index.ts
@@ -3,17 +3,10 @@ import './src/app'
 import './src/perf'
 
 import { AppRegistry } from 'react-native'
-import Animated from 'react-native-reanimated'
 
 import { name as appName } from './app.json'
 import { App } from './src/components/App'
 
 if (typeof BigInt === 'undefined') global.BigInt = require('big-integer')
-
-// See https://github.com/software-mansion/react-native-reanimated/issues/1794#issuecomment-898393331
-// For ReText/SwipeChart support requirements, see:
-// https://github.com/software-mansion/react-native-reanimated/discussions/5621
-// https://github.com/software-mansion/react-native-reanimated/issues/5432
-Animated.addWhitelistedNativeProps({ text: true })
 
 AppRegistry.registerComponent(appName, () => App)
