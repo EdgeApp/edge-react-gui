e061b7964d0ad2cc88a078917e2a6bfd5d0a83d7
Sam Holmes <sam@edge.app>
2025-08-14 15:36:34 -0700

    Add Moonpay Ramp Plugin

--- src/plugins/ramps/allRampPlugins.ts
+++ src/plugins/ramps/allRampPlugins.ts
@@ -1,6 +1,8 @@
+import { moonpayRampPlugin } from './moonpay/moonpayRampPlugin'
 import { paybisRampPlugin } from './paybis/paybisRampPlugin'
 import type { RampPluginFactory } from './rampPluginTypes'
 
 export const pluginFactories: Record<string, RampPluginFactory> = {
+  moonpay: moonpayRampPlugin,
   paybis: paybisRampPlugin
 }
