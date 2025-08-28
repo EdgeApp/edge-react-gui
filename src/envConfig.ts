152e0725b97da12cab6bf56efc446fcda4533392
Sam Holmes <sam@edge.app>
2025-08-28 13:23:05 -0700

    Add DEBUG_LOGBOX to ENV

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -424,6 +424,7 @@ export const asEnvConfig = asObject({
   DEBUG_EXCHANGES: asOptional(asBoolean, false),
   DEBUG_VERBOSE_ERRORS: asOptional(asBoolean, false),
   DEBUG_THEME: asOptional(asBoolean, false),
+  DEBUG_LOGBOX: asOptional(asBoolean, true),
   MUTE_CONSOLE_OUTPUT: asOptional(
     asArray(
       asValue(
