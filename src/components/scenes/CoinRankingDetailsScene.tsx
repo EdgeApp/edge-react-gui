3fb9183b2568ce586fff9881e300ff9798eda8d1
Jon Tzeng <jon@edge.app>
2025-09-01 13:33:09 -0700

    Disable notifications in CoinRank scenes

--- src/components/scenes/CoinRankingDetailsScene.tsx
+++ src/components/scenes/CoinRankingDetailsScene.tsx
@@ -632,7 +632,7 @@ const CoinRankingDetailsSceneComponent = (props: Props) => {
   })
 
   return (
-    <SceneWrapper hasTabs hasNotifications scroll>
+    <SceneWrapper scroll>
       {coinRankingData != null ? (
         <View style={styles.container}>
           <EdgeAnim style={styles.titleContainer} enter={fadeInLeft}>
