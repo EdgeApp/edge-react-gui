212ac916453ddc7d4b8e2cc52d241b3be8ed516a
Sam Holmes <sam@edge.app>
2025-09-09 16:06:32 -0700

    Rename partnerUrl to apiUrl for paybis ramp plugin

--- src/plugins/ramps/paybis/paybisRampTypes.ts
+++ src/plugins/ramps/paybis/paybisRampTypes.ts
@@ -1,8 +1,8 @@
 import { asObject, asOptional, asString } from 'cleaners'
 
 export const asInitOptions = asObject({
-  partnerUrl: asOptional(asString, 'https://widget-api.paybis.com'),
   apiKey: asString,
+  apiUrl: asOptional(asString, 'https://widget-api.paybis.com'),
   privateKeyB64: asString,
   widgetUrl: asOptional(asString, 'https://widget.paybis.com'),
   widgetTestnetUrl: asOptional(asString, 'https://widget.sandbox.paybis.com')
