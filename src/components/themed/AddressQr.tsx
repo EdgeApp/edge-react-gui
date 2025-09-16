e403596f9782c833da6fcdc1a93be150cf4868e7
William Swanson <swansontec@gmail.com>
2025-09-16 16:33:07 -0700

    Replace Carousel with QrCarousel
    We already have EdgeCarousel as the generic component. This one is specialized to handle the particular needs of square items.

--- src/components/themed/AddressQr.tsx
+++ src/components/themed/AddressQr.tsx
@@ -1,13 +1,11 @@
 import type { EdgeCurrencyWallet, EdgeTokenId } from 'edge-core-js'
 import * as React from 'react'
-import { View } from 'react-native'
 
 import { useAsyncValue } from '../../hooks/useAsyncValue'
 import { useHandler } from '../../hooks/useHandler'
 import { getCurrencyCode } from '../../util/CurrencyInfoHelpers'
 import { QrModal } from '../modals/QrModal'
 import { Airship, showError } from '../services/AirshipInstance'
-import { cacheStyles, type Theme, useTheme } from '../services/ThemeContext'
 import { QrCode } from '../themed/QrCode'
 
 interface Props {
@@ -22,8 +20,6 @@ interface Props {
  * An address QR code in the receive scene.
  */
 export const AddressQr: React.FC<Props> = props => {
-  const theme = useTheme()
-  const styles = getStyles(theme)
   const { address, tokenId, wallet, nativeAmount } = props
 
   const [encodedUri] = useAsyncValue(
@@ -50,23 +46,12 @@ export const AddressQr: React.FC<Props> = props => {
   })
 
   return (
-    <View style={styles.container}>
-      <QrCode
-        data={encodedUri}
-        tokenId={tokenId}
-        pluginId={wallet.currencyInfo.pluginId}
-        onPress={handlePress}
-        marginRem={0}
-      />
-    </View>
+    <QrCode
+      data={encodedUri ?? address}
+      tokenId={tokenId}
+      pluginId={wallet.currencyInfo.pluginId}
+      onPress={handlePress}
+      marginRem={0}
+    />
   )
 }
-
-const getStyles = cacheStyles((theme: Theme) => ({
-  container: {
-    aspectRatio: 1,
-    height: '100%',
-    width: '100%',
-    alignSelf: 'center'
-  }
-}))
