b3d625a67aa1bd807ee5992a6a678159ce0bd129
Jon Tzeng <jon@edge.app>
2025-09-24 14:02:45 -0700

    Address warnings in `paybisProvider`

--- src/plugins/gui/providers/paybisProvider.ts
+++ src/plugins/gui/providers/paybisProvider.ts
@@ -20,10 +20,8 @@ import { sprintf } from 'sprintf-js'
 import URL from 'url-parse'
 
 import type { SendScene2Params } from '../../../components/scenes/SendScene2'
-import { showError } from '../../../components/services/AirshipInstance'
 import { locale } from '../../../locales/intl'
 import { lstrings } from '../../../locales/strings'
-import type { FiatProviderLink } from '../../../types/DeepLinkTypes'
 import type { EdgeAsset, StringMap } from '../../../types/types'
 import { sha512HashAndSign } from '../../../util/crypto'
 import { CryptoAmount } from '../../../util/CryptoAmount'
@@ -446,13 +444,15 @@ export const paybisProvider: FiatProviderFactory = {
     } = params
     const { apiKey, partnerUrl: url, privateKeyB64 } = asApiKeys(apiKeys)
 
-    let partnerUserId = await store
+    let partnerUserId = (await store
       .getItem('partnerUserId')
-      .catch(e => undefined)
+      .catch(() => {})) as string | undefined
     if (partnerUserId == null || partnerUserId === '') {
       partnerUserId = await makeUuid()
       await store.setItem('partnerUserId', partnerUserId)
     }
+    if (partnerUserId == null)
+      throw new Error('Paybis missing partnerUserId after initialization')
 
     let userIdHasTransactions: boolean | undefined
 
@@ -518,8 +518,8 @@ export const paybisProvider: FiatProviderFactory = {
           })
           const { hasTransactions } = asUserStatus(response)
           userIdHasTransactions = hasTransactions
-        } catch (e) {
-          console.log(`Paybis: Error getting user status: ${String(e)}`)
+        } catch (e: unknown) {
+          console.log('Paybis: Error getting user status:', e)
         }
 
         const out = allowedCurrencyCodes[direction][paymentType]
@@ -759,17 +759,21 @@ export const paybisProvider: FiatProviderFactory = {
                 lstrings.fiat_plugin_cannot_continue_camera_permission
               )
             }
-            const receiveAddress = await coreWallet.getReceiveAddress({
-              tokenId: null
-            })
+            const addresses = await coreWallet.getAddresses({ tokenId: null })
+            const [defaultAddress] = addresses
+            assert(defaultAddress != null, 'Paybis: Missing receive address')
+            const segwitAddresses = addresses.find(
+              row => row.addressType === 'segwitAddress'
+            )
+            const receivePublicAddress =
+              segwitAddresses?.publicAddress ?? defaultAddress.publicAddress
 
             let bodyParams
             if (direction === 'buy') {
               bodyParams = {
                 cryptoWalletAddress: {
                   currencyCode: paybisCc,
-                  address:
-                    receiveAddress.segwitAddress ?? receiveAddress.publicAddress
+                  address: receivePublicAddress
                 },
                 partnerUserId,
                 locale: locale.localeIdentifier.slice(0, 2),
@@ -827,12 +831,13 @@ export const paybisProvider: FiatProviderFactory = {
               const failureReturnURL = encodeURIComponent(
                 'https://return.edge.app/fiatprovider/buy/paybis?transactionStatus=fail'
               )
-              const deeplinkHandlerAsync = async (
-                link: FiatProviderLink
-              ): Promise<void> => {
+              const deeplinkHandlerAsync = async (link: {
+                query?: any
+                uri?: string
+              }): Promise<void> => {
                 const { query, uri } = link
                 console.log('Paybis WebView launch buy success: ' + uri)
-                const { transactionStatus } = query
+                const { transactionStatus } = query ?? {}
                 if (transactionStatus === 'success') {
                   await showUi.trackConversion('Buy_Success', {
                     conversionValues: {
@@ -879,19 +884,23 @@ export const paybisProvider: FiatProviderFactory = {
                 } else {
                   await showUi.showError(
                     new Error(
-                      `Paybis: Invalid transactionStatus "${transactionStatus}".`
+                      `Paybis: Invalid transactionStatus "${String(
+                        transactionStatus
+                      )}".`
                     )
                   )
                 }
               }
+              const deeplinkHandler = (link: {
+                query?: any
+                uri?: string
+              }): void => {
+                deeplinkHandlerAsync(link).catch(() => {})
+              }
               await showUi.openExternalWebView({
                 url: `${widgetUrl}?requestId=${requestId}${ott}${promoCodeParam}&successReturnURL=${successReturnURL}&failureReturnURL=${failureReturnURL}`,
                 providerId,
-                deeplinkHandler: link => {
-                  deeplinkHandlerAsync(link).catch((error: unknown) => {
-                    showError(error)
-                  })
-                }
+                deeplinkHandler
               })
               return
             }
@@ -902,7 +911,7 @@ export const paybisProvider: FiatProviderFactory = {
             console.log(`webviewUrl: ${webviewUrl}`)
             let inPayment = false
 
-            const openWebView = async () => {
+            const openWebView = async (): Promise<void> => {
               const onUrlChangeAsync = async (
                 newUrl: string
               ): Promise<void> => {
@@ -1070,13 +1079,12 @@ export const paybisProvider: FiatProviderFactory = {
                   }
                 }
               }
+              const onUrlChange = (newUrl: string): void => {
+                onUrlChangeAsync(newUrl).catch(() => {})
+              }
               await showUi.openWebView({
                 url: webviewUrl,
-                onUrlChange: newUrl => {
-                  onUrlChangeAsync(newUrl).catch((error: unknown) => {
-                    showError(error)
-                  })
-                }
+                onUrlChange
               })
             }
             await openWebView()
@@ -1171,7 +1179,7 @@ const initializeBuyPairs = async ({
         .then(response => {
           paybisPairs.buy = asPaybisBuyPairs(response)
         })
-        .catch(e => {
+        .catch((e: unknown) => {
           console.error(String(e))
         })
     ]
@@ -1244,7 +1252,7 @@ const initializeSellPairs = async ({
         .then(response => {
           paybisPairs.sell = asPaybisSellPairs(response)
         })
-        .catch(e => {
+        .catch((e: unknown) => {
           console.error(String(e))
         })
     ]
