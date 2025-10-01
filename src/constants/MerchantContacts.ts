172cf0ce2296270f4533f5ddb2ea9d7db0bba822
Jon Tzeng <jon@edge.app>
2025-09-30 17:15:53 -0700

    Improve Contacts handling
    - Aligned the `GuiContact` type with `react-native-contacts` `Contact`
    - Updated ContactListModal to prioritize displayName, full name, and company for contact labels.
    - Enhanced useContactThumbnail to check merchant contacts first before device contacts for thumbnail resolution and fail on ambiguities.

---
+++ src/constants/MerchantContacts.ts
@@ -0,0 +1,129 @@
+import { EDGE_CONTENT_SERVER_URI } from './CdnConstants'
+
+export interface MerchantContact {
+  displayName: string
+  thumbnailPath: string
+}
+
+export const MERCHANT_CONTACTS: MerchantContact[] = [
+  {
+    displayName: '0x Gasless Swap',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/0xgasless.png`
+  },
+  {
+    displayName: 'Bitrefill',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/bitrefill.png`
+  },
+  {
+    displayName: 'Bits of Gold',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/bits-of-gold-logo.png`
+  },
+  {
+    displayName: 'Bridgeless',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/bridgeless.png`
+  },
+  {
+    displayName: 'Change NOW',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/changenow.png`
+  },
+  {
+    displayName: 'ChangeHero',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/changehero.png`
+  },
+  {
+    displayName: 'Changelly',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/changelly.png`
+  },
+  {
+    displayName: 'IBC Transfer',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/cosmosibc.png`
+  },
+  {
+    displayName: 'Fantom/Sonic Bridge',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/fantomsonicupgrade.png`
+  },
+  {
+    displayName: 'EOS Network',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/eos-logo-solo-64.png`
+  },
+  {
+    displayName: 'Exolix',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/exolix-logo.png`
+  },
+  {
+    displayName: 'Fox Exchange',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/foxEchange.png`
+  },
+  {
+    displayName: 'Godex',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/godex.png`
+  },
+  {
+    displayName: 'LetsExchange',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/letsexchange-logo.png`
+  },
+  {
+    displayName: 'LI.FI',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/lifi.png`
+  },
+  {
+    displayName: 'ShapeShift',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/shapeshift.png`
+  },
+  {
+    displayName: 'SideShift.ai',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/sideshift-logo.png`
+  },
+  {
+    displayName: 'Simplex',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/simplex.png`
+  },
+  {
+    displayName: 'Swapuz',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/swapuz.png`
+  },
+  {
+    displayName: 'Switchain',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/switchain.png`
+  },
+  {
+    displayName: 'Maya Protocol',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/mayaprotocol.png`
+  },
+  {
+    displayName: 'Thorchain',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/thorchain.png`
+  },
+  {
+    displayName: 'Thorchain DEX Aggregator',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/thorchain.png`
+  },
+  {
+    displayName: 'Totle',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/totle-logo.png`
+  },
+  {
+    displayName: 'Unizen',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/unizen.png`
+  },
+  {
+    displayName: 'Velodrome',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/velodrome.png`
+  },
+  {
+    displayName: 'Visa® Prepaid Card',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/visa.png`
+  },
+  {
+    displayName: 'Wyre',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/wyre.png`
+  },
+  {
+    displayName: 'XRP DEX',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/xrpdex.png`
+  },
+  {
+    displayName: 'Rango Exchange',
+    thumbnailPath: `${EDGE_CONTENT_SERVER_URI}/rango.png`
+  }
+]
