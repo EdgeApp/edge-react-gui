be3191e9091c83e62420ebcfd7c39e4a2a536f2e
Matthew <matthew@edge.app>
2025-11-10 14:11:10 -0800

    Fix eslint errors in WalletAndCurrencyConstants.ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -337,7 +337,7 @@ export default [
       'src/components/text/SectionHeading.tsx',
       'src/components/text/TitleText.tsx',
       'src/components/themed/Alert.tsx',
-      'src/components/themed/BuyCrypto.tsx',
+
       'src/components/themed/CreateWalletSelectCryptoRow.tsx',
       'src/components/themed/DividerLine.tsx',
       'src/components/themed/EdgeProviderComponent.tsx',
@@ -396,7 +396,7 @@ export default [
       'src/components/tiles/LtvRatioTile.tsx',
       'src/components/tiles/PercentageChangeArrowTile.tsx',
       'src/components/tiles/TotalDebtCollateralTile.tsx',
-      'src/constants/WalletAndCurrencyConstants.ts',
+
       'src/controllers/action-queue/ActionQueueStore.ts',
       'src/controllers/action-queue/cleaners.ts',
       'src/controllers/action-queue/push.ts',
