43f6633be5a623e55ef45b77827cd2fcfc0e0aa6
Matthew <matthew@edge.app>
2026-02-13 13:04:19 -0800

    Add monero full node url setting

--- src/components/themed/MaybeMoneroUserSettings.tsx
+++ src/components/themed/MaybeMoneroUserSettings.tsx
@@ -1,4 +1,4 @@
-import { asBoolean, asObject, asString } from 'cleaners'
+import { asBoolean, asObject, asOptional, asString } from 'cleaners'
 import * as React from 'react'
 
 import { useHandler } from '../../hooks/useHandler'
@@ -16,7 +16,9 @@ import { SettingsSubHeader } from '../settings/SettingsSubHeader'
 
 const asMoneroUserSettings = asObject({
   enableCustomServers: asBoolean,
-  moneroLightwalletServer: asString
+  enableCustomMonerod: asOptional(asBoolean, false),
+  moneroLightwalletServer: asString,
+  monerodServer: asString
 })
 type MoneroUserSettings = ReturnType<typeof asMoneroUserSettings>
 
@@ -24,20 +26,26 @@ type Props = CurrencySettingProps<MoneroUserSettings, undefined>
 
 const MoneroUserSettingsComponent: React.FC<Props> = props => {
   const { defaultSetting, onUpdate, setting } = props
-  const { enableCustomServers, moneroLightwalletServer } = setting
-  const isEmpty =
+  const {
+    enableCustomServers,
+    enableCustomMonerod,
+    moneroLightwalletServer,
+    monerodServer
+  } = setting
+  const isLwsEmpty =
     moneroLightwalletServer === '' ||
     moneroLightwalletServer === defaultSetting.moneroLightwalletServer
+  const isMonerodEmpty =
+    monerodServer === '' || monerodServer === defaultSetting.monerodServer
 
-  const handleMyMonero = useHandler(async (): Promise<void> => {
-    await onUpdate({
-      enableCustomServers: false,
-      moneroLightwalletServer
-    })
-    logActivity(`Disable Monero Node`)
+  // LWS handlers
+
+  const handleEdgeLws = useHandler(async (): Promise<void> => {
+    await onUpdate({ ...setting, enableCustomServers: false })
+    logActivity(`Disable Monero custom LWS`)
   })
 
-  const handleCustomServer = useHandler(async (): Promise<void> => {
+  const handleCustomLws = useHandler(async (): Promise<void> => {
     const server = await Airship.show<string | undefined>(bridge => (
       <TextInputModal
         autoCapitalize="none"
@@ -48,18 +56,52 @@ const MoneroUserSettingsComponent: React.FC<Props> = props => {
         title={lstrings.settings_edit_custom_node}
       />
     ))
-    if (isEmpty && server == null) return
+    if (isLwsEmpty && server == null) return
 
+    const url = server ?? moneroLightwalletServer
     await onUpdate({
+      ...setting,
       enableCustomServers: true,
-      moneroLightwalletServer: server ?? moneroLightwalletServer
+      moneroLightwalletServer: url
     })
-    logActivity(`Enable Monero Node: "${server ?? moneroLightwalletServer}"`)
+    logActivity(`Enable Monero custom LWS: "${url}"`)
   })
 
-  const customLabel =
+  // Monerod handlers
+
+  const handleEdgeMonerod = useHandler(async (): Promise<void> => {
+    await onUpdate({ ...setting, enableCustomMonerod: false })
+    logActivity(`Disable Monero custom monerod`)
+  })
+
+  const handleCustomMonerod = useHandler(async (): Promise<void> => {
+    const server = await Airship.show<string | undefined>(bridge => (
+      <TextInputModal
+        autoCapitalize="none"
+        autoCorrect={false}
+        bridge={bridge}
+        initialValue={monerodServer ?? ''}
+        inputLabel={lstrings.settings_custom_node_url}
+        title={lstrings.settings_edit_custom_node}
+      />
+    ))
+    if (isMonerodEmpty && server == null) return
+
+    const url = server ?? monerodServer
+    await onUpdate({
+      ...setting,
+      enableCustomMonerod: true,
+      monerodServer: url
+    })
+    logActivity(`Enable Monero custom monerod: "${url}"`)
+  })
+
+  const customLwsLabel =
     lstrings.settings_monero_custom +
-    (isEmpty ? '' : `:\n${moneroLightwalletServer}`)
+    (isLwsEmpty ? '' : `:\n${moneroLightwalletServer}`)
+  const customMonerodLabel =
+    lstrings.settings_monerod_custom_full_node +
+    (isMonerodEmpty ? '' : `:\n${monerodServer}`)
 
   return (
     <>
@@ -68,12 +110,23 @@ const MoneroUserSettingsComponent: React.FC<Props> = props => {
       <SettingsRadioRow
         label={lstrings.settings_monero_edge_lws}
         value={!enableCustomServers}
-        onPress={handleMyMonero}
+        onPress={handleEdgeLws}
       />
       <SettingsRadioRow
-        label={customLabel}
+        label={customLwsLabel}
         value={enableCustomServers}
-        onPress={handleCustomServer}
+        onPress={handleCustomLws}
+      />
+      <SettingsHeaderRow label={lstrings.settings_monerod} />
+      <SettingsRadioRow
+        label={lstrings.settings_monerod_edge_full_node}
+        value={!enableCustomMonerod}
+        onPress={handleEdgeMonerod}
+      />
+      <SettingsRadioRow
+        label={customMonerodLabel}
+        value={enableCustomMonerod}
+        onPress={handleCustomMonerod}
       />
     </>
   )
