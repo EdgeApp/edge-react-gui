4737e5891cacf7a58d690e921a205f8779dd14c6
Paul Puey <paul@edge.app>
2026-08-13 22:20:49 -0700

    Fetch remote secrets via infoRollup appKeys
    Boot from baked-in KEYS, then overlay a signed infoRollup appKeys payload and device
    cache. Mutate KEYS and globalKeys in place and rebuild pluginMaps.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -473,6 +473,8 @@ export default [
 
       'src/plugins/stake-plugins/uniswapV2/Ecosystem.ts',
 
+      'src/plugins/stake-plugins/uniswapV2/Ecosystem.ts',
+
       'src/plugins/stake-plugins/uniswapV2/policies/VelodromeV2StakePolicy.ts',
       'src/plugins/stake-plugins/util/accumulator.ts',
       'src/plugins/stake-plugins/util/biggystringplus.ts',
