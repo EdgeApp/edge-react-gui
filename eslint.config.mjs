05d9b89ed2a5cd8a364dc24fe27a6c8371912b48
Jonathan Tzeng <jnthntzng@gmail.com>
2026-09-03 11:57:11 -0700

    Fix lint warnings in BalanceCard

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -145,7 +145,6 @@ export default [
       'src/components/buttons/ReturnKeyTypeButton.tsx',
       'src/components/buttons/SceneButtons.tsx',
 
-      'src/components/cards/BalanceCard.tsx',
       'src/components/cards/EarnOptionCard.tsx',
 
       'src/components/cards/FiatAmountInputCard.tsx',
